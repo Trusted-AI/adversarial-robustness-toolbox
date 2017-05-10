348f1151bc8c32acbed2e4117c93b266ea277b87
Valentina Zantedeschi <valentina.zantedeschi@ibm.com>
2017-05-10 09:47:32 +0100

    cnn model and tests based on cleverhans utils

---
+++ src/classifiers/cnn.py
@@ -0,0 +1,46 @@
+from cleverhans.utils import conv_2d
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+
+def cnn_model(input_shape, logits=False, input_ph=None, nb_filters=64, nb_classes=10):
+    """
+    Returns a CNN model using Keras sequential model
+    
+    # Arguments
+        logits: If set to False, returns a Keras model, otherwise will also
+                    return logits tensor
+        input_ph: The TensorFlow tensor for the input
+                    (needed if returning logits)
+                    ("ph" stands for placeholder but it need not actually be a
+                    placeholder)
+        img_rows: number of row in the image
+        img_cols: number of columns in the image
+        channels: number of color channels (e.g., 1 for MNIST)
+        nb_filters: number of convolutional filters per layer
+        nb_classes: the number of output classes
+    # Return
+        CNN model
+    """
+    model = Sequential()
+
+    layers = [conv_2d(nb_filters, (8, 8), (2, 2), "same", input_shape=input_shape),
+              Activation('relu'),
+              conv_2d((nb_filters * 2), (6, 6), (2, 2), "valid"),
+              Activation('relu'),
+              conv_2d((nb_filters * 2), (5, 5), (1, 1), "valid"),
+              Activation('relu'),
+              Flatten(),
+              Dense(nb_classes)]
+
+    for layer in layers:
+        model.add(layer)
+
+    if logits:
+        logits_tensor = model(input_ph)
+    model.add(Activation('softmax'))
+
+    if logits:
+        return model, logits_tensor
+    else:
+        return model
---
+++ src/classifiers/cnn_unittest.py
@@ -0,0 +1,55 @@
+import unittest
+
+import keras
+from keras.datasets import cifar10
+from keras.utils import np_utils
+
+import tensorflow as tf
+
+from cleverhans.utils_tf import model_train,model_eval
+
+import cnn
+
+class TestCNNModel(unittest.TestCase):
+
+    def test_cifar(self):
+
+        BATCH_SIZE = 10
+        NB_CLASSES = 10
+        NB_TRAIN = 1000
+        NB_TEST = 100
+
+        session = tf.Session()
+        keras.backend.set_session(session)
+
+        # get CIFAR10
+        (X_train, Y_train), (X_test, Y_test) = cifar10.load_data()
+        X_train, Y_train, X_test, Y_test = X_train[:NB_TRAIN], Y_train[:NB_TRAIN], X_test[:NB_TEST], Y_test[:NB_TEST]
+
+        # convert class vectors to binary class matrices
+        Y_train = np_utils.to_categorical(Y_train,NB_CLASSES)
+        Y_test = np_utils.to_categorical(Y_test,NB_CLASSES)
+
+        im_shape = X_train[0].shape
+
+        x = tf.placeholder(tf.float32, shape=(None,im_shape[0],im_shape[1],im_shape[2]))
+        y = tf.placeholder(tf.float32, shape=(None,NB_CLASSES))
+
+        model = cnn.cnn_model(im_shape)
+        predictions = model(x)
+
+        def evaluate():
+            # Evaluate the accuracy of the CIFAR10 model on legitimate test examples
+            eval_params = {'batch_size': BATCH_SIZE}
+            accuracy = model_eval(session,x,y,predictions,X_test,Y_test,args=eval_params)
+            print('Test accuracy on legitimate test examples: ' + str(accuracy))
+
+        train_params = {
+            'nb_epochs': 1,
+            'batch_size': BATCH_SIZE,
+            'learning_rate': 0.1
+        }
+        model_train(session, x, y, predictions, X_train, Y_train, evaluate=evaluate, args=train_params)
+
+if __name__ == '__main__':
+    unittest.main()
