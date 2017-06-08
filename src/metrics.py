e0fe96b46016c40a4fea120c9622f175e8c163f9
Valentina Zantedeschi <valentina.zantedeschi@ibm.com>
2017-06-08 15:27:15 +0100

    rename and debug

---
+++ src/metrics.py
@@ -0,0 +1,47 @@
+import config
+
+import numpy as np
+import numpy.linalg as LA
+
+from src.attackers.fast_gradient import FastGradientMethod
+
+supported_methods = { "fgsm": {"class": FastGradientMethod,
+                               "params": {"eps_step": 0.1, "eps_max": 1., "clip_min": 0., "clip_max": 1.}},
+                      # "jsma": {"class": SaliencyMapMethod,
+                      #          "params": {"theta": 1., "gamma": 0.01, "clip_min": 0., "clip_max": 1.}}
+                      }
+
+def get_crafter(method, model, session, params=None):
+
+    try:
+        crafter = supported_methods[method]["class"](model, sess=session)
+    except:
+        raise NotImplementedError("{} crafting method not supported.".format(method))
+
+    if params:
+        crafter.set_params(**params)
+    else:
+        crafter.set_params(**supported_methods[method]["params"])
+
+    return crafter
+
+def empirical_robustness(x, model, sess, method_name, method_params=None):
+    """ Computes the Empirical Robustness of a `model` over the sample `x` for a given adversarial crafting method 
+    `method_name`, following https://arxiv.org/abs/1511.04599
+    
+    :param x: 
+    :param model: 
+    :param method_name: 
+    :param sess: 
+    :param method_params: 
+    :return: 
+    """
+
+    crafter = get_crafter(method_name, model, sess, method_params)
+    adv_x = crafter.generate(x, minimal=True, **method_params)
+
+    perts_norm = LA.norm((adv_x-x).reshape(x.shape[0], -1), ord=crafter.ord, axis=1)
+
+    assert perts_norm.shape == (len(x), ), perts_norm.shape
+
+    return np.mean(perts_norm/LA.norm(x))
--- src/metrics_unittest.py
+++ src/metrics_unittest.py
@@ -3,12 +3,13 @@ from config import config_dict
 import unittest
 
 import numpy as np
+import numpy.linalg as LA
 
 import keras.backend as K
 import tensorflow as tf
 
 from src.classifiers import cnn
-from src.attackers import perturbations
+from src.metrics import empirical_robustness
 
 from src.utils import load_mnist
 
@@ -66,23 +67,22 @@ class TestMinimalPerturbations(unittest.TestCase):
                   "clip_min":0.,
                   "clip_max":1.}
 
-        emp_robust = perturbations.empirical_robustness(X_train, model, session, "fgsm", params)
+        emp_robust = empirical_robustness(X_train, model, session, "fgsm", params)
+        self.assertEqual(emp_robust, 0.)
 
-        # self.assertEqual(emp_robust, 0.)
+        params = {"eps_step": 1.,
+                  "eps_max": 1.,
+                  "clip_min": None,
+                  "clip_max": None}
+        emp_robust = empirical_robustness(X_train, model, session, "fgsm", params)
+        self.assertAlmostEqual(emp_robust*LA.norm(X_train), 1., emp_robust**LA.norm(X_train))
 
-        params = {"eps_step": 0.05,
-                  "clip_min": 0.,
-                  "clip_max": 1.}
-        emp_robust_fsgm = perturbations.empirical_robustness(X_train, model, session, "fgsm", params)
-
-        self.assertTrue(isinstance(emp_robust_fsgm, float))
-
-        params = {"theta": 1.,
-                  "gamma": 0.01,
-                  "clip_min": 0.,
-                  "clip_max": 1.}
-        emp_robust_jsma = perturbations.empirical_robustness(X_train, model, session, "jsma", params)
-        self.assertLessEqual(emp_robust_jsma, 1.)
+        # params = {"theta": 1.,
+        #           "gamma": 0.01,
+        #           "clip_min": 0.,
+        #           "clip_max": 1.}
+        # emp_robust_jsma = empirical_robustness(X_train, model, session, "jsma", params)
+        # self.assertLessEqual(emp_robust_jsma, 1.)
 
 if __name__ == '__main__':
     unittest.main()
