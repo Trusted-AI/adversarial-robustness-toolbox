67b784de8a3300e3d82f839cdf81690aa9f35180
Irina Nicolae <Maria-Irina.Nicolae@ibm.com>
2017-06-12 15:01:46 +0100

    Add wrapper for Saliency Map method in cleverhans

---
+++ src/attackers/saliency_map.py
@@ -0,0 +1,148 @@
+from __future__ import absolute_import
+
+from config import config_dict
+from cleverhans.attacks_tf import jacobian_graph, jsma
+import numpy as np
+import tensorflow as tf
+
+from src.attackers.attack import Attack
+
+
+class SaliencyMapMethod(Attack):
+    """
+    The Jacobian-based Saliency Map Method (Papernot et al. 2016). Adapted from Cleverhans.
+    Paper link: https://arxiv.org/pdf/1511.07528.pdf
+    """
+    attack_params = ['theta', 'gamma', 'nb_classes', 'max_iter', 'clip_min', 'clip_max', 'y']
+
+    def __init__(self, model, sess=None, theta=1., gamma=np.inf, nb_classes=10, clip_min=0., clip_max=1., y=None):
+        """
+        Create a SaliencyMapMethod instance.
+
+        Attack-specific parameters:
+        :param theta: (optional float) Perturbation introduced to modified
+                      components (can be positive or negative)
+        :param gamma: (optional float) Maximum percentage of perturbed features
+        :param nb_classes: (optional int) Number of model output classes
+        :param clip_min: (optional float) Minimum component value for clipping
+        :param clip_max: (optional float) Maximum component value for clipping
+        :param y: (optional) Target placeholder if the attack is targeted
+        """
+        super(SaliencyMapMethod, self).__init__(model, sess)
+        kwargs = {'theta': theta,
+                  'gamma': gamma,
+                  'nb_classes': nb_classes,
+                  'clip_min': clip_min,
+                  'clip_max': clip_max,
+                  'y': y}
+        self.set_params(**kwargs)
+
+    def compute_graph(self, x, **kwargs):
+        # Parse and save attack-specific parameters
+        assert self.set_params(**kwargs)
+
+        # Define Jacobian graph wrt to this input placeholder
+        preds = self.model(x)
+        grads = jacobian_graph(preds, x, self.nb_classes)
+
+        # Define appropriate graph (targeted / random target labels)
+        if self.y is not None:
+            def jsma_wrap(x_val, y):
+                return self._jsma_batch(x, preds, grads, x_val, self.theta, self.gamma, self.clip_min,
+                                        self.clip_max, self.nb_classes, y=y)
+
+            # Attack is targeted, target placeholder will need to be fed
+            wrap = tf.py_func(jsma_wrap, [x, self.y], tf.float32)
+        else:
+            def jsma_wrap(x_val):
+                return self._jsma_batch(x, preds, grads, x_val, self.theta, self.gamma, self.clip_min,
+                                        self.clip_max, self.nb_classes, y=None)
+
+            # Attack is untargeted, target values will be chosen at random
+            wrap = tf.py_func(jsma_wrap, [x], tf.float32)
+
+        return wrap
+
+    def generate(self, x_val, **kwargs):
+        """
+        Generate adversarial samples and return them in a Numpy array.
+        :param x_val: (required) A Numpy array with the original inputs.
+        :param y: (optional) Target values if the attack is targeted
+        """
+        # Parse and save attack-specific parameters
+        assert self.set_params(**kwargs)
+
+        input_shape = list(x_val.shape)
+        input_shape[0] = None
+        self._x = tf.placeholder(tf.float32, shape=input_shape)
+        self._x_adv = self.compute_graph(self._x, **kwargs)
+
+        # Run symbolic graph without or with true labels
+        if 'y_val' not in kwargs or kwargs['y_val'] is None:
+            feed_dict = {self._x: x_val}
+        else:
+            if self.y is None:
+                raise Exception("This attack was instantiated untargeted.")
+            else:
+                if len(kwargs['y_val'].shape) > 1:
+                    nb_targets = len(kwargs['y_val'])
+                else:
+                    nb_targets = 1
+                if nb_targets != len(x_val):
+                    raise Exception("Specify exactly one target per input.")
+            feed_dict = {self._x: x_val, self.y: kwargs['y_val']}
+        return self.sess.run(self._x_adv, feed_dict=feed_dict)
+
+    def set_params(self, **kwargs):
+        """
+        Take in a dictionary of parameters and applies attack-specific checks
+        before saving them as attributes.
+
+        Attack-specific parameters:
+        :param theta: (optional float) Perturbation introduced to modified
+                      components (can be positive or negative)
+        :param gamma: (optional float) Maximum percentage of perturbed features
+        :param nb_classes: (optional int) Number of model output classes
+        :param clip_min: (optional float) Minimum component value for clipping
+        :param clip_max: (optional float) Maximum component value for clipping
+        :param y: (optional) Target placeholder if the attack is targeted
+        """
+        # Save attack-specific parameters
+        super(SaliencyMapMethod, self).set_params(**kwargs)
+
+        return True
+
+    def _jsma_batch(self, x, pred, grads, X, theta, gamma, clip_min, clip_max, nb_classes, y=None):
+        """
+        Applies the JSMA to a batch of inputs
+        :param x: the input placeholder
+        :param pred: the model's symbolic output
+        :param grads: symbolic gradients
+        :param X: numpy array with sample inputs
+        :param theta: delta for each feature adjustment
+        :param gamma: a float between 0 - 1 indicating the maximum distortion percentage
+        :param clip_min: minimum value for components of the example returned
+        :param clip_max: maximum value for components of the example returned
+        :param nb_classes: number of model output clSasses
+        :param y: target class for sample input
+        :return: adversarial examples
+        """
+        X_adv = np.zeros(X.shape)
+
+        for ind, val in enumerate(X):
+            val = np.expand_dims(val, axis=0)
+            if y is None:
+                # No targets provided, randomly choose from other classes
+                from cleverhans.utils_tf import model_argmax
+                gt = model_argmax(self.sess, x, pred, val)
+
+                # Randomly choose from the incorrect classes for each sample
+                from src.utils import random_targets
+                target = random_targets(gt, nb_classes)[0]
+            else:
+                target = y[ind]
+
+            X_adv[ind], _, _ = jsma(self.sess, x, pred, grads, val, np.argmax(target),
+                                    theta, gamma, clip_min, clip_max)
+
+        return np.asarray(X_adv, dtype=np.float32)
---
+++ src/attackers/saliency_map_unittest.py
@@ -0,0 +1,41 @@
+import keras.backend as k
+import tensorflow as tf
+import unittest
+
+from src.attackers.saliency_map import SaliencyMapMethod
+from src.classifiers import cnn
+from src.utils import load_mnist, get_labels_np_array
+
+
+class TestSaliencyMap(unittest.TestCase):
+    def test_mnist(self):
+        session = tf.Session()
+        k.set_session(session)
+
+        # get MNIST
+        batch_size, nb_train, nb_test = 100, 1000, 100
+        (X_train, Y_train), (X_test, Y_test) = load_mnist()
+        X_train, Y_train = X_train[:nb_train], Y_train[:nb_train]
+        X_test, Y_test = X_test[:nb_test], Y_test[:nb_test]
+        im_shape = X_train[0].shape
+
+        # get classifier
+        model = cnn.cnn_model(im_shape, act="relu")
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        model.fit(X_train, Y_train, epochs=1, batch_size=batch_size, verbose=0)
+        scores = model.evaluate(X_test, Y_test)
+        print("\naccuracy on test set: %.2f%%" % (scores[1] * 100))
+
+        df = SaliencyMapMethod(model, sess=session)
+        df.set_params(clip_min=0., clip_max=1., gamma=.3)
+        x_test_adv = df.generate(X_test)
+        self.assertFalse((X_test == x_test_adv).all())
+
+        y_pred = get_labels_np_array(model.predict(x_test_adv))
+        self.assertFalse((Y_test == y_pred).all())
+
+        scores = model.evaluate(x_test_adv, Y_test)
+        print('\naccuracy on adversarial examples: %.2f%%' % (scores[1] * 100))
+
+if __name__ == '__main__':
+    unittest.main()
