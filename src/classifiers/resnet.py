69bd8ceadb4eb896880171c0eceecd954ce7d302
Valentina Zantedeschi <valentina.zantedeschi@ibm.com>
2017-06-12 11:46:17 +0100

    resnet for mnist and cifar

---
+++ src/classifiers/resnet.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+''' Code adapted from https://github.com/fchollet/deep-learning-models
+'''
+from __future__ import print_function
+
+from keras.layers import Input
+from keras import layers
+from keras.layers import Dense
+from keras.layers import Activation
+from keras.layers import Flatten
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import BatchNormalization
+from keras.models import Model
+import keras.backend as K
+
+def identity_block(input_tensor, kernel_size, filters, stage, block):
+    """The identity block is the block that has no conv layer at shortcut.
+
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: defualt 3, the kernel size of middle conv layer at main path
+        filters: list of integers, the filterss of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+
+    # Returns
+        Output tensor for the block.
+    """
+    filters1, filters2, filters3 = filters
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = Conv2D(filters1, (1, 1), name=conv_name_base + '2a')(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters2, kernel_size,
+               padding='same', name=conv_name_base + '2b')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters3, (1, 1), name=conv_name_base + '2c')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    x = layers.add([x, input_tensor])
+    x = Activation('relu')(x)
+    return x
+
+
+def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2)):
+    """conv_block is the block that has a conv layer at shortcut
+
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: defualt 3, the kernel size of middle conv layer at main path
+        filters: list of integers, the filterss of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+
+    # Returns
+        Output tensor for the block.
+
+    Note that from stage 3, the first conv layer at main path is with strides=(2,2)
+    And the shortcut should have strides=(2,2) as well
+    """
+    filters1, filters2, filters3 = filters
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = Conv2D(filters1, (1, 1), strides=strides,
+               name=conv_name_base + '2a')(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters2, kernel_size, padding='same',
+               name=conv_name_base + '2b')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters3, (1, 1), name=conv_name_base + '2c')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    shortcut = Conv2D(filters3, (1, 1), strides=strides,
+                      name=conv_name_base + '1')(input_tensor)
+    shortcut = BatchNormalization(axis=bn_axis, name=bn_name_base + '1')(shortcut)
+
+    x = layers.add([x, shortcut])
+    x = Activation('relu')(x)
+    return x
+
+
+def resnet_model(input_shape, act='relu', bnorm=False, logits=False, input_ph=None, nb_filters=64, nb_classes=10, act_params={}):
+
+    img_input = Input(shape=input_shape)
+
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+
+    x = Conv2D(nb_filters, (8, 8), strides=(2, 2), padding="same", input_shape=input_shape, name="conv1")(img_input)
+    x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(nb_filters*2, (6, 6), strides=(2, 2), padding="valid", name="conv2")(x)
+    x = BatchNormalization(axis=bn_axis, name='bn_conv2')(x)
+    x = Activation('relu')(x)
+
+    x = identity_block(x, 3, [nb_filters, nb_filters, nb_filters*2], stage=2, block='a')
+
+    x = MaxPooling2D((3, 3), name='max_pool')(x)
+    x = Flatten()(x)
+    x = Dense(nb_classes)(x)
+
+    if logits:
+        logits_tensor = x
+
+    x = Activation('softmax')(x)
+
+    # Create model.
+    model = Model(img_input, x, name='resnet')
+
+    if logits:
+        return model, logits_tensor
+    else:
+        return model
---
+++ src/classifiers/resnet_unittest.py
@@ -0,0 +1,124 @@
+from config import config_dict
+
+import unittest
+
+import os.path
+import shutil
+
+import keras
+
+import tensorflow as tf
+
+from src.classifiers import resnet, cnn
+from src.classifiers.utils import save_model, load_model
+from src.utils import load_cifar10, load_mnist, make_directory, set_group_permissions_rec
+
+class TestResNetModel(unittest.TestCase):
+
+    def setUp(self):
+        make_directory("./tests/")
+
+    def tearDown(self):
+        shutil.rmtree("./tests/")
+
+    def test_cifar(self):
+
+        BATCH_SIZE = 10
+        NB_TRAIN = 1000
+        NB_TEST = 100
+
+        session = tf.Session()
+        keras.backend.set_session(session)
+
+        # get CIFAR10
+        (X_train, Y_train), (X_test, Y_test) = load_cifar10()
+        X_train, Y_train, X_test, Y_test = X_train[:NB_TRAIN], Y_train[:NB_TRAIN], X_test[:NB_TEST], Y_test[:NB_TEST]
+
+        im_shape = X_train[0].shape
+
+        model = resnet.resnet_model(input_shape=im_shape)
+
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+        # Fit the model
+        model.fit(X_train, Y_train, epochs=2, batch_size=BATCH_SIZE)
+
+        scores = model.evaluate(X_test, Y_test)
+
+        print("\naccuracy: %.2f%%" % (scores[1] * 100))
+
+
+    def test_mnist(self):
+
+        BATCH_SIZE = 10
+        NB_TRAIN = 1000
+        NB_TEST = 100
+
+        session = tf.Session()
+        keras.backend.set_session(session)
+
+        # get MNIST
+        (X_train, Y_train), (X_test, Y_test) = load_mnist()
+        X_train, Y_train, X_test, Y_test = X_train[:NB_TRAIN], Y_train[:NB_TRAIN], X_test[:NB_TEST], Y_test[:NB_TEST]
+
+        im_shape = X_train[0].shape
+
+        model = resnet.resnet_model(input_shape=im_shape)
+
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+        # Fit the model
+        model.fit(X_train, Y_train, epochs=2, batch_size=BATCH_SIZE)
+
+        scores = model.evaluate(X_test, Y_test)
+
+        print("\naccuracy: %.2f%%" % (scores[1] * 100))
+
+    #
+    def test_save_load_model(self):
+        BATCH_SIZE = 10
+        NB_TRAIN = 100
+        NB_TEST = 10
+
+        comp_params = {"loss":'categorical_crossentropy',
+                       "optimizer":'adam',
+                       "metrics":['accuracy']}
+
+        session = tf.Session()
+        keras.backend.set_session(session)
+
+        # get MNIST
+        (X_train, Y_train), (X_test, Y_test) = load_mnist()
+        X_train, Y_train, X_test, Y_test = X_train[:NB_TRAIN], Y_train[:NB_TRAIN], X_test[:NB_TEST], Y_test[:NB_TEST]
+
+        im_shape = X_train[0].shape
+
+        model = resnet.resnet_model(input_shape=im_shape)
+
+        model.compile(**comp_params)
+
+        # Fit the model
+        model.fit(X_train, Y_train, epochs=1, batch_size=BATCH_SIZE)
+
+        path = "./tests/save/cnn/"
+        # test saving
+        save_model(model, path, comp_params)
+
+        if config_dict["profile"] == "CLUSTER":
+            set_group_permissions_rec(path)
+
+        self.assertTrue(os.path.isfile(path + "model.json"))
+        self.assertTrue(os.path.getsize(path + "model.json") > 0)
+        self.assertTrue(os.path.isfile(path + "weights.h5"))
+        self.assertTrue(os.path.getsize(path + "weights.h5") > 0)
+
+        #test loading
+        loaded_model = load_model(path)
+
+        scores = model.evaluate(X_test, Y_test)
+        scores_loaded = loaded_model.evaluate(X_test, Y_test)
+
+        self.assertAlmostEqual(scores, scores_loaded)
+
+if __name__ == '__main__':
+    unittest.main()
