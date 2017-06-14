fec28b6ddffea93eaabf50d1cf61f9bd756cf258
Valentina Zantedeschi <valentina.zantedeschi@ibm.com>
2017-06-14 11:55:13 +0100

    choice of classifier

--- train_with_noise.py
+++ train_with_noise.py
@@ -8,6 +8,7 @@ from keras.callbacks import ModelCheckpoint, TensorBoard
 import tensorflow as tf
 
 from src.classifiers.cnn import CNN
+from src.classifiers.resnet import ResNet
 from src.classifiers.utils import save_classifier, load_classifier
 from src.utils import get_args, get_verbose_print, load_mnist, make_directory, set_group_permissions_rec
 
@@ -23,23 +24,35 @@ comp_params = {"loss": 'categorical_crossentropy',
 # --------------------------------------------------------------------------------------------- GET CLASSIFIER
 # get MNIST
 (X_train, Y_train), (X_test, Y_test) = load_mnist()
-# X_train, Y_train, X_test, Y_test = X_train[:1000], Y_train[:1000], X_test[:1000], Y_test[:1000]
+X_train, Y_train, X_test, Y_test = X_train[:1000], Y_train[:1000], X_test[:1000], Y_test[:1000]
 
 im_shape = X_train[0].shape
 
 session = tf.Session()
 K.set_session(session)
 
-MODEL_PATH = args.save if args.save is not None else os.path.join(os.path.abspath(DATA_PATH), "classifiers", "mnist",
-                       "cnn", args.act, "gaussian", "stdev%.2f" % args.std_dev, "", "pert-insts%d" % args.nb_instances)
+if args.save is not False:
 
-classifier = CNN(im_shape, act=args.act, bnorm=False)
+    if args.save:
+        MODEL_PATH = os.path.join(os.path.abspath(args.save), "")
+
+    else:
+        MODEL_PATH = os.path.join(os.path.abspath(DATA_PATH), "classifiers", "mnist", args.classifier, args.act,
+                                  "gaussian", "stdev%.2f" % args.std_dev, "pert-insts%d" % args.nb_instances, "")
+
+    v_print("Classifier saved in", MODEL_PATH)
+
+if args.classifier == "cnn":
+    classifier = CNN(im_shape, act=args.act, bnorm=False)
+
+elif args.classifier == "resnet":
+    classifier = ResNet(im_shape, act=args.act, bnorm=False)
 
 # Fit the classifier
 
 classifier.compile(comp_params)
 
-if args.save is not None:
+if args.save is not False:
 
     make_directory(MODEL_PATH)
 
@@ -61,8 +74,8 @@ y_gau_perts = np.tile(Y_train, (args.nb_instances, 1))
 classifier.fit(np.vstack((X_train, x_gau_perts)), np.vstack((Y_train, y_gau_perts)), verbose=2*int(args.verbose),
           validation_split=args.val_split, epochs=args.nb_epochs, batch_size=args.batch_size, callbacks=callbacks_list)
 
-if args.save is not None:
-    save_classifier(classifier, MODEL_PATH, comp_params)
+if args.save is not False:
+    save_classifier(classifier, MODEL_PATH)
     # Load classifier with best validation score
     classifier = load_classifier(MODEL_PATH, "best-weights.h5")
 
