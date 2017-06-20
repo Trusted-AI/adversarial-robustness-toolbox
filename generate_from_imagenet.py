0018ba87019cf738d99b2195deb5746bf74ee8e7
Valentina Zantedeschi <valentina.zantedeschi@ibm.com>
2017-06-20 17:56:22 +0100

    generate with different methods

--- generate_from_imagenet.py
+++ generate_from_imagenet.py
@@ -17,7 +17,7 @@ PATH = "./imagenet/"
 WIDTH = 224
 HEIGHT = 224
 
-adv_method = sys.args[1]
+adv_method = sys.argv[1]
 assert adv_method in ["fgsm", "deepfool", "universal"]
 
 session = tf.Session()
@@ -25,32 +25,48 @@ K.set_session(session)
 
 model = VGG16()
 
-attack_params = {"clip_min": 0.,
-                 "clip_max": 255,
-                 "minimal": True,
-                 "eps_step": 1,
-                 "eps_max": 100.}
+if adv_method == "universal":
 
-if adv_method == "fgsm":
-    attack = FastGradientMethod(model, session)
-elif adv_method == "deepfool":
-    attack = DeepFool(model, session)
-else:
+    attack_params = {"clip_min": 0.,
+                     "clip_max": 255,
+                     "minimal": True}
+
     attack = UniversalPerturbation(model, session, p=np.inf, attacker_params=attack_params)
 
+elif adv_method == "deepfool":
+
+    attack_params = {"clip_min": 0.,
+                     "clip_max": 255,}
+
+    attack = DeepFool(model, session, max_iter=1, verbose=2)
+
+else:
+
+    attack_params = {"clip_min": 0.,
+                     "clip_max": 255,
+                     "minimal": True,
+                     "eps_step": 1,
+                     "eps_max": 100.}
+
+
+    attack = FastGradientMethod(model, session)
+
 with open(os.path.join(PATH, "pic_ids.txt"), "r") as infile:
+    lines = [line[:-1] if "\n" in line else line for line in infile]
 
-    for file in infile:
+X = np.empty((len(lines), WIDTH, HEIGHT, 3))
 
-        img = Image.open(file)
-        img = img.resize((WIDTH, HEIGHT), Image.ANTIALIAS)
+for i,file in enumerate(lines):
 
-        X = np.expand_dims(img, axis=0)
+    img = Image.open(file)
+    img = img.resize((WIDTH, HEIGHT), Image.ANTIALIAS)
 
-        adv = attack.generate(X, **attack_params)
+    X[i] = img.copy()
 
-        img_name = file.split("/")[-1]
-        misc.imsave(os.path.join(PATH, img_name.replace(".jpg", adv_method+"_adv.jpg")), adv[0])
-        misc.imsave(os.path.join(PATH, img_name.replace(".jpg", adv_method+"_pert.jpg")), (adv - X)[0])
+advs = attack.generate(X, **attack_params)
 
-        break
+for adv, file in zip(advs, lines):
+    img_name = file.split("/")[-2] + "_" + file.split("/")[-1]
+    misc.imsave(os.path.join(PATH, img_name.replace(".jpg", adv_method + "_adv.jpg")), adv)
+
+# misc.imsave(os.path.join(PATH, "universal.jpg"), (adv - X[0])[0])
