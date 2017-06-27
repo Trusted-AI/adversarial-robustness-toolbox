f93d83d9f2ec5eb34659fb66437392a424375487
Valentina Zantedeschi <valentina.zantedeschi@ibm.com>
2017-06-27 15:48:43 +0100

    feature squeezing

--- src/defences/preprocessings.py
+++ src/defences/preprocessings.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def label_smoothing(y_val, max_value=0.9):
 
     min_value = (1 - max_value) / (y_val.shape[1] - 1)
@@ -9,3 +11,14 @@ def label_smoothing(y_val, max_value=0.9):
     smoothed_y[smoothed_y == 0.] = min_value
 
     return smoothed_y
+
+def feature_squeezing(x_val, bit_depth=8):
+    """ x_val is supposed to be in the range [0,1] """
+
+    assert 60 > bit_depth > 0
+
+    max_value = int(2**bit_depth - 1)
+
+    squeezed_x = np.rint(x_val*max_value) / max_value
+
+    return squeezed_x
--- src/defences/preprocessings_unittest.py
+++ src/defences/preprocessings_unittest.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from src.defences.preprocessings import label_smoothing
+from src.defences.preprocessings import label_smoothing, feature_squeezing
 
 class TestLabelSmoothing(unittest.TestCase):
 
@@ -30,6 +30,39 @@ class TestLabelSmoothing(unittest.TestCase):
         self.assertTrue(np.isclose(np.sum(smoothed_y, axis=1),np.ones(M)).all())
         self.assertTrue((np.max(smoothed_y, axis=1) == np.ones(M)/N).all())
         self.assertTrue(np.isclose(smoothed_y, np.ones((M, N))/N).all())
+
+
+class TestFeatureSqueezing(unittest.TestCase):
+
+    def test_ones(self):
+
+        M, N = 10, 2
+
+        x = np.ones((M,N))
+
+        for depth in range(1,50):
+            with self.subTest("bit depth = {}".format(depth)):
+                squeezed_x = feature_squeezing(x, depth)
+                self.assertTrue((squeezed_x == 1).all())
+
+
+    def test_random(self):
+
+        M, N = 1000, 20
+
+        x = np.random.rand(M, N)
+
+        x_zero = np.where(x < 0.5)
+        x_one = np.where(x >= 0.5)
+
+        squeezed_x = feature_squeezing(x, 1)
+        self.assertTrue((squeezed_x[x_zero] == 0.).all())
+        self.assertTrue((squeezed_x[x_one] == 1.).all())
+
+        squeezed_x = feature_squeezing(x, 2)
+        self.assertFalse(np.logical_and(0. < squeezed_x, squeezed_x < 0.33).any())
+        self.assertFalse(np.logical_and(0.34 < squeezed_x, squeezed_x < 0.66).any())
+        self.assertFalse(np.logical_and(0.67 < squeezed_x, squeezed_x < 1.).any())
 
 if __name__ == '__main__':
     unittest.main()
