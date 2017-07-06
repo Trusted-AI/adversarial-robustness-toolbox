12bfdd95f91dabbaa7af74d354d9434f6eaac0a5
Valentina Zantedeschi <valentina.zantedeschi@ibm.com>
2017-07-06 16:44:41 +0100

    no need to specify dataset for testing

--- test_accuracies.py
+++ test_accuracies.py
@@ -37,7 +37,7 @@ except:
 already_tested = results.keys()
 
 # get dataset
-(X_train, Y_train), (X_test, Y_test) = load_dataset(args.dataset)
+(X_train, Y_train), (X_test, Y_test) = load_dataset(MODEL_PATH)
 
 if "train_accuracy" not in already_tested:
     # Test on true train instances
