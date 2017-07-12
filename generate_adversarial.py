ab5777426ca1acfef7f7d8bc3224c98bfadc478e
Valentina Zantedeschi <valentina.zantedeschi@ibm.com>
2017-07-12 17:24:41 +0100

    new attack: random step before fgsm

--- generate_adversarial.py
+++ generate_adversarial.py
@@ -18,6 +18,7 @@ from src.utils import get_args, get_verbose_print, load_dataset, make_directory,
 # --------------------------------------------------------------------------------------------------- SETTINGS
 args = get_args(__file__)
 v_print = get_verbose_print(args.verbose)
+alpha = 0.05 # constant for random perturbation
 
 # get dataset
 (X_train, Y_train), (X_test, Y_test) = load_dataset(args.load)
@@ -39,20 +40,30 @@ if args.save:
 
     v_print("Adversarials crafted with", args.adv_method, "on", MODEL_PATH, "will be saved in", SAVE_ADV)
 
-if args.adv_method in ['fgsm', "vat"]:
+if args.adv_method in ['fgsm', "vat", "rnd_fgsm"]:
 
     eps_ranges = {'fgsm': [e / 10 for e in range(1, 11)],
+                  'rnd_fgsm': [e / 10 for e in range(1, 11)],
                   'vat': [1.5, 2.1, 5, 7, 10]}
 
-    if args.adv_method == "fgsm":
+    if args.adv_method in ["fgsm", "rnd"]:
         adv_crafter = FastGradientMethod(model=classifier.model, sess=session)
     else:
         adv_crafter = VirtualAdversarialMethod(classifier.model, sess=session)
 
     for eps in eps_ranges[args.adv_method]:
 
-        X_train_adv = adv_crafter.generate(x_val=X_train, eps=eps, clip_min=0., clip_max=1.)
-        X_test_adv = adv_crafter.generate(x_val=X_test, eps=eps, clip_min=0., clip_max=1.)
+        if args.adv_method == "rnd_fgsm":
+            x_train = np.clip(X_train + alpha * np.sign(np.random.randn(*X_train.shape)), 0.0, 1.0)
+            x_test = np.clip(X_test + alpha * np.sign(np.random.randn(*X_test.shape)), 0.0, 1.0)
+            e = eps - alpha
+        else:
+            x_train = X_train
+            x_test = X_test
+            e = eps
+
+        X_train_adv = adv_crafter.generate(x_val=x_train, eps=e, clip_min=0., clip_max=1.)
+        X_test_adv = adv_crafter.generate(x_val=x_test, eps=e, clip_min=0., clip_max=1.)
 
         if args.save:
             np.save(os.path.join(SAVE_ADV, "eps%.2f_train.npy" % eps), X_train_adv)
