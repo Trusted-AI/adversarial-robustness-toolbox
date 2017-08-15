37266c5f61b6b611e2de69beda46aa057fe9d32d
Valentina Zantedeschi <valentina.zantedeschi@ibm.com>
2017-08-15 15:36:19 +0100

    documentation

--- src/defences/preprocessings.py
+++ src/defences/preprocessings.py
@@ -2,6 +2,12 @@ import numpy as np
 import tensorflow as tf
 
 def label_smoothing(y_val, max_value=0.9):
+    """
+    Computes a vector of smooth labels from a vector of hard ones. 
+    :param y_val: (np.ndarray) original vector of label probabilities
+    :param max_value: (float) probability to affect to label of probability 1
+    :return: (np.ndarray) vector of smooth probabilities
+    """
 
     min_value = (1 - max_value) / (y_val.shape[1] - 1)
 
@@ -14,18 +20,26 @@ def label_smoothing(y_val, max_value=0.9):
     return smoothed_y
 
 def feature_squeezing(x_val, bit_depth=8):
-    """ x_val is supposed to be in the range [0,1] """
+    """
+    Reduces the sensibility of the features of a sample.
+    Defence method from https://arxiv.org/abs/1704.01155.    
+    :param x_val: (np.ndarray) Sample to squeeze. `x_val` values are supposed to be in the range [0,1]
+    :param bit_depth: (int) sensibility magnitude
+    :return: (np.ndarray) squeezed sample
+    """
 
     assert 60 > bit_depth > 0
 
-    max_value = int(2**bit_depth - 1)
+    max_value = int(2 ** bit_depth - 1)
 
     squeezed_x = np.rint(x_val*max_value) / max_value
 
     return squeezed_x
 
 def tf_feature_squeezing(x, bit_depth=8):
-    """ feature squeezing on placeholders """
+    """ 
+    feature squeezing on tf.Tensor. See `src.defences.preprocessings.feature_squeezing` for documentation.
+    """
 
     assert 60 > bit_depth > 0
 
