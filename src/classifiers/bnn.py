b0f134c2736c414c99ebbcef583d770377ee80b3
Ambrish Rawat <ambrishrawat@gmail.com>
2017-08-23 11:51:22 -0400

    added load from cnn option for bnn

--- src/classifiers/bnn.py
+++ src/classifiers/bnn.py
@@ -7,6 +7,13 @@ import numpy as np
 from src.classifiers.classifier import Classifier
 from keras.preprocessing.image import ImageDataGenerator
 
+#for _load_from_cnn
+import os
+from src.layers.activations import BoundedReLU
+custom_objects = {'BoundedReLU': BoundedReLU}
+import json
+import warnings
+
 def mnist_layers(input_shape, nb_filters):
 
     layers = [Conv2D(nb_filters, (8, 8), strides=(2, 2), padding="same", input_shape=input_shape),
@@ -41,7 +48,7 @@ def cifar10_layers(input_shape, nb_filters):
 class BNN(Classifier):
 
     def __init__(self, input_shape=None, include_end=True, act='relu', bnorm=False, input_ph=None, nb_filters=64,
-                 nb_classes=10,nb_MC_samples=100,act_params={}, model=None, defences=None, preproc=None, dataset="mnist"):
+                 nb_classes=10,nb_mc_samples=100,act_params={}, model=None, defences=None, preproc=None, dataset="mnist"):
 
         """Instantiates a Bayesian ConvolutionalNeuralNetwork model using Keras sequential model
         
@@ -88,7 +95,7 @@ class BNN(Classifier):
             if include_end:
                 model.add(Activation('softmax'))
 
-        self.nb_MC_samples = nb_MC_samples
+        self.nb_MC_samples = nb_mc_samples
 
         super(BNN, self).__init__(model, defences, preproc)
 
@@ -144,3 +151,47 @@ class BNN(Classifier):
 
         x = self._preprocess(x)
         return self.model.evaluate(x, y_val, **kwargs)
+
+    @staticmethod
+    def _load_from_cnn(file_path, weights_name="weights.h5",nb_mc_samples = None):
+        """ Loads a classifier from given location and tries to compile it
+
+        :param file_path: folder to load the model from (full path)
+        :param weights_name: name of the file containing the weights
+        :return: Classifier
+        """
+
+        # load json and create model
+        with open(os.path.join(file_path, "model.json"), "r") as json_file:
+            model_json = json_file.read()
+        model = model_from_json(model_json, custom_objects=custom_objects)
+
+        # load params to decide what classifier to create
+
+        with open(os.path.join(file_path, "params.json"), "r") as json_file:
+            params_json = json.load(json_file)
+
+        if "defences" in params_json.keys():
+            defences = params_json["defences"]
+        else:
+            defences = None
+
+        if nb_mc_samples is not None:
+            classifier = BNN(model=model, defences=defences, nb_mc_samples = nb_mc_samples)
+        else:
+            classifier = BNN(model=model, defences=defences)
+
+        # load weights into new model
+        classifier.model.load_weights(os.path.join(file_path, weights_name))
+
+        # try to load comp param and compile model
+        try:
+            with open(os.path.join(file_path, 'comp_par.json'), 'r') as fp:
+                classifier.comp_par = json.load(fp)
+                if classifier.comp_par["optimizer"] == "sgd":
+                    classifier.comp_par["optimizer"] = SGD(lr=1e-4, momentum=0.9)
+                classifier.model.compile(**classifier.comp_par)
+        except OSError:
+            warnings.warn("Compilation parameters not found. The loaded model will need to be compiled.")
+
+        return classifier
