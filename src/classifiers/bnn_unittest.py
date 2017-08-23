cf5821139201d79a53b44995c21c908f83f92575
Ambrish Rawat <ambrishrawat@gmail.com>
2017-08-23 11:51:44 -0400

    added some unittests for bnn

--- src/classifiers/bnn_unittest.py
+++ src/classifiers/bnn_unittest.py
@@ -10,6 +10,7 @@ import keras
 import tensorflow as tf
 
 from src.classifiers.bnn import BNN
+from src.classifiers.cnn import CNN
 from src.classifiers.utils import save_classifier, load_classifier
 from src.utils import load_cifar10, load_mnist, make_directory, set_group_permissions_rec
 
@@ -20,9 +21,8 @@ class TestBNNModel(unittest.TestCase):
 
     def tearDown(self):
         shutil.rmtree("./tests/")
-
-
-    def test_mnist(self):
+    
+    def test_mc_preds_cifar(self):
 
         BATCH_SIZE = 10
         NB_TRAIN = 1000
@@ -31,30 +31,28 @@ class TestBNNModel(unittest.TestCase):
         session = tf.Session()
         keras.backend.set_session(session)
 
-        # get MNIST
-        (X_train, Y_train), (X_test, Y_test), _, _ = load_mnist()
+        # get CIFAR10
+        (X_train, Y_train), (X_test, Y_test), _, _ = load_cifar10()
         X_train, Y_train, X_test, Y_test = X_train[:NB_TRAIN], Y_train[:NB_TRAIN], X_test[:NB_TEST], Y_test[:NB_TEST]
 
         im_shape = X_train[0].shape
 
-        classifier = BNN(im_shape, act="relu")
+        classifier = BNN(im_shape, act="brelu", dataset="cifar10")
 
         classifier.compile({'loss':'categorical_crossentropy', 'optimizer':'adam', 'metrics':['accuracy']})
 
         # Fit the classifier
         classifier.fit(X_train, Y_train, epochs=1, batch_size=BATCH_SIZE)
 
+        # monte carlo predictions with dropouts turned on
         samples = classifier._mc_preds(X_test)
         print("\nSamples shape",samples.shape)
-        #scores = classifier.evaluate(X_test, Y_test)
-
-        #print("\naccuracy: %.2f%%" % (scores[1] * 100))
-
-
-    def test_save_load_model(self):
+    
+    def test_load_from_cnn(self):
         BATCH_SIZE = 10
         NB_TRAIN = 100
         NB_TEST = 10
+        NB_MC_SAMPLES = 5
 
         comp_params = {"loss":'categorical_crossentropy',
                        "optimizer":'adam',
@@ -69,14 +67,14 @@ class TestBNNModel(unittest.TestCase):
 
         im_shape = X_train[0].shape
 
-        classifier = BNN(im_shape, act="brelu")
+        classifier = CNN(im_shape, act="brelu")
 
         classifier.compile(comp_params)
 
         # Fit the classifier
         classifier.fit(X_train, Y_train, epochs=1, batch_size=BATCH_SIZE)
 
-        path = "./tests/save/bnn/"
+        path = "./tests/save/cnn/"
         # test saving
         save_classifier(classifier, path)
 
@@ -89,12 +87,11 @@ class TestBNNModel(unittest.TestCase):
         self.assertTrue(os.path.getsize(path + "weights.h5") > 0)
 
         #test loading
-        loaded_classifier = load_classifier(path)
+        loaded_classifier = BNN._load_from_cnn(file_path=path,nb_mc_samples=NB_MC_SAMPLES)
+        # monte carlo predictions with dropouts turned on
+        samples = loaded_classifier._mc_preds(X_test)
+        print("\nSamples shape",samples.shape)
 
-        scores = classifier.evaluate(X_test, Y_test)
-        scores_loaded = loaded_classifier.evaluate(X_test, Y_test)
-
-        self.assertAlmostEqual(scores, scores_loaded)
 
 if __name__ == '__main__':
     unittest.main()
