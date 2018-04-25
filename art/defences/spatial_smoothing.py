4ae72ecb6cca12b95b857dc519378a04f78ed743
Irina Nicolae <Maria-Irina.Nicolae@ibm.com>
2018-04-25 15:41:57 +0100

    Adapt spatial smoothing to new classifier

--- art/defences/spatial_smoothing.py
+++ art/defences/spatial_smoothing.py
@@ -18,15 +18,16 @@ class SpatialSmoothing(Preprocessor):
         :param window_size: The size of the sliding window.
         :type window_size: `int`
         """
-        self.is_fitted = True
+        super(SpatialSmoothing, self).__init__()
+        self._is_fitted = True
         self.set_params(window_size=window_size)
 
-    def __call__(self, x_val, window_size=None):
+    def __call__(self, x, window_size=None):
         """
         Apply local spatial smoothing to sample `x_val`.
 
-        :param x_val: Sample to smooth with shape `(batch_size, width, height, depth)`.
-        :type x_val: `np.ndarray`
+        :param x: Sample to smooth with shape `(batch_size, width, height, depth)`.
+        :type x: `np.ndarray`
         :param window_size: The size of the sliding window.
         :type window_size: `int`
         :return: Smoothed sample
@@ -36,11 +37,11 @@ class SpatialSmoothing(Preprocessor):
             self.set_params(window_size=window_size)
 
         size = (1, self.window_size, self.window_size, 1)
-        result = ndimage.filters.median_filter(x_val, size=size, mode="reflect")
+        result = ndimage.filters.median_filter(x, size=size, mode="reflect")
 
         return result
 
-    def fit(self, x_val, y_val=None, **kwargs):
+    def fit(self, x, y=None, **kwargs):
         """
         No parameters to learn for this method; do nothing.
         """
@@ -58,6 +59,6 @@ class SpatialSmoothing(Preprocessor):
         super(SpatialSmoothing, self).set_params(**kwargs)
 
         if type(self.window_size) is not int or self.window_size <= 0:
-            raise ValueError("Sliding window size must be a positive integer")
+            raise ValueError("Sliding window size must be a positive integer.")
 
         return True
