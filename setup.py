c57ac4add8af7d9739461250521593d6366dad61
Irina Nicolae <Maria-Irina.Nicolae@ibm.com>
2018-06-19 17:06:06 +0100

    Correct config includes in PyPi distribution

--- setup.py
+++ setup.py
@@ -22,10 +22,9 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='adversarial_robustness_toolbox',
-      version='0.2.0',
+      version='0.2.1',
       description='IBM Adversarial machine learning toolbox',
       long_description=long_description,
-      long_description_content_type="text/markdown",
       author='Irina Nicolae',
       author_email='maria-irina.nicolae@ibm.com',
       url='https://github.com/IBM/adversarial-robustness-toolbox',
