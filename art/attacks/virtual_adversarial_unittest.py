3d39035c4dfe680c09abe1c878883737a9092e22
Irina Nicolae <Maria-Irina.Nicolae@ibm.com>
2018-06-22 14:45:11 +0100

    Add tests for PyTorch in Virtual Adversarial Method

--- art/attacks/virtual_adversarial_unittest.py
+++ art/attacks/virtual_adversarial_unittest.py
@@ -8,14 +8,33 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D, Dropout
 import numpy as np
 import tensorflow as tf
+import torch.nn as nn
+import torch.nn.functional as f
+import torch.optim as optim
 
 from art.attacks.virtual_adversarial import VirtualAdversarialMethod
-from art.classifiers import KerasClassifier, TFClassifier
+from art.classifiers import KerasClassifier, PyTorchClassifier, TFClassifier
 from art.utils import load_mnist, get_labels_np_array
 
 BATCH_SIZE = 10
 NB_TRAIN = 100
 NB_TEST = 10
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.conv = nn.Conv2d(1, 16, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc = nn.Linear(2304, 10)
+
+    def forward(self, x):
+        x = self.pool(f.relu(self.conv(x)))
+        x = x.view(-1, 2304)
+        logit_output = self.fc(x)
+        output = f.softmax(logit_output, dim=1)
+
+        return logit_output, output
 
 
 class TestVirtualAdversarial(unittest.TestCase):
@@ -36,6 +55,7 @@ class TestVirtualAdversarial(unittest.TestCase):
         print("\n[Keras, MNIST] Accuracy on training set: %.2f%%" % (scores[1] * 100))
         scores = self.classifier_k._model.evaluate(x_test, y_test)
         print("\n[Keras, MNIST] Accuracy on test set: %.2f%%" % (scores[1] * 100))
+        print(np.argmax(self.classifier_k.predict(x_test), axis=1))
 
         # Create basic CNN on MNIST using TensorFlow
         self.classifier_tf = self._cnn_mnist_tf([28, 28, 1])
@@ -49,16 +69,37 @@ class TestVirtualAdversarial(unittest.TestCase):
         acc = np.sum(np.argmax(scores, axis=1) == np.argmax(y_test, axis=1)) / y_test.shape[0]
         print('\n[TF, MNIST] Accuracy on test set: %.2f%%' % (acc * 100))
 
+        # Create basic PyTorch model
+        self.classifier_py = self._cnn_mnist_py()
+        x_train, x_test = np.swapaxes(x_train, 1, 3), np.swapaxes(x_test, 1, 3)
+        self.classifier_py.fit(x_train, np.argmax(y_train, axis=1), nb_epochs=2, batch_size=BATCH_SIZE)
+
+        scores = get_labels_np_array(self.classifier_py.predict(x_train))
+        acc = np.sum(np.argmax(scores, axis=1) == np.argmax(y_train, axis=1)) / y_train.shape[0]
+        print('\n[PyTorch, MNIST] Accuracy on training set: %.2f%%' % (acc * 100))
+
+        scores = get_labels_np_array(self.classifier_py.predict(x_test))
+        acc = np.sum(np.argmax(scores, axis=1) == np.argmax(y_test, axis=1)) / y_test.shape[0]
+        print('\n[PyTorch, MNIST] Accuracy on test set: %.2f%%' % (acc * 100))
+
     def test_mnist(self):
         # Define all backends to test
         backends = {'keras': self.classifier_k,
-                    'tf': self.classifier_tf}
+                    'tf': self.classifier_tf,
+                    'pytorch': self.classifier_py}
 
         for _, classifier in backends.items():
+            if _ == 'pytorch':
+                self._swap_axes()
             self._test_backend_mnist(classifier)
+            if _ == 'pytorch':
+                self._swap_axes()
 
-        for _, classifier in backends.items():
-            self._test_backend_mnist(classifier)
+    def _swap_axes(self):
+        (x_train, y_train), (x_test, y_test) = self.mnist
+        x_train = np.swapaxes(x_train, 1, 3)
+        x_test = np.swapaxes(x_test, 1, 3)
+        self.mnist = ((x_train, y_train), (x_test, y_test))
 
     def _test_backend_mnist(self, classifier):
         # Get MNIST
@@ -70,6 +111,7 @@ class TestVirtualAdversarial(unittest.TestCase):
         self.assertFalse((x_test == x_test_adv).all())
 
         y_pred = get_labels_np_array(classifier.predict(x_test_adv))
+        print(np.argmax(y_pred, axis=1))
         self.assertFalse((y_test == y_pred).all())
 
         acc = np.sum(np.argmax(y_pred, axis=1) == np.argmax(y_test, axis=1)) / y_test.shape[0]
@@ -112,6 +154,18 @@ class TestVirtualAdversarial(unittest.TestCase):
                       metrics=['accuracy'])
 
         classifier = KerasClassifier((0, 1), model, use_logits=False)
+        return classifier
+
+    @staticmethod
+    def _cnn_mnist_py():
+        model = Model()
+
+        # Define a loss function and optimizer
+        loss_fn = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+        # Get classifier
+        classifier = PyTorchClassifier((0, 1), model, loss_fn, optimizer, (1, 28, 28), 10)
         return classifier
 
 
