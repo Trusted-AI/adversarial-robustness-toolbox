f49a3dc0d14ae9f86eaf10c3d4a4e164a9598c8d
Irina Nicolae <Maria-Irina.Nicolae@ibm.com>
2018-06-28 10:17:44 +0100

    Add BIM to docs, README and init files

--- art/attacks/__init__.py
+++ art/attacks/__init__.py
@@ -5,6 +5,7 @@ from art.attacks.attack import Attack
 from art.attacks.carlini import CarliniL2Method
 from art.attacks.deepfool import DeepFool
 from art.attacks.fast_gradient import FastGradientMethod
+from art.attacks.iterative_method import BasicIterativeMethod
 from art.attacks.newtonfool import NewtonFool
 from art.attacks.saliency_map import SaliencyMapMethod
 from art.attacks.universal_perturbation import UniversalPerturbation
