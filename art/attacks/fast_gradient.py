bee11fed1ac952ce80cd4302ee341e7e5dd6b47d
Irina Nicolae <Maria-Irina.Nicolae@ibm.com>
2018-07-13 12:05:38 +0100

    Correct double setting of parameter in FGSM
    (cherry picked from commit d99cd72ffca331d28b6e5c6a2c5f7c311a892222)

--- art/attacks/fast_gradient.py
+++ art/attacks/fast_gradient.py
@@ -100,8 +100,9 @@ class FastGradientMethod(Attack):
         :rtype: `np.ndarray`
         """
         self.set_params(**kwargs)
+        params_cpy = dict(kwargs)
 
-        if 'y' not in kwargs or kwargs[str('y')] is None:
+        if 'y' not in params_cpy or params_cpy[str('y')] is None:
             # Throw error if attack is targeted, but no targets are provided
             if self.targeted:
                 raise ValueError('Target labels `y` need to be provided for a targeted attack.')
@@ -109,12 +110,12 @@ class FastGradientMethod(Attack):
             # Use model predictions as correct outputs
             y = get_labels_np_array(self.classifier.predict(x))
         else:
-            y = kwargs[str('y')]
+            y = params_cpy.pop(str('y'))
         y = y / np.sum(y, axis=1, keepdims=True)
 
         # Return adversarial examples computed with minimal perturbation if option is active
-        if 'minimal' in kwargs and kwargs[str('minimal')]:
-            return self._minimal_perturbation(x, y, **kwargs)
+        if 'minimal' in params_cpy and params_cpy[str('minimal')]:
+            return self._minimal_perturbation(x, y, **params_cpy)
 
         return self._compute(x, y, self.eps)
 
