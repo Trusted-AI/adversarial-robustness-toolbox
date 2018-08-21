aa875548d4f001ef52d16b4d6990c334e15efb69
Ngoc Minh Tran <M.N.Tran@ibm.com>
2018-08-21 15:32:16 +0100

    implement batch support in predict of Pytorch classifier

--- art/classifiers/pytorch.py
+++ art/classifiers/pytorch.py
@@ -59,7 +59,7 @@ class PyTorchClassifier(Classifier):
         self._device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self._model.to(self._device)
 
-    def predict(self, x, logits=False):
+    def predict(self, x, logits=False, batch_size=128):
         """
         Perform prediction for a batch of inputs.
 
@@ -67,6 +67,8 @@ class PyTorchClassifier(Classifier):
         :type x: `np.ndarray`
         :param logits: `True` if the prediction should be done at the logits layer.
         :type logits: `bool`
+        :param batch_size: Size of batches.
+        :type batch_size: `int`
         :return: Array of predictions of shape `(nb_inputs, self.nb_classes)`.
         :rtype: `np.ndarray`
         """
@@ -85,15 +87,22 @@ class PyTorchClassifier(Classifier):
         #     exp = np.exp(preds - np.max(preds, axis=1, keepdims=True))
         #     preds = exp / np.sum(exp, axis=1, keepdims=True)
 
-        model_outputs = self._model(torch.from_numpy(x_).to(self._device).float())
-        (logit_output, output) = (model_outputs[-2], model_outputs[-1])
+        # Run prediction with batch processing
+        results = np.zeros((x_.shape[0], self.nb_classes), dtype=np.float32)
+        num_batch = int(np.ceil(len(x_) / float(batch_size)))
+        for m in range(num_batch):
+            # Batch indexes
+            begin, end = m * batch_size, min((m + 1) * batch_size, x_.shape[0])
 
-        if logits:
-            preds = logit_output.detach().cpu().numpy()
-        else:
-            preds = output.detach().cpu().numpy()
+            model_outputs = self._model(torch.from_numpy(x_[begin:end]).to(self._device).float())
+            (logit_output, output) = (model_outputs[-2], model_outputs[-1])
 
-        return preds
+            if logits:
+                results[begin:end] = logit_output.detach().cpu().numpy()
+            else:
+                results[begin:end] = output.detach().cpu().numpy()
+
+        return results
 
     def fit(self, x, y, batch_size=128, nb_epochs=10):
         """
