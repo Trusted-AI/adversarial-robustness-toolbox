Fixes problem with targeted attacks on fast gradient and basic iterative method

art/attacks/fast_gradient.py
@@ -17,7 +17,6 @@ class FastGradientMethod(Attack):
     def __init__(self, classifier, norm=np.inf, eps=.3, targeted=False, random_init=False, batch_size=128):
         """
         Create a :class:`FastGradientMethod` instance.
-
         :param classifier: A trained model.
         :type classifier: :class:`Classifier`
         :param norm: Order of the norm. Possible values: np.inf, 1 or 2.
@@ -42,7 +41,6 @@ class FastGradientMethod(Attack):
     def _minimal_perturbation(self, x, y, eps_step=0.1, eps_max=1., **kwargs):
         """Iteratively compute the minimal perturbation necessary to make the class prediction change. Stop when the
         first adversarial example was found.
-
         :param x: An array with the original inputs
         :type x: `np.ndarray`
         :param y:
@@ -69,15 +67,18 @@ class FastGradientMethod(Attack):
             # Get current predictions
             active_indices = np.arange(len(batch))
             current_eps = eps_step
-
             while len(active_indices) != 0 and current_eps <= eps_max:
                 # Adversarial crafting
                 current_x = self._apply_perturbation(x[batch_index_1:batch_index_2], perturbation, current_eps)
-
                 # Update
                 batch[active_indices] = current_x[active_indices]
                 adv_preds = self.classifier.predict(batch)
-                active_indices = np.where(np.argmax(batch_labels, axis=1) == np.argmax(adv_preds, axis=1))[0]
+                # If targeted active check to see whether we have hit the target, otherwise head to anything but
+                if self.targeted:
+                    active_indices = np.where(np.argmax(batch_labels, axis=1) != np.argmax(adv_preds, axis=1))[0]
+                else:
+                    active_indices = np.where(np.argmax(batch_labels, axis=1) == np.argmax(adv_preds, axis=1))[0]
+
                 current_eps += eps_step
 
             adv_x[batch_index_1:batch_index_2] = batch
@@ -86,7 +87,6 @@ class FastGradientMethod(Attack):
 
     def generate(self, x, **kwargs):
         """Generate adversarial samples and return them in an array.
-
         :param x: An array with the original inputs.
         :type x: `np.ndarray`
         :param eps: Attack step size (input variation)
@@ -131,7 +131,6 @@ class FastGradientMethod(Attack):
     def set_params(self, **kwargs):
         """
         Take in a dictionary of parameters and applies attack-specific checks before saving them as attributes.
-
         :param norm: Order of the norm. Possible values: np.inf, 1 or 2.
         :type norm: `int` or `float`
         :param eps: Attack step size (input variation)