42f629ecb9e96f9c13d57be0174c57b2f45045a0
Ngoc Minh Tran <M.N.Tran@ibm.com>
2018-10-09 17:02:58 +0100

    Fix bug VAT with Pytorch

--- art/attacks/virtual_adversarial.py
+++ art/attacks/virtual_adversarial.py
@@ -5,6 +5,7 @@ import logging
 import numpy as np
 
 from art.attacks.attack import Attack
+from art import NUMPY_DTYPE
 
 logger = logging.getLogger(__name__)
 
@@ -69,7 +70,7 @@ class VirtualAdversarialMethod(Attack):
 
             for _ in range(self.max_iter):
                 d = self._normalize(d)
-                preds_new = self.classifier.predict((val + d)[None, ...], logits=False)
+                preds_new = self.classifier.predict((val + d)[None, ...].astype(NUMPY_DTYPE), logits=False)
 
                 from scipy.stats import entropy
                 kl_div1 = entropy(preds[ind], preds_new[0])
@@ -79,7 +80,7 @@ class VirtualAdversarialMethod(Attack):
                 array_iter = np.nditer(d, op_flags=['readwrite'], flags=['multi_index'])
                 for x in array_iter:
                     x[...] += self.finite_diff
-                    preds_new = self.classifier.predict((val + d)[None, ...], logits=False)
+                    preds_new = self.classifier.predict((val + d)[None, ...].astype(NUMPY_DTYPE), logits=False)
                     kl_div2 = entropy(preds[ind], preds_new[0])
                     d_new[array_iter.multi_index] = (kl_div2 - kl_div1) / (self.finite_diff + tol)
                     x[...] -= self.finite_diff
