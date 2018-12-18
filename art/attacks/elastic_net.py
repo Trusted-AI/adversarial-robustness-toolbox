c130f7811529752d1e015ef2a04edcf1485b065f
Ngoc Minh Tran <M.N.Tran@ibm.com>
2018-12-18 13:09:24 +0000

    clean init

--- art/attacks/elastic_net.py
+++ art/attacks/elastic_net.py
@@ -18,55 +18,11 @@ class ElasticNet(Attack):
     attack_params = Attack.attack_params + ['confidence', 'targeted', 'learning_rate', 'max_iter', 'beta',
                                             'binary_search_steps', 'initial_const', 'batch_size', 'decision_rule']
 
-    def __init__(self, classifier, confidence=0.0, targeted=True, learning_rate=0.01, binary_search_steps=10,
-                 max_iter=10, initial_const=0.01, max_halving=5, max_doubling=5, batch_size=128):
-        """
-        Create a Carlini L_2 attack instance.
 
-        :param classifier: A trained model.
-        :type classifier: :class:`Classifier`
-        :param confidence: Confidence of adversarial examples: a higher value produces examples that are farther away,
-                from the original input, but classified with higher confidence as the target class.
-        :type confidence: `float`
-        :param targeted: Should the attack target one specific class.
-        :type targeted: `bool`
-        :param learning_rate: The initial learning rate for the attack algorithm. Smaller values produce better results
-                but are slower to converge.
-        :type learning_rate: `float`
-        :param binary_search_steps: number of times to adjust constant with binary search (positive value).
-        :type binary_search_steps: `int`
-        :param max_iter: The maximum number of iterations.
-        :type max_iter: `int`
-        :param initial_const: The initial trade-off constant `c` to use to tune the relative importance of distance and
-                confidence. If `binary_search_steps` is large, the initial constant is not important, as discussed in
-                Carlini and Wagner (2016).
-        :type initial_const: `float`
-        :param max_halving: Maximum number of halving steps in the line search optimization.
-        :type max_halving: `int`
-        :param max_doubling: Maximum number of doubling steps in the line search optimization.
-        :type max_doubling: `int`
-        :param batch_size: Internal size of batches on which adversarial samples are generated.
-        :type batch_size: `int`
-        """
-        super(CarliniL2Method, self).__init__(classifier)
 
-        kwargs = {'confidence': confidence,
-                  'targeted': targeted,
-                  'learning_rate': learning_rate,
-                  'binary_search_steps': binary_search_steps,
-                  'max_iter': max_iter,
-                  'initial_const': initial_const,
-                  'max_halving': max_halving,
-                  'max_doubling': max_doubling,
-                  'batch_size': batch_size
-                  }
-        assert self.set_params(**kwargs)
 
-        # There are internal hyperparameters:
-        # Abort binary search for c if it exceeds this threshold (suggested in Carlini and Wagner (2016)):
-        self._c_upper_bound = 10e10
-        # Smooth arguments of arctanh by multiplying with this constant to avoid division by zero:
-        self._tanh_smoother = 0.999999
+
+
 
     def _loss(self, x, x_adv, target, c):
         """
