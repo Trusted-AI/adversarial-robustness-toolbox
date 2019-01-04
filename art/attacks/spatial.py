c385a8c8b54717590be2d19757f522deb9d8dcda
Beat Buesser <beat.buesser@ie.ibm.com>
2019-01-04 21:59:31 +0000

    Create class for spatial attacks

---
+++ art/attacks/spatial.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+
+import numpy as np
+
+
+
+class Spatial():
+    """
+    Spatial attack
+    """
+
+    def __init__(self):
+        """
+
+        """
