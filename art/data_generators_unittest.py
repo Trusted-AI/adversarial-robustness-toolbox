130b448eef6a72bee47aa894a10984d0177e4894
Ngoc Minh Tran <M.N.Tran@ibm.com>
2019-01-28 15:18:43 +0000

    TF data gen test

--- art/data_generators_unittest.py
+++ art/data_generators_unittest.py
@@ -12,220 +12,220 @@ from art.utils import master_seed
 logger = logging.getLogger('testLogger')
 
 
-class TestKerasDataGenerator(unittest.TestCase):
-    def setUp(self):
-        import keras
-
-        # Set master seed
-        master_seed(42)
-
-        class DummySequence(keras.utils.Sequence):
-            def __init__(self):
-                self._size = 5
-                self._x = np.random.rand(self._size, 28, 28, 1)
-                self._y = np.random.randint(0, high=10, size=(self._size, 10))
-
-            def __len__(self):
-                return self._size
-
-            def __getitem__(self, idx):
-                return self._x[idx], self._y[idx]
-
-        sequence = DummySequence()
-        self.data_gen = KerasDataGenerator(sequence, size=5, batch_size=1)
-
-    def tearDown(self):
-        import keras.backend as k
-
-        k.clear_session()
-
-    def test_gen_interface(self):
-        gen = self._dummy_gen()
-        data_gen = KerasDataGenerator(gen, size=None, batch_size=5)
-
-        x, y = data_gen.get_batch()
-
-        # Check return types
-        self.assertTrue(isinstance(x, np.ndarray))
-        self.assertTrue(isinstance(y, np.ndarray))
-
-        # Check shapes
-        self.assertTrue(x.shape == (5, 28, 28, 1))
-        self.assertTrue(y.shape == (5, 10))
-
-    def test_gen_keras_specific(self):
-        gen = self._dummy_gen()
-        data_gen = KerasDataGenerator(gen, size=None, batch_size=5)
-
-        iter_ = iter(data_gen.generator)
-        x, y = next(iter_)
-
-        # Check return types
-        self.assertTrue(isinstance(x, np.ndarray))
-        self.assertTrue(isinstance(y, np.ndarray))
-
-        # Check shapes
-        self.assertTrue(x.shape == (5, 28, 28, 1))
-        self.assertTrue(y.shape == (5, 10))
-
-    def test_sequence_keras_specific(self):
-        iter_ = iter(self.data_gen.generator)
-        x, y = next(iter_)
-
-        # Check return types
-        self.assertTrue(isinstance(x, np.ndarray))
-        self.assertTrue(isinstance(y, np.ndarray))
-
-        # Check shapes
-        self.assertTrue(x.shape == (28, 28, 1))
-        self.assertTrue(y.shape == (10,))
-
-    def test_sequence_interface(self):
-        x, y = self.data_gen.get_batch()
-
-        # Check return types
-        self.assertTrue(isinstance(x, np.ndarray))
-        self.assertTrue(isinstance(y, np.ndarray))
-
-        # Check shapes
-        self.assertTrue(x.shape == (28, 28, 1))
-        self.assertTrue(y.shape == (10,))
-
-    def test_imagedatagen_interface(self):
-        train_size, batch_size = 20, 5
-        x_train, y_train = np.random.rand(train_size, 28, 28, 1), np.random.randint(0, 2, size=(train_size, 10))
-
-        datagen = ImageDataGenerator(width_shift_range=0.075, height_shift_range=0.075, rotation_range=12,
-                                     shear_range=0.075, zoom_range=0.05, fill_mode='constant', cval=0)
-        datagen.fit(x_train)
-
-        # Create wrapper and get batch
-        data_gen = KerasDataGenerator(datagen.flow(x_train, y_train, batch_size=batch_size), size=None,
-                                      batch_size=batch_size)
-        x, y = data_gen.get_batch()
-
-        # Check return types
-        self.assertTrue(isinstance(x, np.ndarray))
-        self.assertTrue(isinstance(y, np.ndarray))
-
-        # Check shapes
-        self.assertTrue(x.shape == (batch_size, 28, 28, 1))
-        self.assertTrue(y.shape == (batch_size, 10))
-
-    def test_imagedatagen_keras_specific(self):
-        train_size, batch_size = 20, 5
-        x_train, y_train = np.random.rand(train_size, 28, 28, 1), np.random.randint(0, 2, size=(train_size, 10))
-
-        datagen = ImageDataGenerator(width_shift_range=0.075, height_shift_range=0.075, rotation_range=12,
-                                     shear_range=0.075, zoom_range=0.05, fill_mode='constant', cval=0)
-        datagen.fit(x_train)
-
-        # Create wrapper and get batch
-        data_gen = KerasDataGenerator(datagen.flow(x_train, y_train, batch_size=batch_size), size=None,
-                                      batch_size=batch_size)
-        x, y = next(data_gen.generator)
-
-        # Check return types
-        self.assertTrue(isinstance(x, np.ndarray))
-        self.assertTrue(isinstance(y, np.ndarray))
-
-        # Check shapes
-        self.assertTrue(x.shape == (batch_size, 28, 28, 1))
-        self.assertTrue(y.shape == (batch_size, 10))
-
-    @staticmethod
-    def _dummy_gen(size=5):
-        yield np.random.rand(size, 28, 28, 1), np.random.randint(low=0, high=10, size=(size, 10))
-
-
-class TestPyTorchGenerator(unittest.TestCase):
-    def setUp(self):
-        import torch
-        from torch.utils.data import DataLoader
-
-        # Set master seed
-        master_seed(42)
-
-        class DummyDataset(torch.utils.data.Dataset):
-            def __init__(self):
-                self._size = 10
-                self._x = np.random.rand(self._size, 1, 5, 5)
-                self._y = np.random.randint(0, high=10, size=self._size)
-
-            def __len__(self):
-                return self._size
-
-            def __getitem__(self, idx):
-                return self._x[idx], self._y[idx]
-
-        dataset = DummyDataset()
-        data_loader = DataLoader(dataset=dataset, batch_size=5, shuffle=True)
-        self.data_gen = PyTorchDataGenerator(data_loader, size=10, batch_size=5)
-
-    def test_gen_interface(self):
-        x, y = self.data_gen.get_batch()
-
-        # Check return types
-        self.assertTrue(isinstance(x, np.ndarray))
-        self.assertTrue(isinstance(y, np.ndarray))
-
-        # Check shapes
-        self.assertTrue(x.shape == (5, 1, 5, 5))
-        self.assertTrue(y.shape == (5,))
-
-    def test_pytorch_specific(self):
-        import torch
-
-        iter_ = iter(self.data_gen.data_loader)
-        x, y = next(iter_)
-
-        # Check return types
-        self.assertTrue(isinstance(x, torch.Tensor))
-        self.assertTrue(isinstance(y, torch.Tensor))
-
-        # Check shapes
-        self.assertTrue(x.shape == (5, 1, 5, 5))
-        self.assertTrue(y.shape == (5,))
-
-
-class TestMXGenerator(unittest.TestCase):
-    def setUp(self):
-        import mxnet as mx
-
-        # Set master seed
-        master_seed(42)
-
-        x = mx.random.uniform(shape=(10, 1, 5, 5))
-        y = mx.random.uniform(shape=10)
-        dataset = mx.gluon.data.dataset.ArrayDataset(x, y)
-
-        data_loader = mx.gluon.data.DataLoader(dataset, batch_size=5, shuffle=True)
-        self.data_gen = MXDataGenerator(data_loader, size=10, batch_size=5)
-
-    def test_gen_interface(self):
-        x, y = self.data_gen.get_batch()
-
-        # Check return types
-        self.assertTrue(isinstance(x, np.ndarray))
-        self.assertTrue(isinstance(y, np.ndarray))
-
-        # Check shapes
-        self.assertTrue(x.shape == (5, 1, 5, 5))
-        self.assertTrue(y.shape == (5,))
-
-    def test_mxnet_specific(self):
-        import mxnet as mx
-
-        iter_ = iter(self.data_gen.data_loader)
-        x, y = next(iter_)
-
-        # Check return types
-        self.assertTrue(isinstance(x, mx.ndarray.NDArray))
-        self.assertTrue(isinstance(y, mx.ndarray.NDArray))
-
-        # Check shapes
-        self.assertTrue(x.shape == (5, 1, 5, 5))
-        self.assertTrue(y.shape == (5,))
+# class TestKerasDataGenerator(unittest.TestCase):
+#     def setUp(self):
+#         import keras
+#
+#         # Set master seed
+#         master_seed(42)
+#
+#         class DummySequence(keras.utils.Sequence):
+#             def __init__(self):
+#                 self._size = 5
+#                 self._x = np.random.rand(self._size, 28, 28, 1)
+#                 self._y = np.random.randint(0, high=10, size=(self._size, 10))
+#
+#             def __len__(self):
+#                 return self._size
+#
+#             def __getitem__(self, idx):
+#                 return self._x[idx], self._y[idx]
+#
+#         sequence = DummySequence()
+#         self.data_gen = KerasDataGenerator(sequence, size=5, batch_size=1)
+#
+#     def tearDown(self):
+#         import keras.backend as k
+#
+#         k.clear_session()
+#
+#     def test_gen_interface(self):
+#         gen = self._dummy_gen()
+#         data_gen = KerasDataGenerator(gen, size=None, batch_size=5)
+#
+#         x, y = data_gen.get_batch()
+#
+#         # Check return types
+#         self.assertTrue(isinstance(x, np.ndarray))
+#         self.assertTrue(isinstance(y, np.ndarray))
+#
+#         # Check shapes
+#         self.assertTrue(x.shape == (5, 28, 28, 1))
+#         self.assertTrue(y.shape == (5, 10))
+#
+#     def test_gen_keras_specific(self):
+#         gen = self._dummy_gen()
+#         data_gen = KerasDataGenerator(gen, size=None, batch_size=5)
+#
+#         iter_ = iter(data_gen.generator)
+#         x, y = next(iter_)
+#
+#         # Check return types
+#         self.assertTrue(isinstance(x, np.ndarray))
+#         self.assertTrue(isinstance(y, np.ndarray))
+#
+#         # Check shapes
+#         self.assertTrue(x.shape == (5, 28, 28, 1))
+#         self.assertTrue(y.shape == (5, 10))
+#
+#     def test_sequence_keras_specific(self):
+#         iter_ = iter(self.data_gen.generator)
+#         x, y = next(iter_)
+#
+#         # Check return types
+#         self.assertTrue(isinstance(x, np.ndarray))
+#         self.assertTrue(isinstance(y, np.ndarray))
+#
+#         # Check shapes
+#         self.assertTrue(x.shape == (28, 28, 1))
+#         self.assertTrue(y.shape == (10,))
+#
+#     def test_sequence_interface(self):
+#         x, y = self.data_gen.get_batch()
+#
+#         # Check return types
+#         self.assertTrue(isinstance(x, np.ndarray))
+#         self.assertTrue(isinstance(y, np.ndarray))
+#
+#         # Check shapes
+#         self.assertTrue(x.shape == (28, 28, 1))
+#         self.assertTrue(y.shape == (10,))
+#
+#     def test_imagedatagen_interface(self):
+#         train_size, batch_size = 20, 5
+#         x_train, y_train = np.random.rand(train_size, 28, 28, 1), np.random.randint(0, 2, size=(train_size, 10))
+#
+#         datagen = ImageDataGenerator(width_shift_range=0.075, height_shift_range=0.075, rotation_range=12,
+#                                      shear_range=0.075, zoom_range=0.05, fill_mode='constant', cval=0)
+#         datagen.fit(x_train)
+#
+#         # Create wrapper and get batch
+#         data_gen = KerasDataGenerator(datagen.flow(x_train, y_train, batch_size=batch_size), size=None,
+#                                       batch_size=batch_size)
+#         x, y = data_gen.get_batch()
+#
+#         # Check return types
+#         self.assertTrue(isinstance(x, np.ndarray))
+#         self.assertTrue(isinstance(y, np.ndarray))
+#
+#         # Check shapes
+#         self.assertTrue(x.shape == (batch_size, 28, 28, 1))
+#         self.assertTrue(y.shape == (batch_size, 10))
+#
+#     def test_imagedatagen_keras_specific(self):
+#         train_size, batch_size = 20, 5
+#         x_train, y_train = np.random.rand(train_size, 28, 28, 1), np.random.randint(0, 2, size=(train_size, 10))
+#
+#         datagen = ImageDataGenerator(width_shift_range=0.075, height_shift_range=0.075, rotation_range=12,
+#                                      shear_range=0.075, zoom_range=0.05, fill_mode='constant', cval=0)
+#         datagen.fit(x_train)
+#
+#         # Create wrapper and get batch
+#         data_gen = KerasDataGenerator(datagen.flow(x_train, y_train, batch_size=batch_size), size=None,
+#                                       batch_size=batch_size)
+#         x, y = next(data_gen.generator)
+#
+#         # Check return types
+#         self.assertTrue(isinstance(x, np.ndarray))
+#         self.assertTrue(isinstance(y, np.ndarray))
+#
+#         # Check shapes
+#         self.assertTrue(x.shape == (batch_size, 28, 28, 1))
+#         self.assertTrue(y.shape == (batch_size, 10))
+#
+#     @staticmethod
+#     def _dummy_gen(size=5):
+#         yield np.random.rand(size, 28, 28, 1), np.random.randint(low=0, high=10, size=(size, 10))
+#
+#
+# class TestPyTorchGenerator(unittest.TestCase):
+#     def setUp(self):
+#         import torch
+#         from torch.utils.data import DataLoader
+#
+#         # Set master seed
+#         master_seed(42)
+#
+#         class DummyDataset(torch.utils.data.Dataset):
+#             def __init__(self):
+#                 self._size = 10
+#                 self._x = np.random.rand(self._size, 1, 5, 5)
+#                 self._y = np.random.randint(0, high=10, size=self._size)
+#
+#             def __len__(self):
+#                 return self._size
+#
+#             def __getitem__(self, idx):
+#                 return self._x[idx], self._y[idx]
+#
+#         dataset = DummyDataset()
+#         data_loader = DataLoader(dataset=dataset, batch_size=5, shuffle=True)
+#         self.data_gen = PyTorchDataGenerator(data_loader, size=10, batch_size=5)
+#
+#     def test_gen_interface(self):
+#         x, y = self.data_gen.get_batch()
+#
+#         # Check return types
+#         self.assertTrue(isinstance(x, np.ndarray))
+#         self.assertTrue(isinstance(y, np.ndarray))
+#
+#         # Check shapes
+#         self.assertTrue(x.shape == (5, 1, 5, 5))
+#         self.assertTrue(y.shape == (5,))
+#
+#     def test_pytorch_specific(self):
+#         import torch
+#
+#         iter_ = iter(self.data_gen.data_loader)
+#         x, y = next(iter_)
+#
+#         # Check return types
+#         self.assertTrue(isinstance(x, torch.Tensor))
+#         self.assertTrue(isinstance(y, torch.Tensor))
+#
+#         # Check shapes
+#         self.assertTrue(x.shape == (5, 1, 5, 5))
+#         self.assertTrue(y.shape == (5,))
+#
+#
+# class TestMXGenerator(unittest.TestCase):
+#     def setUp(self):
+#         import mxnet as mx
+#
+#         # Set master seed
+#         master_seed(42)
+#
+#         x = mx.random.uniform(shape=(10, 1, 5, 5))
+#         y = mx.random.uniform(shape=10)
+#         dataset = mx.gluon.data.dataset.ArrayDataset(x, y)
+#
+#         data_loader = mx.gluon.data.DataLoader(dataset, batch_size=5, shuffle=True)
+#         self.data_gen = MXDataGenerator(data_loader, size=10, batch_size=5)
+#
+#     def test_gen_interface(self):
+#         x, y = self.data_gen.get_batch()
+#
+#         # Check return types
+#         self.assertTrue(isinstance(x, np.ndarray))
+#         self.assertTrue(isinstance(y, np.ndarray))
+#
+#         # Check shapes
+#         self.assertTrue(x.shape == (5, 1, 5, 5))
+#         self.assertTrue(y.shape == (5,))
+#
+#     def test_mxnet_specific(self):
+#         import mxnet as mx
+#
+#         iter_ = iter(self.data_gen.data_loader)
+#         x, y = next(iter_)
+#
+#         # Check return types
+#         self.assertTrue(isinstance(x, mx.ndarray.NDArray))
+#         self.assertTrue(isinstance(y, mx.ndarray.NDArray))
+#
+#         # Check shapes
+#         self.assertTrue(x.shape == (5, 1, 5, 5))
+#         self.assertTrue(y.shape == (5,))
 
 
 class TestTFDataGenerator(unittest.TestCase):
@@ -237,23 +237,23 @@ class TestTFDataGenerator(unittest.TestCase):
 
         def generator(batch_size=5):
             while True:
-                yield np.random.rand(batch_size, 5, 5, 1), np.random.randint(0, 10, size=10 * batch_size). \
+                yield np.random.rand(batch_size, 5, 5, 1), np.random.randint(0, 10, size=10 * batch_size).\
                     reshape(batch_size, -1)
 
-        sess = tf.Session()
-        dataset = tf.data.Dataset.from_generator(generator, (tf.float32, tf.int32))
-        iter_ = dataset.make_one_shot_iterator()
-        sess.run(tf.global_variables_initializer())
-        self.data_gen = TFDataGenerator(sess=sess, iterator=iter_, size=10, batch_size=5)
+        self.sess = tf.Session()
+        self.dataset = tf.data.Dataset.from_generator(generator, (tf.float32, tf.int32))
 
     def tearDown(self):
         import tensorflow as tf
 
-        self.data_gen.sess.close()
+        self.sess.close()
         tf.reset_default_graph()
 
-    def test_gen_interface(self):
-        x, y = self.data_gen.get_batch()
+    def test_init(self):
+        iter_ = self.dataset.make_initializable_iterator()
+        data_gen = TFDataGenerator(sess=self.sess, iterator=iter_, iterator_type='initializable',
+                                   iterator_arg={}, size=10, batch_size=5)
+        x, y = data_gen.get_batch()
 
         # Check return types
         self.assertTrue(isinstance(x, np.ndarray))
@@ -262,6 +262,43 @@ class TestTFDataGenerator(unittest.TestCase):
         # Check shapes
         self.assertTrue(x.shape == (5, 5, 5, 1))
         self.assertTrue(y.shape == (5, 10))
+
+    def test_reinit(self):
+        import tensorflow as tf
+
+        iter_ = tf.data.Iterator.from_structure(self.dataset.output_types, self.dataset.output_shapes)
+        init_op = iter_.make_initializer(self.dataset)
+        data_gen = TFDataGenerator(sess=self.sess, iterator=iter_, iterator_type='reinitializable',
+                                   iterator_arg=init_op, size=10, batch_size=5)
+        x, y = data_gen.get_batch()
+
+        # Check return types
+        self.assertTrue(isinstance(x, np.ndarray))
+        self.assertTrue(isinstance(y, np.ndarray))
+
+        # Check shapes
+        self.assertTrue(x.shape == (5, 5, 5, 1))
+        self.assertTrue(y.shape == (5, 10))
+
+    def test_feedable(self):
+        import tensorflow as tf
+
+        handle = tf.placeholder(tf.string, shape=[])
+        iter_ = tf.data.Iterator.from_string_handle(handle, self.dataset.output_types, self.dataset.output_shapes)
+        feed_iterator = self.dataset.make_initializable_iterator()
+        feed_handle = self.sess.run(feed_iterator.string_handle())
+        data_gen = TFDataGenerator(sess=self.sess, iterator=iter_, iterator_type='feedable',
+                                   iterator_arg=(feed_iterator, {handle: feed_handle}), size=10, batch_size=5)
+        x, y = data_gen.get_batch()
+
+        # Check return types
+        self.assertTrue(isinstance(x, np.ndarray))
+        self.assertTrue(isinstance(y, np.ndarray))
+
+        # Check shapes
+        self.assertTrue(x.shape == (5, 5, 5, 1))
+        self.assertTrue(y.shape == (5, 10))
+
 
 if __name__ == '__main__':
     unittest.main()
