22ff1e4541e6c5e780eae0a33b38e0d2623f9f3b
Beat Buesser <beat.buesser@ie.ibm.com>
2019-02-13 13:01:24 +0000

    Apply standard classifiers in gradient descent attack unit tests

--- art/attacks/projected_gradient_descent_unittest.py
+++ art/attacks/projected_gradient_descent_unittest.py
@@ -3,40 +3,18 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import logging
 import unittest
 
-import keras
 import keras.backend as k
 import numpy as np
-import tensorflow as tf
-import torch.nn as nn
-import torch.nn.functional as f
-import torch.optim as optim
-from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
-from keras.models import Sequential
 
 from art.attacks.projected_gradient_descent import ProjectedGradientDescent
-from art.classifiers import KerasClassifier, PyTorchClassifier, TFClassifier
-from art.utils import load_mnist, get_labels_np_array, master_seed
+from art.utils import load_mnist, get_labels_np_array, master_seed, get_classifier_tf, get_classifier_kr, \
+    get_classifier_pt
 
 logger = logging.getLogger('testLogger')
 
 BATCH_SIZE = 10
 NB_TRAIN = 100
 NB_TEST = 11
-
-
-class Model(nn.Module):
-    def __init__(self):
-        super(Model, self).__init__()
-        self.conv = nn.Conv2d(1, 16, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc = nn.Linear(2304, 10)
-
-    def forward(self, x):
-        x = self.pool(f.relu(self.conv(x)))
-        x = x.view(-1, 2304)
-        logit_output = self.fc(x)
-
-        return logit_output
 
 
 class TestPGD(unittest.TestCase):
@@ -50,8 +28,7 @@ class TestPGD(unittest.TestCase):
         cls.mnist = (x_train, y_train), (x_test, y_test)
 
         # Keras classifier
-        cls.classifier_k = cls._cnn_mnist_k([28, 28, 1])
-        cls.classifier_k.fit(x_train, y_train, batch_size=BATCH_SIZE, nb_epochs=2)
+        cls.classifier_k, sess = get_classifier_kr()
 
         scores = cls.classifier_k._model.evaluate(x_train, y_train)
         logger.info('[Keras, MNIST] Accuracy on training set: %.2f%%', scores[1] * 100)
@@ -59,8 +36,7 @@ class TestPGD(unittest.TestCase):
         logger.info('[Keras, MNIST] Accuracy on test set: %.2f%%', scores[1] * 100)
 
         # Create basic CNN on MNIST using TensorFlow
-        cls.classifier_tf = cls._cnn_mnist_tf([28, 28, 1])
-        cls.classifier_tf.fit(x_train, y_train, nb_epochs=2, batch_size=BATCH_SIZE)
+        cls.classifier_tf, sess = get_classifier_tf()
 
         scores = get_labels_np_array(cls.classifier_tf.predict(x_train))
         acc = np.sum(np.argmax(scores, axis=1) == np.argmax(y_train, axis=1)) / y_train.shape[0]
@@ -71,9 +47,8 @@ class TestPGD(unittest.TestCase):
         logger.info('[TF, MNIST] Accuracy on test set: %.2f%%', acc * 100)
 
         # Create basic PyTorch model
-        cls.classifier_py = cls._cnn_mnist_py()
+        cls.classifier_py = get_classifier_pt()
         x_train, x_test = np.swapaxes(x_train, 1, 3), np.swapaxes(x_test, 1, 3)
-        cls.classifier_py.fit(x_train, y_train, nb_epochs=2, batch_size=BATCH_SIZE)
 
         scores = get_labels_np_array(cls.classifier_py.predict(x_train))
         acc = np.sum(np.argmax(scores, axis=1) == np.argmax(y_train, axis=1)) / y_train.shape[0]
@@ -129,58 +104,6 @@ class TestPGD(unittest.TestCase):
 
         acc = np.sum(np.argmax(test_y_pred, axis=1) == np.argmax(y_test, axis=1)) / y_test.shape[0]
         logger.info('Accuracy on adversarial test examples: %.2f%%', acc * 100)
-
-    @staticmethod
-    def _cnn_mnist_tf(input_shape):
-        labels_tf = tf.placeholder(tf.float32, [None, 10])
-        inputs_tf = tf.placeholder(tf.float32, [None] + list(input_shape))
-
-        # Define the tensorflow graph
-        conv = tf.layers.conv2d(inputs_tf, 4, 5, activation=tf.nn.relu)
-        conv = tf.layers.max_pooling2d(conv, 2, 2)
-        fc = tf.contrib.layers.flatten(conv)
-
-        # Logits layer
-        logits = tf.layers.dense(fc, 10)
-
-        # Train operator
-        loss = tf.reduce_mean(tf.losses.softmax_cross_entropy(logits=logits, onehot_labels=labels_tf))
-        optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
-        train_tf = optimizer.minimize(loss)
-
-        sess = tf.Session()
-        sess.run(tf.global_variables_initializer())
-
-        classifier = TFClassifier((0, 1), inputs_tf, logits, loss=loss, train=train_tf, output_ph=labels_tf,
-                                  sess=sess)
-        return classifier
-
-    @staticmethod
-    def _cnn_mnist_k(input_shape):
-        # Create simple CNN
-        model = Sequential()
-        model.add(Conv2D(4, kernel_size=(5, 5), activation='relu', input_shape=input_shape))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Flatten())
-        model.add(Dense(10, activation='softmax'))
-
-        model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(lr=0.01),
-                      metrics=['accuracy'])
-
-        classifier = KerasClassifier((0, 1), model, use_logits=False)
-        return classifier
-
-    @staticmethod
-    def _cnn_mnist_py():
-        model = Model()
-
-        # Define a loss function and optimizer
-        loss_fn = nn.CrossEntropyLoss()
-        optimizer = optim.Adam(model.parameters(), lr=0.01)
-
-        # Get classifier
-        classifier = PyTorchClassifier((0, 1), model, loss_fn, optimizer, (1, 28, 28), 10)
-        return classifier
 
 
 if __name__ == '__main__':
