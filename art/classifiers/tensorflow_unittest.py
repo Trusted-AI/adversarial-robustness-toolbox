8504c8438e0784a69b33d6f7e8b9d66155e645f9
Ngoc Minh Tran <M.N.Tran@ibm.com>
2019-02-13 11:34:33 +0000

    tf unittest getactivation batching

--- art/classifiers/tensorflow_unittest.py
+++ art/classifiers/tensorflow_unittest.py
@@ -158,14 +158,14 @@ class TestTFClassifier(unittest.TestCase):
                                         'Flatten/flatten/Reshape:0', 'dense/BiasAdd:0'])
 
         for i, name in enumerate(layer_names):
-            act_i = self.classifier.get_activations(x_test, i)
-            act_name = self.classifier.get_activations(x_test, name)
+            act_i = self.classifier.get_activations(x_test, i, batch_size=5)
+            act_name = self.classifier.get_activations(x_test, name, batch_size=5)
             self.assertAlmostEqual(np.sum(act_name - act_i), 0)
 
-        self.assertTrue(self.classifier.get_activations(x_test, 0).shape == (20, 24, 24, 16))
-        self.assertTrue(self.classifier.get_activations(x_test, 1).shape == (20, 12, 12, 16))
-        self.assertTrue(self.classifier.get_activations(x_test, 2).shape == (20, 2304))
-        self.assertTrue(self.classifier.get_activations(x_test, 3).shape == (20, 10))
+        self.assertTrue(self.classifier.get_activations(x_test, 0, batch_size=5).shape == (20, 24, 24, 16))
+        self.assertTrue(self.classifier.get_activations(x_test, 1, batch_size=5).shape == (20, 12, 12, 16))
+        self.assertTrue(self.classifier.get_activations(x_test, 2, batch_size=5).shape == (20, 2304))
+        self.assertTrue(self.classifier.get_activations(x_test, 3, batch_size=5).shape == (20, 10))
         tf.reset_default_graph()
 
     def test_save(self):
