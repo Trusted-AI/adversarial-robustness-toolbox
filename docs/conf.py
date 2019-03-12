dd481db0af5517cad6e30747c47be4fa441484de
Irina Nicolae <Maria-Irina.Nicolae@ibm.com>
2019-03-12 21:27:21 +0000

    Update version to 0.6.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, IBM Corporation'
 author = 'Maria-Irina Nicolae'
 
 # The short X.Y version
-version = '0.5'
+version = '0.6'
 # The full version, including alpha/beta/rc tags
-release = '0.5.0'
+release = '0.6.0'
 
 
 # -- General configuration ---------------------------------------------------
