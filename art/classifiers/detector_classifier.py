fix 4th, 5th, 6th cases of class gradient of detector classifier

art/classifiers/detector_classifier.py
@@ -242,10 +242,21 @@ class DetectorClassifier(Classifier):
             # Then compute the detector gradients
             detector_grads = self.detector.class_gradient(x=x_, label=None, logits=True)
 
-            # Chain the detector gradients
+            # Chain the detector gradients for the first component
             classifier_logits = self.classifier.predict(x=x_, logits=True)
             max_classifier_logits = np.max(classifier_logits, axis=1)
-            detector_grads = max_classifier_logits[:, None, None, None, None] * detector_grads
+            first_detector_grads = max_classifier_logits[:, None, None, None, None] * detector_grads
+
+            # Chain the detector gradients for the second component
+            maxind_classifier_logits = np.argmax(classifier_logits, axis=1)
+            max_classifier_grads = classifier_grads[np.arange(len(classifier_grads)), maxind_classifier_logits]
+            detector_logits = self.detector.predict(x=x_, logits=True)
+            second_detector_grads = max_classifier_grads * (detector_logits + 1)[:, None, None]
+            second_detector_grads = second_detector_grads[None, ...]
+            second_detector_grads = np.swapaxes(second_detector_grads, 0, 1)
+
+            # Update detector gradients
+            detector_grads = first_detector_grads + second_detector_grads
 
             # Combine the gradients
             combined_logits_grads = np.concatenate([classifier_grads, detector_grads], axis=1)
@@ -314,6 +325,7 @@ class DetectorClassifier(Classifier):
 
         # Apply gradient post-processing
         combined_grads = self._apply_processing_gradient(combined_grads)
+
         return combined_grads
 
     def loss_gradient(self, x, y):