f2273550db7c02d0ca0ee25e1a501d4304bc97d7
Irina Nicolae <Maria-Irina.Nicolae@ibm.com>
2019-04-01 16:10:36 +0100

    Update version to 0.7.0
    (cherry picked from commit d9958493a96244476867bcf5970e7877f1b05dc8)

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, IBM Corporation'
 author = 'Maria-Irina Nicolae'
 
 # The short X.Y version
-version = '0.6'
+version = '0.7'
 # The full version, including alpha/beta/rc tags
-release = '0.6.0'
+release = '0.7.0'
 
 
 # -- General configuration ---------------------------------------------------
