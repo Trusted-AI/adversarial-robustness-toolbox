5649909f8a3d662a134d72f0338f3f42c2b0e756
Ngoc Minh Tran <M.N.Tran@ibm.com>
2019-04-01 15:59:46 +0100

    boundary attack - set params
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- art/attacks/boundary.py
+++ art/attacks/boundary.py
@@ -101,29 +101,35 @@ class Boundary(Attack):
         """
         Take in a dictionary of parameters and applies attack-specific checks before saving them as attributes.
 
+        :param targeted: Should the attack target one specific class.
+        :type targeted: `bool`
+        :param delta: Initial step size for the orthogonal step.
+        :type delta: `float`
+        :param epsilon: Initial step size for the step towards the target.
+        :type epsilon: `float`
+        :param step_adapt: Factor by which the step sizes are multiplied or divided, must be in the range (0, 1).
+        :type step_adapt: `float`
         :param max_iter: The maximum number of iterations.
         :type max_iter: `int`
-        :param epsilon: Overshoot parameter.
-        :type epsilon: `float`
-        :param nb_grads: The number of class gradients (top nb_grads w.r.t. prediction) to compute. This way only the
-                         most likely classes are considered, speeding up the computation.
-        :type nb_grads: `int`
-        :param batch_size: Internal size of batches on which adversarial samples are generated.
-        :type batch_size: `int`
+        :param sample_size: Maximum number of trials per iteration.
+        :type sample_size: `int`
         """
         # Save attack-specific parameters
-        super(DeepFool, self).set_params(**kwargs)
+        super(Boundary, self).set_params(**kwargs)
 
         if not isinstance(self.max_iter, (int, np.int)) or self.max_iter <= 0:
             raise ValueError("The number of iterations must be a positive integer.")
 
-        if not isinstance(self.nb_grads, (int, np.int)) or self.nb_grads <= 0:
-            raise ValueError("The number of class gradients to compute must be a positive integer.")
+        if not isinstance(self.sample_size, (int, np.int)) or self.sample_size <= 0:
+            raise ValueError("The number of trials must be a positive integer.")
 
-        if self.epsilon < 0:
-            raise ValueError("The overshoot parameter must not be negative.")
+        if self.epsilon <= 0:
+            raise ValueError("The initial step size for the step towards the target must be positive.")
 
-        if self.batch_size <= 0:
-            raise ValueError('The batch size `batch_size` has to be positive.')
+        if self.delta <= 0:
+            raise ValueError("The initial step size for the orthogonal step must be positive.")
+
+        if self.step_adapt <= 0 or self.step_adapt >= 1:
+            raise ValueError("The adaptation factor must be in the range (0, 1).")
 
         return True
