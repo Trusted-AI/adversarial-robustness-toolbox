11a4b7bad734f0c4593e4e520b9b3d745c0593b2
Irina Nicolae <irinutza.n@gmail.com>
2019-04-25 13:52:08 +0100

    Update art/attacks/fast_gradient.py
    Co-Authored-By: beat-buesser <49047826+beat-buesser@users.noreply.github.com>

--- art/attacks/fast_gradient.py
+++ art/attacks/fast_gradient.py
@@ -48,7 +48,7 @@ class FastGradientMethod(Attack):
         :param targeted: Should the attack target one specific class
         :type targeted: `bool`
         :param num_random_init: Number of random initialisations within the epsilon ball. For random_init=0 starting at the
-        original input.
+            original input.
         :type num_random_init: `int`
         :param batch_size: Batch size
         :type batch_size: `int`
