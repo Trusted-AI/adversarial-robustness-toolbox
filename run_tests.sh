09a6abe06d3f926582107ff6b088e096971b6230
Irina Nicolae <Maria-Irina.Nicolae@ibm.com>
2019-05-10 11:29:43 +0100

    Update script for running tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-python -m unittest discover art/ -p '*_unittest.py'
+python -m unittest discover art/attacks -p '*_unittest.py'
+python -m unittest art/classifiers/*_unittest.py
+python -m unittest discover art/defences -p '*_unittest.py'
+python -m unittest discover art/detection -p '*_unittest.py'
+python -m unittest discover art/poison_detection -p '*_unittest.py'
+python -m unittest discover art/wrappers -p '*_unittest.py'
+python -m unittest art/*_unittest.py
