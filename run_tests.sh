93cf7f521db0e391e8d3a52badb2b86f7c863f8e
Irina Nicolae <Maria-Irina.Nicolae@ibm.com>
2019-05-14 23:01:21 +0100

    Change tests script for python2

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 python -m unittest discover tests/attacks -p 'test_*.py'
-python -m unittest tests/classifiers/test_*.py
+python -m unittest tests.classifiers.test_*
 python -m unittest discover tests/defences -p 'test_*.py'
 python -m unittest discover tests/detection -p '*test_*.py'
 python -m unittest discover tests/poison_detection -p 'test_*.py'
