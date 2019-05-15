2b64e8cb6050ecb0f672bafb0bc0fc53d05670eb
Irina Nicolae <Maria-Irina.Nicolae@ibm.com>
2019-05-15 12:38:47 +0100

    Fix test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 python -m unittest discover tests/attacks -p 'test_*.py'
-python -m unittest tests.classifiers.test_*
+python -m unittest discover tests/classifiers -p 'test_*.py'
 python -m unittest discover tests/defences -p 'test_*.py'
-python -m unittest discover tests/detection -p '*test_*.py'
+python -m unittest discover tests/detection -p 'test_*.py'
 python -m unittest discover tests/poison_detection -p 'test_*.py'
 python -m unittest discover tests/wrappers -p 'test_*.py'
-python -m unittest tests/test_*.py
+python -m unittest tests.test_data_generators
+python -m unittest tests.test_metrics
+python -m unittest tests.test_utils
+python -m unittest tests.test_visualization
