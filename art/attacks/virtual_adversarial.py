1e216aa19458d2b7ef8787a3c9af730be88bcdda
Irina Nicolae <Maria-Irina.Nicolae@ibm.com>
2019-05-15 23:01:16 +0100

    Remove commented code to address PR review

--- art/attacks/virtual_adversarial.py
+++ art/attacks/virtual_adversarial.py
@@ -123,12 +123,9 @@ class VirtualAdversarialMethod(Attack):
         :rtype: `np.ndarray`
         """
         tol = 1e-10
-        # dims = x.shape
 
-        # x = x.reshape(dims[0], -1)
         inverse = (np.sum(x**2, axis=1) + tol) ** -.5
         x = x * inverse[:, None]
-        # x = np.reshape(x, dims)
 
         return x
 
