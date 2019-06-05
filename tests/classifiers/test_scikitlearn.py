0ea54a9a7b6e96b23df90999c8767333572f089f
Beat Buesser <beat.buesser@ie.ibm.com>
2019-06-05 23:26:52 +0100

    Implement unit tests for LogisticRegression

--- tests/classifiers/test_scikitlearn.py
+++ tests/classifiers/test_scikitlearn.py
@@ -3,37 +3,44 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import logging
 import unittest
 
-import tensorflow as tf
 import numpy as np
 
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
 
-from art.classifiers import SklearnDecisionTreeClassifier
-from art.utils import load_mnist
+from art.classifiers import ScikitlearnLogisticRegression
+from art.utils import load_dataset
 
 logger = logging.getLogger('testLogger')
 np.random.seed(seed=1234)
-tf.set_random_seed(1234)
 
 NB_TRAIN = 20
 
 
-class TestSklearnDecisionTreeClassifier(unittest.TestCase):
+class TestScikitlearnLogisticRegression(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        num_classes = 10
-        cls.num_features = 784
-        cls.num_samples = NB_TRAIN
-        cls.num_samples_loss = 20
+        (x_train, y_train), (x_test, y_test), _, _ = load_dataset('iris')
 
-        (x_train, y_train), (_, _), min_, max_ = load_mnist()
+        cls.x_train = x_train
+        cls.y_train = y_train
+        cls.x_test = x_test
+        cls.y_test = y_test
+
+        sklearn_model = LogisticRegression(verbose=0, C=1, solver='newton-cg', dual=False, fit_intercept=True)
+        cls.classifier = ScikitlearnLogisticRegression(model=sklearn_model)
+        cls.classifier.fit(x=cls.x_train, y=cls.y_train)
 
     def test_predict(self):
-        print('test_predict')
+        y_predicted = self.classifier.predict(self.x_test[0:1])
+        y_expected = [0.07809449, 0.36258262, 0.55932295]
+
+        for i in range(3):
+            self.assertAlmostEqual(y_predicted[0, i], y_expected[i], places=4)
 
     def test_class_gradient(self):
-        print('test_class_gradient')
+        grad_predicted = self.classifier.class_gradient(self.x_test[0:1], label=np.asarray([[1, 0, 0]]))
+        grad_expected = [-1.9793415, 1.3634679, -6.2971964, -2.613862]
 
-    def test_loss_gradient(self):
-        print('test_loss_gradient')
+        for i in range(4):
+            self.assertAlmostEqual(grad_predicted[0, i], grad_expected[i], 4)
