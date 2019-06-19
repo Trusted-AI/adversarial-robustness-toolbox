1db68dec0a0b7b5c166d798fc91f83c653fddccd
Beat Buesser <beat.buesser@ie.ibm.com>
2019-06-19 22:15:54 +0100

    Update run_test.sh
    Fixes #95

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
+exit_code=0
 python -m unittest discover tests/attacks -p 'test_*.py'
+if [[ $? -ne 0 ]]; then echo exit_code=1; fi
 python -m unittest discover tests/classifiers -p 'test_*.py'
+if [[ $? -ne 0 ]]; then echo exit_code=1; fi
 python -m unittest discover tests/defences -p 'test_*.py'
+if [[ $? -ne 0 ]]; then echo exit_code=1; fi
 python -m unittest discover tests/detection -p 'test_*.py'
+if [[ $? -ne 0 ]]; then echo exit_code=1; fi
 python -m unittest discover tests/poison_detection -p 'test_*.py'
+if [[ $? -ne 0 ]]; then echo exit_code=1; fi
 python -m unittest discover tests/wrappers -p 'test_*.py'
+if [[ $? -ne 0 ]]; then echo exit_code=1; fi
 python -m unittest tests.test_data_generators
+if [[ $? -ne 0 ]]; then echo exit_code=1; fi
 python -m unittest tests.test_metrics
+if [[ $? -ne 0 ]]; then echo exit_code=1; fi
 python -m unittest tests.test_utils
+if [[ $? -ne 0 ]]; then echo exit_code=1; fi
 python -m unittest tests.test_visualization
+if [[ $? -ne 0 ]]; then echo exit_code=1; fi
+exit $exit_code
