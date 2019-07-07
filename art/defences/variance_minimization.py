826899547e146231132923204c37639528330508
Beat Buesser <beat.buesser@ie.ibm.com>
2019-07-07 20:11:00 +0100

    Fix too short variable names
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/defences/variance_minimization.py
+++ art/defences/variance_minimization.py
@@ -113,22 +113,22 @@ class TotalVarMin(Preprocessor):
         :return: A new image.
         :rtype: `np.ndarray`
         """
-        z = x.copy()
+        z_min = x.copy()
 
         for i in range(x.shape[2]):
-            res = minimize(self._loss_func, z[:, :, i].flatten(), (x[:, :, i], mask[:, :, i], self.norm, self.lamb),
+            res = minimize(self._loss_func, z_min[:, :, i].flatten(), (x[:, :, i], mask[:, :, i], self.norm, self.lamb),
                            method=self.solver, jac=self._deri_loss_func, options={'maxiter': self.max_iter})
-            z[:, :, i] = np.reshape(res.x, z[:, :, i].shape)
+            z_min[:, :, i] = np.reshape(res.x, z_min[:, :, i].shape)
 
-        return z
+        return z_min
 
     @staticmethod
-    def _loss_func(z, x, mask, norm, lamb):
+    def _loss_func(z_init, x, mask, norm, lamb):
         """
         Loss function to be minimized.
 
-        :param z: Initial guess.
-        :type z: `np.ndarray`
+        :param z_init: Initial guess.
+        :type z_init: `np.ndarray`
         :param x: Original image.
         :type x: `np.ndarray`
         :param mask: A matrix that decides which points are kept.
@@ -140,20 +140,20 @@ class TotalVarMin(Preprocessor):
         :return: Loss value.
         :rtype: `float`
         """
-        res = np.sqrt(np.power(z - x.flatten(), 2).dot(mask.flatten()))
-        z = np.reshape(z, x.shape)
-        res += lamb * np.linalg.norm(z[1:, :] - z[:-1, :], norm, axis=1).sum()
-        res += lamb * np.linalg.norm(z[:, 1:] - z[:, :-1], norm, axis=0).sum()
+        res = np.sqrt(np.power(z_init - x.flatten(), 2).dot(mask.flatten()))
+        z_init = np.reshape(z_init, x.shape)
+        res += lamb * np.linalg.norm(z_init[1:, :] - z_init[:-1, :], norm, axis=1).sum()
+        res += lamb * np.linalg.norm(z_init[:, 1:] - z_init[:, :-1], norm, axis=0).sum()
 
         return res
 
     @staticmethod
-    def _deri_loss_func(z, x, mask, norm, lamb):
+    def _deri_loss_func(z_init, x, mask, norm, lamb):
         """
         Derivative of loss function to be minimized.
 
-        :param z: Initial guess.
-        :type z: `np.ndarray`
+        :param z_init: Initial guess.
+        :type z_init: `np.ndarray`
         :param x: Original image.
         :type x: `np.ndarray`
         :param mask: A matrix that decides which points are kept.
@@ -166,28 +166,28 @@ class TotalVarMin(Preprocessor):
         :rtype: `float`
         """
         # First compute the derivative of the first component of the loss function
-        nor1 = np.sqrt(np.power(z - x.flatten(), 2).dot(mask.flatten()))
+        nor1 = np.sqrt(np.power(z_init - x.flatten(), 2).dot(mask.flatten()))
         if nor1 < 1e-6:
             nor1 = 1e-6
-        der1 = ((z - x.flatten()) * mask.flatten()) / (nor1 * 1.0)
+        der1 = ((z_init - x.flatten()) * mask.flatten()) / (nor1 * 1.0)
 
         # Then compute the derivative of the second component of the loss function
-        z = np.reshape(z, x.shape)
+        z_init = np.reshape(z_init, x.shape)
 
         if norm == 1:
-            z_d1 = np.sign(z[1:, :] - z[:-1, :])
-            z_d2 = np.sign(z[:, 1:] - z[:, :-1])
+            z_d1 = np.sign(z_init[1:, :] - z_init[:-1, :])
+            z_d2 = np.sign(z_init[:, 1:] - z_init[:, :-1])
         else:
-            z_d1_norm = np.power(np.linalg.norm(z[1:, :] - z[:-1, :], norm, axis=1), norm - 1)
-            z_d2_norm = np.power(np.linalg.norm(z[:, 1:] - z[:, :-1], norm, axis=0), norm - 1)
+            z_d1_norm = np.power(np.linalg.norm(z_init[1:, :] - z_init[:-1, :], norm, axis=1), norm - 1)
+            z_d2_norm = np.power(np.linalg.norm(z_init[:, 1:] - z_init[:, :-1], norm, axis=0), norm - 1)
             z_d1_norm[z_d1_norm < 1e-6] = 1e-6
             z_d2_norm[z_d2_norm < 1e-6] = 1e-6
-            z_d1_norm = np.repeat(z_d1_norm[:, np.newaxis], z.shape[1], axis=1)
-            z_d2_norm = np.repeat(z_d2_norm[np.newaxis, :], z.shape[0], axis=0)
-            z_d1 = norm * np.power(z[1:, :] - z[:-1, :], norm - 1) / z_d1_norm
-            z_d2 = norm * np.power(z[:, 1:] - z[:, :-1], norm - 1) / z_d2_norm
+            z_d1_norm = np.repeat(z_d1_norm[:, np.newaxis], z_init.shape[1], axis=1)
+            z_d2_norm = np.repeat(z_d2_norm[np.newaxis, :], z_init.shape[0], axis=0)
+            z_d1 = norm * np.power(z_init[1:, :] - z_init[:-1, :], norm - 1) / z_d1_norm
+            z_d2 = norm * np.power(z_init[:, 1:] - z_init[:, :-1], norm - 1) / z_d2_norm
 
-        der2 = np.zeros(z.shape)
+        der2 = np.zeros(z_init.shape)
         der2[:-1, :] -= z_d1
         der2[1:, :] += z_d1
         der2[:, :-1] -= z_d2
