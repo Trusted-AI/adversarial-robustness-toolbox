90e7966f98ef1d16502a700ea78a32fe2b7ecd00
Beat Buesser <beat.buesser@ie.ibm.com>
2019-07-08 12:37:01 +0100

    Add module docstring
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/attacks/newtonfool.py
+++ art/attacks/newtonfool.py
@@ -15,6 +15,12 @@
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+"""
+This module implements the white-box attack `NewtonFool`.
+
+Paper link:
+    http://doi.acm.org/10.1145/3134600.3134635
+"""
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import logging
