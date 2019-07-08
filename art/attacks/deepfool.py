e2fe8e89281b333c0777d919f20e75c30c29e748
Beat Buesser <beat.buesser@ie.ibm.com>
2019-07-08 11:36:52 +0100

    Add module docstring
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/attacks/deepfool.py
+++ art/attacks/deepfool.py
@@ -15,6 +15,12 @@
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+"""
+This module implements the white-box attack `DeepFool`.
+
+Paper link:
+    https://arxiv.org/abs/1511.04599
+"""
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import logging
