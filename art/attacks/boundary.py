52acdf14f483f532d4f9e3e968b7e7b616b29e38
Beat Buesser <beat.buesser@ie.ibm.com>
2019-07-08 11:21:32 +0100

    Add module docstring
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/attacks/boundary.py
+++ art/attacks/boundary.py
@@ -15,6 +15,13 @@
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+"""
+This module implements the boundary attack `BoundaryAttack`. This is a black-box attack which only requires class
+predictions.
+
+Paper link:
+    https://arxiv.org/abs/1712.04248
+"""
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import logging
