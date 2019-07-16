716f7420eecad4e9bc50d458e5e7471e4e6b1286
Beat Buesser <beat.buesser@ie.ibm.com>
2019-07-16 13:24:30 +0100

    Comment unit tests for catbooos
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/classifiers/catboost.py
+++ art/classifiers/catboost.py
@@ -15,81 +15,81 @@
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-import logging
-import numpy as np
-
-from art.classifiers import Classifier
-
-logger = logging.getLogger(__name__)
-
-
-class CatBoostARTClassifier(Classifier):
-    """
-    Wrapper class for importing CatBoost models.
-    """
-
-    def __init__(self, model=None, defences=None, preprocessing=None, clip_values=None, num_features=None):
-        """
-        Create a `Classifier` instance from a CatBoost model.
-
-        :param clip_values: Tuple of the form `(min, max)` representing the minimum and maximum values allowed
-               for features.
-        :type clip_values: `tuple`
-        :param model: CatBoost model
-        :type model: `catboost.core.CatBoostClassifier`
-        :param defences: Defences to be activated with the classifier.
-        :type defences: :class:`.Preprocessor` or `list(Preprocessor)` instances
-        :param preprocessing: Tuple of the form `(subtractor, divider)` of floats or `np.ndarray` of values to be
-               used for data preprocessing. The first value will be subtracted from the input. The input will then
-               be divided by the second one.
-        :type preprocessing: `tuple`
-        """
-        from catboost.core import CatBoostClassifier
-
-        if not isinstance(model, CatBoostClassifier):
-            raise TypeError('Model must be of type catboost.core.CatBoostClassifier')
-
-        super(CatBoostARTClassifier, self).__init__(clip_values=clip_values, defences=defences,
-                                                    preprocessing=preprocessing)
-
-        self.model = model
-        self._input_shape = (num_features,)
-
-    def fit(self, x, y, **kwargs):
-        """
-        Fit the classifier on the training set `(x, y)`.
-
-        :param x: Training data.
-        :type x: `np.ndarray`
-        :param y: Labels, one-vs-rest encoding.
-        :type y: `np.ndarray`
-        :param kwargs: Dictionary of framework-specific arguments. These should be parameters supported by the
-               `fit` function in `catboost.core.CatBoostClassifier` and will be passed to this function as such.
-        :type kwargs: `dict`
-        :return: `None`
-        """
-        # Apply preprocessing
-        x_preprocessed, y_preprocessed = self._apply_preprocessing(x, y, fit=True)
-
-        self.model.fit(x_preprocessed, y_preprocessed, **kwargs)
-
-    def predict(self, x):
-        """
-        Perform prediction for a batch of inputs.
-
-        :param x: Test set.
-        :type x: `np.ndarray`
-        :return: Array of predictions of shape `(nb_inputs, self.nb_classes)`.
-        :rtype: `np.ndarray`
-        """
-        # Apply defences
-        x_preprocessed, _ = self._apply_preprocessing(x, y=None, fit=False)
-
-        return self.model.predict_proba(x_preprocessed)
-
-    def save(self, filename, path=None):
-        import pickle
-        with open(filename + '.pickle', 'wb') as f:
-            pickle.dump(self.model, file=f)
+# from __future__ import absolute_import, division, print_function, unicode_literals
+#
+# import logging
+# import numpy as np
+#
+# from art.classifiers import Classifier
+#
+# logger = logging.getLogger(__name__)
+#
+#
+# class CatBoostARTClassifier(Classifier):
+#     """
+#     Wrapper class for importing CatBoost models.
+#     """
+#
+#     def __init__(self, model=None, defences=None, preprocessing=None, clip_values=None, num_features=None):
+#         """
+#         Create a `Classifier` instance from a CatBoost model.
+#
+#         :param clip_values: Tuple of the form `(min, max)` representing the minimum and maximum values allowed
+#                for features.
+#         :type clip_values: `tuple`
+#         :param model: CatBoost model
+#         :type model: `catboost.core.CatBoostClassifier`
+#         :param defences: Defences to be activated with the classifier.
+#         :type defences: :class:`.Preprocessor` or `list(Preprocessor)` instances
+#         :param preprocessing: Tuple of the form `(subtractor, divider)` of floats or `np.ndarray` of values to be
+#                used for data preprocessing. The first value will be subtracted from the input. The input will then
+#                be divided by the second one.
+#         :type preprocessing: `tuple`
+#         """
+#         from catboost.core import CatBoostClassifier
+#
+#         if not isinstance(model, CatBoostClassifier):
+#             raise TypeError('Model must be of type catboost.core.CatBoostClassifier')
+#
+#         super(CatBoostARTClassifier, self).__init__(clip_values=clip_values, defences=defences,
+#                                                     preprocessing=preprocessing)
+#
+#         self.model = model
+#         self._input_shape = (num_features,)
+#
+#     def fit(self, x, y, **kwargs):
+#         """
+#         Fit the classifier on the training set `(x, y)`.
+#
+#         :param x: Training data.
+#         :type x: `np.ndarray`
+#         :param y: Labels, one-vs-rest encoding.
+#         :type y: `np.ndarray`
+#         :param kwargs: Dictionary of framework-specific arguments. These should be parameters supported by the
+#                `fit` function in `catboost.core.CatBoostClassifier` and will be passed to this function as such.
+#         :type kwargs: `dict`
+#         :return: `None`
+#         """
+#         # Apply preprocessing
+#         x_preprocessed, y_preprocessed = self._apply_preprocessing(x, y, fit=True)
+#
+#         self.model.fit(x_preprocessed, y_preprocessed, **kwargs)
+#
+#     def predict(self, x):
+#         """
+#         Perform prediction for a batch of inputs.
+#
+#         :param x: Test set.
+#         :type x: `np.ndarray`
+#         :return: Array of predictions of shape `(nb_inputs, self.nb_classes)`.
+#         :rtype: `np.ndarray`
+#         """
+#         # Apply defences
+#         x_preprocessed, _ = self._apply_preprocessing(x, y=None, fit=False)
+#
+#         return self.model.predict_proba(x_preprocessed)
+#
+#     def save(self, filename, path=None):
+#         import pickle
+#         with open(filename + '.pickle', 'wb') as f:
+#             pickle.dump(self.model, file=f)
