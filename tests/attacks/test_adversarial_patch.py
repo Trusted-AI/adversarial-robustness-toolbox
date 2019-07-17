13bc709b09c9024b3fd3fae109b9caebd38578ac
Beat Buesser <beat.buesser@ie.ibm.com>
2019-07-17 16:47:50 +0100

    Fix unit test
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- tests/attacks/test_adversarial_patch.py
+++ tests/attacks/test_adversarial_patch.py
@@ -26,7 +26,7 @@ import tensorflow as tf
 
 from art.attacks import AdversarialPatch
 from art.utils import load_mnist, master_seed
-from art.utils_test import get_classifier_tf, get_classifier_kr, get_classifier_pt
+from art.utils_test import get_classifier_tf, get_classifier_kr, get_classifier_pt, get_iris_classifier_kr
 
 logger = logging.getLogger('testLogger')
 
@@ -121,7 +121,8 @@ class TestAdversarialPatch(unittest.TestCase):
     def test_failure_feature_vectors(self):
         attack_params = {"rotation_max": 22.5, "scale_min": 0.1, "scale_max": 1.0,
                          "learning_rate": 5.0, "number_of_steps": 5, "batch_size": 10}
-        attack = AdversarialPatch(classifier=None)
+        classifier, _ = get_iris_classifier_kr()
+        attack = AdversarialPatch(classifier=classifier)
         attack.set_params(**attack_params)
         data = np.random.rand(10, 4)
 
