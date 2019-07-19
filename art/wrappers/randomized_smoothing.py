11b339fb6356290580816759af2c9caa846b86ca
robo-sq <31593127+robo-sq@users.noreply.github.com>
2019-07-19 12:44:37 +0100

    changing util functionality + print statement
    Signed-off-by: Ian Molloy <molloyim@us.ibm.com>

--- art/wrappers/randomized_smoothing.py
+++ art/wrappers/randomized_smoothing.py
@@ -94,10 +94,10 @@ class RandomizedSmoothing(ClassifierWrapper):
                 smooth_prediction[np.argmax(counts_pred)] = 1
             elif is_abstain:
                 n_abstained += 1
-                
+
             prediction.append(smooth_prediction)
         if n_abstained > 0:
-            logger.info('%s prediction(s) abstained.', n_abstained)
+            print('%s prediction(s) abstained.'% n_abstained)
         return np.array(prediction)
 
     def loss_gradient(self, x, y):
