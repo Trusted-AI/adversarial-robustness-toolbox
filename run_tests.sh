31cb06525cb9b3840d1763e06c261f0e05ad38e1
robo-sq <31593127+robo-sq@users.noreply.github.com>
2019-07-23 15:28:35 +0100

    memory errors

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 exit_code=0
-python -m unittest discover tests/attacks -p 'test_[a-i]*.py'
+python -m unittest discover tests/attacks -p 'test_[a-d]*.py'
 if [[ $? -ne 0 ]]; then exit_code=1; fi
-python -m unittest discover tests/attacks -p 'test_[j-z]*.py'
+python -m unittest discover tests/attacks -p 'test_[e-h]*.py'
 if [[ $? -ne 0 ]]; then exit_code=1; fi
-python -m unittest discover tests/classifiers -p 'test_*.py'
+python -m unittest discover tests/attacks -p 'test_[i-l]*.py'
+if [[ $? -ne 0 ]]; then exit_code=1; fi
+python -m unittest discover tests/attacks -p 'test_[m-p]*.py'
+if [[ $? -ne 0 ]]; then exit_code=1; fi
+python -m unittest discover tests/attacks -p 'test_[q-t]*.py'
+if [[ $? -ne 0 ]]; then exit_code=1; fi
+python -m unittest discover tests/attacks -p 'test_[u-z]*.py'
+if [[ $? -ne 0 ]]; then exit_code=1; fi
+python -m unittest discover tests/classifiers -p 'test_[a-e]*.py'
+if [[ $? -ne 0 ]]; then exit_code=1; fi
+python -m unittest discover tests/classifiers -p 'test_[f-l]*.py'
+if [[ $? -ne 0 ]]; then exit_code=1; fi
+python -m unittest discover tests/classifiers -p 'test_[m-q]*.py'
+if [[ $? -ne 0 ]]; then exit_code=1; fi
+python -m unittest discover tests/classifiers -p 'test_[r-z]*.py'
 if [[ $? -ne 0 ]]; then exit_code=1; fi
 python -m unittest discover tests/defences -p 'test_*.py'
 if [[ $? -ne 0 ]]; then exit_code=1; fi
