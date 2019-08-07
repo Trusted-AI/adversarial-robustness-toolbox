92258277555eade4de9229fab30ce41a1f06893a
Beat Buesser <beat.buesser@ie.ibm.com>
2019-08-07 14:24:06 +0100

    Change KerasClassifier to previous behavior
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/classifiers/keras.py
+++ art/classifiers/keras.py
@@ -119,8 +119,15 @@ class KerasClassifier(Classifier):
             else:
                 loss_function = getattr(k, self._model.loss.__name__)
 
-        preds = self._output
-        loss_ = loss_function(label_ph, self._output, from_logits=use_logits)
+        # The implementation of categorical_crossentropy is different in keras and tensorflow.keras. To ensure
+        # consistent behavior of `KerasClassifier` for keras and tensorflow.keras we follow the approach of
+        # tensorflow.keras for all cases of `from_logits` if the loss_function is categorical_crossentropy.
+        if loss_function.__name__ == 'categorical_crossentropy':
+            preds = self._output.op.inputs[-1]
+            loss_ = loss_function(label_ph, preds, from_logits=True)
+        else:
+            preds = self._output
+            loss_ = loss_function(label_ph, self._output, from_logits=use_logits)
 
         if preds == self._input:  # recent Tensorflow version does not allow a model with an output same as the input.
             preds = k.identity(preds)
