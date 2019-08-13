a93bdf8d9d7b22bc4239503003329d430ff136fb
Kathrin <Kathrin.Grosse@ibm.com>
2019-08-13 09:19:38 -0400

    removing super class for gpy
    Signed-off-by: Ebube Chuba <ebube.chuba@ibm.com>

--- art/classifiers/GPy.py
+++ art/classifiers/GPy.py
@@ -25,88 +25,7 @@ from art.classifiers import Classifier, ClassifierGradients
 logger = logging.getLogger(__name__)
 
 
-class GPyClassifier(Classifier):
-    """
-    Wrapper class for scikit-learn classifier models.
-    """
-
-    def __init__(self, model=None, channel_index=None, clip_values=None, defences=None, preprocessing=(0, 1)):
-        """
-        Create a `Classifier` instance from a GPy classifier model.
-
-        :param clip_values: Tuple of the form `(min, max)` representing the minimum and maximum values allowed
-               for features.
-        :type clip_values: `tuple`
-        :param model: scikit-learn classifier model.
-        :type model: `sklearn`
-        :param channel_index: Index of the axis in data containing the color channels or features. Not used in this
-               class.
-        :type channel_index: `int`
-        :param defences: Defences to be activated with the classifier.
-        :type defences: :class:`.Preprocessor` or `list(Preprocessor)` instances
-        :param preprocessing: Tuple of the form `(subtractor, divider)` of floats or `np.ndarray` of values to be
-               used for data preprocessing. The first value will be subtracted from the input. The input will then
-               be divided by the second one.
-        :type preprocessing: `tuple`
-        """
-        super(GPyClassifier, self).__init__(clip_values=clip_values,
-                                            defences=defences, preprocessing=preprocessing)
-
-        self.model = model
-        self._input_shape = None
-
-    def fit(self, x, y, batch_size=128, nb_epochs=20, **kwargs):
-        """
-        Fit the classifier on the training set `(x, y)`.
-
-        :param x: Training data. Not used, as goven to model in initialized earlier.
-        :type x: `np.ndarray`
-        :param y: Labels, one-vs-rest encoding. Not used, as goven to model in initialized earlier.
-        :type y: `np.ndarray`
-        :param batch_size: Size of batches. Not used in this class
-        :type batch_size: `int`
-        :param nb_epochs: Number of iterations to be optimized.
-        :type nb_epochs: `int`
-        :type kwargs: `dict`
-        :return: `None`
-        """
-        raise NotImplementedError
-
-    def predict(self, x, logits=False, batch_size=128):
-        """
-        Perform prediction for a batch of inputs.
-
-        :param x: Test set.
-        :type x: `np.ndarray`
-        :param logits: `True` if the prediction should be done at the logits layer.
-        :type logits: `bool`
-        :param batch_size: Size of batches. Not used in this function.
-        :type batch_size: `int`
-        :return: Array of predictions of shape `(nb_inputs, self.nb_classes)`.
-        :rtype: `np.ndarray`
-        """
-        raise NotImplementedError
-
-    def predict_uncertainty(self, x, logits=False, batch_size=128):
-        """
-        Perform uncertainty prediction for a batch of inputs.
-
-        :param x: Test set.
-        :type x: `np.ndarray`
-        :param logits: `True` if the prediction should be done at the logits layer.
-        :type logits: `bool`
-        :param batch_size: Size of batches. Not used in this function.
-        :type batch_size: `int`
-        :return: Array of uncertainty predictions of shape `(nb_inputs, self.nb_classes)`.
-        :rtype: `np.ndarray`
-        """
-        raise NotImplementedError
-
-    def save(self, filename, path=None):
-        self.model.save_model(filename, save_data=False)
-
-
-class GPyGaussianProcessClassifier(GPyClassifier, ClassifierGradients):
+class GPyGaussianProcessClassifier(Classifier, ClassifierGradients):
     """
     Wrapper class for scikit-learn Decision Tree Classifier models.
     """
@@ -135,10 +54,11 @@ class GPyGaussianProcessClassifier(GPyClassifier, ClassifierGradients):
         if not isinstance(model, GPC):
             raise TypeError(
                 'Model must be of type GPy.models.GPClassification')
-        self._nb_classes = 2  # always binary
-        super(GPyGaussianProcessClassifier, self).__init__(model=model, clip_values=clip_values,
-                                                           channel_index=channel_index, defences=defences,
+        super(GPyGaussianProcessClassifier, self).__init__(clip_values=clip_values,
+                                                           defences=defences,
                                                            preprocessing=preprocessing)
+        self._nb_classes = 2  # always binary
+        self.model = model
 
     def class_gradient(self, x, label=None, logits=False, eps=0.0001):
         """
@@ -235,20 +155,22 @@ class GPyGaussianProcessClassifier(GPyClassifier, ClassifierGradients):
         # geturn uncertainty, only in one dimension
         return self.model.predict_noiseless(x)[1]
 
-    @property
-    def layer_names(self):
+    def fit(self, x, y, batch_size=128, nb_epochs=20, **kwargs):
         """
-        Return the hidden layers in the model, if applicable.
+        Fit the classifier on the training set `(x, y)`.
 
-        :return: The hidden layers in the model, input and output layers excluded.
-        :rtype: `list`
-
-        .. warning:: `layer_names` tries to infer the internal structure of the model.
-                     This feature comes with no guarantees on the correctness of the result.
-                     The intended order of the layers tries to match their order in the model, but this is not
-                     guaranteed either.
+        :param x: Training data. Not used, as goven to model in initialized earlier.
+        :type x: `np.ndarray`
+        :param y: Labels, one-vs-rest encoding. Not used, as goven to model in initialized earlier.
+        :type y: `np.ndarray`
+        :param batch_size: Size of batches. Not used in this class
+        :type batch_size: `int`
+        :param nb_epochs: Number of iterations to be optimized.
+        :type nb_epochs: `int`
+        :type kwargs: `dict`
+        :return: `None`
         """
         raise NotImplementedError
 
-    def set_learning_phase(self, train):
-        raise NotImplementedError
+    def save(self, filename, path=None):
+        self.model.save_model(filename, save_data=False)
