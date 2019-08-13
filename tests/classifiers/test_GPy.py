de119ece58640d99407defb1f7084ebd8bc4d770
Kathrin <Kathrin.Grosse@ibm.com>
2019-08-13 14:23:07 -0400

    more cls/self confusion

--- tests/classifiers/test_GPy.py
+++ tests/classifiers/test_GPy.py
@@ -4,6 +4,7 @@ import logging
 import unittest
 
 import numpy as np
+import GPy
 
 from art.classifiers import GPyGaussianProcessClassifier
 from art.utils import load_dataset
@@ -31,25 +32,25 @@ class TestGPyGaussianProcessClassifier(unittest.TestCase):
         cls.classifier = GPyGaussianProcessClassifier(m)
 
     def test_predict(self):
-        (_, _), (x_test, y_test) = cls.iris
+        (_, _), (x_test, y_test) = self.iris
         # predictions should be correct
         self.assertTrue(
-            np.mean((cls.classifier.predict(x_test[:3])[:, 0] > 0.5) == y_test[:3]) > 0.6)
+            np.mean((self.classifier.predict(x_test[:3])[:, 0] > 0.5) == y_test[:3]) > 0.6)
         outlier = np.ones(np.shape(x_test[:3]))*10.0
         # output for random points should be 0.5 (as classifier is uncertain)
-        self.assertTrue(np.sum(cls.classifier.predict(
+        self.assertTrue(np.sum(self.classifier.predict(
             outlier).flatten() == 0.5) == 6.0)
 
     def test_predict_unc(self):
-        (_, _), (x_test, y_test) = cls.iris
+        (_, _), (x_test, y_test) = self.iris
         outlier = np.ones(np.shape(x_test[:3]))*(np.max(x_test.flatten())*10.0)
         # uncertainty should increase as we go deeper into data
-        self.assertTrue(np.mean(cls.classifier.predict_uncertainty(
-            outlier) > cls.classifier.predict_uncertainty(x_test[:3])) == 1.0)
+        self.assertTrue(np.mean(self.classifier.predict_uncertainty(
+            outlier) > self.classifier.predict_uncertainty(x_test[:3])) == 1.0)
 
     def test_loss_gradient(self):
-        (_, _), (x_test, y_test) = cls.iris
-        grads = cls.classifier.loss_gradient(x_test[0:1], y_test[0:1]))
+        (_, _), (x_test, y_test) = self.iris
+        grads = self.classifier.loss_gradient(x_test[0:1], y_test[0:1]))
         # gradfs wiuth given seed should be [[-2.25244234e-11 -5.63282695e-11  1.74214328e-11 -1.21877914e-11]]
         # we test roughly: amount of positive/negative and largest gradient
         self.assertTrue(np.sum(grads < 0.0) == 3.0)
@@ -57,8 +58,8 @@ class TestGPyGaussianProcessClassifier(unittest.TestCase):
         self.assertTrue(np.argmax(grads) == 2)
 
     def test_class_gradient(self):
-        (_, _), (x_test, y_test)=cls.iris
-        grads=cls.classifier.class_gradient(x_test[0:1], int(y_test[0:1])))
+        (_, _), (x_test, y_test)=self.iris
+        grads=self.classifier.class_gradient(x_test[0:1], int(y_test[0:1])))
         # gradfs wiuth given seed should be [[[2.25244234e-11  5.63282695e-11 -1.74214328e-11  1.21877914e-11]]]
         # we test roughly: amount of positive/negative and largest gradient
         self.assertTrue(np.sum(grads < 0.0) == 1.0)
