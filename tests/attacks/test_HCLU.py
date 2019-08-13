304c3d243cbf4015aaa4797a88b451d11d682def
Kathrin <Kathrin.Grosse@ibm.com>
2019-08-13 14:19:48 -0400

    more feedback from beat :-)
    Signed-off-by: Ebube Chuba <ebube.chuba@ibm.com>

--- tests/attacks/test_HCLU.py
+++ tests/attacks/test_HCLU.py
@@ -24,7 +24,7 @@ import numpy as np
 
 from art.attacks.HCLU import HCLU
 from art.classifiers.GPy import GPyGaussianProcessClassifier
-from art.utils import load_dataset, master_seed,
+from art.utils import load_dataset, master_seed
 
 
 logger = logging.getLogger('testLogger')
@@ -35,40 +35,40 @@ class TestHCLU(unittest.TestCase):
     def setUpClass(cls):
         # Get Iris
         (x_train, y_train), (x_test, y_test), _, _ = load_dataset('iris')
-        #change iris to binary problem, so it is learnable for GPC
+        # change iris to binary problem, so it is learnable for GPC
         cls.iris = (x_train, y_train[:, 1]), (x_test, y_test[:, 1])
 
     def setUp(self):
         master_seed(1234)
 
     def test_GPy(self):
-        (X, y), (x_test, y_test) = cls.iris
-        #set up GPclassifier
+        (X, y), (x_test, y_test) = self.iris
+        # set up GPclassifier
         gpkern = GPy.kern.RBF(np.shape(X)[1])
         m = GPy.models.GPClassification(X, y.reshape(-1, 1), kernel=gpkern)
         m.inference_method = GPy.inference.latent_function_inference.laplace.Laplace()
         m.optimize(messages=True, optimizer='lbfgs')
-        #get ART classifier + clean accuracy
+        # get ART classifier + clean accuracy
         m_art = GPyGaussianProcessClassifier(m)
         clean_acc = np.mean(np.argmin(m_art.predict(x_test), axis=1) == y_test)
-        #get adversarial examples, accuracy, and uncertainty
+        # get adversarial examples, accuracy, and uncertainty
         attack = HCLU(m_art, conf=0.9, min_val=-0.0, max_val=1.0)
         adv = attack.generate(x_test)
         adv_acc = np.mean(np.argmin(m_art.predict(adv), axis=1) == y_test)
         unc_f = m_art.predict_uncertainty(adv)
-        #not all attacks suceed due to the decision surface landscape of GP, some should
+        # not all attacks suceed due to the decision surface landscape of GP, some should
         self.assertTrue(clean_acc > adv_acc)
 
-        #now take into account uncertainty
+        # now take into account uncertainty
         attack = HCLU(m_art, unc_increase=0.9,
                       conf=0.9, min_val=0.0, max_val=1.0)
         adv = attack.generate(x_test)
         adv_acc = np.mean(np.argmin(m_art.predict(adv), axis=1) == y_test)
         unc_o = m_art.predict_uncertainty(adv)
-        #same above
+        # same above
         self.assertTrue(clean_acc > adv_acc)
-        #uncertainty should indeed be lower when used as a constraint
-        #however, same as above, crafting might fail
+        # uncertainty should indeed be lower when used as a constraint
+        # however, same as above, crafting might fail
         self.assertTrue(np.mean(unc_f > unc_o) > 0.7)
 
 
