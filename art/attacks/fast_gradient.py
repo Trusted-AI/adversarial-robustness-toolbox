92d7b4442632fdfb4fd3256a5eb66223af38e6b6
Beat Buesser <beat.buesser@ie.ibm.com>
2019-08-15 15:18:27 +0100

    Update kwarg format in fast gradient attack
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>
    Signed-off-by: Ebube Chuba <ebube.chuba@ibm.com>

--- art/attacks/fast_gradient.py
+++ art/attacks/fast_gradient.py
@@ -145,7 +145,7 @@ class FastGradientMethod(Attack):
 
             # Use model predictions as correct outputs
             logger.info('Using model predictions as correct labels for FGM.')
-            y = get_labels_np_array(self.classifier.predict(x, **{'batch_size': self.batch_size}))
+            y = get_labels_np_array(self.classifier.predict(x, batch_size=self.batch_size))
         y = y / np.sum(y, axis=1, keepdims=True)
 
         # Return adversarial examples computed with minimal perturbation if option is active
