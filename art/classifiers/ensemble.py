aa854c1c1f503ddf38c93fe381b8a8d58e189a3b
Beat Buesser <beat.buesser@ie.ibm.com>
2019-08-18 00:19:43 +0100

    Update typos
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>
    Signed-off-by: Ebube Chuba <ebube.chuba@ibm.com>

--- art/classifiers/ensemble.py
+++ art/classifiers/ensemble.py
@@ -55,8 +55,8 @@ class EnsembleClassifier(ClassifierNeuralNetwork, ClassifierGradients, Classifie
         :type clip_values: `tuple`
         :param defences: Defences to be activated with the classifier.
         :type defences: `str` or `list(str)`
-        :param preprocessing: Tuple of the form `(substractor, divider)` of floats or `np.ndarray` of values to be
-               used for data preprocessing. The first value will be substracted from the input. The input will then
+        :param preprocessing: Tuple of the form `(subtractor, divider)` of floats or `np.ndarray` of values to be
+               used for data preprocessing. The first value will be subtracted from the input. The input will then
                be divided by the second one.
         :type preprocessing: `tuple`
         """
