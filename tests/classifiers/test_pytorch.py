20d021a121fd12ec577950225fe0c8146ac26672
Beat Buesser <beat.buesser@ie.ibm.com>
2019-08-22 15:49:58 +0100

    Update unittests
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- tests/classifiers/test_pytorch.py
+++ tests/classifiers/test_pytorch.py
@@ -134,7 +134,7 @@ class TestPyTorchClassifier(unittest.TestCase):
         logger.info('Accuracy: %.2f%%', (accuracy_2 * 100))
 
         self.assertEqual(accuracy, 0.3)
-        self.assertEqual(accuracy_2, 0.55)
+        self.assertAlmostEqual(accuracy_2, 0.55, delta=0.05)
 
     def test_nb_classes(self):
         classifier = get_classifier_pt()
