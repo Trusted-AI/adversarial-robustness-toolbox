a29f7dd606e2e9edb7b8abe5558bd3b76cc8943a
Beat Buesser <beat.buesser@ie.ibm.com>
2019-08-22 19:56:05 +0100

    Update Tensorflow v2 classifier with new classifier API
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/classifiers/tensorflow.py
+++ art/classifiers/tensorflow.py
@@ -601,7 +601,7 @@ class TFClassifier(ClassifierNeuralNetwork, ClassifierGradients, Classifier):
         return repr_
 
 
-class TensorflowV2Classifier(Classifier):
+class TensorflowV2Classifier(ClassifierNeuralNetwork, ClassifierGradients, Classifier):
     """
     This class implements a classifier with the Tensorflow v2 framework.
     """
