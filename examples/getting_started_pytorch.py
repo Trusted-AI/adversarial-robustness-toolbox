c8212e1b386ae6f69e6812cc0d2cec09493f9b26
Beat Buesser <beat.buesser@ie.ibm.com>
2019-09-02 19:58:04 +0100

    Update PyTorch example
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>
    Signed-off-by: Ebube Chuba <ebube.chuba@ibm.com>

--- examples/getting_started_pytorch.py
+++ examples/getting_started_pytorch.py
@@ -1,61 +1,81 @@
+"""
+The script demonstrates a simple example of using ART with PyTorch. The example train a small model on the MNISt dataset
+and creates adversarial examples using the Fast Gradient Sign Method. Here we use the ART classifier to train the model,
+it would also be possible to provide a pretrained model to the ART classifier.
+The parameters are chosen for reduced computational requirements of the script and not optimised for accuracy.
+"""
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 
-from art.attacks.fast_gradient import FastGradientMethod
-from art.classifiers.pytorch import PyTorchClassifier
+from art.attacks import FastGradientMethod
+from art.classifiers import PyTorchClassifier
 from art.utils import load_mnist
 
-#Create the neural network architecture, return logits instead of activation in forward method (Eg. softmax).
+
+# Step 0: Create the neural network model, return logits instead of activation in forward method
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 20, 5, 1)
-        self.conv2 = nn.Conv2d(20, 50, 5, 1)
-        self.fc1 = nn.Linear(4*4*50, 500)
-        self.fc2 = nn.Linear(500, 10)
+        self.conv_1 = nn.Conv2d(in_channels=1, out_channels=4, kernel_size=5, stride=1)
+        self.conv_2 = nn.Conv2d(in_channels=4, out_channels=10, kernel_size=5, stride=1)
+        self.fc_1 = nn.Linear(in_features=4 * 4 * 10, out_features=100)
+        self.fc_2 = nn.Linear(in_features=100, out_features=10)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv_1(x))
         x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv_2(x))
         x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4*4*50)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = x.view(-1, 4 * 4 * 10)
+        x = F.relu(self.fc_1(x))
+        x = self.fc_2(x)
         return x
 
-# Load the MNIST dataset
-(x_train, y_train), (x_test, y_test), min_, max_ = load_mnist()
+
+# Step 1: Load the MNIST dataset
+
+(x_train, y_train), (x_test, y_test), min_pixel_value, max_pixel_value = load_mnist()
+
+# Step 1a: Swap axes to PyTorch's NCHW format
+
 x_train = np.swapaxes(x_train, 1, 3)
 x_test = np.swapaxes(x_test, 1, 3)
 
-# Obtain the model object
+# Step 2: Create the model
+
 model = Net()
 
-# Define the loss function and the optimizer
+# Step 2a: Define the loss function and the optimizer
+
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
 
-# Initialize the classifier
-mnist_classifier = PyTorchClassifier(clip_values=(0, 1), model=model, loss=criterion, optimizer=optimizer, 
-                                     input_shape=(1, 28, 28), nb_classes=10)
+# Step 3: Create the ART classifier
 
-# Train the classifier
-mnist_classifier.fit(x_train, y_train, batch_size=64, nb_epochs=10)
+classifier = PyTorchClassifier(clip_values=(0, 1), model=model, loss=criterion, optimizer=optimizer,
+                               input_shape=(1, 28, 28), nb_classes=10)
 
-# Test the classifier
-predictions = mnist_classifier.predict(x_test)
+# Step 4: Train the ART classifier
+
+classifier.fit(x_train, y_train, batch_size=64, nb_epochs=10)
+
+# Step 5: Evaluate the ART classifier on benign test examples
+
+predictions = classifier.predict(x_test)
 accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(y_test)
-print('Accuracy before attack: {}%'.format(accuracy * 100))
+print('Accuracy on benign test examples: {}%'.format(accuracy * 100))
 
-# Craft the adversarial examples
+# Step 6: Generate adversarial test examples
+
 epsilon = 0.2  # Maximum perturbation
-adv_crafter = FastGradientMethod(mnist_classifier, eps=epsilon)
-x_test_adv = adv_crafter.generate(x=x_test)
+attack = FastGradientMethod(classifier=classifier, eps=epsilon)
+x_test_adv = attack.generate(x=x_test)
 
-# Test the classifier on adversarial exmaples
-predictions = mnist_classifier.predict(x_test_adv)
+# Step 7: Evaluate the ART classifier on adversarial test examples
+
+predictions = classifier.predict(x_test_adv)
 accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(y_test)
-print('Accuracy after attack: {}%'.format(accuracy * 100))
+print('Accuracy on adversarial test examples: {}%'.format(accuracy * 100))
