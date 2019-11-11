bf0396060262985d629f7059a9d014f043e1d385
Beat Buesser <beat.buesser@ie.ibm.com>
2019-11-11 00:19:20 +0000

    Update unittests for updated utils test classifiers
    - change PyTorch to makes the same predictions as Keras and TensorFlow
    - use logits output in attacks expecting logits
    
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- tests/classifiers/test_pytorch.py
+++ tests/classifiers/test_pytorch.py
@@ -134,7 +134,7 @@ class TestPyTorchClassifier(unittest.TestCase):
         logger.info('Accuracy: %.2f%%', (accuracy_2 * 100))
 
         self.assertEqual(accuracy, 0.4)
-        self.assertAlmostEqual(accuracy_2, 0.8, delta=0.1)
+        self.assertAlmostEqual(accuracy_2, 0.75, delta=0.1)
 
     def test_nb_classes(self):
         classifier = get_classifier_pt()
