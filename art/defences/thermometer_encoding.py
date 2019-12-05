68a8209ed77a803557e461dd3bb57bb1fcfbe8a1
KaGrosse <kathrin.grosse@cispa.saarland>
2019-12-05 12:06:10 +0100

    Update thermometer_encoding.py
    add source

--- art/defences/thermometer_encoding.py
+++ art/defences/thermometer_encoding.py
@@ -20,7 +20,7 @@ This module implements the thermometer encoding defence `ThermometerEncoding`.
 
 | Paper link: https://openreview.net/forum?id=S18Su--CW
 
-| Please keep in mind the limitations of defences. For more information on the limitations of this defence, see https://arxiv.org/abs/1802.00420
+| Please keep in mind the limitations of defences. For more information on the limitations of this defence, see https://arxiv.org/abs/1802.00420 . For details on how to evaluate classifier security in general, see https://arxiv.org/abs/1902.06705
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 
