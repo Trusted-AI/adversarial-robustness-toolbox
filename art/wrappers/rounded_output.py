87a93052213eb5ddd12c5bd1b23377643a9cf1c5
Beat Buesser <beat.buesser@ie.ibm.com>
2019-12-06 16:22:43 +0000

    Implement classifier wrapper for rounded output
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

---
+++ art/wrappers/rounded_output.py
@@ -0,0 +1,188 @@
+# MIT License
+#
+# Copyright (C) IBM Corporation 2019
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""
+This module implements the defence against model extraction attacks by returning only rounded probabilities.
+
+| Paper link:
+"""
+import logging
+
+import numpy as np
+
+from art.wrappers.wrapper import ClassifierWrapper
+from art.classifiers.classifier import Classifier
+
+logger = logging.getLogger(__name__)
+
+
+class RoundedOutput(ClassifierWrapper, Classifier):
+    """
+    Implementation
+
+    | Paper link:
+    """
+
+    def __init__(self, classifier, decimals=3):
+        """
+        Create a wrapper for rounded output.
+
+        :param classifier: The Classifier we want to wrap the functionality for the purpose of smoothing.
+        :type classifier: :class:`.Classifier`
+        :param decimals: Number of decimal places after the decimal point.
+        :type decimals: `int`
+        """
+        super(RoundedOutput, self).__init__(classifier)
+        self.decimals = decimals
+
+    # pylint: disable=W0221
+    def predict(self, x, batch_size=128, **kwargs):
+        """
+        Prediction the wrapped classifier and round output to provided number of decimal places.
+
+        :param x: Input data
+        :type x: `np.ndarray`
+        :param batch_size: Batch size
+        :type batch_size: `int`
+        :return: Rounded predictions of shape `(nb_inputs, nb_classes)`.
+        :rtype: `np.ndarray`
+        """
+        return np.around(self.classifier.predict(x, batch_size=batch_size), decimals=self.decimals)
+
+    def fit(self, x, y, batch_size=128, nb_epochs=20, **kwargs):
+        """
+        Fit the classifier using the training data `(x, y)`.
+
+        :param x: Features in array of shape (nb_samples, nb_features) or (nb_samples, nb_pixels_1, nb_pixels_2,
+                  nb_channels) or (nb_samples, nb_channels, nb_pixels_1, nb_pixels_2)
+        :type x: `np.ndarray`
+        :param y: Target values (class labels in classification) in array of shape (nb_samples, nb_classes) in
+                  One Hot Encoding format.
+        :type y: `np.ndarray`
+        :param kwargs: Dictionary of framework-specific arguments.
+        :type kwargs: `dict`
+        :return: `None`
+        """
+        raise NotImplementedError
+
+    def loss_gradient(self, x, y, **kwargs):
+        """
+        Compute the gradient of the given classifier's loss function w.r.t. `x`, taking an expectation
+        over transformations.
+
+        :param x: Sample input with shape as expected by the model.
+        :type x: `np.ndarray`
+        :param y: Correct labels, one-hot encoded.
+        :type y: `np.ndarray`
+        :return: Array of gradients of the same shape as `x`.
+        :rtype: `np.ndarray`
+        """
+        raise NotImplementedError
+
+    def class_gradient(self, x, label=None, **kwargs):
+        """
+        Compute per-class derivatives of the given classifier w.r.t. `x`, taking an expectation over transformations.
+
+        :param x: Sample input with shape as expected by the model.
+        :type x: `np.ndarray`
+        :param label: Index of a specific per-class derivative. If an integer is provided, the gradient of that class
+                      output is computed for all samples. If multiple values as provided, the first dimension should
+                      match the batch size of `x`, and each value will be used as target for its corresponding sample in
+                      `x`. If `None`, then gradients for all classes will be computed for each sample.
+        :type label: `int` or `list`
+        :return: Array of gradients of input features w.r.t. each class in the form
+                 `(batch_size, nb_classes, input_shape)` when computing for all classes, otherwise shape becomes
+                 `(batch_size, 1, input_shape)` when `label` parameter is specified.
+        :rtype: `np.ndarray`
+        """
+        raise NotImplementedError
+
+    @property
+    def layer_names(self):
+        """
+        Return the hidden layers in the model, if applicable.
+
+        :return: The hidden layers in the model, input and output layers excluded.
+        :rtype: `list`
+
+        .. warning:: `layer_names` tries to infer the internal structure of the model.
+                     This feature comes with no guarantees on the correctness of the result.
+                     The intended order of the layers tries to match their order in the model, but this is not
+                     guaranteed either.
+        """
+        raise NotImplementedError
+
+    def get_activations(self, x, layer, batch_size):
+        """
+        Return the output of the specified layer for input `x`. `layer` is specified by layer index (between 0 and
+        `nb_layers - 1`) or by name. The number of layers can be determined by counting the results returned by
+        calling `layer_names`.
+
+        :param x: Input for computing the activations.
+        :type x: `np.ndarray`
+        :param layer: Layer for computing the activations
+        :type layer: `int` or `str`
+        :param batch_size: Size of batches.
+        :type batch_size: `int`
+        :return: The output of `layer`, where the first dimension is the batch size corresponding to `x`.
+        :rtype: `np.ndarray`
+        """
+        raise NotImplementedError
+
+    def set_learning_phase(self, train):
+        """
+        Set the learning phase for the backend framework.
+
+        :param train: `True` if the learning phase is training, `False` if learning phase is not training.
+        :type train: `bool`
+        """
+        raise NotImplementedError
+
+    def nb_classes(self):
+        """
+        Return the number of output classes.
+
+        :return: Number of classes in the data.
+        :rtype: `int`
+        """
+        return self._nb_classes
+
+    def save(self, filename, path=None):
+        """
+        Save a model to file specific to the backend framework.
+
+        :param filename: Name of the file where to save the model.
+        :type filename: `str`
+        :param path: Path of the directory where to save the model. If no path is specified, the model will be stored in
+                     the default data location of ART at `DATA_PATH`.
+        :type path: `str`
+        :return: None
+        """
+        raise NotImplementedError
+
+
+if __name__ == '__main__':
+
+    from art.utils import load_dataset
+    from art.utils_test import get_classifier_kr
+
+    (x_train, y_train), (x_test, y_test), _, _ = load_dataset('mnist')
+    classifier = get_classifier_kr()
+
+    rp = RoundedProbabilities(classifier=classifier, decimals=3)
+    print(rp.predict(x_test[0:1]))
+    print(classifier.predict(x_test[0:1]))
