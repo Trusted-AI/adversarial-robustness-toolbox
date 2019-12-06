ca72328d23a70d6e5f0df13468b307bcfe2c379e
KaGrosse <kathrin.grosse@cispa.saarland>
2019-12-06 11:29:36 +0100

    Update hclu.py
    reformat code

--- art/attacks/hclu.py
+++ art/attacks/hclu.py
@@ -35,93 +35,98 @@ logger = logging.getLogger(__name__)
 
 
 class HighConfidenceLowUncertainty(Attack):
-    """
-    Implementation of the High-Confidence-Low-Uncertainty (HCLU) adversarial example formulation by Grosse et al. (2018)
-
-    | Paper link: https://arxiv.org/abs/1812.02606
-    """
-    attack_params = ['conf', 'unc_increase', 'min_val', 'max_val']
-
-    def __init__(self, classifier, conf=0.95, unc_increase=100.0, min_val=0.0, max_val=1.0):
         """
-        :param classifier: A trained model of type GPYGaussianProcessClassifier.
-        :type classifier: :class:`.Classifier.GPyGaussianProcessClassifier
-        :param conf: Confidence that examples should have, if there were to be classified as 1.0 maximally
-        :type conf: :float:
-        :param unc_increase: Value uncertainty is allowed to deviate, where 1.0 is original value
-        :type unc_increase: :float:
-        :param min_val: minimal value any feature can take, defaults to 0.0
-        :type min_val: :float:
-        :param max_val: maximal value any feature can take, defaults to 1.0
-        :type max_val: :float:
+        Implementation of the High-Confidence-Low-Uncertainty (HCLU) adversarial example formulation by Grosse et al. (2018)
+
+        | Paper link: https://arxiv.org/abs/1812.02606
         """
-        super(HighConfidenceLowUncertainty, self).__init__(classifier=classifier)
-        if not isinstance(classifier, GPyGaussianProcessClassifier):
-            raise TypeError('Model must be a GPy Gaussian Process classifier!')
-        params = {'conf': conf,
-                  'unc_increase': unc_increase,
-                  'min_val': min_val,
-                  'max_val': max_val
-                  }
-        self.set_params(**params)
+        attack_params = ['conf', 'unc_increase', 'min_val', 'max_val']
 
-    def generate(self, x, y=None, **kwargs):
-        """
-        Generate adversarial examples and return them as an array. This method should be overridden by all concrete
-        attack implementations.
+        def __init__(self, classifier, conf=0.95, unc_increase=100.0, min_val=0.0, max_val=1.0):
+                """
+                :param classifier: A trained model of type GPYGaussianProcessClassifier.
+                :type classifier: :class:`.Classifier.GPyGaussianProcessClassifier
+                :param conf: Confidence that examples should have, if there were to be classified as 1.0 maximally
+                :type conf: :float:
+                :param unc_increase: Value uncertainty is allowed to deviate, where 1.0 is original value
+                :type unc_increase: :float:
+                :param min_val: minimal value any feature can take, defaults to 0.0
+                :type min_val: :float:
+                :param max_val: maximal value any feature can take, defaults to 1.0
+                :type max_val: :float:
+                """
+                super(HighConfidenceLowUncertainty, self).__init__(classifier=classifier)
+                if not isinstance(classifier, GPyGaussianProcessClassifier):
+                        raise TypeError('Model must be a GPy Gaussian Process classifier!')
+                params = {'conf': conf,
+                          'unc_increase': unc_increase,
+                          'min_val': min_val,
+                          'max_val': max_val
+                          }
+                self.set_params(**params)
 
-        :param x: An array with the original inputs to be attacked.
-        :type x: `np.ndarray`
-        :param y: Target values (class labels) one-hot-encoded of shape (nb_samples, nb_classes) or indices of shape
-                  (nb_samples,).
-        :type y: `np.ndarray`
-        :return: An array holding the adversarial examples.
-        :rtype: `np.ndarray`
-        """
-        x_adv = copy.copy(x)
+        def generate(self, x, y=None, **kwargs):
+                """
+                Generate adversarial examples and return them as an array. This method should be overridden by all concrete
+                attack implementations.
 
-        def minfun(x, args):  # minimize L2 norm
-            return np.sum(np.sqrt((x - args['orig']) ** 2))
+                :param x: An array with the original inputs to be attacked.
+                :type x: `np.ndarray`
+                :param y: Target values (class labels) one-hot-encoded of shape (nb_samples, nb_classes) or indices of shape
+                          (nb_samples,).
+                :type y: `np.ndarray`
+                :return: An array holding the adversarial examples.
+                :rtype: `np.ndarray`
+                """
+                x_adv = copy.copy(x)
 
-        def constraint_conf(x, args):  # constraint for confidence
-            pred = args['classifier'].predict(x.reshape(1, -1))[0, 0]
-            if args['class_zero']:
-                pred = 1.0 - pred
-            return (pred - args['conf']).reshape(-1)
+                def minfun(x, args):  # minimize L2 norm
+                        return np.sum(np.sqrt((x - args['orig']) ** 2))
 
-        def constraint_unc(x, args):  # constraint for uncertainty
-            return (args['max_uncertainty'] - (args['classifier'].predict_uncertainty(x.reshape(1, -1))).reshape(-1))[0]
+                def constraint_conf(x, args):  # constraint for confidence
+                        pred = args['classifier'].predict(x.reshape(1, -1))[0, 0]
+                        if args['class_zero']:
+                                pred = 1.0 - pred
+                        return (pred - args['conf']).reshape(-1)
 
-        bounds = []
-        # adding bounds, to not go away from original data
-        for i in range(np.shape(x)[1]):
-            bounds.append((self.min_val, self.max_val))
-        for i in range(np.shape(x)[0]):  # go though data amd craft
-            # get properties for attack
-            max_uncertainty = self.unc_increase * self.classifier.predict_uncertainty(x_adv[i].reshape(1, -1))
-            class_zero = not self.classifier.predict(x_adv[i].reshape(1, -1))[0, 0] < 0.5
-            init_args = {'classifier': self.classifier, 'class_zero': class_zero, 'max_uncertainty': max_uncertainty,'conf':self.conf}
-            constr_conf = {'type': 'ineq', 'fun': constraint_conf, 'args': (init_args,)}
-            constr_unc = {'type': 'ineq', 'fun': constraint_unc, 'args': (init_args,)}
-            args = {'args': init_args, 'orig': x[i].reshape(-1)}
-            # #finally, run optimization
-            x_adv[i] = minimize(minfun, x_adv[i], args=args, bounds=bounds, constraints=[constr_conf, constr_unc])['x']
-        return x_adv
+                def constraint_unc(x, args):  # constraint for uncertainty
+                        return (
+                        args['max_uncertainty'] - (args['classifier'].predict_uncertainty(x.reshape(1, -1))).reshape(
+                                -1))[0]
 
-    def set_params(self, **kwargs):
-        """
-        Take in a dictionary of parameters and apply attack-specific checks before saving them as attributes.
+                bounds = []
+                # adding bounds, to not go away from original data
+                for i in range(np.shape(x)[1]):
+                        bounds.append((self.min_val, self.max_val))
+                for i in range(np.shape(x)[0]):  # go though data amd craft
+                        # get properties for attack
+                        max_uncertainty = self.unc_increase * self.classifier.predict_uncertainty(
+                                x_adv[i].reshape(1, -1))
+                        class_zero = not self.classifier.predict(x_adv[i].reshape(1, -1))[0, 0] < 0.5
+                        init_args = {'classifier': self.classifier, 'class_zero': class_zero,
+                                     'max_uncertainty': max_uncertainty, 'conf': self.conf}
+                        constr_conf = {'type': 'ineq', 'fun': constraint_conf, 'args': (init_args,)}
+                        constr_unc = {'type': 'ineq', 'fun': constraint_unc, 'args': (init_args,)}
+                        args = {'args': init_args, 'orig': x[i].reshape(-1)}
+                        # #finally, run optimization
+                        x_adv[i] = \
+                        minimize(minfun, x_adv[i], args=args, bounds=bounds, constraints=[constr_conf, constr_unc])['x']
+                return x_adv
 
-        :param kwargs: a dictionary of attack-specific parameters
-        :type kwargs: `dict`
-        :return: `True` when parsing was successful
-        """
-        super(HighConfidenceLowUncertainty, self).set_params(**kwargs)
-        if self.conf <= 0.5 or self.conf > 1.0:
-            raise ValueError(
-                "Confidence value has to be a value between 0.5 and 1.0.")
-        if self.unc_increase <= 0.0:
-            raise ValueError(
-                "Value by which we allow to increase uncertainty has to be a positive number.")
-        if self.min_val > self.max_val:
-            raise ValueError("Maximum has to be larger than minimum.")
+        def set_params(self, **kwargs):
+                """
+                Take in a dictionary of parameters and apply attack-specific checks before saving them as attributes.
+
+                :param kwargs: a dictionary of attack-specific parameters
+                :type kwargs: `dict`
+                :return: `True` when parsing was successful
+                """
+                super(HighConfidenceLowUncertainty, self).set_params(**kwargs)
+                if self.conf <= 0.5 or self.conf > 1.0:
+                        raise ValueError(
+                                "Confidence value has to be a value between 0.5 and 1.0.")
+                if self.unc_increase <= 0.0:
+                        raise ValueError(
+                                "Value by which we allow to increase uncertainty has to be a positive number.")
+                if self.min_val > self.max_val:
+                        raise ValueError("Maximum has to be larger than minimum.")
