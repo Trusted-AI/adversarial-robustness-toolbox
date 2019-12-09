c0325f94b92afc95bd77c67f63213a591c742c02
Sean Morgan <seanmorgan91@gmail.com>
2019-12-09 09:16:51 -0500

    Fix LGTM check
    Signed-off-by: Sean Morgan <seanmorgan91@gmail.com>

--- setup.py
+++ setup.py
@@ -43,8 +43,8 @@ def get_version(rel_path):
         if line.startswith('__version__'):
             delim = '"' if '"' in line else "'"
             return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(name='Adversarial Robustness Toolbox',
       version=get_version("art/__init__.py"),
