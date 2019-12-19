32d800dafbe341b669f0a9ebe38e63c457a64ca9
Ngoc Minh Tran <M.N.Tran@ibm.com>
2019-12-19 17:31:28 +0000

    knockoff net overall adaptive
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- art/attacks/extraction/knockoff_nets.py
+++ art/attacks/extraction/knockoff_nets.py
@@ -179,6 +179,30 @@ class KnockoffNets(ExtractionAttack):
         :return: The stolen classifier.
         :rtype: :class:`.Classifier`
         """
+
+        # k = len()
+        # H = np.zeros(k)
+        # P = np.ones(k) / k
+        # N = np.zeros(k)
+        #
+        #  = np.zeros(iterations)
+        # avg_reward = 0
+        # for t in range(1, iterations + 1):
+        #     A = np.argmax(P)
+        #
+        #     R = .reward(A)
+        #     avg_reward = avg_reward + (1.0 / t) * (R - avg_reward)
+        #     baseline = 0.0 if no_baseline else avg_reward
+        #
+        #     H[A] = H[A] + step_size * (R - baseline) * (1 - P[A])
+        #     for a in range(k):
+        #         if a != A:
+        #             H[a] = H[a] - step_size * (R - baseline) * P[a]
+        #
+        #     aux_exp = np.exp(H)
+        #     P = aux_exp / np.sum(aux_exp)
+        #     [t - 1] = int(.(A)) * 1.0
+
         return thieved_classifier
 
     def set_params(self, **kwargs):
