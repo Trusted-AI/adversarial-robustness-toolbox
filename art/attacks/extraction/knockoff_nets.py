7aca6136018c18e8608d8458a404eb30aeee1cc4
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-01-16 15:26:30 +0000

    fix bug in sample data
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- art/attacks/extraction/knockoff_nets.py
+++ art/attacks/extraction/knockoff_nets.py
@@ -250,7 +250,7 @@ class KnockoffNets(ExtractionAttack):
         return thieved_classifier
 
     @staticmethod
-    def _sample_data(self, x, y, action):
+    def _sample_data(x, y, action):
         """
         Sample data with a specific action.
 
