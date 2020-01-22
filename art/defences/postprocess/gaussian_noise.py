fdb4fd30796c0f9759ba25ca521abf446796b996
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-01-22 16:38:34 +0000

    gaussian noise init
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- art/defences/postprocess/gaussian_noise.py
+++ art/defences/postprocess/gaussian_noise.py
@@ -16,122 +16,27 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 """
-This module implements wrapper adding random Gaussian noise to the classifier output.
+This module implements Gaussian noise added to the classifier output.
 """
 import logging
 
-import numpy as np
-
-from art.wrappers.wrapper import ClassifierWrapper
-from art.classifiers.classifier import Classifier
+from art.defences import Postprocessor
 
 logger = logging.getLogger(__name__)
 
 
-class OutputRandomNoise(ClassifierWrapper, Classifier):
+class GaussianNoise(Postprocessor):
     """
-    Implementation of a classifier wrapper adding random Gaussian noise to the classifier output.
+    Implementation of a postprocessor based on adding Gaussian noise to classifier output.
     """
 
-    def __init__(self, classifier, scale=0.2):
+    def __init__(self, scale=0.2):
         """
-        Create a wrapper for rounded prediction output.
+        Create a GaussianNoise postprocessor.
 
-        :param classifier: A trained classifier.
-        :type classifier: :class:`.Classifier`
         :param scale: Standard deviation of the distribution.
         :type scale: `float`
         """
-        super(OutputRandomNoise, self).__init__(classifier)
+        super(GaussianNoise, self).__init__()
         self.scale = scale
 
-    # pylint: disable=W0221
-    def predict(self, x, batch_size=128, **kwargs):
-        """
-        Make a prediction with the wrapped classifier, add random Gaussian noise and re-normalize predicted output.
-
-        :param x: Input data
-        :type x: `np.ndarray`
-        :param batch_size: Batch size
-        :type batch_size: `int`
-        :return: Rounded predictions of shape `(nb_inputs, nb_classes)`.
-        :rtype: `np.ndarray`
-        """
-        predictions = self.classifier.predict(x, batch_size=batch_size, **kwargs)
-        noise = np.random.normal(loc=0.0, scale=self.scale, size=predictions.shape)
-        predictions += noise
-        predictions[predictions < 0.0] = 0.0
-        if predictions.shape[1] > 1:
-            sums = np.sum(predictions, axis=1)
-            predictions /= sums
-        return predictions
-
-    def fit(self, x, y, batch_size=128, nb_epochs=20, **kwargs):
-        """
-        Fit the classifier using the training data `(x, y)`.
-
-        :param x: Features in array of shape (nb_samples, nb_features) or (nb_samples, nb_pixels_1, nb_pixels_2,
-                  nb_channels) or (nb_samples, nb_channels, nb_pixels_1, nb_pixels_2)
-        :type x: `np.ndarray`
-        :param y: Target values (class labels in classification) in array of shape (nb_samples, nb_classes) in
-                  One Hot Encoding format.
-        :type y: `np.ndarray`
-        :param kwargs: Dictionary of framework-specific arguments.
-        :type kwargs: `dict`
-        :return: `None`
-        """
-        raise NotImplementedError
-
-    def loss_gradient(self, x, y, **kwargs):
-        """
-        Compute the gradient of the given classifier's loss function w.r.t. `x`, taking an expectation
-        over transformations.
-
-        :param x: Sample input with shape as expected by the model.
-        :type x: `np.ndarray`
-        :param y: Correct labels, one-hot encoded.
-        :type y: `np.ndarray`
-        :return: Array of gradients of the same shape as `x`.
-        :rtype: `np.ndarray`
-        """
-        raise NotImplementedError
-
-    def class_gradient(self, x, label=None, **kwargs):
-        """
-        Compute per-class derivatives of the given classifier w.r.t. `x`, taking an expectation over transformations.
-
-        :param x: Sample input with shape as expected by the model.
-        :type x: `np.ndarray`
-        :param label: Index of a specific per-class derivative. If an integer is provided, the gradient of that class
-                      output is computed for all samples. If multiple values as provided, the first dimension should
-                      match the batch size of `x`, and each value will be used as target for its corresponding sample in
-                      `x`. If `None`, then gradients for all classes will be computed for each sample.
-        :type label: `int` or `list`
-        :return: Array of gradients of input features w.r.t. each class in the form
-                 `(batch_size, nb_classes, input_shape)` when computing for all classes, otherwise shape becomes
-                 `(batch_size, 1, input_shape)` when `label` parameter is specified.
-        :rtype: `np.ndarray`
-        """
-        raise NotImplementedError
-
-    def nb_classes(self):
-        """
-        Return the number of output classes.
-
-        :return: Number of classes in the data.
-        :rtype: `int`
-        """
-        return self._nb_classes
-
-    def save(self, filename, path=None):
-        """
-        Save a model to file specific to the backend framework.
-
-        :param filename: Name of the file where to save the model.
-        :type filename: `str`
-        :param path: Path of the directory where to save the model. If no path is specified, the model will be stored in
-                     the default data location of ART at `DATA_PATH`.
-        :type path: `str`
-        :return: None
-        """
-        raise NotImplementedError
