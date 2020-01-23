c90f79738fc33a28ae629a879b3594c2f4a98779
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-01-23 17:18:11 +0000

    imp class labels - init
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- art/defences/postprocess/class_labels.py
+++ art/defences/postprocess/class_labels.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (C) IBM Corporation 2019
+# Copyright (C) IBM Corporation 2020
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
@@ -16,119 +16,31 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 """
-This module implements a classifier wrapper which returns class labels.
+This module implements class labels added to the classifier output.
 """
 import logging
 
-import numpy as np
-
-from art.wrappers.wrapper import ClassifierWrapper
-from art.classifiers.classifier import Classifier
+from art.defences import Postprocessor
 
 logger = logging.getLogger(__name__)
 
 
-class OutputClassLabels(ClassifierWrapper, Classifier):
+class ClassLabels(Postprocessor):
     """
-    Implementation of a classifier wrapper which returns class labels.
+    Implementation of a postprocessor based on adding class labels to classifier output.
     """
 
-    def __init__(self, classifier):
+    def __init__(self, apply_fit=False, apply_predict=True):
         """
-        Create a wrapper for class label predictions.
+        Create a ClassLabels postprocessor.
 
-        :param classifier: A trained classifier.
-        :type classifier: :class:`.Classifier`
+        :param apply_fit: True if applied during fitting/training.
+        :type apply_fit: `bool`
+        :param apply_predict: True if applied during predicting.
+        :type apply_predict: `bool`
         """
-        super(OutputClassLabels, self).__init__(classifier)
+        super(ClassLabels, self).__init__()
+        self._is_fitted = True
+        self._apply_fit = apply_fit
+        self._apply_predict = apply_predict
 
-    # pylint: disable=W0221
-    def predict(self, x, batch_size=128, **kwargs):
-        """
-        Make a prediction with the wrapped classifier and return class labels.
-
-        :param x: Input data
-        :type x: `np.ndarray`
-        :param batch_size: Batch size
-        :type batch_size: `int`
-        :return: Rounded predictions of shape `(nb_inputs, nb_classes)`.
-        :rtype: `np.ndarray`
-        """
-        predictions = self.classifier.predict(x, batch_size=batch_size, **kwargs)
-        class_labels = np.zeros_like(predictions)
-        if predictions.shape[1] > 1:
-            index_labels = np.argmax(predictions, axis=1)
-            class_labels[:, index_labels] = 1
-        else:
-            class_labels[predictions > 0.5] = 1
-        return class_labels
-
-    def fit(self, x, y, batch_size=128, nb_epochs=20, **kwargs):
-        """
-        Fit the classifier using the training data `(x, y)`.
-
-        :param x: Features in array of shape (nb_samples, nb_features) or (nb_samples, nb_pixels_1, nb_pixels_2,
-                  nb_channels) or (nb_samples, nb_channels, nb_pixels_1, nb_pixels_2)
-        :type x: `np.ndarray`
-        :param y: Target values (class labels in classification) in array of shape (nb_samples, nb_classes) in
-                  One Hot Encoding format.
-        :type y: `np.ndarray`
-        :param kwargs: Dictionary of framework-specific arguments.
-        :type kwargs: `dict`
-        :return: `None`
-        """
-        raise NotImplementedError
-
-    def loss_gradient(self, x, y, **kwargs):
-        """
-        Compute the gradient of the given classifier's loss function w.r.t. `x`, taking an expectation
-        over transformations.
-
-        :param x: Sample input with shape as expected by the model.
-        :type x: `np.ndarray`
-        :param y: Correct labels, one-hot encoded.
-        :type y: `np.ndarray`
-        :return: Array of gradients of the same shape as `x`.
-        :rtype: `np.ndarray`
-        """
-        raise NotImplementedError
-
-    def class_gradient(self, x, label=None, **kwargs):
-        """
-        Compute per-class derivatives of the given classifier w.r.t. `x`, taking an expectation over transformations.
-
-        :param x: Sample input with shape as expected by the model.
-        :type x: `np.ndarray`
-        :param label: Index of a specific per-class derivative. If an integer is provided, the gradient of that class
-                      output is computed for all samples. If multiple values as provided, the first dimension should
-                      match the batch size of `x`, and each value will be used as target for its corresponding sample in
-                      `x`. If `None`, then gradients for all classes will be computed for each sample.
-        :type label: `int` or `list`
-        :return: Array of gradients of input features w.r.t. each class in the form
-                 `(batch_size, nb_classes, input_shape)` when computing for all classes, otherwise shape becomes
-                 `(batch_size, 1, input_shape)` when `label` parameter is specified.
-        :rtype: `np.ndarray`
-        """
-        raise NotImplementedError
-
-    def nb_classes(self):
-        """
-        Return the number of output classes.
-
-        :return: Number of classes in the data.
-        :rtype: `int`
-        """
-        return self._nb_classes
-
-    def save(self, filename, path=None):
-        """
-        Save a model to file specific to the backend framework.
-
-        :param filename: Name of the file where to save the model.
-        :type filename: `str`
-        :param path: Path of the directory where to save the model. If no path is specified, the model will be stored in
-                     the default data location of ART at `DATA_PATH`.
-        :type path: `str`
-        :return: None
-        """
-        raise NotImplementedError
