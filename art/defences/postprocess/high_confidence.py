272a95bb41370423ead28421e3fdeb35594bf302
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-01-27 14:25:17 +0000

    high conf - init
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- art/defences/postprocess/high_confidence.py
+++ art/defences/postprocess/high_confidence.py
@@ -16,115 +16,37 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 """
-This module implements a classifier wrapper which only returns predictions with high confidence.
+This module implements confidence added to the classifier output.
 """
 import logging
 
-from art.wrappers.wrapper import ClassifierWrapper
-from art.classifiers.classifier import Classifier
+from art.defences import Postprocessor
 
 logger = logging.getLogger(__name__)
 
 
-class OutputHighConfidence(ClassifierWrapper, Classifier):
+class HighConfidence(Postprocessor):
     """
-    Implementation of a classifier wrapper which only returns predictions with high confidence.
+    Implementation of a postprocessor based on selecting high confidence predictions to return as classifier output.
     """
 
-    def __init__(self, classifier, cutoff=0.25):
+    def __init__(self, cutoff=0.25, apply_fit=False, apply_predict=True):
         """
-        Create a wrapper which only returns predictions with high confidence.
+        Create a HighConfidence postprocessor.
 
-        :param classifier: A trained classifier.
-        :type classifier: :class:`.Classifier`
         :param cutoff: Minimal value for returned prediction output.
         :type cutoff: `float`
+        :param apply_fit: True if applied during fitting/training.
+        :type apply_fit: `bool`
+        :param apply_predict: True if applied during predicting.
+        :type apply_predict: `bool`
         """
-        super(OutputHighConfidence, self).__init__(classifier)
-        self.cutoff = cutoff
+        super(HighConfidence, self).__init__()
+        self._is_fitted = True
+        self._apply_fit = apply_fit
+        self._apply_predict = apply_predict
+        self.set_params(cutoff=cutoff)
 
-    # pylint: disable=W0221
-    def predict(self, x, batch_size=128, **kwargs):
-        """
-        Make a prediction with the wrapped classifier and return only predictions with high confidence.
 
-        :param x: Input data
-        :type x: `np.ndarray`
-        :param batch_size: Batch size
-        :type batch_size: `int`
-        :return: Rounded predictions of shape `(nb_inputs, nb_classes)`.
-        :rtype: `np.ndarray`
-        """
-        predictions = self.classifier.predict(x, batch_size=batch_size, **kwargs)
-        predictions[predictions < self.cutoff] = 0.0
-        return predictions
 
-    def fit(self, x, y, batch_size=128, nb_epochs=20, **kwargs):
-        """
-        Fit the classifier using the training data `(x, y)`.
 
-        :param x: Features in array of shape (nb_samples, nb_features) or (nb_samples, nb_pixels_1, nb_pixels_2,
-                  nb_channels) or (nb_samples, nb_channels, nb_pixels_1, nb_pixels_2)
-        :type x: `np.ndarray`
-        :param y: Target values (class labels in classification) in array of shape (nb_samples, nb_classes) in
-                  One Hot Encoding format.
-        :type y: `np.ndarray`
-        :param kwargs: Dictionary of framework-specific arguments.
-        :type kwargs: `dict`
-        :return: `None`
-        """
-        raise NotImplementedError
-
-    def loss_gradient(self, x, y, **kwargs):
-        """
-        Compute the gradient of the given classifier's loss function w.r.t. `x`, taking an expectation
-        over transformations.
-
-        :param x: Sample input with shape as expected by the model.
-        :type x: `np.ndarray`
-        :param y: Correct labels, one-hot encoded.
-        :type y: `np.ndarray`
-        :return: Array of gradients of the same shape as `x`.
-        :rtype: `np.ndarray`
-        """
-        raise NotImplementedError
-
-    def class_gradient(self, x, label=None, **kwargs):
-        """
-        Compute per-class derivatives of the given classifier w.r.t. `x`, taking an expectation over transformations.
-
-        :param x: Sample input with shape as expected by the model.
-        :type x: `np.ndarray`
-        :param label: Index of a specific per-class derivative. If an integer is provided, the gradient of that class
-                      output is computed for all samples. If multiple values as provided, the first dimension should
-                      match the batch size of `x`, and each value will be used as target for its corresponding sample in
-                      `x`. If `None`, then gradients for all classes will be computed for each sample.
-        :type label: `int` or `list`
-        :return: Array of gradients of input features w.r.t. each class in the form
-                 `(batch_size, nb_classes, input_shape)` when computing for all classes, otherwise shape becomes
-                 `(batch_size, 1, input_shape)` when `label` parameter is specified.
-        :rtype: `np.ndarray`
-        """
-        raise NotImplementedError
-
-    def nb_classes(self):
-        """
-        Return the number of output classes.
-
-        :return: Number of classes in the data.
-        :rtype: `int`
-        """
-        return self._nb_classes
-
-    def save(self, filename, path=None):
-        """
-        Save a model to file specific to the backend framework.
-
-        :param filename: Name of the file where to save the model.
-        :type filename: `str`
-        :param path: Path of the directory where to save the model. If no path is specified, the model will be stored in
-                     the default data location of ART at `DATA_PATH`.
-        :type path: `str`
-        :return: None
-        """
-        raise NotImplementedError
