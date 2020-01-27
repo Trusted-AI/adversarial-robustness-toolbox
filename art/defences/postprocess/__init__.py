ecdc6aede19915f10f469b702147b0075cc73da6
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-01-27 15:14:15 +0000

    start imp reverse sigmoid
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- art/defences/postprocess/__init__.py
+++ art/defences/postprocess/__init__.py
@@ -1,5 +1,7 @@
 """
 Module implementing postprocessing defences against adversarial attacks.
 """
+from art.defences.postprocess.class_labels import ClassLabels
 from art.defences.postprocess.gaussian_noise import GaussianNoise
+from art.defences.postprocess.high_confidence import HighConfidence
 from art.defences.postprocess.postprocessor import Postprocessor
