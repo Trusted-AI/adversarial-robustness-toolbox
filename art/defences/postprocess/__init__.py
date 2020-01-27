9fdbd18f76ed1b02f40880b8b13c03293e8170ad
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-01-27 17:04:00 +0000

    start rounded postprocessing
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- art/defences/postprocess/__init__.py
+++ art/defences/postprocess/__init__.py
@@ -5,3 +5,5 @@ from art.defences.postprocess.class_labels import ClassLabels
 from art.defences.postprocess.gaussian_noise import GaussianNoise
 from art.defences.postprocess.high_confidence import HighConfidence
 from art.defences.postprocess.postprocessor import Postprocessor
+from art.defences.postprocess.reverse_sigmoid import ReverseSigmoid
+from art.defences.postprocess.rounded import Rounded
