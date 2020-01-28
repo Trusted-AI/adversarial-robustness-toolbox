66dea8fc2f8fe8286c9d45c8e2cbb8df39634763
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-01-28 14:50:03 +0000

    add postprocessor to defence init
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- art/defences/__init__.py
+++ art/defences/__init__.py
@@ -12,3 +12,10 @@ from art.defences.preprocess.preprocessor import Preprocessor
 from art.defences.preprocess.spatial_smoothing import SpatialSmoothing
 from art.defences.preprocess.thermometer_encoding import ThermometerEncoding
 from art.defences.preprocess.variance_minimization import TotalVarMin
+
+from art.defences.postprocess.class_labels import ClassLabels
+from art.defences.postprocess.gaussian_noise import GaussianNoise
+from art.defences.postprocess.high_confidence import HighConfidence
+from art.defences.postprocess.postprocessor import Postprocessor
+from art.defences.postprocess.reverse_sigmoid import ReverseSigmoid
+from art.defences.postprocess.rounded import Rounded
