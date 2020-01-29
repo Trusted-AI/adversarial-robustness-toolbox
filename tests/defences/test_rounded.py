1461d5f8582b4243b98f259068c709469d477ed6
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-01-29 13:32:51 +0000

    test rounded
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- tests/defences/test_rounded.py
+++ tests/defences/test_rounded.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (C) IBM Corporation 2019
+# Copyright (C) IBM Corporation 2020
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
@@ -15,8 +15,6 @@
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 import logging
 import unittest
 
@@ -24,14 +22,14 @@ import numpy as np
 
 from art.utils import load_dataset, master_seed
 from tests.utils_test import get_classifier_kr
-from art.wrappers.output_rounded import OutputRounded
+from art.defences import Rounded
 
 logger = logging.getLogger(__name__)
 
 
-class TestRoundedOutput(unittest.TestCase):
+class TestRounded(unittest.TestCase):
     """
-    A unittest class for testing the Rounded Output wrapper.
+    A unittest class for testing the Rounded postprocessor.
     """
 
     @classmethod
@@ -48,20 +46,26 @@ class TestRoundedOutput(unittest.TestCase):
         Test with 2 decimal places.
         """
         (_, _), (x_test, _) = self.mnist
-        wrapper = OutputRounded(classifier=self.classifier, decimals=2)
+        preds = self.classifier.predict(x_test[0:1])
+        postprocessor = Rounded(decimals=2)
+        post_preds = postprocessor(preds=preds)
+
         expected_predictions = np.asarray([[0.12, 0.05, 0.1, 0.06, 0.11, 0.05, 0.06, 0.31, 0.08, 0.06]],
                                           dtype=np.float32)
-        np.testing.assert_array_equal(wrapper.predict(x_test[0:1]), expected_predictions)
+        np.testing.assert_array_equal(post_preds, expected_predictions)
 
     def test_decimals_3(self):
         """
         Test with 3 decimal places.
         """
         (_, _), (x_test, _) = self.mnist
-        wrapper = OutputRounded(classifier=self.classifier, decimals=3)
+        preds = self.classifier.predict(x_test[0:1])
+        postprocessor = Rounded(decimals=3)
+        post_preds = postprocessor(preds=preds)
+
         expected_predictions = np.asarray([[0.121, 0.05, 0.099, 0.064, 0.114, 0.046, 0.064, 0.307, 0.076, 0.058]],
                                           dtype=np.float32)
-        np.testing.assert_array_equal(wrapper.predict(x_test[0:1]), expected_predictions)
+        np.testing.assert_array_equal(post_preds, expected_predictions)
 
 
 if __name__ == '__main__':
