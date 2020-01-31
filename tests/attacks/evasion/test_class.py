8ae3ea72c2775aa62e779606e96f3b80ab96af8f
killian <killian.levacher@gmail.com>
2020-01-31 16:42:34 +0000

    added pytest test

--- tests/attacks/evasion/test_class.py
+++ tests/attacks/evasion/test_class.py
@@ -1,9 +1,12 @@
 
+# Run with pytest -q tests/attacks/evasion/test_class.py
+# or  python -m pytest  tests/attacks/evasion/test_class.py
+
 class TestClass:
-    def test_one(self):
+    def test_thatPasses(self):
         x = "this"
         assert "h" in x
 
-    def test_two(self):
+    def test_thatFails(self):
         x = "hello"
-        assert x == "hello"
+        assert x == "hello", "This test was supposed to do that {0}".format(1)
