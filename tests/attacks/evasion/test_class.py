730ffd168da89fe89f54e36b3ad9efa8bf185044
killian <killian.levacher@gmail.com>
2020-01-31 16:52:55 +0000

    added a conftest pytest file

--- tests/attacks/evasion/test_class.py
+++ tests/attacks/evasion/test_class.py
@@ -1,6 +1,10 @@
 
 # Run with pytest -q tests/attacks/evasion/test_class.py
 # or  python -m pytest  tests/attacks/evasion/test_class.py
+# or python -m pytest  tests/attacks/evasion
+
+import pytest
+
 
 class TestClass:
     def test_thatPasses(self):
@@ -10,3 +14,10 @@ class TestClass:
     def test_thatFails(self):
         x = "hello"
         assert x == "hello", "This test was supposed to do that {0}".format(1)
+
+    def test_answer(cmdopt):
+        if cmdopt == "type1":
+            print("first")
+        elif cmdopt == "type2":
+            print("second")
+        # assert 0  # to see what was printed
