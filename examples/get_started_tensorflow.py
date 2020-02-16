4e30ddfc03b7e6529ee6e8252a8176e3ca9ef048
Beat Buesser <beat.buesser@ie.ibm.com>
2020-02-16 00:07:25 +0000

    Apply Black style
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- examples/get_started_tensorflow.py
+++ examples/get_started_tensorflow.py
@@ -36,8 +36,16 @@ sess.run(tf.global_variables_initializer())
 
 # Step 3: Create the ART classifier
 
-classifier = TFClassifier(clip_values=(min_pixel_value, max_pixel_value), input_ph=input_ph, output=logits,
-                          labels_ph=labels_ph, train=train, loss=loss, learning=None, sess=sess)
+classifier = TFClassifier(
+    clip_values=(min_pixel_value, max_pixel_value),
+    input_ph=input_ph,
+    output=logits,
+    labels_ph=labels_ph,
+    train=train,
+    loss=loss,
+    learning=None,
+    sess=sess,
+)
 
 # Step 4: Train the ART classifier
 
@@ -47,7 +55,7 @@ classifier.fit(x_train, y_train, batch_size=64, nb_epochs=3)
 
 predictions = classifier.predict(x_test)
 accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(y_test)
-print('Accuracy on benign test examples: {}%'.format(accuracy * 100))
+print("Accuracy on benign test examples: {}%".format(accuracy * 100))
 
 # Step 6: Generate adversarial test examples
 attack = FastGradientMethod(classifier=classifier, eps=0.2)
@@ -57,4 +65,4 @@ x_test_adv = attack.generate(x=x_test)
 
 predictions = classifier.predict(x_test_adv)
 accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(y_test)
-print('Accuracy on adversarial test examples: {}%'.format(accuracy * 100))
+print("Accuracy on adversarial test examples: {}%".format(accuracy * 100))
