67cff1fb7a012326e30b3be564574bce3ba2ead4
minhitbk <minhitbk@gmail.com>
2020-02-18 11:17:11 +0000

    Update art/classifiers/catboost.py
    Co-Authored-By: Beat Buesser <49047826+beat-buesser@users.noreply.github.com>

--- art/classifiers/catboost.py
+++ art/classifiers/catboost.py
@@ -95,7 +95,7 @@ class CatBoostARTClassifier(Classifier):
         :return: Array of predictions of shape `(nb_inputs, nb_classes)`.
         :rtype: `np.ndarray`
         """
-        # Apply preprocessing defences
+        # Apply preprocessing
         x_preprocessed, _ = self._apply_preprocessing(x, y=None, fit=False)
 
         # Perform prediction
