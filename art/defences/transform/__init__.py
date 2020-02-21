ca5d8ef591bb0fb2313ae062d2e72e3f2847000a
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-02-21 14:19:00 +0000

    import defence into init file
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- art/defences/transform/__init__.py
+++ art/defences/transform/__init__.py
@@ -1,4 +1,5 @@
 """
 Module implementing transform-based defences against adversarial attacks.
 """
+from art.defences.transform.distillation import Distillation
 from art.defences.transform.transformer import Transformer
