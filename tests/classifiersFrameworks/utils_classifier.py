b34bd4f8a3b3b44e95f6044861b45edc6f596529
killian <killian.levacher@ibm.com>
2020-02-25 16:41:55 +0000

    fixing pep8 issues
    Signed-off-by: killian <killian.levacher@ibm.com>

--- tests/classifiersFrameworks/utils_classifier.py
+++ tests/classifiersFrameworks/utils_classifier.py
@@ -6,7 +6,8 @@ from tests.utils_test import ExpectedValue
 logger = logging.getLogger(__name__)
 
 
-def backend_test_layers(get_mlFramework, get_default_mnist_subset, get_image_classifier_list, batch_size, layer_count=None):
+def backend_test_layers(get_mlFramework, get_default_mnist_subset, get_image_classifier_list, batch_size,
+                        layer_count=None):
     (x_train_mnist, y_train_mnist), (x_test_mnist, y_test_mnist) = get_default_mnist_subset
     classifier, _ = get_image_classifier_list(one_classifier=True)
     if layer_count is not None:
@@ -37,7 +38,6 @@ def backend_test_class_gradient(get_default_mnist_subset, classifier, expected_v
 
     assert gradients.shape == (x_test_mnist.shape[0], 10, 28, 28, 1)
 
-
     if "expected_gradients_1_all_labels" in expected_values:
         np.testing.assert_array_almost_equal(gradients[0, 5, 14, :, 0],
                                              expected_values["expected_gradients_1_all_labels"].value,
@@ -54,11 +54,14 @@ def backend_test_class_gradient(get_default_mnist_subset, classifier, expected_v
     assert gradients.shape == (x_test_mnist.shape[0], 1, 28, 28, 1)
 
     if "expected_gradients_1_label5" in expected_values:
-        np.testing.assert_array_almost_equal(gradients[0, 0, 14, :, 0], expected_values["expected_gradients_1_label5"].value, decimal=expected_values["expected_gradients_1_label5"].decimals)
+        np.testing.assert_array_almost_equal(gradients[0, 0, 14, :, 0],
+                                             expected_values["expected_gradients_1_label5"].value,
+                                             decimal=expected_values["expected_gradients_1_label5"].decimals)
 
     if "expected_gradients_2_label5" in expected_values:
-        np.testing.assert_array_almost_equal(gradients[0, 0, :, 14, 0], expected_values["expected_gradients_2_label5"].value, decimal=expected_values["expected_gradients_2_label5"].decimals)
-
+        np.testing.assert_array_almost_equal(gradients[0, 0, :, 14, 0],
+                                             expected_values["expected_gradients_2_label5"].value,
+                                             decimal=expected_values["expected_gradients_2_label5"].decimals)
 
     # Test a set of gradients label = array
     # label = np.random.randint(5, size=self.n_test)
@@ -67,17 +70,24 @@ def backend_test_class_gradient(get_default_mnist_subset, classifier, expected_v
     assert gradients.shape == (x_test_mnist.shape[0], 1, 28, 28, 1)
 
     if "expected_gradients_1_labelArray" in expected_values:
-        np.testing.assert_array_almost_equal(gradients[0, 0, 14, :, 0], expected_values["expected_gradients_1_labelArray"].value, decimal=expected_values["expected_gradients_1_labelArray"].decimals)
+        np.testing.assert_array_almost_equal(gradients[0, 0, 14, :, 0],
+                                             expected_values["expected_gradients_1_labelArray"].value,
+                                             decimal=expected_values["expected_gradients_1_labelArray"].decimals)
     if "expected_gradients_2_labelArray" in expected_values:
-        np.testing.assert_array_almost_equal(gradients[0, 0, :, 14, 0], expected_values["expected_gradients_2_labelArray"].value, decimal=expected_values["expected_gradients_2_labelArray"].decimals)
+        np.testing.assert_array_almost_equal(gradients[0, 0, :, 14, 0],
+                                             expected_values["expected_gradients_2_labelArray"].value,
+                                             decimal=expected_values["expected_gradients_2_labelArray"].decimals)
+
 
 def backend_test_nb_classes(get_image_classifier_list):
     classifier, _ = get_image_classifier_list(one_classifier=True)
     assert classifier.nb_classes() == 10
 
+
 def backend_test_input_shape(get_image_classifier_list):
     classifier, _ = get_image_classifier_list(one_classifier=True)
     assert classifier.input_shape == (28, 28, 1)
+
 
 def backend_test_loss_gradient(get_default_mnist_subset, get_image_classifier_list, expected_values):
     (x_train_mnist, y_train_mnist), (x_test_mnist, y_test_mnist) = get_default_mnist_subset
@@ -89,10 +99,13 @@ def backend_test_loss_gradient(get_default_mnist_subset, get_image_classifier_li
     assert gradients.shape == (x_test_mnist.shape[0], 28, 28, 1)
 
     if "expected_gradients_1" in expected_values:
-        np.testing.assert_array_almost_equal(gradients[0, 14, :, 0], expected_values["expected_gradients_1"].value, decimal=expected_values["expected_gradients_1"].decimals)
+        np.testing.assert_array_almost_equal(gradients[0, 14, :, 0], expected_values["expected_gradients_1"].value,
+                                             decimal=expected_values["expected_gradients_1"].decimals)
 
     if "expected_gradients_2" in expected_values:
-        np.testing.assert_array_almost_equal(gradients[0, :, 14, 0], expected_values["expected_gradients_2"].value, decimal=expected_values["expected_gradients_2"].decimals)
+        np.testing.assert_array_almost_equal(gradients[0, :, 14, 0], expected_values["expected_gradients_2"].value,
+                                             decimal=expected_values["expected_gradients_2"].decimals)
+
 
 def backend_test_fit_generator(expected_values, classifier, data_gen, get_default_mnist_subset, nb_epochs):
     (x_train_mnist, y_train_mnist), (x_test_mnist, y_test_mnist) = get_default_mnist_subset
@@ -105,7 +118,8 @@ def backend_test_fit_generator(expected_values, classifier, data_gen, get_defaul
     logger.info('Accuracy: %.2f%%', (pre_fit_accuracy * 100))
 
     if "pre_fit_accuracy" in expected_values:
-        np.testing.assert_array_almost_equal(pre_fit_accuracy, expected_values["pre_fit_accuracy"].value, decimal=expected_values["pre_fit_accuracy"].decimals)
+        np.testing.assert_array_almost_equal(pre_fit_accuracy, expected_values["pre_fit_accuracy"].value,
+                                             decimal=expected_values["pre_fit_accuracy"].decimals)
 
     classifier.fit_generator(generator=data_gen, nb_epochs=nb_epochs)
     predictions = classifier.predict(x_test_mnist)
@@ -114,5 +128,5 @@ def backend_test_fit_generator(expected_values, classifier, data_gen, get_defaul
     logger.info('Accuracy after fitting classifier with generator: %.2f%%', (post_fit_accuracy * 100))
 
     if "post_fit_accuracy" in expected_values:
-        np.testing.assert_array_almost_equal(post_fit_accuracy, expected_values["post_fit_accuracy"].value, decimal=expected_values["post_fit_accuracy"].decimals)
-
+        np.testing.assert_array_almost_equal(post_fit_accuracy, expected_values["post_fit_accuracy"].value,
+                                             decimal=expected_values["post_fit_accuracy"].decimals)
