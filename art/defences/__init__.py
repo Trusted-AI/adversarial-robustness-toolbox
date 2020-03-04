e6760a71f42e0a8f06024c3223b4dacdadf5f6fc
minhitbk <minhitbk@gmail.com>
2020-03-04 14:43:03 +0000

    Update art/defences/__init__.py
    Co-Authored-By: Beat Buesser <49047826+beat-buesser@users.noreply.github.com>

--- art/defences/__init__.py
+++ art/defences/__init__.py
@@ -22,5 +22,5 @@ from art.defences.trainer.adversarial_trainer import AdversarialTrainer
 from art.defences.trainer.adversarial_trainer_madry_pgd import AdversarialTrainerMadryPGD
 from art.defences.trainer.trainer import Trainer
 
-from art.defences.transform.distillation import Distillation
+from art.defences.transform.defensive_distillation import DefensiveDistillation
 from art.defences.transform.transformer import Transformer
