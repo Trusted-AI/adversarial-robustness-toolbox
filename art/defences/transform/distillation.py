8b44b910421e57fc5cc24e1112293ca38f02cf91
minhitbk <minhitbk@gmail.com>
2020-03-04 14:45:44 +0000

    Update art/defences/transform/distillation.py
    Co-Authored-By: Beat Buesser <49047826+beat-buesser@users.noreply.github.com>

--- art/defences/transform/distillation.py
+++ art/defences/transform/distillation.py
@@ -16,7 +16,7 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 """
-This module implements the distillation defence `Distillation`.
+This module implements the transforming defence mechanism of defensive distillation.
 
 | Paper link: https://arxiv.org/abs/1511.04508
 """
