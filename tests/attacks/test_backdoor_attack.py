8acda7ed6359f59fbd821cec54ca9de8495106df
Ebube Chuba <ebube.chuba@ibm.com>
2020-03-04 16:31:12 -0800

    finalize tests
    Signed-off-by: Ebube Chuba <ebube.chuba@ibm.com>

--- tests/attacks/test_backdoor_attack.py
+++ tests/attacks/test_backdoor_attack.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (C) IBM Corporation 2018
+# Copyright (C) IBM Corporation 2020
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
@@ -21,13 +21,16 @@ import logging
 import unittest
 
 import numpy as np
+from keras import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
 
 from art.attacks.poisoning.backdoor_attack import PoisoningAttackBackdoor
 from art.attacks.poisoning.perturbations import add_pattern_bd, add_single_bd, insert_image
-from art.utils import preprocess
-from tests.utils import TestBase, master_seed, get_classifier_kr
-logger = logging.getLogger(__name__)
+from art.classifiers import KerasClassifier
+from art.utils import preprocess, load_mnist
+from tests.utils import TestBase, master_seed
 
+logger = logging.getLogger(__name__)
 
 PP_POISON = 0.33
 
@@ -39,24 +42,40 @@ class TestBackdoorAttack(TestBase):
 
     @classmethod
     def setUpClass(cls):
-        master_seed(seed=1234)
         super().setUpClass()
+        master_seed(seed=1234)
 
-        cls.n_train = 100
-        cls.n_test = 100
-        cls.x_train_mnist = cls.x_train_mnist[0:cls.n_train]
-        cls.y_train_mnist = cls.y_train_mnist[0:cls.n_train]
-        cls.x_test_mnist = cls.x_test_mnist[0:cls.n_test]
-        cls.y_test_mnist = cls.y_test_mnist[0:cls.n_test]
+        (cls.x_train_mnist_raw, cls.y_train_mnist_raw), (cls.x_test_mnist_raw, cls.y_test_mnist_raw), \
+            cls._min, cls._max = load_mnist(raw=True)
+
+        cls.n_train_raw = 7500
+        cls.n_test_raw = 7500
+        cls.x_train_mnist_raw = cls.x_train_mnist_raw[0:cls.n_train_raw]
+        cls.y_train_mnist_raw = cls.y_train_mnist_raw[0:cls.n_train_raw]
+        cls.x_test_mnist_raw = cls.x_test_mnist_raw[0:cls.n_test_raw]
+        cls.y_test_mnist_raw = cls.y_test_mnist_raw[0:cls.n_test_raw]
 
     def setUp(self):
-        super().setUpClass()
         master_seed(seed=1234)
         super().setUp()
 
-    def poison_dataset(self, x_clean, y_clean, percent_poison, poison_func):
-        max_val = np.max(x_clean)
+    def get_keras_model(self):
+        model = Sequential()
+        model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=self.x_train_mnist.shape[1:]))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(10, activation='softmax'))
 
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        classifier = KerasClassifier(model=model, clip_values=(self._min, self._max))
+        return classifier
+
+    @staticmethod
+    def poison_dataset(x_clean, y_clean, percent_poison, poison_func):
         x_poison = np.copy(x_clean)
         y_poison = np.copy(y_clean)
         is_poison = np.zeros(np.shape(y_poison))
@@ -83,29 +102,41 @@ class TestBackdoorAttack(TestBase):
 
         return is_poison, x_poison, y_poison
 
+    def poison_func_1(self, x):
+        max_val = np.max(self.x_train_mnist_raw)
+        return add_pattern_bd(x, pixel_value=max_val)
+
+    def poison_func_2(self, x):
+        max_val = np.max(self.x_train_mnist_raw)
+        return add_single_bd(x, pixel_value=max_val)
+
+    def poison_func_3(self, x):
+        return insert_image(x, backdoor_path='../../data/backdoors/flower.png', size=(5, 5), random=False,
+                            x_shift=3, y_shift=3)
+
+    def poison_func_4(self, x):
+        return insert_image(x, backdoor_path='../../data/backdoors/post_it.png', size=(5, 5), random=True)
+
+    def poison_func_5(self, x):
+        return insert_image(x, backdoor_path='../../data/backdoors/flower.png', random=True)
+
     def test_backdoor_pattern(self):
         """
         Test the backdoor attack with a pattern-based perturbation
-        :return:
         """
-        max_val = np.max(self.x_train_mnist)
 
-        def poison_func(x):
-            return add_pattern_bd(x, pixel_value=max_val)
-            # return add_single_bd(x, pixel_value=max_val)
-            # return insert_image(x, backdoor_path='../data/backdoors/post_it.png', size=(5, 5))
-
-        krc = get_classifier_kr()
-        (is_poison_train, x_poisoned_raw, y_poisoned_raw) = self.poison_dataset(self.x_train_mnist, self.y_train_mnist,
-                                                                                PP_POISON, poison_func)
+        krc = self.get_keras_model()
+        (is_poison_train, x_poisoned_raw, y_poisoned_raw) = self.poison_dataset(self.x_train_mnist_raw,
+                                                                                self.y_train_mnist_raw,
+                                                                                PP_POISON, self.poison_func_1)
         x_train, y_train = preprocess(x_poisoned_raw, y_poisoned_raw)
         # Add channel axis:
         x_train = np.expand_dims(x_train, axis=3)
 
         # Poison test data
-        (is_poison_test, x_poisoned_raw_test, y_poisoned_raw_test) = self.poison_dataset(self.x_test_mnist,
-                                                                                         self.y_test_mnist, PP_POISON,
-                                                                                         poison_func)
+        (is_poison_test, x_poisoned_raw_test, y_poisoned_raw_test) = self.poison_dataset(self.x_test_mnist_raw,
+                                                                                         self.y_test_mnist_raw,
+                                                                                         PP_POISON, self.poison_func_1)
         x_test, y_test = preprocess(x_poisoned_raw_test, y_poisoned_raw_test)
         # Add channel axis:
         x_test = np.expand_dims(x_test, axis=3)
@@ -128,7 +159,8 @@ class TestBackdoorAttack(TestBase):
 
         clean_acc = clean_correct / clean_total
 
-        self.assertGreater(clean_acc, 90)
+        # check for high accuracy on clean samples
+        self.assertGreater(clean_acc, 0.9)
 
         poison_x_test = x_test[is_poison_test]
         poison_y_test = y_test[is_poison_test]
@@ -138,24 +170,203 @@ class TestBackdoorAttack(TestBase):
         poison_total = poison_y_test.shape[0]
 
         poison_acc = poison_correct / poison_total
+
+        # check for targeted misclassification for poisoned samples
         self.assertGreater(poison_acc, 0.8)
 
-    # TODO: just copy and paste from here
     def test_backdoor_pixel(self):
-        self.assertEqual(1, 1)
+        """
+        Test the backdoor attack with a pixel-based perturbation
+        """
+
+        tfc = self.get_keras_model()
+        (is_poison_train, x_poisoned_raw, y_poisoned_raw) = self.poison_dataset(self.x_train_mnist_raw,
+                                                                                self.y_train_mnist_raw,
+                                                                                PP_POISON, self.poison_func_2)
+        x_train, y_train = preprocess(x_poisoned_raw, y_poisoned_raw)
+        # Add channel axis:
+        x_train = np.expand_dims(x_train, axis=3)
+
+        # Poison test data
+        (is_poison_test, x_poisoned_raw_test, y_poisoned_raw_test) = self.poison_dataset(self.x_test_mnist_raw,
+                                                                                         self.y_test_mnist_raw,
+                                                                                         PP_POISON, self.poison_func_2)
+        x_test, y_test = preprocess(x_poisoned_raw_test, y_poisoned_raw_test)
+        # Add channel axis:
+        x_test = np.expand_dims(x_test, axis=3)
+
+        # Shuffle training data
+        n_train = np.shape(y_train)[0]
+        shuffled_indices = np.arange(n_train)
+        np.random.shuffle(shuffled_indices)
+        x_train = x_train[shuffled_indices]
+        y_train = y_train[shuffled_indices]
+
+        tfc.fit(x_train, y_train, nb_epochs=5, batch_size=128)
+
+        clean_x_test = x_test[is_poison_test == 0]
+        clean_y_test = y_test[is_poison_test == 0]
+
+        clean_preds = np.argmax(tfc.predict(clean_x_test), axis=1)
+        clean_correct = np.sum(clean_preds == np.argmax(clean_y_test, axis=1))
+        clean_total = clean_y_test.shape[0]
+
+        clean_acc = clean_correct / clean_total
+
+        # check for high accuracy on clean samples
+        self.assertGreater(clean_acc, 0.9)
+
+        poison_x_test = x_test[is_poison_test]
+        poison_y_test = y_test[is_poison_test]
+
+        poison_preds = np.argmax(tfc.predict(poison_x_test), axis=1)
+        poison_correct = np.sum(poison_preds == np.argmax(poison_y_test, axis=1))
+        poison_total = poison_y_test.shape[0]
+
+        poison_acc = poison_correct / poison_total
+
+        # check for targeted misclassification for poisoned samples
+        self.assertGreater(poison_acc, 0.8)
 
     def test_backdoor_image(self):
-        self.assertEqual(1, 1)
+        """
+        Test the backdoor attack with a image-based perturbation
+        """
+        tfc = self.get_keras_model()
+        (is_poison_train, x_poisoned_raw, y_poisoned_raw) = self.poison_dataset(self.x_train_mnist_raw,
+                                                                                self.y_train_mnist_raw,
+                                                                                PP_POISON, self.poison_func_3)
+        x_train, y_train = preprocess(x_poisoned_raw, y_poisoned_raw)
+        # Add channel axis:
+        x_train = np.expand_dims(x_train, axis=3)
+
+        # Poison test data
+        (is_poison_test, x_poisoned_raw_test, y_poisoned_raw_test) = self.poison_dataset(self.x_test_mnist_raw,
+                                                                                         self.y_test_mnist_raw,
+                                                                                         PP_POISON, self.poison_func_3)
+        x_test, y_test = preprocess(x_poisoned_raw_test, y_poisoned_raw_test)
+        # Add channel axis:
+        x_test = np.expand_dims(x_test, axis=3)
+
+        # Shuffle training data
+        n_train = np.shape(y_train)[0]
+        shuffled_indices = np.arange(n_train)
+        np.random.shuffle(shuffled_indices)
+        x_train = x_train[shuffled_indices]
+        y_train = y_train[shuffled_indices]
+
+        tfc.fit(x_train, y_train, nb_epochs=5, batch_size=128)
+
+        clean_x_test = x_test[is_poison_test == 0]
+        clean_y_test = y_test[is_poison_test == 0]
+
+        clean_preds = np.argmax(tfc.predict(clean_x_test), axis=1)
+        clean_correct = np.sum(clean_preds == np.argmax(clean_y_test, axis=1))
+        clean_total = clean_y_test.shape[0]
+
+        clean_acc = clean_correct / clean_total
+
+        # check for high accuracy on clean samples
+        self.assertGreater(clean_acc, 0.9)
+
+        poison_x_test = x_test[is_poison_test]
+        poison_y_test = y_test[is_poison_test]
+
+        poison_preds = np.argmax(tfc.predict(poison_x_test), axis=1)
+        poison_correct = np.sum(poison_preds == np.argmax(poison_y_test, axis=1))
+        poison_total = poison_y_test.shape[0]
+
+        poison_acc = poison_correct / poison_total
+
+        # check for targeted misclassification for poisoned samples
+        self.assertGreater(poison_acc, 0.8)
 
     def test_multiple_perturbations(self):
-        import os
-        print(os.getcwd())
-        self.assertEqual(1, 1)
+        """
+        Test using multiple perturbation functions in the same attack
+        """
 
-    def test_failure_image(self):
-        # TODO: test for backdoor images that are too big
-        # TODO: test for invalid array shapes
-        self.assertEqual(1, 1)
+        tfc = self.get_keras_model()
+        (is_poison_train, x_poisoned_raw, y_poisoned_raw) = self.poison_dataset(self.x_train_mnist_raw,
+                                                                                self.y_train_mnist_raw,
+                                                                                PP_POISON, [self.poison_func_4,
+                                                                                            self.poison_func_1])
+        x_train, y_train = preprocess(x_poisoned_raw, y_poisoned_raw)
+
+        # Add channel axis:
+        x_train = np.expand_dims(x_train, axis=3)
+
+        # Poison test data
+        (is_poison_test, x_poisoned_raw_test, y_poisoned_raw_test) = self.poison_dataset(self.x_test_mnist_raw,
+                                                                                         self.y_test_mnist_raw,
+                                                                                         PP_POISON,
+                                                                                         [self.poison_func_4,
+                                                                                          self.poison_func_1])
+        x_test, y_test = preprocess(x_poisoned_raw_test, y_poisoned_raw_test)
+
+        # Add channel axis:
+        x_test = np.expand_dims(x_test, axis=3)
+
+        # Shuffle training data
+        n_train = np.shape(y_train)[0]
+        shuffled_indices = np.arange(n_train)
+        np.random.shuffle(shuffled_indices)
+        x_train = x_train[shuffled_indices]
+        y_train = y_train[shuffled_indices]
+
+        tfc.fit(x_train, y_train, nb_epochs=5, batch_size=128)
+
+        clean_x_test = x_test[is_poison_test == 0]
+        clean_y_test = y_test[is_poison_test == 0]
+
+        clean_preds = np.argmax(tfc.predict(clean_x_test), axis=1)
+        clean_correct = np.sum(clean_preds == np.argmax(clean_y_test, axis=1))
+        clean_total = clean_y_test.shape[0]
+
+        clean_acc = clean_correct / clean_total
+
+        # check for high accuracy on clean samples
+        self.assertGreater(clean_acc, 0.9)
+
+        poison_x_test = x_test[is_poison_test]
+        poison_y_test = y_test[is_poison_test]
+
+        poison_preds = np.argmax(tfc.predict(poison_x_test), axis=1)
+        poison_correct = np.sum(poison_preds == np.argmax(poison_y_test, axis=1))
+        poison_total = poison_y_test.shape[0]
+
+        poison_acc = poison_correct / poison_total
+
+        # check for targeted misclassification for poisoned samples
+        self.assertGreater(poison_acc, 0.8)
+
+    def test_image_failure_modes(self):
+        """
+        Tests failure modes for image perturbation functions
+        """
+
+        backdoor_attack = PoisoningAttackBackdoor(self.poison_func_5)
+        with self.assertRaises(ValueError) as context:
+            backdoor_attack.poison(self.x_train_mnist_raw[0], y=self.y_train_mnist_raw[0]+1)
+
+        self.assertIn('Backdoor does not fit inside original image', str(context.exception))
+
+        with self.assertRaises(ValueError) as context:
+            backdoor_attack.poison(np.zeros(5), y=self.y_train_mnist_raw[0] + 1)
+
+        self.assertIn('Invalid array shape', str(context.exception))
+
+        backdoor_attack = PoisoningAttackBackdoor(self.poison_func_1)
+        with self.assertRaises(ValueError) as context:
+            backdoor_attack.poison(np.zeros(5), y=self.y_train_mnist_raw[0] + 1)
+
+        self.assertIn('Invalid array shape', str(context.exception))
+
+        backdoor_attack = PoisoningAttackBackdoor(self.poison_func_2)
+        with self.assertRaises(ValueError) as context:
+            backdoor_attack.poison(np.zeros(5), y=self.y_train_mnist_raw[0] + 1)
+
+        self.assertIn('Invalid array shape', str(context.exception))
 
 
 if __name__ == '__main__':
