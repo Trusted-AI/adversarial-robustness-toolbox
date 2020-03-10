bac2ef4228331697b06e7d1e5a68929f1c537600
killian <killian.levacher@ibm.com>
2020-03-10 10:26:16 +0000

    fixing tensorflow test_layers issue
    Signed-off-by: killian <killian.levacher@ibm.com>

--- conftest.py
+++ conftest.py
@@ -32,8 +32,9 @@ def setup_tear_down_framework(framework):
     if framework == "keras":
         pass
     if framework == "tensorflow":
-        if tf.__version__[0] == '2':
-            tf.reset_default_graph()
+        tf.reset_default_graph()
+        # if tf.__version__[0] == '2':
+        #     tf.reset_default_graph()
     if framework == "pytorch":
         pass
     if framework == "scikitlearn":
