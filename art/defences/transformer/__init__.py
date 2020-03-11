d39b9dd51ec43fff632c09d5f6123291d9c3e556
Beat Buesser <beat.buesser@ie.ibm.com>
2020-03-11 16:59:06 +0000

    PEP 8
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/defences/transformer/__init__.py
+++ art/defences/transformer/__init__.py
@@ -3,4 +3,3 @@ Module implementing transformer-based defences against adversarial attacks.
 """
 from art.defences.transformer.transformer import Transformer
 from art.defences.transformer.defensive_distillation import DefensiveDistillation
-
