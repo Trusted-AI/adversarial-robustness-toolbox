e6ea0e993cd52988fd384f16909499d494c88923
Beat Buesser <beat.buesser@ie.ibm.com>
2020-03-15 19:16:28 +0000

    Increase version number to ART v1.2.0
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, IBM Corporation'
 author = 'Maria-Irina Nicolae'
 
 # The short X.Y version
-version = '1.1'
+version = '1.2'
 # The full version, including alpha/beta/rc tags
-release = '1.1.1'
+release = '1.2.0'
 
 
 # -- General configuration ---------------------------------------------------
