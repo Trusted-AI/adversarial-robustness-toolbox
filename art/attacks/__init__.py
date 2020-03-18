ef8951291fca28f1989ca615403cdbb890c0f8ad
Beat Buesser <beat.buesser@ie.ibm.com>
2020-03-18 14:56:24 +0000

    Fix line length
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/attacks/__init__.py
+++ art/attacks/__init__.py
@@ -1,7 +1,8 @@
 """
 Module providing adversarial attacks under a common interface.
 """
-# from art.attacks.attack import Attack, EvasionAttack, PoisoningAttackBlackBox, PoisoningAttackWhiteBox, ExtractionAttack
+# from art.attacks.attack import Attack, EvasionAttack, PoisoningAttackBlackBox, PoisoningAttackWhiteBox, \
+# ExtractionAttack
 #
 # from art.attacks.evasion.adversarial_patch import AdversarialPatch
 # from art.attacks.evasion.boundary import BoundaryAttack
