af6ba6e0adc1ac095bc1d32768444c7b3f0f7f93
Beat Buesser <beat.buesser@ie.ibm.com>
2020-03-18 23:51:43 +0000

    Add string representation to ClassifierError
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/exceptions.py
+++ art/exceptions.py
@@ -26,9 +26,6 @@ class ClassifierError(TypeError):
     """
 
     def __init__(self, this_class, class_expected_list, classifier_given):
-        self.this_class = this_class
-        self.class_expected_list = class_expected_list
-        self.classifier_given = classifier_given
 
         classes_expected_message = ""
         for idx, class_expected in enumerate(class_expected_list):
@@ -38,8 +35,14 @@ class ClassifierError(TypeError):
                 classes_expected_message += " and {0}".format(class_expected)
 
         self.message = (
-            "For {0} classifier must be an instance of {1}, "
-            "the provided classifier is instance of {2}.".format(
-                this_class.__name__, classes_expected_message, classifier_given
+            "{0} requires an estimator derived from {1}, "
+            "the provided classifier is an instance of {2} and is derived from {3}.".format(
+                this_class.__name__,
+                classes_expected_message,
+                type(classifier_given),
+                classifier_given.__class__.__bases__,
             )
         )
+
+    def __str__(self):
+        return self.message
