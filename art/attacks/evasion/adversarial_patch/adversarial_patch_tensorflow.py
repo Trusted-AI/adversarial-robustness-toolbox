a138fc8aa7adbe1558473860e2791cdf4bc9a326
Beat Buesser <beat.buesser@ie.ibm.com>
2020-03-21 01:12:00 +0000

    Add init to adversarial patch for TensorFlow
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>
    Signed-off-by: Ambrish Rawat <ambrish.rawat@ie.ibm.com>

--- art/attacks/evasion/adversarial_patch/adversarial_patch_tensorflow.py
+++ art/attacks/evasion/adversarial_patch/adversarial_patch_tensorflow.py
@@ -36,4 +36,67 @@ class AdversarialPatchTensorFlow(EvasionAttack):
 
     | Paper link: https://arxiv.org/abs/1712.09665
     """
-    pass
+    attack_params = EvasionAttack.attack_params + [
+        "target",
+        "rotation_max",
+        "scale_min",
+        "scale_max",
+        "learning_rate",
+        "max_iter",
+        "batch_size",
+        "clip_patch",
+    ]
+
+    def __init__(
+            self,
+            classifier,
+            target=0,
+            rotation_max=22.5,
+            scale_min=0.1,
+            scale_max=1.0,
+            learning_rate=5.0,
+            max_iter=500,
+            clip_patch=None,
+            batch_size=16,
+    ):
+        """
+        Create an instance of the :class:`.AdversarialPatch`.
+
+        :param classifier: A trained classifier.
+        :type classifier: :class:`.Classifier`
+        :param target: The target label for the created patch.
+        :type target: `int`
+        :param rotation_max: The maximum rotation applied to random patches. The value is expected to be in the
+               range `[0, 180]`.
+        :type rotation_max: `float`
+        :param scale_min: The minimum scaling applied to random patches. The value should be in the range `[0, 1]`,
+               but less than `scale_max`.
+        :type scale_min: `float`
+        :param scale_max: The maximum scaling applied to random patches. The value should be in the range `[0, 1]`, but
+               larger than `scale_min.`
+        :type scale_max: `float`
+        :param learning_rate: The learning rate of the optimization.
+        :type learning_rate: `float`
+        :param max_iter: The number of optimization steps.
+        :type max_iter: `int`
+        :param clip_patch: The minimum and maximum values for each channel
+        :type clip_patch: [(float, float), (float, float), (float, float)]
+        :param batch_size: The size of the training batch.
+        :type batch_size: `int`
+        """
+        super(AdversarialPatchTensorFlow, self).__init__(classifier=classifier)
+        if not isinstance(classifier, ClassifierNeuralNetwork) or not isinstance(classifier, ClassifierGradients):
+            raise ClassifierError(self.__class__, [ClassifierNeuralNetwork, ClassifierGradients], classifier)
+
+        kwargs = {
+            "target": target,
+            "rotation_max": rotation_max,
+            "scale_min": scale_min,
+            "scale_max": scale_max,
+            "learning_rate": learning_rate,
+            "max_iter": max_iter,
+            "batch_size": batch_size,
+            "clip_patch": clip_patch,
+        }
+        self.set_params(**kwargs)
+        self.patch = None
