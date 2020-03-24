24c300a96c1b6cd9b0d0d2b86057bd910c7227c2
Beat Buesser <beat.buesser@ie.ibm.com>
2020-03-24 22:11:24 +0000

    Update object detector and example
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>
    Signed-off-by: Ambrish Rawat <ambrish.rawat@ie.ibm.com>

--- examples/application_object_detection.py
+++ examples/application_object_detection.py
@@ -10,11 +10,11 @@ COCO_INSTANCE_CATEGORY_NAMES = [
     "traffic light", "fire hydrant", "N/A", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse",
     "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "N/A", "backpack", "umbrella", "N/A", "N/A", "handbag",
     "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove",
-    "skateboard", "surfboard", "tennis racket", "bottle", "N/A", "wine glass", "cup", "fork", "knife", "spoon",
-    "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
-    "chair", "couch", "potted plant", "bed", "N/A", "dining table", "N/A", "N/A", "toilet", "N/A", "tv", "laptop",
-    "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "N/A",
-    "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush", ]
+    "skateboard", "surfboard", "tennis racket", "bottle", "N/A", "wine glass", "cup", "fork", "knife", "spoon", "bowl",
+    "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair",
+    "couch", "potted plant", "bed", "N/A", "dining table", "N/A", "N/A", "toilet", "N/A", "tv", "laptop", "mouse",
+    "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "N/A", "book", "clock",
+    "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
 
 
 def extract_predictions(predictions_):
@@ -23,14 +23,14 @@ def extract_predictions(predictions_):
 
     # Get the predicted class
     predictions_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(predictions_["labels"].numpy())]
-    print('predicted classes:', predictions_class)
+    print("\npredicted classes:", predictions_class)
 
     # Get the predicted bounding boxes
     predictions_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(predictions_["boxes"].detach().numpy())]
 
     # Get the predicted prediction score
     predictions_score = list(predictions_["scores"].detach().numpy())
-    print('predicted score:', predictions_score)
+    print("predicted score:", predictions_score)
 
     # Get a list of index with score greater than threshold
     threshold = 0.5
@@ -61,62 +61,69 @@ def plot_image_with_boxes(img, boxes, pred_cls):
 
 
 def main():
-    # Create object detector
-    frcnn = PyTorchFasterRCNN(clip_values=(0, 255))
+    # Create ART object detector
+    frcnn = PyTorchFasterRCNN(
+        clip_values=(0, 255), attack_losses=["loss_classifier", "loss_box_reg", "loss_objectness", "loss_rpn_box_reg"]
+    )
 
     # Load image 1
-    image_1 = cv2.imread("./10best-cars-group-cropped-1542126037.jpg")
-    image_1 = cv2.cvtColor(image_1, cv2.COLOR_BGR2RGB)  # Convert to RGB
-    print('image_1.shape:', image_1.shape)
+    image_0 = cv2.imread("./10best-cars-group-cropped-1542126037.jpg")
+    image_0 = cv2.cvtColor(image_0, cv2.COLOR_BGR2RGB)  # Convert to RGB
+    print("image_0.shape:", image_0.shape)
 
     # Load image 2
-    # image_2 = cv2.imread("./banner-diverse-group-of-people-2.jpg")
-    # image_2 = cv2.cvtColor(image_2, cv2.COLOR_BGR2RGB)  # Convert to RGB
-    # print('image_2.shape:', image_2.shape)
+    image_1 = cv2.imread("./banner-diverse-group-of-people-2.jpg")
+    image_1 = cv2.cvtColor(image_1, cv2.COLOR_BGR2RGB)  # Convert to RGB
+    image_1 = cv2.resize(image_1, dsize=(image_0.shape[1], image_0.shape[0]), interpolation=cv2.INTER_CUBIC)
+    print("image_1.shape:", image_1.shape)
 
     # Stack images
-    image = np.stack([image_1], axis=0)
-    print('image.shape:', image.shape)
+    image = np.stack([image_0, image_1], axis=0)
+    print("image.shape:", image.shape)
 
     for i in range(image.shape[0]):
         plt.axis("off")
-        plt.title('image {}'.format(i))
+        plt.title("image {}".format(i))
         plt.imshow(image[i], interpolation="nearest")
         plt.show()
 
     # Make prediction on benign samples
     predictions = frcnn.predict(x=image)
 
-    # Process predictions
-    predictions_class, predictions_boxes, predictions_class = extract_predictions(predictions[0])
+    for i in range(image.shape[0]):
+        print("\nPredictions image {}:".format(i))
 
-    # Plot predictions
-    plot_image_with_boxes(img=image[0].copy(), boxes=predictions_boxes, pred_cls=predictions_class)
+        # Process predictions
+        predictions_class, predictions_boxes, predictions_class = extract_predictions(predictions[i])
 
-    # # Calculate loss gradients
-    # gradients = frcnn.loss_gradient(x=image, y=None)
-    #
-    # # Create adversarial image
-    # image_adv = image + np.sign(gradients) * 8 * 1
-    # image_adv = np.clip(image_adv, a_min=0, a_max=255).astype(np.uint8)
+        # Plot predictions
+        plot_image_with_boxes(img=image[i].copy(), boxes=predictions_boxes, pred_cls=predictions_class)
 
-    attack = FastGradientMethod(classifier=frcnn, eps=8)
+    # Create and run attack
+    eps = 8
+    attack = FastGradientMethod(estimator=frcnn, eps=eps)
     image_adv = attack.generate(x=image, y=None)
 
-    print('np.amax(np.abs(image - image_adv))')
-    print(np.amax(np.abs(image - image_adv)))
+    print("\nThe attack budget eps is {}".format(eps))
+    print("The resulting maximal difference in pixel values is {}.".format(np.amax(np.abs(image - image_adv))))
+    assert np.amax(np.abs(image - image_adv)) == eps
 
     for i in range(image_adv.shape[0]):
         plt.axis("off")
-        plt.title('image_adv {}'.format(i))
+        plt.title("image_adv {}".format(i))
         plt.imshow(image_adv[i].astype(np.uint8), interpolation="nearest")
         plt.show()
 
     predictions_adv = frcnn.predict(x=image_adv)
 
-    predictions_adv_class, predictions_adv_boxes, predictions_adv_class = extract_predictions(predictions_adv[0])
+    for i in range(image.shape[0]):
+        print("\nPredictions adversarial image {}:".format(i))
 
-    plot_image_with_boxes(img=image_adv[0].copy(), boxes=predictions_adv_boxes, pred_cls=predictions_adv_class)
+        # Process predictions
+        predictions_adv_class, predictions_adv_boxes, predictions_adv_class = extract_predictions(predictions_adv[i])
+
+        # Plot predictions
+        plot_image_with_boxes(img=image_adv[i].copy(), boxes=predictions_adv_boxes, pred_cls=predictions_adv_class)
 
 
 if __name__ == "__main__":
