c42da0af6f42786e5a655341610a45ef870e6c0c
Beat Buesser <beat.buesser@ie.ibm.com>
2020-04-09 16:39:45 +0100

    Change classifier to estimator
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/attacks/evasion/adversarial_patch/adversarial_patch_tensorflow.py
+++ art/attacks/evasion/adversarial_patch/adversarial_patch_tensorflow.py
@@ -258,7 +258,7 @@ class AdversarialPatchTensorFlowV2(EvasionAttack):
 
         import tensorflow as tf
 
-        y = check_and_transform_label_format(labels=y, nb_classes=self.classifier.nb_classes())
+        y = check_and_transform_label_format(labels=y, nb_classes=self.estimator.nb_classes())
 
         shuffle = kwargs.get('shuffle', True)
 
