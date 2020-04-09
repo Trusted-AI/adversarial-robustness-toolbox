201bd5788a28910b33558613b2bd257b7ad9d338
Beat Buesser <beat.buesser@ie.ibm.com>
2020-04-09 23:02:43 +0100

    Implement unittests for DPatch
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>
    Signed-off-by: Ambrish Rawat <ambrish.rawat@ie.ibm.com>

--- tests/attacks/evasion/test_dpatch.py
+++ tests/attacks/evasion/test_dpatch.py
@@ -15,3 +15,108 @@
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+import logging
+import numpy as np
+import pytest
+
+from art.attacks.evasion import DPatch
+from art.estimators.estimator import BaseEstimator, LossGradientsMixin
+from art.estimators.object_detection.object_detector import ObjectDetectorMixin
+
+from tests.attacks.utils import backend_test_classifier_type_check_fail
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture()
+def fix_get_mnist_subset(get_mnist_dataset):
+    (x_train_mnist, y_train_mnist), (x_test_mnist, y_test_mnist) = get_mnist_dataset
+    n_train = 100
+    n_test = 11
+    yield (x_train_mnist[:n_train], y_train_mnist[:n_train], x_test_mnist[:n_test], y_test_mnist[:n_test])
+
+
+@pytest.mark.parametrize("random_location", [True, False])
+def test_augment_images_with_patch(random_location, fix_get_mnist_subset):
+    (x_train_mnist, y_train_mnist, x_test_mnist, y_test_mnist) = fix_get_mnist_subset
+
+    patch = np.ones(shape=(4, 4, 1)) * 0.5
+    patched_images, transformations = DPatch._augment_images_with_patch(
+        x=x_train_mnist[0:3], patch=patch, random_location=random_location, channel_index=3
+    )
+
+    if random_location:
+        transformation_expected = {"i_x_1": 0, "i_y_1": 2, "i_x_2": 4, "i_y_2": 6}
+        patched_images_column = [
+            0.0,
+            0.0,
+            0.5,
+            0.5,
+            0.5,
+            0.5,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+        ]
+    else:
+        transformation_expected = {"i_x_1": 0, "i_y_1": 0, "i_x_2": 4, "i_y_2": 4}
+        patched_images_column = [
+            0.5,
+            0.5,
+            0.5,
+            0.5,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+        ]
+
+    assert transformations[1] == transformation_expected
+    np.testing.assert_array_equal(patched_images[1, 2, :, 0], patched_images_column)
+
+
+def test_classifier_type_check_fail():
+    backend_test_classifier_type_check_fail(DPatch, [ObjectDetectorMixin, LossGradientsMixin, BaseEstimator])
+
+
+if __name__ == "__main__":
+    pytest.cmdline.main("-q -s {} --mlFramework=tensorflow --durations=0".format(__file__).split(" "))
