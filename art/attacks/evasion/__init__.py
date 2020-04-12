7b7560728e8780e784fb01cf344f51ec7b74eb5a
Beat Buesser <beat.buesser@ie.ibm.com>
2020-04-12 02:02:08 +0100

    Update imports
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/attacks/evasion/__init__.py
+++ art/attacks/evasion/__init__.py
@@ -21,3 +21,7 @@ from art.attacks.evasion.zoo import ZooAttack
 from art.attacks.evasion.pixel_threshold import PixelAttack
 from art.attacks.evasion.pixel_threshold import ThresholdAttack
 from art.attacks.evasion.frame_saliency import FrameSaliencyAttack
+from art.attacks.evasion.auto_attack import AutoAttack
+from art.attacks.evasion.auto_projected_gradient_descent import AutoProjectedGradientDescent
+from art.attacks.evasion.fast_adaptive_boundary import FastAdaptiveBoundary
+from art.attacks.evasion.square_attack import SquareAttack
