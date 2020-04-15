5948df38659fdd475349cc2b51acf5a49fd7b161
Ambrish Rawat <ambrish.rawat@ie.ibm.com>
2020-04-15 13:43:10 +0100

    changes to FBF trainer
    Signed-off-by: Ambrish Rawat <ambrish.rawat@ie.ibm.com>

--- art/defences/trainer/__init__.py
+++ art/defences/trainer/__init__.py
@@ -4,3 +4,4 @@ Module implementing train-based defences against adversarial attacks.
 from art.defences.trainer.trainer import Trainer
 from art.defences.trainer.adversarial_trainer import AdversarialTrainer
 from art.defences.trainer.adversarial_trainer_madry_pgd import AdversarialTrainerMadryPGD
+from art.defences.trainer.adversarial_trainer_FBF import AdversarialTrainerFB
