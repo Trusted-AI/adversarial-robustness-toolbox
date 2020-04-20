Fix gradient check metric and its unittests
Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>
Signed-off-by: Ambrish Rawat <ambrish.rawat@ie.ibm.com>

tests/metrics/test_gradient_check.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (C) IBM Corporation 2018
+# Copyright (C) IBM Corporation 2020
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
@@ -20,17 +20,12 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import logging
 import unittest
 
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 import numpy as np
-import tensorflow as tf
 
-from art.estimators.classification.keras import KerasClassifier
 from art.metrics.gradient_check import loss_gradient_check
 from art.utils import load_mnist
 
-from tests.utils import master_seed
+from tests.utils import TestBase, master_seed, get_image_classifier_kr
 
 logger = logging.getLogger(__name__)
 
@@ -39,9 +34,10 @@ NB_TRAIN = 100
 NB_TEST = 100
 
 
-class Test_Gradient_Check(unittest.TestCase):
+class TestGradientCheck(TestBase):
     def setUp(self):
         master_seed(seed=42)
+        super().setUp()
 
     def test_loss_gradient_check(self):
         (x_train, y_train), (x_test, y_test), _, _ = load_mnist()
@@ -49,11 +45,8 @@ class Test_Gradient_Check(unittest.TestCase):
         x_test, y_test = x_test[:NB_TEST], y_test[:NB_TEST]
 
         # Get classifier and train like normal
-        classifier = _cnn_mnist([28, 28, 1])
+        classifier = get_image_classifier_kr()
         classifier.fit(x_train, y_train, batch_size=BATCH_SIZE, nb_epochs=2)
-
-        # Check if the function works
-        is_bad = loss_gradient_check(classifier, x_test, y_test)
 
         # Now check that the function detects bad gradients
         # Set the weights of the convolution layer to zero
@@ -61,9 +54,9 @@ class Test_Gradient_Check(unittest.TestCase):
         new_weights = [np.zeros(w.shape) for w in weights]
         classifier._model.layers[0].set_weights(new_weights)
         is_bad = loss_gradient_check(classifier, x_test, y_test)
-        
-        self.assertTrue(np.all(np.any(is_bad,1)))
-        
+
+        self.assertTrue(np.all(np.any(is_bad, 1)))
+
         # Set the weights of the convolution layer to nan
         weights = classifier._model.layers[0].get_weights()
         new_weights = [np.empty(w.shape) for w in weights]
@@ -72,23 +65,7 @@ class Test_Gradient_Check(unittest.TestCase):
         classifier._model.layers[0].set_weights(new_weights)
         is_bad = loss_gradient_check(classifier, x_test, y_test)
 
-        self.assertTrue(np.all(np.any(is_bad,1)))
-
-    @staticmethod
-    def _cnn_mnist(input_shape):
-        # Create simple CNN
-        model = Sequential()
-        model.add(Conv2D(4, kernel_size=(5, 5), activation="relu", input_shape=input_shape))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Flatten())
-        model.add(Dense(10, activation="softmax"))
-
-        model.compile(
-            loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(lr=0.01), metrics=["accuracy"]
-        )
-
-        classifier = KerasClassifier(model=model, clip_values=(0, 1), use_logits=False)
-        return classifier
+        self.assertTrue(np.all(np.any(is_bad, 1)))
 
 
 if __name__ == "__main__":