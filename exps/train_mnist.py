a9183bf439196ec8f71e1763ebdd1d2d5c2df3b2
Ambrish Rawat <ambrishrawat@gmail.com>
2020-04-21 09:53:24 -0400

    address path changes to mnist
    Signed-off-by: Ambrish Rawat <ambrish.rawat@ie.ibm.com>

--- exps/train_mnist.py
+++ exps/train_mnist.py
@@ -54,7 +54,7 @@ model.compile(loss=keras.losses.categorical_crossentropy,
 
 model.fit(x_train, y_train,
           batch_size=batch_size,
-          epochs=epochs,
+          epochs=20,
           verbose=1,
           validation_data=(x_test, y_test))
 score = model.evaluate(x_test, y_test, verbose=0)
