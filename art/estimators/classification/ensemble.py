59c38a0ffdf5e22a0d48554c45c10e57a4f0776d
Lin Li <linli.tree@outlook.com>
2020-04-30 20:15:52 +0800

    Ensemble - clip values compare
    Signed-off-by: Lin Li <linli.tree@outlook.com>
    Signed-off-by: abigailt <abigailt@il.ibm.com>

--- art/estimators/classification/ensemble.py
+++ art/estimators/classification/ensemble.py
@@ -92,7 +92,7 @@ class EnsembleClassifier(ClassGradientsMixin, ClassifierMixin, LossGradientsMixi
             if not isinstance(classifier, NeuralNetworkMixin):
                 raise TypeError("Expected type `Classifier`, found %s instead." % type(classifier))
 
-            if clip_values != classifier.clip_values:
+            if np.array_equal(clip_values, classifier.clip_values):
                 raise ValueError(
                     "Incompatible `clip_values` between classifiers in the ensemble. Found %s and %s."
                     % (str(clip_values), str(classifier.clip_values))
