f431cfeaf691a770aad875964cdc7339bd720027
Beat Buesser <beat.buesser@ie.ibm.com>
2020-05-01 00:29:44 +0100

    Reorganise detection defences
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>
    Signed-off-by: abigailt <abigailt@il.ibm.com>

---
+++ art/defences/detection/evasion/__init__.py
@@ -0,0 +1,4 @@
+"""
+Module providing methods for detecting adversarial samples under a common interface.
+"""
+from art.defences.detection.evasion.detector import BinaryInputDetector, BinaryActivationDetector
---
+++ art/defences/detection/poison/__init__.py
@@ -0,0 +1,5 @@
+"""
+Poison detection defence API. Use the :class:`.PoisonFilteringDefence` wrapper to be able to apply a defence for a
+preexisting model.
+"""
+from art.defences.detection.poison.ground_truth_evaluator import GroundTruthEvaluator
