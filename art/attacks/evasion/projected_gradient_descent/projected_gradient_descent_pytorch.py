00ab1af31293048ee10b887d62059cb131403c9e
minhitbk <minhitbk@gmail.com>
2020-05-01 11:17:14 +0100

    Update art/attacks/evasion/projected_gradient_descent/projected_gradient_descent_pytorch.py
    Co-authored-by: Beat Buesser <49047826+beat-buesser@users.noreply.github.com>
    Signed-off-by: Ambrish Rawat <ambrish.rawat@ie.ibm.com>

--- art/attacks/evasion/projected_gradient_descent/projected_gradient_descent_pytorch.py
+++ art/attacks/evasion/projected_gradient_descent/projected_gradient_descent_pytorch.py
@@ -39,7 +39,7 @@ from art.utils import compute_success, get_labels_np_array, check_and_transform_
 logger = logging.getLogger(__name__)
 
 
-class ProjectedGradientDescentPytorch(EvasionAttack):
+class ProjectedGradientDescentPyTorch(EvasionAttack):
     """
     The Projected Gradient Descent attack is an iterative method in which,
     after each iteration, the perturbation is projected on an lp-ball of specified radius (in
