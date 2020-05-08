94cf3777a7f10fdb7d2322f5e003de53e03bc42d
Beat Buesser <beat.buesser@ie.ibm.com>
2020-05-08 20:17:18 +0100

    Remove unused method
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/attacks/evasion/feature_adversaries.py
+++ art/attacks/evasion/feature_adversaries.py
@@ -72,15 +72,6 @@ class FeatureAdversaries(EvasionAttack):
 
         self.norm = np.inf
 
-    @classmethod
-    def is_valid_classifier_type(cls, classifier):
-        """
-        Checks whether the classifier provided is a classifer which this class can perform an attack on
-        :param classifier:
-        :return:
-        """
-        return True if isinstance(classifier, ClassifierNeuralNetwork) else False
-
     def generate(self, x, y=None, **kwargs):
         """
         Generate adversarial samples and return them in an array.
