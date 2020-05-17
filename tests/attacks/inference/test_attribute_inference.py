69f46d05ee6f5ba2efedca928ec63bc4bfc7c120
abigailt <abigailt@il.ibm.com>
2020-05-17 10:23:08 +0300

    Black box attack can now receive art classifier as attack model. Signed-off-By: Abigail Goldsteen<abigailt@il.ibm.com>
    Signed-off-by: abigailt <abigailt@il.ibm.com>

--- tests/attacks/inference/test_attribute_inference.py
+++ tests/attacks/inference/test_attribute_inference.py
@@ -21,13 +21,15 @@ import logging
 import numpy as np
 import pytest
 
+import torch.nn as nn
+import torch.optim as optim
+
 from art.attacks.inference import AttributeInferenceWhiteBoxDecisionTree, AttributeInferenceBlackBox, \
     AttributeInferenceWhiteBoxLifestyleDecisionTree
+from art.estimators.classification.pytorch import PyTorchClassifier
 
-from tests.utils import ExpectedValue
 
 logger = logging.getLogger(__name__)
-
 
 def test_black_box(get_tabular_classifier_list, get_iris_dataset):
     classifier_list = get_tabular_classifier_list(AttributeInferenceBlackBox)
@@ -62,7 +64,65 @@ def test_black_box(get_tabular_classifier_list, get_iris_dataset):
 
     for classifier in classifier_list:
         # print(classifier)
-        attack = AttributeInferenceBlackBox(classifier, attack_feature)
+        attack = AttributeInferenceBlackBox(classifier, attack_feature=attack_feature)
+        # get original model's predictions
+        x_train_predictions = np.array([np.argmax(arr) for arr in classifier.predict(x_train_iris)]).reshape(-1,1)
+        x_test_predictions = np.array([np.argmax(arr) for arr in classifier.predict(x_test_iris)]).reshape(-1,1)
+        # train attack model
+        attack.fit(x_train)
+        # infer attacked feature
+        inferred_train = attack.infer(x_train_for_attack, x_train_predictions)
+        inferred_test = attack.infer(x_test_for_attack, x_test_predictions)
+        # check accuracy
+        train_acc = np.sum(inferred_train == x_train_feature) / len(inferred_train)
+        test_acc = np.sum(inferred_test == x_test_feature) / len(inferred_test)
+        # print(train_acc)
+        # print(test_acc)
+        assert train_acc > test_acc
+
+def test_black_box_with_model(get_tabular_classifier_list, get_iris_dataset):
+    classifier_list = get_tabular_classifier_list(AttributeInferenceBlackBox)
+    if classifier_list is None:
+        logging.warning("Couldn't perform  this test because no classifier is defined")
+        return
+
+    attack_feature = 2 # petal length
+
+    # need to transform attacked feature into categorical
+    def transform_feature(x):
+        x[x > 0.5] = 2.0
+        x[(x > 0.2) & (x <= 0.5)] = 1.0
+        x[x <= 0.2] = 0.0
+
+
+    (x_train_iris, y_train_iris), (x_test_iris, y_test_iris) = get_iris_dataset
+    # training data without attacked feature
+    x_train_for_attack = np.delete(x_train_iris, attack_feature, 1)
+    # only attacked feature
+    x_train_feature = x_train_iris[:, attack_feature].copy().reshape(-1, 1)
+    transform_feature(x_train_feature)
+    # training data with attacked feature (after transformation)
+    x_train = np.concatenate((x_train_for_attack[:, :attack_feature], x_train_feature), axis=1)
+    x_train = np.concatenate((x_train, x_train_for_attack[:, attack_feature:]), axis=1)
+
+    # test data without attacked feature
+    x_test_for_attack = np.delete(x_test_iris, attack_feature, 1)
+    # only attacked feature
+    x_test_feature = x_test_iris[:, attack_feature].copy().reshape(-1, 1)
+    transform_feature(x_test_feature)
+
+    model = nn.Linear(4, 3)
+
+    # Define a loss function and optimizer
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    attack_model = PyTorchClassifier(
+            model=model, clip_values=(0, 1), loss=loss_fn, optimizer=optimizer, input_shape=(4,), nb_classes=3
+    )
+
+    for classifier in classifier_list:
+        # print(classifier)
+        attack = AttributeInferenceBlackBox(classifier, attack_model=attack_model, attack_feature=attack_feature)
         # get original model's predictions
         x_train_predictions = np.array([np.argmax(arr) for arr in classifier.predict(x_train_iris)]).reshape(-1,1)
         x_test_predictions = np.array([np.argmax(arr) for arr in classifier.predict(x_test_iris)]).reshape(-1,1)
