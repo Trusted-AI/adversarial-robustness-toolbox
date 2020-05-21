1f9b822816c27989910c1dc8b2ef250556e38bbd
Ebube Chuba <ebube.chuba@ibm.com>
2020-05-21 00:10:11 -0700

    revert untouched file...again
    Signed-off-by: Ebube Chuba <ebube.chuba@ibm.com>

--- tests/defences/preprocessor/test_jpeg_compression.py
+++ tests/defences/preprocessor/test_jpeg_compression.py
@@ -18,102 +18,121 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import logging
-import unittest
 
-from keras.datasets import cifar10
 import numpy as np
+import pytest
+from numpy.testing import assert_array_equal
 
+# import art
+from art.config import ART_NUMPY_DTYPE
 from art.defences.preprocessor import JpegCompression
-from art.utils import load_mnist
-
-from tests.utils import master_seed
 
 logger = logging.getLogger(__name__)
 
 
-class TestJpegCompression(unittest.TestCase):
-    def setUp(self):
-        master_seed(seed=1234)
+class DataGenerator:
+    """
+    Create image or video batch.
+    """
 
-    def test_one_channel(self):
-        clip_values = (0, 1)
-        (x_train, _), (_, _), _, _ = load_mnist()
-        x_train = x_train[:2]
-        preprocess = JpegCompression(clip_values=clip_values, quality=70)
-        x_compressed, _ = preprocess(x_train)
-        self.assertEqual(x_compressed.shape, x_train.shape)
-        self.assertTrue((x_compressed >= clip_values[0]).all())
-        self.assertTrue((x_compressed <= clip_values[1]).all())
+    def __init__(self, channels_first, channels, image_data=True, batch_size=2):
+        self.channels_first = channels_first
+        self.channels = (channels,)
+        self.batch_size = batch_size
+        self.image_data = image_data
 
-    def test_three_channels_0_1(self):
-        clip_values = (0, 1)
-        (train_features, _), (_, _) = cifar10.load_data()
-        x = train_features[:2] / 255.0
-        x_original = x.copy()
-        preprocess = JpegCompression(clip_values=clip_values, quality=80)
-        x_compressed, _ = preprocess(x)
-        self.assertEqual(x_compressed.shape, x.shape)
-        self.assertTrue((x_compressed >= clip_values[0]).all())
-        self.assertTrue((x_compressed <= clip_values[1]).all())
-        self.assertAlmostEqual(x_compressed[0, 14, 14, 0], 0.92941177)
-        self.assertAlmostEqual(x_compressed[0, 14, 14, 1], 0.8039216)
-        self.assertAlmostEqual(x_compressed[0, 14, 14, 2], 0.6117647)
-        # Check that x has not been modified by attack and classifier
-        self.assertAlmostEqual(float(np.max(np.abs(x_original - x))), 0.0, delta=0.00001)
+    def get_data(self):
+        temporal_index = () if self.image_data else (2,)
+        if self.channels_first:
+            data_shape = (self.batch_size,) + self.channels + temporal_index + (8, 12)
+        else:
+            data_shape = (self.batch_size,) + temporal_index + (8, 12) + self.channels
+        return (255 * np.ones(data_shape)).astype(ART_NUMPY_DTYPE)
 
-    def test_three_channels_0_255(self):
-        clip_values = (0, 255)
-        (train_features, _), (_, _) = cifar10.load_data()
-        x = train_features[:2]
-        preprocess = JpegCompression(clip_values=clip_values, quality=80)
-        x_compressed, _ = preprocess(x)
-        self.assertEqual(x_compressed.shape, x.shape)
-        self.assertTrue((x_compressed >= clip_values[0]).all())
-        self.assertTrue((x_compressed <= clip_values[1]).all())
-        self.assertAlmostEqual(x_compressed[0, 14, 14, 0], 0.92941177 * clip_values[1], places=4)
-        self.assertAlmostEqual(x_compressed[0, 14, 14, 1], 0.8039216 * clip_values[1], places=4)
-        self.assertAlmostEqual(x_compressed[0, 14, 14, 2], 0.6117647 * clip_values[1], places=4)
 
-    def test_channel_index(self):
-        clip_values = (0, 255)
-        (train_features, _), (_, _) = cifar10.load_data()
-        x = train_features[:2]
-        x = np.swapaxes(x, 1, 3)
-        preprocess = JpegCompression(clip_values=clip_values, channel_index=1, quality=80)
-        x_compressed, _ = preprocess(x)
-        self.assertTrue((x_compressed.shape == x.shape))
-        self.assertTrue((x_compressed >= clip_values[0]).all())
-        self.assertTrue((x_compressed <= clip_values[1]).all())
+@pytest.fixture(params=[1, 3], ids=["grayscale", "RGB"])
+def image_batch(request, channels_first):
+    """
+    Image fixtures of shape NHWC and NCHW.
+    """
+    channels = request.param
+    image_input = DataGenerator(channels_first, channels)
+    test_input = image_input.get_data()
+    test_output = test_input.copy()
+    return test_input, test_output
 
-    def test_failure_feature_vectors(self):
-        clip_values = (0, 1)
-        x = np.random.rand(10, 3)
-        preprocess = JpegCompression(clip_values=clip_values, channel_index=1, quality=80)
 
-        # Assert that value error is raised for feature vectors
-        with self.assertRaises(ValueError) as context:
-            preprocess(x)
+@pytest.fixture(params=[1, 3], ids=["grayscale", "RGB"])
+def video_batch(request, channels_first):
+    """
+    Video fixtures of shape NFHWC and NCFHW.
+    """
+    channels = request.param
+    video_input = DataGenerator(channels_first, channels, image_data=False)
+    test_input = video_input.get_data()
+    test_output = test_input.copy()
+    return test_input, test_output
 
-        self.assertTrue("Feature vectors detected." in str(context.exception))
 
-    def test_failure_clip_values_negative(self):
-        clip_values = (-1, 1)
+@pytest.fixture
+def tabular_batch():
+    """Create tabular data fixture of shape (batch_size, features)."""
+    return np.zeros((2, 4))
 
-        # Assert that value error is raised
-        with self.assertRaises(ValueError) as context:
-            _ = JpegCompression(clip_values=clip_values, channel_index=1, quality=80)
 
-        self.assertTrue("min value must be 0." in str(context.exception))
+class TestJpegCompression:
+    """Test JpegCompression."""
 
-    def test_failure_clip_values_unexpected_maximum(self):
-        clip_values = (0, 2)
+    @pytest.mark.parametrize("channels_first", [True, False])
+    def test_jpeg_compression_image_data(self, image_batch, channels_first):
+        channel_index = 1 if channels_first else 3
+        test_input, test_output = image_batch
+        jpeg_compression = JpegCompression(clip_values=(0, 255), channel_index=channel_index)
 
-        # Assert that value error is raised
-        with self.assertRaises(ValueError) as context:
-            _ = JpegCompression(clip_values=clip_values, channel_index=1, quality=80)
+        assert_array_equal(jpeg_compression(test_input)[0], test_output)
 
-        self.assertIn("max value must be either 1 or 255.", str(context.exception))
+    @pytest.mark.parametrize("channels_first", [True, False])
+    def test_jpeg_compression_video_data(self, video_batch, channels_first):
+        channel_index = 1 if channels_first else 4
+        test_input, test_output = video_batch
+        jpeg_compression = JpegCompression(clip_values=(0, 255), channel_index=channel_index)
+
+        assert_array_equal(jpeg_compression(test_input)[0], test_output)
+
+    @pytest.mark.parametrize("channels_first", [False])
+    def test_jpeg_compress(self, image_batch, channels_first):
+        test_input, test_output = image_batch
+        jpeg_compression = JpegCompression(clip_values=(0, 255))
+
+        image_mode = "RGB" if test_input.shape[-1] == 3 else "L"
+        test_single_input = np.squeeze(test_input[0]).astype(np.uint8)
+        test_single_output = np.squeeze(test_output[0]).astype(np.uint8)
+
+        assert_array_equal(jpeg_compression._compress(test_single_input, image_mode), test_single_output)
+
+    def test_channel_index_error(self):
+        exc_msg = "Data channel must be an integer equal to 1, 3 or 4. The batch dimension is not a valid channel."
+        with pytest.raises(ValueError, match=exc_msg):
+            JpegCompression(clip_values=(0, 255), channel_index=0)
+
+    def test_non_spatial_data_error(self, tabular_batch):
+        test_input = tabular_batch
+        jpeg_compression = JpegCompression(clip_values=(0, 255), channel_index=1)
+
+        exc_msg = "Feature vectors detected. JPEG compression can only be applied to data with spatial dimensions."
+        with pytest.raises(ValueError, match=exc_msg):
+            jpeg_compression(test_input)
+
+    def test_negative_clip_values_error(self):
+        exc_msg = "'clip_values' min value must be 0."
+        with pytest.raises(ValueError, match=exc_msg):
+            JpegCompression(clip_values=(-1, 255), channel_index=1)
+
+    def test_maximum_clip_values_error(self):
+        exc_msg = "'clip_values' max value must be either 1 or 255."
+        with pytest.raises(ValueError, match=exc_msg):
+            JpegCompression(clip_values=(0, 2), channel_index=1)
 
 
 if __name__ == "__main__":
-    unittest.main()
+    pytest.cmdline.main("-q -s {} --mlFramework=tensorflow --durations=0".format(__file__).split(" "))
