76f1baf2511b113d12540827bef858e79ac5cc5e
Beat Buesser <49047826+beat-buesser@users.noreply.github.com>
2020-05-22 16:29:22 +0100

    Update art/attacks/evasion/projected_gradient_descent/projected_gradient_descent.py Signed-off-by: Ambrish Rawat <ambrish.rawat@ie.ibm.com>

--- art/attacks/evasion/projected_gradient_descent/projected_gradient_descent.py
+++ art/attacks/evasion/projected_gradient_descent/projected_gradient_descent.py
@@ -79,7 +79,7 @@ class ProjectedGradientDescent(EvasionAttack):
         max_iter=100,
         targeted=False,
         num_random_init=0,
-        batch_size=1,
+        batch_size=32,
         random_eps=False,
     ):
         """
