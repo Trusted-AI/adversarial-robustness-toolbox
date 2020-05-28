a24e8a2fcbcffa6a076dff4a1780e67b8c0ee733
Beat Buesser <beat.buesser@ie.ibm.com>
2020-05-28 11:37:48 +0100

    Update docstrings of classifier.fit
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>
    Signed-off-by: Ambrish Rawat <ambrish.rawat@ie.ibm.com>

--- art/estimators/classification/pytorch.py
+++ art/estimators/classification/pytorch.py
@@ -173,8 +173,7 @@ class PyTorchClassifier(ClassGradientsMixin, ClassifierMixin, PyTorchEstimator):
 
         :param x: Training data.
         :type x: `np.ndarray`
-        :param y: Target values (class labels) one-hot-encoded of shape (nb_samples, nb_classes) or indices of shape
-                  (nb_samples,).
+        :param y: Target values (class labels) one-hot-encoded of shape (nb_samples, nb_classes).
         :type y: `np.ndarray`
         :param batch_size: Size of batches.
         :type batch_size: `int`
--- art/estimators/classification/tensorflow.py
+++ art/estimators/classification/tensorflow.py
@@ -170,8 +170,7 @@ class TensorFlowClassifier(ClassGradientsMixin, ClassifierMixin, TensorFlowEstim
 
         :param x: Training data.
         :type x: `np.ndarray`
-        :param y: Target values (class labels) one-hot-encoded of shape (nb_samples, nb_classes) or indices of shape
-                  (nb_samples,).
+        :param y: Target values (class labels) one-hot-encoded of shape (nb_samples, nb_classes).
         :type y: `np.ndarray`
         :param batch_size: Size of batches.
         :type batch_size: `int`
@@ -775,7 +774,7 @@ class TensorFlowV2Classifier(ClassGradientsMixin, ClassifierMixin, TensorFlowV2E
 
         :param x: Training data.
         :type x: `np.ndarray`
-        :param y: Labels, one-vs-rest encoding.
+        :param y: Labels, one-hot-encoded of shape (nb_samples, nb_classes).
         :type y: `np.ndarray`
         :param batch_size: Size of batches.
         :type batch_size: `int`
