c8f09801fcb63c4b2ed7541b1a5a4e306a98aa1e
Ambrish Rawat <ambrish.rawat@ie.ibm.com>
2020-06-02 14:26:39 +0100

    pep8 corrections in examples/adversarial_trainer_FBF
    Signed-off-by: Ambrish Rawat <ambrish.rawat@ie.ibm.com>

--- examples/adversarial_training_FBF.py
+++ examples/adversarial_training_FBF.py
@@ -14,6 +14,7 @@ import torch
 import torch.nn as nn
 import math
 
+
 class PreActBlock(nn.Module):
     '''Pre-activation version of the BasicBlock.'''
     expansion = 1
@@ -25,9 +26,9 @@ class PreActBlock(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
 
-        if stride != 1 or in_planes != self.expansion*planes:
+        if stride != 1 or in_planes != self.expansion * planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False)
             )
 
     def forward(self, x):
@@ -50,11 +51,11 @@ class PreActBottleneck(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn3 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
+        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
 
-        if stride != 1 or in_planes != self.expansion*planes:
+        if stride != 1 or in_planes != self.expansion * planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False)
             )
 
     def forward(self, x):
@@ -77,10 +78,10 @@ class PreActResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.linear = nn.Linear(512 * block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
+        strides = [stride] + [1] * (num_blocks - 1)
         layers = []
         for stride in strides:
             layers.append(block(self.in_planes, planes, stride))
@@ -100,7 +101,8 @@ class PreActResNet(nn.Module):
 
 
 def PreActResNet18():
-    return PreActResNet(PreActBlock, [2,2,2,2])
+    return PreActResNet(PreActBlock, [2, 2, 2, 2])
+
 
 def initialize_weights(module):
     if isinstance(module, nn.Conv2d):
@@ -130,8 +132,8 @@ cifar_std[2, :, :] = 0.2616
 x_train = x_train.transpose(0, 3, 1, 2).astype('float32')
 x_test = x_test.transpose(0, 3, 1, 2).astype('float32')
 
-np.save('x_train.npy',x_train)
-np.save('y_train.npy',y_train)
+np.save('x_train.npy', x_train)
+np.save('y_train.npy', y_train)
 model = PreActResNet18()
 
 # model = PreActResNet18().cuda()
@@ -172,6 +174,4 @@ art_datagen = KerasDataGenerator(
     batch_size=batch_size,
 )
 
-
 trainer.fit_generator(art_datagen, nb_epochs=30)
-
