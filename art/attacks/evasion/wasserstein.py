1bb6b54c7c92c7af64c295492a134c5deba6bd82
Beat Buesser <beat.buesser@ie.ibm.com>
2020-06-10 01:45:22 +0100

    Remove unnecessary redefinition of variable
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/attacks/evasion/wasserstein.py
+++ art/attacks/evasion/wasserstein.py
@@ -492,7 +492,6 @@ class Wasserstein(EvasionAttack):
 
         # Initialize
         alpha = np.log(np.ones(x.shape) / m)
-        exp_alpha = np.exp(-alpha)
 
         beta = np.log(np.ones(x.shape) / m)
         exp_beta = np.exp(-beta)
