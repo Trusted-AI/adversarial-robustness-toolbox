2471cc4fd10790f14a303023f6b0d8f08900ab7f
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-06-30 17:42:36 +0100

    test texture
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- tests/attacks/test_shapeshifter.py
+++ tests/attacks/test_shapeshifter.py
@@ -51,7 +51,11 @@ class TestShapeShifter(TestBase):
         cls.x_test_mnist = cls.x_test_mnist[0: cls.n_test]
         cls.y_test_mnist = cls.y_test_mnist[0: cls.n_test]
 
-    def test_image_as_input(self):
+    def est_image_as_input(self):
+        self._test_image_as_input(True)
+        self._test_image_as_input(False)
+
+    def _test_image_as_input(self, sign_gradients):
         # We must start a new graph
         tf.reset_default_graph()
 
@@ -99,7 +103,7 @@ class TestShapeShifter(TestBase):
             optimizer='RMSPropOptimizer',
             momentum=0.01,
             decay=0.01,
-            sign_gradients=True,
+            sign_gradients=sign_gradients,
             random_size=2,
             max_iter=2,
             texture_as_input=False,
@@ -107,11 +111,119 @@ class TestShapeShifter(TestBase):
             soft_clip=True
         )
 
-        # Attack
+        # Targeted attack
         adv_x = attack.generate(x=self.x_test_mnist[:1].astype(np.float32), label=y, target_class=2, victim_class=5)
 
-        print(adv_x)
+        self.assertTrue(adv_x.shape == (1, 28, 28, 1))
+        self.assertTrue((adv_x >= 0).all())
+        self.assertTrue((adv_x <= 1).all())
 
+        # Untargeted attack
+        adv_x = attack.generate(x=self.x_test_mnist[:1].astype(np.float32), label=y, victim_class=8)
+
+        self.assertTrue(adv_x.shape == (1, 28, 28, 1))
+        self.assertTrue((adv_x >= 0).all())
+        self.assertTrue((adv_x <= 1).all())
+
+    def test_texture_as_input(self):
+        self._test_texture_as_input(True, True, True)
+        self._test_texture_as_input(True, True, False)
+        self._test_texture_as_input(True, False, True)
+        self._test_texture_as_input(True, False, False)
+        self._test_texture_as_input(False, True, True)
+        self._test_texture_as_input(False, True, False)
+        self._test_texture_as_input(False, False, True)
+        self._test_texture_as_input(False, False, False)
+
+    def _test_texture_as_input(self, sign_gradients, use_spectral, soft_clip):
+        # We must start a new graph
+        tf.reset_default_graph()
+
+        # Only import if object detection module is available
+        from art.estimators.object_detection.tensorflow_faster_rcnn import TensorFlowFasterRCNN
+
+        # Define object detector
+        images = tf.Variable(initial_value=np.zeros([1, 28, 28, 1]), dtype=tf.float32)
+        obj_dec = TensorFlowFasterRCNN(images=images)
+
+        # Create labels
+        result = obj_dec.predict(self.x_test_mnist[:1].astype(np.float32))
+
+        groundtruth_boxes_list = [result['detection_boxes'][i] for i in range(1)]
+        groundtruth_classes_list = [result['detection_classes'][i] for i in range(1)]
+        groundtruth_weights_list = [np.ones_like(r) for r in groundtruth_classes_list]
+
+        y = {}
+        y['groundtruth_boxes_list'] = groundtruth_boxes_list
+        y['groundtruth_classes_list'] = groundtruth_classes_list
+        y['groundtruth_weights_list'] = groundtruth_weights_list
+
+        # Define random transform
+        def random_transform(x):
+            return
+
+        # Define attack
+        attack = ShapeShifter(
+            estimator=obj_dec,
+            random_transform=random_transform,
+            box_classifier_weight=1.0,
+            box_localizer_weight=1.0,
+            rpn_classifier_weight=1.0,
+            rpn_localizer_weight=1.0,
+            box_iou_threshold=0.3,
+            box_victim_weight=1.0,
+            box_target_weight=1.0,
+            box_victim_cw_weight=1.0,
+            box_victim_cw_confidence=1.0,
+            box_target_cw_weight=1.0,
+            box_target_cw_confidence=1.0,
+            rpn_iou_threshold=0.3,
+            rpn_background_weight=1.0,
+            rpn_foreground_weight=1.0,
+            rpn_cw_weight=1.0,
+            rpn_cw_confidence=1.0,
+            similarity_weight=1.0,
+            learning_rate=0.1,
+            optimizer='RMSPropOptimizer',
+            momentum=0.01,
+            decay=0.01,
+            sign_gradients=sign_gradients,
+            random_size=2,
+            max_iter=2,
+            texture_as_input=True,
+            use_spectral=use_spectral,
+            soft_clip=soft_clip
+        )
+
+        # Define rendering function
+        def rendering_function():
+            return
+
+        # Targeted attack
+        adv_x = attack.generate(
+            x=self.x_test_mnist[:1].astype(np.float32),
+            label=y,
+            target_class=2,
+            victim_class=5,
+            rendering_function=rendering_function
+        )
+
+        self.assertTrue(adv_x.shape == (1, 28, 28, 1))
+        self.assertTrue((adv_x >= 0).all())
+        self.assertTrue((adv_x <= 1).all())
+
+        # Untargeted attack
+        adv_x = attack.generate(
+            x=self.x_test_mnist[:1].astype(np.float32),
+            label=y,
+            target_class=8,
+            victim_class=8,
+            rendering_function=rendering_function
+        )
+
+        self.assertTrue(adv_x.shape == (1, 28, 28, 1))
+        self.assertTrue((adv_x >= 0).all())
+        self.assertTrue((adv_x <= 1).all())
 
 
 if __name__ == "__main__":
