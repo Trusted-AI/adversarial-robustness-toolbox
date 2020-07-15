eaf8beca29bf047cd98b44e10a8fe15c31805b84
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-07-15 14:58:25 +0100

    add to __init__
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- art/estimators/object_detection/__init__.py
+++ art/estimators/object_detection/__init__.py
@@ -4,3 +4,4 @@ Module containing estimators for object detection.
 from art.estimators.object_detection.object_detector import ObjectDetectorMixin
 
 from art.estimators.object_detection.PyTorchFasterRCNN import PyTorchFasterRCNN
+from art.estimators.object_detection.tensorflow_faster_rcnn import TensorFlowFasterRCNN
