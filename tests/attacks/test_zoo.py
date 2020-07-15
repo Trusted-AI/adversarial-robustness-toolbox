cf87b3fa5cefab97801a2ff171dfce595aafd9b6
minhitbk <minhitbk@gmail.com>
2020-07-15 14:07:09 +0100

    Update tests/attacks/test_zoo.py
    Co-authored-by: Beat Buesser <49047826+beat-buesser@users.noreply.github.com>

--- tests/attacks/test_zoo.py
+++ tests/attacks/test_zoo.py
@@ -47,10 +47,10 @@ class TestZooAttack(TestBase):
 
         cls.n_train = 1
         cls.n_test = 1
-        cls.x_train_mnist = cls.x_train_mnist[0: cls.n_train]
-        cls.y_train_mnist = cls.y_train_mnist[0: cls.n_train]
-        cls.x_test_mnist = cls.x_test_mnist[0: cls.n_test]
-        cls.y_test_mnist = cls.y_test_mnist[0: cls.n_test]
+        cls.x_train_mnist = cls.x_train_mnist[0 : cls.n_train]
+        cls.y_train_mnist = cls.y_train_mnist[0 : cls.n_train]
+        cls.x_test_mnist = cls.x_test_mnist[0 : cls.n_test]
+        cls.y_test_mnist = cls.y_test_mnist[0 : cls.n_test]
 
     def test_tensorflow_failure_attack(self):
         """
