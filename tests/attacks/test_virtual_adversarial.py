58c94b7760a8224886718f71a6a73a8e08e3c5f0
minhitbk <minhitbk@gmail.com>
2020-07-15 13:59:56 +0100

    Update tests/attacks/test_virtual_adversarial.py
    Co-authored-by: Beat Buesser <49047826+beat-buesser@users.noreply.github.com>

--- tests/attacks/test_virtual_adversarial.py
+++ tests/attacks/test_virtual_adversarial.py
@@ -42,10 +42,10 @@ class TestVirtualAdversarial(TestBase):
 
         cls.n_train = 100
         cls.n_test = 10
-        cls.x_train_mnist = cls.x_train_mnist[0: cls.n_train]
-        cls.y_train_mnist = cls.y_train_mnist[0: cls.n_train]
-        cls.x_test_mnist = cls.x_test_mnist[0: cls.n_test]
-        cls.y_test_mnist = cls.y_test_mnist[0: cls.n_test]
+        cls.x_train_mnist = cls.x_train_mnist[0 : cls.n_train]
+        cls.y_train_mnist = cls.y_train_mnist[0 : cls.n_train]
+        cls.x_test_mnist = cls.x_test_mnist[0 : cls.n_test]
+        cls.y_test_mnist = cls.y_test_mnist[0 : cls.n_test]
 
     def test_keras_mnist(self):
         classifier = get_image_classifier_kr()
