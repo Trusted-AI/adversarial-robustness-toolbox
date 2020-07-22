1fab8e7914237e85398a68f1ebfab91ff9d5d332
abigailt <abigailt@il.ibm.com>
2020-07-22 09:40:12 +0300

    Initial code for membership inference attacks.
    Signed-off-by: abigailt <abigailt@il.ibm.com>

---
+++ art/attacks/inference/membership_inference.py
@@ -0,0 +1,333 @@
+# MIT License
+#
+# Copyright (C) The Adversarial Robustness Toolbox (ART) Authors 2020
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+This module implements membership inference attacks.
+
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+from typing import Optional
+
+import numpy as np
+from scipy.special import softmax
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+
+from art.attacks import InferenceAttack
+from art.estimators.estimator import BaseEstimator, NeuralNetworkMixin
+from art.estimators.classification.classifier import ClassifierMixin, Classifier
+from art.utils import check_and_transform_label_format
+from art.estimators.classification.pytorch import PyTorchClassifier
+
+logger = logging.getLogger(__name__)
+
+use_cuda = torch.cuda.is_available()
+def to_cuda(x):
+    if use_cuda:
+        x = x.cuda()
+    return x
+
+
+class AttackDataset(Dataset):
+    def __init__(self, x1, x2, y=None):
+        self.x1 = torch.from_numpy(x1.astype(np.float64)).type(torch.FloatTensor)
+
+        self.x2 = torch.from_numpy(x2.astype(np.int32)).type(torch.FloatTensor)
+        # self.x2 = torch.FloatTensor(int(x2_t.size(0)), max(x2) + 1).zero_(). \
+        #     scatter_(1, x2_t.view([-1, 1]).data, 1).type(torch.FloatTensor)
+
+        if y is not None:
+            self.y = torch.from_numpy(y.astype(np.int8)).type(torch.FloatTensor)
+        else:
+            self.y = torch.zeros(x1.shape[0])
+
+    def __len__(self):
+        return len(self.x1)
+
+    def __getitem__(self, idx):
+        if idx >= len(self.x1):
+            raise IndexError("Invalid Index")
+
+        return self.x1[idx], self.x2[idx], self.y[idx]
+
+
+class MembershipInferenceAttackModel(nn.Module):
+
+    def __init__(self, num_classes, num_features=None):
+
+        self.num_classes = num_classes
+        if num_features:
+            self.num_features = num_features
+        else:
+            self.num_features = num_classes
+
+        super(MembershipInferenceAttackModel, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Linear(self.num_features, 512),
+            nn.ReLU(),
+            nn.Linear(512, 100),
+            nn.ReLU(),
+            nn.Linear(100, 64),
+            nn.ReLU(),
+        )
+
+        self.labels = nn.Sequential(
+            nn.Linear(self.num_classes, 256),
+            nn.ReLU(),
+            nn.Linear(256, 64),
+            nn.ReLU(),
+        )
+
+        self.combine = nn.Sequential(
+            nn.Linear(64 * 2, 1),
+        )
+
+        self.output = nn.Sigmoid()
+
+    def forward(self, x1, l):
+        out_x1 = self.features(x1)
+        out_l = self.labels(l)
+        is_member = self.combine(torch.cat((out_x1, out_l), 1))
+        return self.output(is_member)
+
+
+class MembershipInferenceBlackBoxRuleBased(InferenceAttack):
+    """
+        Implementation of a simple, rule-based black-box membership inference attack.
+
+        This implementation uses the simple rule: if the model's prediction for a record is correct, then it is a
+        member. Otherwise, it is not a member.
+    """
+    _estimator_requirements = [BaseEstimator]
+
+    def __init__(self, classifier: Classifier):
+        """
+        Create a MembershipInferenceBlackBoxRuleBased attack instance.
+
+        :param classifier: Target classifier.
+        """
+        super(MembershipInferenceBlackBoxRuleBased, self).__init__(estimator=classifier)
+
+
+    def infer(self, x: np.ndarray, y: np.ndarray, **kwargs) -> np.ndarray:
+        """
+        Infer membership in the training set of the target estimator.
+
+        :param x: Input records to attack.
+        :param y: True labels for `x`.
+        :return: An array holding the inferred membership status, 1 indicates a member and 0 indicates non-member.
+        """
+        if y.shape[0] != x.shape[0]:
+            raise ValueError("Number of rows in x and y do not match")
+        if self.estimator.input_shape[0] != x.shape[1]:
+            raise ValueError("Shape of x does not match input_shape of classifier")
+        # get model's predictions for x
+        predictions = np.array([np.argmax(arr) for arr in self.estimator.predict(x)]).reshape(-1, 1)
+        y = check_and_transform_label_format(y, len(np.unique(y)), return_one_hot=True)
+        y = np.array([np.argmax(arr) for arr in y]).reshape(-1, 1)
+        return [1 if p == y[index] else 0 for index, p in enumerate(predictions)]
+
+
+class MembershipInferenceBlackBox(InferenceAttack):
+    """
+        Implementation of a learned black-box membership inference attack.
+
+        This implementation can use as input to the learning process probabilities, logits, predictions, losses or
+        activations, depending on the type of model and provided configuration.
+    """
+    _estimator_requirements = [BaseEstimator]
+
+    def __init__(self, classifier: Classifier, attack_model: Optional[Classifier] = None):
+        """
+        Create a MembershipInferenceBlackBox attack instance.
+
+        :param classifier: Target classifier.
+        :param attack_model: The attack model to train, optional. If none is provided, a default model will be created.
+        """
+        super(MembershipInferenceBlackBox, self).__init__(estimator=classifier)
+
+        #TODO: should we provide as default RF or GradientBoosting?
+        if attack_model:
+            if ClassifierMixin not in type(attack_model).__mro__:
+                raise ValueError("Attack model must be of type Classifier.")
+            self.attack_model = attack_model
+            self.default_model = False
+        else:
+            self.attack_model = MembershipInferenceAttackModel(classifier.nb_classes)
+            self.epochs = 100
+            self.bs = 100
+            # self.attack_model = PyTorchClassifier(
+            #         model=model, loss=loss_fn, optimizer=optimizer,
+            #         input_shape=(num_classes, num_classes,), nb_classes=2
+            # )
+            self.default_model = True
+
+    def fit(self, x: np.ndarray, y: np.ndarray, test_x: np.ndarray, test_y: np.ndarray, **kwargs) -> np.ndarray:
+        """
+        Infer membership in the training set of the target estimator.
+
+        :param x: Records that were used in training the target model.
+        :param y: True labels for `x`.
+        :param test_x: Records that were not used in training the target model.
+        :param test_y: True labels for `test_x`.
+        :param input_type: the type of input to train the attack on. Can be one of: 'probability' or 'loss'.
+                            Default is probability.
+        :return: An array holding the inferred membership status, 1 indicates a member and 0 indicates non-member.
+        """
+        if "input_type" not in kwargs.keys():
+            self.input_type = 'probability'
+        else:
+            self.input_type = kwargs.get('input_type')
+        if y.shape[0] != x.shape[0]:
+            raise ValueError("Number of rows in x and y do not match")
+        if self.estimator.input_shape[0] != x.shape[1]:
+            raise ValueError("Shape of x does not match input_shape of classifier")
+        if test_y.shape[0] != test_x.shape[0]:
+            raise ValueError("Number of rows in test_x and test_y do not match")
+        if self.estimator.input_shape[0] != test_x.shape[1]:
+            raise ValueError("Shape of test_x does not match input_shape of classifier")
+
+        # Create attack dataset
+        # uses final predictions for models with no activations, softmax for models with activations
+        if self.input_type == 'probability':
+            # members
+            features = self.estimator.predict(x).astype(np.float32)
+            # non-members
+            test_features = self.estimator.predict(test_x).astype(np.float32)
+            # if NeuralNetworkMixin not in type(self.estimator).__mro__:
+            #     # members
+            #     features = self.estimator.predict(x)
+            #     # non-members
+            #     test_features = self.estimator.predict(test_x)
+            # else:
+            #     # members
+            #     activations = self.estimator.get_activations(x, self.estimator.layer_names[-1], batch_size=self.bs)
+            #     features = softmax(activations)
+            #     # non-members
+            #     test_activations = self.estimator.get_activations(test_x, self.estimator.layer_names[-1],
+            #                                                       batch_size=self.bs)
+            #     test_features = softmax(test_activations)
+        # only for models with activations
+        # elif self.input_type == 'logit':
+        #     if NeuralNetworkMixin not in type(self.estimator).__mro__:
+        #         raise ValueError("Can only use `input_type` logit with Neural Network model.")
+        #     else:
+        #         # members
+        #         features = self.estimator.get_activations(x, self.estimator.layer_names[-1], batch_size=self.bs)
+        #         # non-members
+        #         test_features = self.estimator.get_activations(test_x, self.estimator.layer_names[-1],
+        #                                                           batch_size=self.bs)
+        # only for models with loss
+        elif self.input_type == 'loss':
+            raise ValueError("`input_type` loss not yet implemented.")
+        else:
+            raise ValueError("Illegal value for parameter `input_type`.")
+
+        # members
+        labels = np.ones(x.shape[0])
+        # non-members
+        test_labels = np.zeros(test_x.shape[0])
+
+        y = check_and_transform_label_format(y, len(np.unique(y)), return_one_hot=True)
+        test_y = check_and_transform_label_format(test_y, len(np.unique(y)), return_one_hot=True)
+
+        x1 = np.concatenate((features, test_features))
+        x2 = np.concatenate((y, test_y))
+        y_new = np.concatenate((labels, test_labels))
+
+        if self.default_model:
+            loss_fn = nn.BCELoss()
+            optimizer = optim.Adam(self.attack_model.parameters(), lr=0.0001)
+
+            attack_train_set = AttackDataset(x1=x1, x2=x2, y=y_new)
+            train_loader = DataLoader(attack_train_set, batch_size=self.bs, shuffle=True, num_workers=0)
+
+            self.attack_model = to_cuda(self.attack_model)
+            self.attack_model.train()
+
+            for epoch in range(self.epochs):
+                for (input1, input2, targets) in train_loader:
+                    input1, input2, targets = to_cuda(input1), to_cuda(input2), to_cuda(targets)
+                    inputs1, input2 = torch.autograd.Variable(input1), torch.autograd.Variable(input2)
+                    targets = torch.autograd.Variable(targets)
+
+                    optimizer.zero_grad()
+                    outputs = self.attack_model(input1, input2)
+                    loss = loss_fn(outputs, targets.unsqueeze(1))
+
+                    loss.backward()
+                    optimizer.step()
+        else:
+            y_ready = check_and_transform_label_format(y_new, len(np.unique(y_new)), return_one_hot=True)
+            self.attack_model.fit(np.c_[x1, x2], y_ready)
+
+    def infer(self, x: np.ndarray, y: np.ndarray, **kwargs) -> np.ndarray:
+        """
+        Infer membership in the training set of the target estimator.
+
+        :param x: Input records to attack.
+        :param y: True labels for `x`.
+        :return: An array holding the inferred membership status, 1 indicates a member and 0 indicates non-member.
+        """
+        if y.shape[0] != x.shape[0]:
+            raise ValueError("Number of rows in x and y do not match")
+        if self.estimator.input_shape[0] != x.shape[1]:
+            raise ValueError("Shape of x does not match input_shape of classifier")
+
+        if self.input_type == 'probability':
+            features = self.estimator.predict(x).astype(np.float32)
+            # if NeuralNetworkMixin not in type(self.estimator).__mro__:
+            #     features = self.estimator.predict(x)
+            # else:
+            #     activations = self.estimator.get_activations(x, self.estimator.layer_names[-1], batch_size=self.bs)
+            #     features = softmax(activations)
+        # elif self.input_type == 'logit':
+        #     if NeuralNetworkMixin not in type(self.estimator).__mro__:
+        #         raise ValueError("Can only use `input_type` logit with Neural Network model.")
+        #     else:
+        #         features = self.estimator.get_activations(x, self.estimator.layer_names[-1], batch_size=self.bs)
+        elif self.input_type == 'loss':
+            raise ValueError("`input_type` loss not yet implemented.")
+
+        if self.default_model:
+            self.attack_model.eval()
+            inferred = None
+            test_set = AttackDataset(x1=features, x2=y)
+            test_loader = DataLoader(test_set, batch_size=self.bs, shuffle=True, num_workers=0)
+            for input1, input2, targets in test_loader:
+                outputs = self.attack_model(input1, input2)
+                predicted = torch.round(outputs)
+                if inferred is None:
+                    inferred = predicted.detach().numpy()
+                else:
+                    inferred = np.vstack((inferred, predicted.detach().numpy()))
+        else:
+            inferred = np.array([np.argmax(arr) for arr in self.attack_model.predict(np.c_[features, y])])
+        return inferred
+
+
+class MembershipInferenceWhiteBoxBox(InferenceAttack):
+    #TODO: Not yet implemented. Use activations of inner layers and/or gradients
+    pass
---
+++ tests/attacks/inference/test_membership_inference.py
@@ -0,0 +1,233 @@
+# MIT License
+#
+# Copyright (C) The Adversarial Robustness Toolbox (ART) Authors 2020
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+import pytest
+
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+
+from art.attacks.inference import (
+    MembershipInferenceBlackBoxRuleBased,
+    MembershipInferenceBlackBox,
+)
+from art.estimators.classification.pytorch import PyTorchClassifier
+from art.estimators.estimator import BaseEstimator
+
+from tests.attacks.utils import backend_test_classifier_type_check_fail
+
+
+logger = logging.getLogger(__name__)
+attack_train_ratio = 0.5
+num_classes_iris = 3
+num_classes_mnist = 10
+
+
+@pytest.fixture()
+def fix_get_mnist_subset(get_mnist_dataset, get_iris_dataset):
+    (x_train_mnist, y_train_mnist), (x_test_mnist, y_test_mnist) = get_mnist_dataset
+    n_train = 1000
+    n_test = 200
+    yield x_train_mnist[:n_train], y_train_mnist[:n_train], x_test_mnist[:n_test], y_test_mnist[:n_test]
+
+
+def test_rule_based_image(fix_get_mnist_subset, get_image_classifier_list_for_attack):
+    classifier_list = get_image_classifier_list_for_attack(MembershipInferenceBlackBoxRuleBased)
+    if not classifier_list:
+        logging.warning("Couldn't perform this test because no classifier is defined")
+        return
+
+    x_train, y_train, x_test, y_test = fix_get_mnist_subset
+
+    for classifier in classifier_list:
+        # print(type(classifier).__name__)
+        attack = MembershipInferenceBlackBoxRuleBased(classifier)
+        # infer attacked feature
+        inferred_train = attack.infer(x_train, y_train)
+        inferred_test = attack.infer(x_test, y_test)
+        # check accuracy
+        # print(inferred_train)
+        # print(inferred_test)
+        train_pos = sum(inferred_train) / len(inferred_train)
+        test_pos = sum(inferred_test) / len(inferred_test)
+        assert (train_pos > test_pos or
+                train_pos == pytest.approx(test_pos, abs=0.03) or
+                test_pos == 1)
+
+
+def test_rule_based_tabular(get_iris_dataset, get_tabular_classifier_list):
+    classifier_list = get_tabular_classifier_list(MembershipInferenceBlackBoxRuleBased)
+    if not classifier_list:
+        logging.warning("Couldn't perform this test because no classifier is defined")
+        return
+
+    (x_train, y_train), (x_test, y_test) = get_iris_dataset
+
+    for classifier in classifier_list:
+        # print(type(classifier).__name__)
+        attack = MembershipInferenceBlackBoxRuleBased(classifier)
+        # infer attacked feature
+        inferred_train = attack.infer(x_train, y_train)
+        inferred_test = attack.infer(x_test, y_test)
+        # check accuracy
+        train_pos = sum(inferred_train) / len(inferred_train)
+        test_pos = sum(inferred_test) / len(inferred_test)
+        assert (train_pos > test_pos or
+                train_pos == pytest.approx(test_pos, abs=0.03) or
+                test_pos == 1)
+
+
+def test_black_box_image(fix_get_mnist_subset, get_image_classifier_list_for_attack):
+    classifier_list = get_image_classifier_list_for_attack(MembershipInferenceBlackBox)
+    if not classifier_list:
+        logging.warning("Couldn't perform this test because no classifier is defined")
+        return
+
+    x_train, y_train, x_test, y_test = fix_get_mnist_subset
+    attack_train_size = int(len(x_train) * attack_train_ratio)
+    attack_test_size = int(len(x_test) * attack_train_ratio)
+
+    for classifier in classifier_list:
+        attack = MembershipInferenceBlackBox(classifier)
+        # train attack model using only attack_train_ratio of data
+        attack.fit(x_train[:attack_train_size], y_train[:attack_train_size],
+                   x_test[:attack_test_size], y_test[:attack_test_size])
+        # infer attacked feature on remainder of data
+        inferred_train = attack.infer(x_train[attack_train_size:], y_train[attack_train_size:])
+        inferred_test = attack.infer(x_test[attack_test_size:], y_test[attack_test_size:])
+        # check accuracy
+        train_pos = sum(inferred_train) / len(inferred_train)
+        test_pos = sum(inferred_test) / len(inferred_test)
+        assert (train_pos > test_pos or
+                train_pos == pytest.approx(test_pos, abs=0.03) or
+                test_pos == 1)
+
+
+def test_black_box_tabular(get_tabular_classifier_list, get_iris_dataset):
+    classifier_list = get_tabular_classifier_list(MembershipInferenceBlackBox)
+    if not classifier_list:
+        logging.warning("Couldn't perform this test because no classifier is defined")
+        return
+
+    (x_train, y_train), (x_test, y_test) = get_iris_dataset
+    attack_train_size = int(len(x_train) * attack_train_ratio)
+    attack_test_size = int(len(x_test) * attack_train_ratio)
+
+    for classifier in classifier_list:
+        attack = MembershipInferenceBlackBox(classifier)
+        # train attack model using only attack_train_ratio of data
+        attack.fit(x_train[:attack_train_size], y_train[:attack_train_size],
+                   x_test[:attack_test_size], y_test[:attack_test_size])
+        # infer attacked feature on remainder of data
+        inferred_train = attack.infer(x_train[attack_train_size:], y_train[attack_train_size:])
+        inferred_test = attack.infer(x_test[attack_test_size:], y_test[attack_test_size:])
+        # check accuracy
+        train_pos = sum(inferred_train) / len(inferred_train)
+        test_pos = sum(inferred_test) / len(inferred_test)
+        assert (train_pos > test_pos or
+                train_pos == pytest.approx(test_pos, abs=0.03) or
+                test_pos == 1)
+
+
+def test_black_box_loss(get_tabular_classifier_list, get_iris_dataset):
+    classifier_list = get_tabular_classifier_list(MembershipInferenceBlackBox)
+    if not classifier_list:
+        logging.warning("Couldn't perform this test because no classifier is defined")
+        return
+
+    (x_train, y_train), (x_test, y_test) = get_iris_dataset
+    attack_train_size = int(len(x_train) * attack_train_ratio)
+    attack_test_size = int(len(x_test) * attack_train_ratio)
+
+    for classifier in classifier_list:
+        attack = MembershipInferenceBlackBox(classifier)
+        # train attack model using only attack_train_ratio of data
+        with pytest.raises(ValueError):
+            attack.fit(x_train[:attack_train_size], y_train[:attack_train_size],
+                       x_test[:attack_test_size], y_test[:attack_test_size],
+                       input_type = 'loss')
+        # # infer attacked feature on remainder of data
+        # inferred_train = attack.infer(x_train[attack_train_size:], y_train[attack_train_size:])
+        # inferred_test = attack.infer(x_test[attack_test_size:], y_test[attack_test_size:])
+        # # check accuracy
+        # print(inferred_train)
+        # print(inferred_test)
+        # train_pos = sum(inferred_train) / len(inferred_train)
+        # test_pos = sum(inferred_test) / len(inferred_test)
+        # assert (train_pos > test_pos or
+        #         train_pos == pytest.approx(test_pos, abs=0.03) or
+        #         test_pos == 1)
+
+
+class AttackModel(nn.Module):
+    def __init__(self, num_features):
+        super(AttackModel, self).__init__()
+        self.layer = nn.Linear(num_features, 1)
+        self.output = nn.Sigmoid()
+
+    def forward(self, x):
+        return self.output(self.layer(x))
+
+
+@pytest.mark.skipMlFramework("scikitlearn")
+def test_black_box_with_model(get_tabular_classifier_list, get_iris_dataset):
+    classifier_list = get_tabular_classifier_list(MembershipInferenceBlackBox)
+    if not classifier_list:
+        logging.warning("Couldn't perform this test because no classifier is defined")
+        return
+
+    (x_train, y_train), (x_test, y_test) = get_iris_dataset
+    attack_train_size = int(len(x_train) * attack_train_ratio)
+    attack_test_size = int(len(x_test) * attack_train_ratio)
+
+    model = AttackModel(2*num_classes_iris)
+
+    # Define a loss function and optimizer
+    loss_fn = nn.BCELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.0001)
+    attack_model = PyTorchClassifier(
+        model=model, loss=loss_fn, optimizer=optimizer, input_shape=(2*num_classes_iris,), nb_classes=1
+    )
+
+    for classifier in classifier_list:
+        print(type(classifier).__name__)
+        attack = MembershipInferenceBlackBox(classifier, attack_model=attack_model)
+        # train attack model using only attack_train_ratio of data
+        attack.fit(x_train[:attack_train_size], y_train[:attack_train_size],
+                   x_test[:attack_test_size], y_test[:attack_test_size])
+        # infer attacked feature on remainder of data
+        inferred_train = attack.infer(x_train[attack_train_size:], y_train[attack_train_size:])
+        inferred_test = attack.infer(x_test[attack_test_size:], y_test[attack_test_size:])
+        # check accuracy
+        train_pos = sum(inferred_train) / len(inferred_train)
+        test_pos = sum(inferred_test) / len(inferred_test)
+        assert (train_pos > test_pos or
+                train_pos == pytest.approx(test_pos, abs=0.03) or
+                test_pos == 1)
+
+
+
+def test_classifier_type_check_fail():
+    backend_test_classifier_type_check_fail(MembershipInferenceBlackBoxRuleBased, [BaseEstimator])
+    backend_test_classifier_type_check_fail(MembershipInferenceBlackBox, [BaseEstimator])
+
+
+if __name__ == "__main__":
+    pytest.cmdline.main("-q -s {} --mlFramework=tensorflow --durations=0".format(__file__).split(" "))
