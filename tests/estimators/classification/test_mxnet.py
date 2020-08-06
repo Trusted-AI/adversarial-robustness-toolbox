86005958695007d846a24ac4c5bb7f5066e55348
killian <killian.levacher@ibm.com>
2020-08-06 11:47:17 +0100

    added the last test to kerastf
    Signed-off-by: killian <killian.levacher@ibm.com>

--- tests/estimators/classification/test_mxnet.py
+++ tests/estimators/classification/test_mxnet.py
@@ -35,37 +35,18 @@ def test_set_learning(get_image_classifier_list):
     assert hasattr(classifier, "_learning_phase")
 
 
-# def test_save(self):
-#     classifier = self.classifier
-#     t_file = tempfile.NamedTemporaryFile()
-#     full_path = t_file.name
-#     t_file.close()
-#     base_name = os.path.basename(full_path)
-#     dir_name = os.path.dirname(full_path)
-#
-#     classifier.save(base_name, path=dir_name)
-#     self.assertTrue(os.path.exists(full_path + ".params"))
-#     os.remove(full_path + ".params")
+@pytest.mark.only_with_platform("mxnet")
+def test_save(get_image_classifier_list, get_default_mnist_subset, tmp_path):
+    classifier, _ = get_image_classifier_list(one_classifier=True)
 
-# @pytest.mark.only_with_platform("mxnet")
-# def test_preprocessing(get_default_mnist_subset, get_image_classifier_list):
-#     (_, _), (x_test_mnist, y_test_mnist) = get_default_mnist_subset
-#
-#     classifier, _ = get_image_classifier_list(one_classifier=True)
-#     classifier_preproc, _ = get_image_classifier_list(one_classifier=True, preprocessing=(0, 1))
-#
-#     # Create classifier
-#     # loss = gluon.loss.SoftmaxCrossEntropyLoss()
-#     # classifier_preproc = MXClassifier(
-#     #     model=self.classifier._model,
-#     #     loss=loss,
-#     #     clip_values=(0, 1),
-#     #     input_shape=(1, 28, 28),
-#     #     nb_classes=10,
-#     #     optimizer=self.classifier._optimizer,
-#     #     preprocessing=(1, 2),
-#     # )
-#
-#     preds = classifier.predict((x_test_mnist - 1.0) / 2)
-#     preds_preproc = classifier_preproc.predict(x_test_mnist)
-#     np.sum(preds - preds_preproc) == 0
+    (x_train_mnist, y_train_mnist), (_, _) = get_default_mnist_subset
+    classifier.fit(x_train_mnist, y_train_mnist, batch_size=128, nb_epochs=2)
+    full_path = tmp_path / "sub"
+    full_path.mkdir()
+
+    base_name = os.path.basename(full_path)
+    dir_name = os.path.dirname(full_path)
+
+    assert os.path.exists(full_path._str + ".params") is False
+    classifier.save(base_name, path=dir_name)
+    assert os.path.exists(full_path._str + ".params")
