d6e71212805e230a8d6c289c806914ee24be0a07
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-08-26 17:42:28 +0100

    add init
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

---
+++ art/estimators/speed_recognition/__init__.py
@@ -0,0 +1,6 @@
+"""
+Module containing estimators for speed recognition.
+"""
+from art.estimators.speed_recognition.speed_recognizer import SpeedRecognizerMixin
+
+from art.estimators.speed_recognition.pytorch_deep_speech import PyTorchDeepSpeech
