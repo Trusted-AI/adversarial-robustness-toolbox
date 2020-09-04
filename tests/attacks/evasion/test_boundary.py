b1a84cad7d0f2d141dd5d210af43e572045a0663
killian <killian.levacher@ibm.com>
2020-09-04 16:39:38 +0100

    fixed testbaoundary
    Signed-off-by: killian <killian.levacher@ibm.com>

--- tests/attacks/evasion/test_boundary.py
+++ tests/attacks/evasion/test_boundary.py
@@ -25,6 +25,7 @@ from art.estimators.classification.classifier import ClassifierMixin
 
 from tests.attacks.utils import backend_targeted_tabular, backend_untargeted_tabular, backend_targeted_images
 from tests.attacks.utils import back_end_untargeted_images, backend_test_classifier_type_check_fail
+from tests.utils import add_warning, ARTTestException
 
 logger = logging.getLogger(__name__)
 
@@ -37,40 +38,40 @@ def fix_get_mnist_subset(get_mnist_dataset):
     yield x_train_mnist[:n_train], y_train_mnist[:n_train], x_test_mnist[:n_test], y_test_mnist[:n_test]
 
 
+@pytest.mark.framework_agnostic
 @pytest.mark.parametrize("clipped_classifier, targeted", [(True, True), (True, False), (False, True), (False, False)])
 def test_tabular(tabular_dl_estimator, framework, get_iris_dataset, clipped_classifier, targeted):
-    classifier_list = tabular_dl_estimator(BoundaryAttack, clipped=clipped_classifier)
-    if classifier_list is None:
-        logging.warning("Couldn't perform  this test because no classifier is defined")
-        return
-
-    for classifier in classifier_list:
-
+    try:
+        classifier = tabular_dl_estimator(BoundaryAttack, clipped=clipped_classifier)
         attack = BoundaryAttack(classifier, targeted=targeted, max_iter=10)
         if targeted:
             backend_targeted_tabular(attack, get_iris_dataset)
         else:
             backend_untargeted_tabular(attack, get_iris_dataset, clipped=clipped_classifier)
+    except ARTTestException as e:
+        add_warning(e)
 
 
+@pytest.mark.framework_agnostic
 @pytest.mark.parametrize("targeted", [True, False])
 def test_images(fix_get_mnist_subset, image_dl_estimator_for_attack, framework, targeted):
-    classifier_list = image_dl_estimator_for_attack(BoundaryAttack)
-    if classifier_list is None:
-        logging.warning("Couldn't perform  this test because no classifier is defined")
-        return
-
-    for classifier in classifier_list:
-
+    try:
+        classifier = image_dl_estimator_for_attack(BoundaryAttack)
         attack = BoundaryAttack(estimator=classifier, targeted=targeted, max_iter=20)
         if targeted:
             backend_targeted_images(attack, fix_get_mnist_subset)
         else:
             back_end_untargeted_images(attack, fix_get_mnist_subset, framework)
+    except ARTTestException as e:
+        add_warning(e)
 
 
+@pytest.mark.framework_agnostic
 def test_classifier_type_check_fail():
-    backend_test_classifier_type_check_fail(BoundaryAttack, [BaseEstimator, ClassifierMixin])
+    try:
+        backend_test_classifier_type_check_fail(BoundaryAttack, [BaseEstimator, ClassifierMixin])
+    except ARTTestException as e:
+        add_warning(e)
 
 
 if __name__ == "__main__":
