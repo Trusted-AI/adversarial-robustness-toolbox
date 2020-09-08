8ad5c2304e8de5f81c284e6d9a09ebbc5bdc60b5
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-09-08 13:37:08 +0100

    imp init
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- art/attacks/evasion/imperceptible_asr/imperceptible_attack_pytorch.py
+++ art/attacks/evasion/imperceptible_asr/imperceptible_attack_pytorch.py
@@ -69,26 +69,16 @@ class ImperceptibleAttackPytorch(EvasionAttack):
     def __init__(
         self,
         estimator: PyTorchDeepSpeech,
-        eps: float = 0.3,
+        initial_eps: float = 1000,
         max_iter: int = 100,
         batch_size: int = 32,
     ):
         """
-        Create a :class:`.ProjectedGradientDescentPytorch` instance.
+        Create a :class:`.ImperceptibleAttackPytorch` instance.
 
-        :param estimator: An trained estimator.
-        :type estimator: :class:`.BaseEstimator`
-        :param norm: The norm of the adversarial perturbation. Possible values: np.inf, 1 or 2.
-        :param eps: Maximum perturbation that the attacker can introduce.
-        :param eps_step: Attack step size (input variation) at each iteration.
-        :param random_eps: When True, epsilon is drawn randomly from truncated normal distribution. The literature
-                           suggests this for FGSM based training to generalize across different epsilons. eps_step is
-                           modified to preserve the ratio of eps / eps_step. The effectiveness of this method with PGD
-                           is untested (https://arxiv.org/pdf/1611.01236.pdf).
+        :param estimator: A trained estimator.
+        :param initial_eps: Initial maximum perturbation that the attacker can introduce.
         :param max_iter: The maximum number of iterations.
-        :param targeted: Indicates whether the attack is targeted (True) or untargeted (False).
-        :param num_random_init: Number of random initialisations within the epsilon ball. For num_random_init=0 starting
-                                at the original input.
         :param batch_size: Size of the batch on which adversarial samples are generated.
         """
         if (
@@ -103,17 +93,11 @@ class ImperceptibleAttackPytorch(EvasionAttack):
                 "preprocessing defences."
             )
 
-        super(ProjectedGradientDescentPyTorch, self).__init__(
-            estimator=estimator,
-            norm=norm,
-            eps=eps,
-            eps_step=eps_step,
-            max_iter=max_iter,
-            targeted=targeted,
-            num_random_init=num_random_init,
-            batch_size=batch_size,
-            random_eps=random_eps,
-        )
+        super(ImperceptibleAttackPytorch, self).__init__(estimator=estimator)
+
+        self.initial_eps = initial_eps
+        self.max_iter = max_iter
+        self.batch_size = batch_size
 
     def generate(self, x: np.ndarray, y: Optional[np.ndarray] = None, **kwargs) -> np.ndarray:
         """
