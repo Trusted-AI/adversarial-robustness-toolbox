0b8f4c4200c16a63ae2385d9c9197440a94baa73
Ebube Chuba <ebube.chuba@ibm.com>
2020-09-08 09:10:54 -0700

    Update test_verification_decision_trees.py

--- tests/metrics/test_verification_decision_trees.py
+++ tests/metrics/test_verification_decision_trees.py
@@ -117,7 +117,7 @@ class TestMetricsTrees(unittest.TestCase):
             x=self.x_test, y=self.y_test, eps_init=0.3, nb_search_steps=10, max_clique=2, max_level=2
         )
 
-        self.assertEqual(average_bound, 0.009117187499999995)
+        self.assertAlmostEqual(average_bound, 0.009, delta=0.0002)
         self.assertEqual(verified_error, 1.0)
 
     def test_RandomForest(self):
