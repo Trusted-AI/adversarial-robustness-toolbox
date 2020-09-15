f97c6ba408dd1f518b3052e3313c45d892051740
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-09-15 17:20:40 +0100

    implement asr attack
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- art/attacks/evasion/imperceptible_asr/imperceptible_attack_pytorch.py
+++ art/attacks/evasion/imperceptible_asr/imperceptible_attack_pytorch.py
@@ -553,6 +553,122 @@ class ImperceptibleAttackPytorch(EvasionAttack):
 
         return loss
 
+    def _compute_masking_threshold(self, x: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Compute the masking threshold and the maximum psd of the original audio.
+
+        :param x: Samples of shape (seq_length,).
+        :return: A tuple of the masking threshold and the maximum psd.
+        """
+        from scipy.fftpack import fft
+        from scipy.fftpack import ifft
+        import scipy
+
+        import librosa
+
+        # First compute the psd matrix
+        # These parameters are needed for the transformation
+        sample_rate = self.estimator.model.audio_conf.sample_rate
+        window_size = self.estimator.model.audio_conf.window_size
+        window_stride = self.estimator.model.audio_conf.window_stride
+
+        n_fft = int(sample_rate * window_size)
+        hop_length = int(sample_rate * window_stride)
+        win_length = n_fft
+
+        window = self.estimator.model.audio_conf.window.value
+        if window == "hamming":
+            window_fn = scipy.signal.windows.hamming
+        elif window == "hann":
+            window_fn = scipy.signal.windows.hann
+        elif window == "blackman":
+            window_fn = scipy.signal.windows.blackman
+        elif window == "bartlett":
+            window_fn = scipy.signal.windows.bartlett
+        else:
+            raise NotImplementedError("Spectrogram window %s not supported." % window)
+
+        transformed_x = librosa.core.stft(
+            y=x, n_fft=n_fft, hop_length=hop_length, win_length=win_length, window=window_fn, center=False
+        )
+        transformed_x *= np.sqrt(8.0 / 3.)
+
+        psd = abs(transformed_x / win_length)
+        original_max_psd = np.max(psd * psd)
+        psd = 10 * np.log10(psd * psd + 10e-20)
+        psd = 96 - np.max(psd) + psd
+
+        # Compute freqs and barks
+        freqs = librosa.core.fft_frequencies(sample_rate, win_length)
+        barks = 13 * np.arctan(0.00076 * freqs) + 3.5 * np.arctan(pow(freqs / 7500.0, 2))
+
+        # Compute quiet threshold
+        ath = np.zeros(len(barks)) - np.inf
+        bark_idx = np.argmax(barks > 1)
+        ath[bark_idx:] = (3.64 * pow(freqs[bark_idx:] * 0.001, -0.8) -
+                          6.5 * np.exp(-0.6 * pow(0.001 * freqs[bark_idx:] - 3.3, 2)) +
+                          0.001 * pow(0.001 * freqs[bark_idx:], 4) - 12)
+
+        # Compute the global masking threshold theta
+        theta = []
+
+        for i in range(psd.shape[1]):
+            # Compute masker index
+            masker_idx = scipy.signal.argrelextrema(psd[:, i], np.greater)[0]
+
+            if 0 in masker_idx:
+                masker_idx = np.delete(masker_idx, 0)
+
+            if len(psd[:, i]) - 1 in masker_idx:
+                masker_idx = np.delete(masker_idx, len(psd[:, i]) - 1)
+
+            barks_psd = np.zeros([len(masker_idx), 3])
+            barks_psd[:, 0] = barks[masker_idx]
+            barks_psd[:, 1] = 10 * np.log10(
+                pow(10, psd[:, i][masker_idx - 1] / 10.) +
+                pow(10, psd[:, i][masker_idx] / 10.) +
+                pow(10, psd[:, i][masker_idx + 1] / 10.)
+            )
+            barks_psd[:, 2] = masker_idx
+
+            for j in range(len(masker_idx)):
+                if barks_psd.shape[0] <= j + 1:
+                    break
+
+                while barks_psd[j + 1, 0] - barks_psd[j, 0] < 0.5:
+                    quiet_threshold = (3.64 * pow(freqs[int(barks_psd[j, 2])] * 0.001, -0.8) -
+                                       6.5 * np.exp(-0.6 * pow(0.001 * freqs[int(barks_psd[j, 2])] - 3.3, 2)) +
+                                       0.001 * pow(0.001 * freqs[int(barks_psd[j, 2])], 4) - 12)
+                    if barks_psd[j, 1] < quiet_threshold:
+                        barks_psd = np.delete(barks_psd, j, axis=0)
+
+                    if barks_psd.shape[0] == j + 1:
+                        break
+
+                    if barks_psd[j, 1] < barks_psd[j + 1, 1]:
+                        barks_psd = np.delete(barks_psd, j, axis=0)
+                    else:
+                        barks_psd = np.delete(barks_psd, j + 1, axis=0)
+
+                    if barks_psd.shape[0] == j + 1:
+                        break
+
+            # Compute the global masking threshold
+            delta_TM = 1 * (-6.025 - 0.275 * bark_psd[:, 0])
+            Ts = two_slops(bark_psd, delta_TM, barks)
+            Ts = np.array(Ts)
+
+
+            theta_x = np.sum(pow(10, Ts / 10.), axis=0) + pow(10, ATH / 10.)
+
+
+            theta_xs.append(compute_th(PSD[:,i], barks, ATH, freqs))
+        theta_xs = np.array(theta_xs)
+
+        return theta, original_max_psd
+
+
+
     def _psd_transform(
         self,
         delta: "torch.Tensor",
