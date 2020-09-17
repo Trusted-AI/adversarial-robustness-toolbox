9c345ebd4c71ce21f41c23c2bae8c7dda456ac5e
Ebube Chuba <ebube.chuba@ibm.com>
2020-09-17 04:46:25 -0700

    re-add abstain module
    Signed-off-by: Ebube Chuba <ebube.chuba@ibm.com>

---
+++ art/estimators/certification/abstain.py
@@ -0,0 +1,54 @@
+# MIT License
+#
+# Copyright (C) The Adversarial Robustness Toolbox (ART) Authors 2020
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""
+This module implements Randomized Smoothing applied to classifier predictions.
+
+| Paper link: https://arxiv.org/abs/1902.02918
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+
+import numpy as np
+
+from art.estimators.classification.classifier import ClassifierMixin
+
+logger = logging.getLogger(__name__)
+
+
+class AbstainPredictorMixin(ClassifierMixin):
+    """
+    Implementation of Randomized Smoothing applied to classifier predictions and gradients, as introduced
+    in Cohen et al. (2019).
+
+    | Paper link: https://arxiv.org/abs/1902.02918
+    """
+
+    def __init__(self, **kwargs):
+        """
+        Creates a predictor that can abstain from predictions
+
+        """
+        super().__init__(**kwargs)
+
+    def abstain(self) -> np.ndarray:
+        """
+        Abstain from a prediction
+        :return: A numpy array of zeros
+        """
+        return np.zeros(self.nb_classes)
