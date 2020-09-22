7130b00e71319969ad5c6eaa284d157195d89bd0
killian <killian.levacher@ibm.com>
2020-09-22 17:34:34 +0100

    reformatted all preprocessor tests to ART convention
    Signed-off-by: killian <killian.levacher@ibm.com>

--- tests/defences/preprocessor/test_spatial_smoothing_tensorflow.py
+++ tests/defences/preprocessor/test_spatial_smoothing_tensorflow.py
@@ -109,15 +109,14 @@ def test_spatial_smoothing_median_filter_call_expected_behavior(art_warning, is_
 def test_spatial_smoothing_image_data(art_warning, image_batch, channels_first, window_size, is_tf_version_2):
     try:
         if is_tf_version_2:
-            test_input, test_output = image_batch
+            test_input, test_output = video_batch
 
             if channels_first:
                 exc_msg = "Only channels last input data is supported"
                 with pytest.raises(ValueError, match=exc_msg):
-                    _ = SpatialSmoothingTensorFlowV2(channels_first=channels_first, window_size=window_size)
+                    _ = SpatialSmoothingTensorFlowV2(channels_first=channels_first, window_size=2)
             else:
-                spatial_smoothing = SpatialSmoothingTensorFlowV2(channels_first=channels_first,
-                                                                 window_size=window_size)
+                spatial_smoothing = SpatialSmoothingTensorFlowV2(channels_first=channels_first, window_size=2)
                 assert_array_equal(spatial_smoothing(test_input)[0], test_output)
     except ARTTestException as e:
         art_warning(e)
