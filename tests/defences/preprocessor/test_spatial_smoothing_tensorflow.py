2d5ed8ac44328c4958508a7979f01cf4336d5691
killian <killian.levacher@ibm.com>
2020-10-07 10:36:00 +0100

    removed is_tf_version_2 in test
    Signed-off-by: killian <killian.levacher@ibm.com>

--- tests/defences/preprocessor/test_spatial_smoothing_tensorflow.py
+++ tests/defences/preprocessor/test_spatial_smoothing_tensorflow.py
@@ -103,52 +103,49 @@ def test_spatial_smoothing_median_filter_call_expected_behavior(art_warning):
         ),
     ],
 )
-@pytest.mark.only_with_platform("tensorflow")
-def test_spatial_smoothing_image_data(art_warning, image_batch, channels_first, window_size, is_tf_version_2):
+@pytest.mark.only_with_platform("tensorflow2")
+def test_spatial_smoothing_image_data(art_warning, image_batch, channels_first, window_size):
     try:
-        if is_tf_version_2:
-            test_input, test_output = image_batch
+        test_input, test_output = image_batch
 
-            if channels_first:
-                exc_msg = "Only channels last input data is supported"
-                with pytest.raises(ValueError, match=exc_msg):
-                    _ = SpatialSmoothingTensorFlowV2(channels_first=channels_first, window_size=window_size)
-            else:
-                spatial_smoothing = SpatialSmoothingTensorFlowV2(channels_first=channels_first,
-                                                                 window_size=window_size)
-                assert_array_equal(spatial_smoothing(test_input)[0], test_output)
-    except ARTTestException as e:
-        art_warning(e)
-
-
-@pytest.mark.only_with_platform("tensorflow")
-@pytest.mark.parametrize("channels_first", [True, False])
-def test_spatial_smoothing_video_data(art_warning, video_batch, channels_first, is_tf_version_2):
-    try:
-        if is_tf_version_2:
-            test_input, test_output = video_batch
-
-            if channels_first:
-                exc_msg = "Only channels last input data is supported"
-                with pytest.raises(ValueError, match=exc_msg):
-                    _ = SpatialSmoothingTensorFlowV2(channels_first=channels_first, window_size=2)
-            else:
-                spatial_smoothing = SpatialSmoothingTensorFlowV2(channels_first=channels_first, window_size=2)
-                assert_array_equal(spatial_smoothing(test_input)[0], test_output)
-    except ARTTestException as e:
-        art_warning(e)
-
-
-@pytest.mark.only_with_platform("tensorflow")
-def test_non_spatial_data_error(art_warning, tabular_batch, is_tf_version_2):
-    try:
-        if is_tf_version_2:
-            test_input = tabular_batch
-            spatial_smoothing = SpatialSmoothingTensorFlowV2(channels_first=False)
-
-            exc_msg = "Unrecognized input dimension. Spatial smoothing can only be applied to image"
+        if channels_first:
+            exc_msg = "Only channels last input data is supported"
             with pytest.raises(ValueError, match=exc_msg):
-                spatial_smoothing(test_input)
+                _ = SpatialSmoothingTensorFlowV2(channels_first=channels_first, window_size=window_size)
+        else:
+            spatial_smoothing = SpatialSmoothingTensorFlowV2(channels_first=channels_first,
+                                                             window_size=window_size)
+            assert_array_equal(spatial_smoothing(test_input)[0], test_output)
+    except ARTTestException as e:
+        art_warning(e)
+
+
+@pytest.mark.only_with_platform("tensorflow2")
+@pytest.mark.parametrize("channels_first", [True, False])
+def test_spatial_smoothing_video_data(art_warning, video_batch, channels_first):
+    try:
+        test_input, test_output = video_batch
+
+        if channels_first:
+            exc_msg = "Only channels last input data is supported"
+            with pytest.raises(ValueError, match=exc_msg):
+                _ = SpatialSmoothingTensorFlowV2(channels_first=channels_first, window_size=2)
+        else:
+            spatial_smoothing = SpatialSmoothingTensorFlowV2(channels_first=channels_first, window_size=2)
+            assert_array_equal(spatial_smoothing(test_input)[0], test_output)
+    except ARTTestException as e:
+        art_warning(e)
+
+
+@pytest.mark.only_with_platform("tensorflow")
+def test_non_spatial_data_error(art_warning, tabular_batch):
+    try:
+        test_input = tabular_batch
+        spatial_smoothing = SpatialSmoothingTensorFlowV2(channels_first=False)
+
+        exc_msg = "Unrecognized input dimension. Spatial smoothing can only be applied to image"
+        with pytest.raises(ValueError, match=exc_msg):
+            spatial_smoothing(test_input)
     except ARTTestException as e:
         art_warning(e)
 
