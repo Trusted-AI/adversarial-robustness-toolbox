a978f21a3ee303f22aea8dd06d043b82dc009d6d
Ebube Chuba <ebube.chuba@ibm.com>
2020-10-12 16:32:35 -0700

    fix inits
    Signed-off-by: Ebube Chuba <ebube.chuba@ibm.com>

--- art/estimators/poison_mitigation/neural_cleanse/__init__.py
+++ art/estimators/poison_mitigation/neural_cleanse/__init__.py
@@ -2,3 +2,4 @@
 Neural cleanse estimators.
 """
 from art.estimators.poison_mitigation.neural_cleanse.neural_cleanse import NeuralCleanseMixin
+from art.estimators.poison_mitigation.neural_cleanse.keras import KerasNeuralCleanse
