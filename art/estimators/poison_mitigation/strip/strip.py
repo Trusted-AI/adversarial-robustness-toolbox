5c0997c84c3a3d3f5723ef0fd84ea57152846d39
Ebube Chuba <ebube.chuba@ibm.com>
2020-10-13 16:06:57 -0700

    add strip transformer and mixin changes
    Signed-off-by: Ebube Chuba <ebube.chuba@ibm.com>

--- art/estimators/poison_mitigation/strip/strip.py
+++ art/estimators/poison_mitigation/strip/strip.py
@@ -23,128 +23,113 @@ This module implements STRIP: A Defence Against Trojan Attacks on Deep Neural Ne
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import logging
-from typing import List, Optional
-
 import numpy as np
+from typing import Callable, Optional
 
-from art.estimators import BaseEstimator
+from scipy.stats import entropy, norm
+from tqdm import tqdm
+
 from art.estimators.certification.abstain import AbstainPredictorMixin
 
 logger = logging.getLogger(__name__)
 
 
-class STRIPMixin(AbstainPredictorMixin, BaseEstimator):
+class STRIPMixin(AbstainPredictorMixin):
     """
     Implementation of STRIP: A Defence Against Trojan Attacks on Deep Neural Networks (Gao et. al. 2020)
 
-    | Paper link: https://people.cs.uchicago.edu/~ravenben/publications/pdf/backdoor-sp19.pdf
+    | Paper link: https://arxiv.org/abs/1902.06531
     """
     def __init__(
         self,
-        num_samples: int = 1000,
-        *args,
+        predict_fn: Callable[[np.ndarray], np.ndarray],
+        num_samples: int = 20,
+        false_acceptance_rate: float = 0.01,
         **kwargs
     ) -> None:
         """
-        Create a neural cleanse wrapper.
+        Create a STRIP defense
 
         :param num_samples: The number of samples to use to test entropy at inference time
+        :param false_acceptance_rate: The percentage of acceptable false acceptance
+        :param predict function
         """
-        super().__init__(*args, **kwargs)
+        super().__init__(**kwargs)
+        self.predict_fn = predict_fn
         self.num_samples = num_samples
+        self.false_acceptance_rate = false_acceptance_rate
         self.entropy_threshold: Optional[float] = None
+        self.validation_data: Optional[np.ndarray] = None
 
-    def fit(self, x, y, **kwargs) -> None:
-        return super().fit(x, y, **kwargs)
-
-    def predict(self, x: np.ndarray, batch_size: int = 128) -> np.ndarray:
+    def predict(self, x: np.ndarray) -> np.ndarray:
         """
         Perform prediction of the given classifier for a batch of inputs, potentially filtering suspicious input
 
         :param x: Test set.
-        :param batch_size: Batch size.
         :return: Array of predictions of shape `(nb_inputs, nb_classes)`.
         """
-        predictions = super().predict(x, batch_size=batch_size)
+        raw_predictions = self.predict_fn(x)
 
-        if self.entropy_threshold is None:
+        if self.entropy_threshold is None or self.validation_data is None:
             logger.warning("Mitigation has not been performed. Predictions may be unsafe.")
-            return predictions
+            return raw_predictions
 
-        all_activations = self._get_penultimate_layer_activations(x)
-        suspected_neuron_activations = all_activations[:, self.top_indices]
-        predictions[np.any(suspected_neuron_activations > self.activation_threshold, axis=1)] = self.abstain()
+        x_val = self.validation_data
+        final_predictions = []
 
-        return predictions
+        for i, img in enumerate(x):
+            # Randomly select samples from test set
+            selected_indices = np.random.choice(np.arange(len(x_val)), self.num_samples)
 
-    def mitigate(self, x_val: np.ndarray, y_val: np.ndarray, mitigation_types: List[str]) -> None:
+            # Perturn the images by combining them
+            perturbed_images = np.array([combine_images(img, x_val[idx]) for idx in selected_indices])
+
+            # Predict on the perturbed images
+            perturbed_predictions = self.predict_fn(perturbed_images)
+
+            # Calculate normalized entropy
+            normalized_entropy = np.sum(entropy(perturbed_predictions, base=2, axis=0)) / float(self.num_samples)
+
+            # Abstain if entropy is below threshold
+            if normalized_entropy <= self.entropy_threshold:
+                final_predictions.append(self.abstain())
+            else:
+                final_predictions.append(raw_predictions[i])
+
+        return np.array(final_predictions)
+
+    def mitigate(self, x_val: np.ndarray) -> None:
         """
         Mitigates the effect of poison on a classifier
 
         :param x_val: Validation data to use to mitigate the effect of poison.
-        :param y_val: Validation labels to use to mitigate the effect of poison.
-        :param mitigation_types: The types of mitigation method, can for this defense only 'filtering' is supported
-        :return: Tuple of length 2 of the selected class and certified radius.
         """
-        clean_data, backdoor_data, backdoor_labels = self.backdoor_examples(x_val, y_val)
+        self.validation_data = x_val
+        entropies = []
 
-        # If no backdoors detected from outlier detection, do nothing
-        if len(backdoor_data) == 0:
-            logger.info("No backdoor labels were detected")
-            return
+        # Find normal entropy distribution
+        for i, img in enumerate(tqdm(x_val)):
+            selected_indices = np.random.choice(np.arange(len(x_val)), self.num_samples)
+            perturbed_images = np.array([combine_images(img, x_val[idx]) for idx in selected_indices])
+            perturbed_predictions = self.predict_fn(perturbed_images)
+            normalized_entropy = np.sum(entropy(perturbed_predictions, base=2, axis=0)) / float(self.num_samples)
+            entropies.append(normalized_entropy)
 
-        if "pruning" in mitigation_types or "filtering" in mitigation_types:
-            # get activations from penultimate layer from clean and backdoor images
-            clean_activations = self._get_penultimate_layer_activations(clean_data)
-            backdoor_activations = self._get_penultimate_layer_activations(backdoor_data)
+        mean_entropy, std_entropy = norm.fit(entropies)
 
-            # rank activations descending by difference in backdoor and clean inputs
-            ranked_indices = np.argsort(np.sum(clean_activations - backdoor_activations, axis=0))
+        # Set threshold to FAR percentile
+        self.entropy_threshold = norm.ppf(self.false_acceptance_rate, loc=mean_entropy, scale=std_entropy)
+        if self.entropy_threshold < 0:
+            logger.warning("Entropy value is negative. Increase FAR for reasonable performance.")
 
-        for mitigation_type in mitigation_types:
-            if mitigation_type == "unlearning":
-                # Train one epoch on generated backdoors
-                # This mitigation method works well for Trojan attacks
 
-                self._fit_classifier(backdoor_data, backdoor_labels, batch_size=1, nb_epochs=1)
+def combine_images(img1: np.ndarray, img2: np.ndarray, alpha=0.5) -> np.ndarray:
+    """
+    Combine two Numpy arrays of the same shape
 
-            elif mitigation_type == "pruning":
-                # zero out activations from highly ranked neurons until backdoor is unresponsive
-                # This mitigation method works well for backdoors.
-
-                backdoor_effective = self.check_backdoor_effective(backdoor_data, backdoor_labels)
-                num_neurons_pruned = 0
-                total_neurons = clean_activations.shape[1]
-
-                # starting from indices of high activation neurons, set weights (and biases) of high activation
-                # neurons to zero, until backdoor ineffective or pruned 30% of neurons
-                logger.info("Pruning model...")
-                while (
-                    backdoor_effective
-                    and num_neurons_pruned < 0.3 * total_neurons
-                    and num_neurons_pruned < len(ranked_indices)
-                ):
-                    self._prune_neuron_at_index(ranked_indices[num_neurons_pruned])
-                    num_neurons_pruned += 1
-                    backdoor_effective = self.check_backdoor_effective(backdoor_data, backdoor_labels)
-                logger.info("Pruning complete. Pruned {} neurons".format(num_neurons_pruned))
-
-            elif mitigation_type == "filtering":
-                # using top 1% of ranked neurons by activation difference to adv vs. clean inputs
-                # generate a profile of average activation, when above threshold, abstain
-
-                # get indicies of top 1% of ranked neurons
-                num_top = int(np.ceil(len(ranked_indices) * 0.01))
-                self.top_indices = ranked_indices[:num_top]
-
-                # measure average activation for clean images and backdoor images
-                avg_clean_activation = np.average(clean_activations[:, self.top_indices], axis=0)
-                std_clean_activation = np.std(clean_activations[:, self.top_indices], axis=0)
-
-                # if average activation for selected neurons is above a threshold, flag input and abstain
-                # activation over threshold function can be called at predict
-                # TODO: explore different values for threshold
-                self.activation_threshold = avg_clean_activation + 1 * std_clean_activation
-
-            else:
-                raise TypeError("Mitigation type: `" + mitigation_type + "` not supported")
+    :param img1: a Numpy array
+    :param img2: a Numpy
+    :param alpha: percentage weight for the first image
+    :return: The combined image
+    """
+    return alpha * img1 + (1 - alpha) * img2
