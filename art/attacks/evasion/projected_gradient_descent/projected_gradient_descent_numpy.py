663614df2fc1bbbc8f3f088c14537c9531462bb2
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-10-19 17:25:55 +0100

    check param pgd
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- art/attacks/evasion/projected_gradient_descent/projected_gradient_descent_numpy.py
+++ art/attacks/evasion/projected_gradient_descent/projected_gradient_descent_numpy.py
@@ -181,8 +181,12 @@ class ProjectedGradientDescentCommon(FastGradientMethod):
     def _check_params(self) -> None:
         super(ProjectedGradientDescentCommon, self)._check_params()
 
-        if self.eps_step > self.eps:
-            raise ValueError("The iteration step `eps_step` has to be smaller than the total attack `eps`.")
+        if isinstance(self.eps, float):
+            if self.eps_step > self.eps:
+                raise ValueError("The iteration step `eps_step` has to be smaller than the total attack `eps`.")
+        else:
+            if (self.eps_step > self.eps).any():
+                raise ValueError("The iteration step `eps_step` has to be smaller than the total attack `eps`.")
 
         if self.max_iter <= 0:
             raise ValueError("The number of iterations `max_iter` has to be a positive integer.")
