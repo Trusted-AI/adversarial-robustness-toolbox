a3f27b93dd5663087ccf2e31ba6d424dabe08a96
killian <killian.levacher@ibm.com>
2020-10-19 14:43:13 +0100

    moved all inference tests in main loop
    Signed-off-by: killian <killian.levacher@ibm.com>

--- run_tests.sh
+++ run_tests.sh
@@ -6,17 +6,6 @@ export TF_CPP_MIN_LOG_LEVEL="3"
 
 # --------------------------------------------------------------------------------------------------------------- TESTS
 
-mlFrameworkList=("tensorflow" "scikitlearn")
-for mlFramework in "${mlFrameworkList[@]}"; do
-  echo "#######################################################################"
-  echo "############## Running tests with framework $mlFramework ##############"
-  echo "#######################################################################"
-
-  #TODO FIX Failing with Keras at test_membership_inference.test_black_box_keras_loss
-  pytest --cov-report=xml --cov=art --cov-append -q -vv tests/attacks/inference/test_membership_inference.py --mlFramework=$mlFramework --skip_travis=True --durations=0
-  if [[ $? -ne 0 ]]; then exit_code=1; echo "Failed attacks/inference tests"; fi
-done
-
 #NOTE: All the tests should be ran within this loop. All other tests are legacy tests that must be
 # made framework independent to be incorporated within this loop
 mlFrameworkList=("tensorflow" "keras" "pytorch" "scikitlearn" "mxnet" "kerastf")
@@ -34,10 +23,8 @@ for mlFramework in "${mlFrameworkList[@]}"; do
   pytest --cov-report=xml --cov=art --cov-append  -q -vv -s tests/attacks/evasion/ --mlFramework=$mlFramework  --skip_travis=True --durations=0
   if [[ $? -ne 0 ]]; then exit_code=1; echo "Failed attacks/evasion/test_shadow_attack.py"; fi
 
-  pytest --cov-report=xml --cov=art --cov-append  -q -vv tests/attacks/inference/test_model_inversion.py --mlFramework=$mlFramework --skip_travis=True --durations=0
-  if [[ $? -ne 0 ]]; then exit_code=1; echo "Failed attacks/inference/model_inversion tests"; fi
-  pytest --cov-report=xml --cov=art --cov-append  -q -vv tests/attacks/inference/test_attribute_inference.py --mlFramework=$mlFramework --skip_travis=True --durations=0
-  if [[ $? -ne 0 ]]; then exit_code=1; echo "Failed attacks/inference/attribute_inference tests"; fi
+  pytest --cov-report=xml --cov=art --cov-append  -q -vv tests/attacks/inference/ --mlFramework=$mlFramework --skip_travis=True --durations=0
+  if [[ $? -ne 0 ]]; then exit_code=1; echo "Failed attacks/inference"; fi
 
   pytest --cov-report=xml --cov=art --cov-append  -q -vv tests/classifiersFrameworks/  --mlFramework=$mlFramework --skip_travis=True --durations=0
   if [[ $? -ne 0 ]]; then exit_code=1; echo "Failed classifiersFrameworks tests"; fi
