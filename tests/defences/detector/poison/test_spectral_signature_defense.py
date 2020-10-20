c3ec16a233037aec2f7adf1a898f2719fa0de47e
Ebube Chuba <ebube.chuba@ibm.com>
2020-10-20 16:41:27 -0700

    remove old tests
    Signed-off-by: Ebube Chuba <ebube.chuba@ibm.com>

--- tests/defences/detector/poison/test_spectral_signature_defense.py
+++ tests/defences/detector/poison/test_spectral_signature_defense.py
@@ -31,63 +31,6 @@ logger = logging.getLogger(__name__)
 NB_TRAIN, NB_TEST, BATCH_SIZE, EPS_MULTIPLIER, UB_PCT_POISON = 30000, 10, 128, 1.5, 0.2
 
 
-# class TestSpectralSignatureDefense(unittest.TestCase):
-#     @classmethod
-#     def setUpClass(cls):
-#
-#         (x_train, y_train), (x_test, y_test), min_, max_ = load_mnist()
-#         x_train, y_train = x_train[:NB_TRAIN], y_train[:NB_TRAIN]
-#         cls.mnist = (x_train, y_train), (x_test, y_test), (min_, max_)
-#         from tests.utils import get_image_classifier_kr
-#
-#         cls.classifier = get_image_classifier_kr()
-#         cls.defence = SpectralSignatureDefense(
-#             cls.classifier,
-#             x_train,
-#             y_train,
-#             batch_size=BATCH_SIZE,
-#             eps_multiplier=EPS_MULTIPLIER,
-#             ub_pct_poison=UB_PCT_POISON,
-#         )
-#
-#     def setUp(self):
-#         # Set master seed
-#         master_seed(1234)
-#
-#     @unittest.expectedFailure
-#     def test_wrong_parameters_1(self):
-#         self.defence.set_params(batch_size=-1)
-#
-#     @unittest.expectedFailure
-#     def test_wrong_parameters_2(self):
-#         self.defence.set_params(eps_multiplier=-1.0)
-#
-#     @unittest.expectedFailure
-#     def test_wrong_parameters_3(self):
-#         self.defence.set_params(ub_pct_poison=2.0)
-#
-#     def test_detect_poison(self):
-#         # Get MNIST
-#         (x_train, _), (_, _), (_, _) = self.mnist
-#
-#         _, is_clean_lst = self.defence.detect_poison()
-#
-#         # Check number of items in is_clean
-#         self.assertEqual(len(x_train), len(is_clean_lst))
-#
-#     def test_evaluate_defense(self):
-#         # Get MNIST
-#         (x_train, _), (_, _), (_, _) = self.mnist
-#
-#         is_clean = np.zeros(len(x_train))
-#         self.defence.evaluate_defence(is_clean)
-#
-#
-# if __name__ == "__main__":
-#     unittest.main()
-
-#
-
 @pytest.mark.xfail
 @pytest.mark.parametrize("params", [dict(batch_size=-1), dict(eps_multiplier=-1.0), dict(ub_pct_poison=2.0)])
 @pytest.mark.framework_agnostic
