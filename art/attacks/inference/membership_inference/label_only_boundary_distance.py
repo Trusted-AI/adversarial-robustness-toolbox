3a261d91ebdd0f3028532e456625dd43e0b7ca51
Beat Buesser <beat.buesser@ie.ibm.com>
2020-11-07 20:57:11 +0000

    Add Label-Only Gap Attack
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/attacks/inference/membership_inference/label_only_boundary_distance.py
+++ art/attacks/inference/membership_inference/label_only_boundary_distance.py
@@ -16,7 +16,7 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 """
-This module implements the classifier `BlackBoxClassifier` for black-box classifiers.
+This module implements the Label-Only Inference Attack based on Decision Boundary.
 """
 import logging
 from typing import Optional
