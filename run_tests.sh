0d39385ab533cb4d5a3a379510fed7f9257b7984
Ebube Chuba <ebube.chuba@ibm.com>
2020-11-09 15:53:21 -0800

    revert declaration statement
    Signed-off-by: Ebube Chuba <ebube.chuba@ibm.com>

--- run_tests.sh
+++ run_tests.sh
@@ -131,7 +131,7 @@ declare -a art=("tests/test_data_generators.py" \
                 "tests/test_utils.py" \
                 "tests/test_visualization.py" )
 
-declare -a tests_modules=("attacks" \
+tests_modules=("attacks" \
                "classifiers" \
                "object_detectors" \
                "speech_recognizers" \
