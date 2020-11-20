401ea8b316cb38a57094c695004e4f1714079677
Beat Buesser <beat.buesser@ie.ibm.com>
2020-11-20 22:27:02 +0000

    Update for Gap Attack
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/attacks/inference/membership_inference/label_only_gap_attack.py
+++ art/attacks/inference/membership_inference/label_only_gap_attack.py
@@ -21,49 +21,11 @@ This module implements the Label Only Gap Attack `.
 | Paper link: https://arxiv.org/abs/2007.14321
 """
 import logging
-from typing import TYPE_CHECKING
 
-import numpy as np
+from art.attacks.inference.membership_inference import MembershipInferenceBlackBoxRuleBased
 
-from art.attacks.attack import InferenceAttack
-from art.estimators.estimator import BaseEstimator
-from art.estimators.classification.classifier import ClassifierMixin
-
-if TYPE_CHECKING:
-    from art.utils import CLASSIFIER_TYPE
 
 logger = logging.getLogger(__name__)
 
 
-class LabelOnlyGapAttack(InferenceAttack):
-    """
-    Implementation of Label Only Gap Attack.
-
-    | Paper link: https://arxiv.org/abs/2007.14321
-    """
-
-    attack_params = InferenceAttack.attack_params
-    _estimator_requirements = (BaseEstimator, ClassifierMixin)
-
-    def __init__(self, estimator: "CLASSIFIER_TYPE"):
-        """
-        Create a `LabelOnlyGapAttack` instance of a Label Only Gap Attack.
-
-        :param estimator: A trained classification estimator.
-        """
-        super().__init__(estimator=estimator)
-        self._check_params()
-
-    def infer(self, x: np.ndarray, y: np.ndarray, **kwargs) -> np.ndarray:
-        """
-        Infer membership of input `x` in estimator's training data.
-
-        :param x: Input data.
-        :param y: True labels for `x`.
-        :return: An array holding the inferred membership status, 1 indicates a member and 0 indicates non-member.
-        """
-        y_pred = self.estimator.predict(x=x)
-        return np.argmax(y, axis=1) == np.argmax(y_pred, axis=1)
-
-    def _check_params(self) -> None:
-        pass
+LabelOnlyGapAttack = MembershipInferenceBlackBoxRuleBased
