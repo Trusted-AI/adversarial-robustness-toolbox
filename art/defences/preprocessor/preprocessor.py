00565b653bb4ce13abf33d8b16d06e508fb013fe
Beat Buesser <beat.buesser@ie.ibm.com>
2020-11-21 01:55:29 +0000

    Update gradients for object arrays
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/defences/preprocessor/preprocessor.py
+++ art/defences/preprocessor/preprocessor.py
@@ -203,14 +203,14 @@ class PreprocessorPyTorch(Preprocessor):
 
             return x_grad
 
-        if x.shape == grad.shape:
-            x_grad = get_gradient(x=x, grad=grad)
-        elif x.dtype == np.object:
+        if x.dtype == np.object:
             x_grad_list = list()
             for i, x_i in enumerate(x):
                 x_grad_list.append(get_gradient(x=x_i, grad=grad[i]))
             x_grad = np.empty(len(x.shape[0]), dtype=object)
             x_grad[:] = list(x_grad_list)
+        elif x.shape == grad.shape:
+            x_grad = get_gradient(x=x, grad=grad)
         else:
             # Special case for loss gradients
             x_grad = np.zeros_like(grad)
