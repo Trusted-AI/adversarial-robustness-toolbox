47fa8333218c81f5eb31281d220d37d852423f5a
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-11-23 17:38:26 +0000

    test asr attack
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- tests/attacks/evasion/test_imperceptible_asr_pytorch.py
+++ tests/attacks/evasion/test_imperceptible_asr_pytorch.py
@@ -18,187 +18,112 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import logging
-import importlib
 
-import torch
 import numpy as np
 import pytest
 
-from tests.utils import master_seed
-
-deepspeech_pytorch_spec = importlib.util.find_spec("deepspeech_pytorch")
-deepspeech_pytorch_found = deepspeech_pytorch_spec is not None
-
-apex_spec = importlib.util.find_spec("apex")
-if apex_spec is not None:
-    amp_spec = importlib.util.find_spec("apex.amp")
-else:
-    amp_spec = None
-amp_found = amp_spec is not None
+from art.config import ART_NUMPY_DTYPE
+from tests.utils import ARTTestException
 
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.skipif(
-    not deepspeech_pytorch_found,
-    reason="Skip unittests if deep speech module is not found because of pre-trained model.",
-)
-@pytest.mark.skipif(not amp_found, reason="Skip unittests if apex module is not found.")
-class TestImperceptibleASRPytorch:
-    """
-    This class tests the ImperceptibleASRPytorch attack.
-    """
+@pytest.mark.skipModule("apex.amp", "deepspeech_pytorch", "torchaudio")
+@pytest.mark.skipMlFramework("tensorflow", "keras", "kerastf", "mxnet", "non_dl_frameworks")
+@pytest.mark.parametrize("use_amp", [False, True])
+@pytest.mark.parametrize("device_type", ["cpu", "gpu"])
+def test_imperceptible_asr_pytorch(art_warning, expected_values, use_amp, device_type):
+    # Only import if deepspeech_pytorch module is available
+    import torch
 
-    @pytest.fixture
-    def setup_class(self):
-        master_seed(seed=1234)
+    from art.estimators.speech_recognition.pytorch_deep_speech import PyTorchDeepSpeech
+    from art.attacks.evasion.imperceptible_asr.imperceptible_asr_pytorch import ImperceptibleASRPytorch
+    from art.defences.preprocessor import AudioFilterPyTorch
 
-        # Small data for testing
-        x1 = np.array(
-            [
-                -1.0376293e-03,
-                -1.0681478e-03,
-                -1.0986663e-03,
-                -1.1291848e-03,
-                -1.1291848e-03,
-                -1.1291848e-03,
-                -1.1902219e-03,
-                -1.1597034e-03,
-                -1.1902219e-03,
-                -1.1291848e-03,
-                -1.1291848e-03,
-                -1.0681478e-03,
-                -9.1555528e-04,
-            ]
-            * 100
-        )
+    try:
+        # Load data for testing
+        expected_data = expected_values()
 
-        x2 = np.array(
-            [
-                -1.8311106e-04,
-                -1.2207404e-04,
-                -6.1037019e-05,
-                0.0000000e00,
-                3.0518509e-05,
-                0.0000000e00,
-                -3.0518509e-05,
-                0.0000000e00,
-                0.0000000e00,
-                9.1555528e-05,
-                2.1362957e-04,
-                3.3570360e-04,
-                4.2725913e-04,
-                4.5777764e-04,
-                -1.8311106e-04,
-            ]
-            * 100
-        )
+        x1 = expected_data[0]
+        x2 = expected_data[1]
+        x3 = expected_data[2]
 
-        x3 = np.array(
-            [
-                -8.2399976e-04,
-                -7.0192572e-04,
-                -5.4933317e-04,
-                -4.2725913e-04,
-                -3.6622211e-04,
-                -2.7466659e-04,
-                -2.1362957e-04,
-                5.4933317e-04,
-                5.7985168e-04,
-                6.1037019e-04,
-                6.7140721e-04,
-                7.0192572e-04,
-                6.7140721e-04,
-                -1.5259255e-04,
-            ]
-            * 100
-        )
-
-        self.x = np.array([x1, x2, x3])
+        # Create signal data
+        x = np.array([np.array(x1 * 100), np.array(x2 * 100), np.array(x3 * 100)])
 
         # Create labels
-        self.y = np.array(["S", "I", "GD"])
+        y = np.array(["S", "I", "GD"])
 
-    @pytest.mark.only_with_platform("pytorch")
-    def test_all(self, _test_all):
-        pass
+        # Create DeepSpeech estimator with preprocessing
+        numerator_coef = np.array([0.0000001, 0.0000002, -0.0000001, -0.0000002], dtype=ART_NUMPY_DTYPE)
+        denumerator_coef = np.array([1.0, 0.0, 0.0, 0.0], dtype=ART_NUMPY_DTYPE)
+        audio_filter = AudioFilterPyTorch(
+            numerator_coef=numerator_coef, denumerator_coef=denumerator_coef, device_type=device_type
+        )
 
-    @pytest.fixture(params=[False, True])
-    def _test_all(self, request, setup_class):
-        # Only import if deep speech module is available
-        from art.estimators.speech_recognition.pytorch_deep_speech import PyTorchDeepSpeech
-        from art.attacks.evasion.imperceptible_asr.imperceptible_asr_pytorch import ImperceptibleASRPytorch
+        speech_recognizer = PyTorchDeepSpeech(
+            pretrained_model="librispeech",
+            device_type=device_type,
+            use_amp=use_amp,
+            preprocessing_defences=audio_filter
+        )
 
-        # Without amp
-        if request.param is False:
-            # Create DeepSpeech estimator
-            speech_recognizer = PyTorchDeepSpeech(pretrained_model="librispeech")
+        # Create attack
+        asr_attack = ImperceptibleASRPytorch(
+            estimator=speech_recognizer,
+            initial_eps=0.001,
+            max_iter_1st_stage=50,
+            max_iter_2nd_stage=50,
+            learning_rate_1st_stage=0.00001,
+            learning_rate_2nd_stage=0.001,
+            optimizer_1st_stage=torch.optim.SGD,
+            optimizer_2nd_stage=torch.optim.SGD,
+            global_max_length=2000,
+            initial_rescale=1.0,
+            rescale_factor=0.8,
+            num_iter_adjust_rescale=5,
+            initial_alpha=0.01,
+            increase_factor_alpha=1.2,
+            num_iter_increase_alpha=5,
+            decrease_factor_alpha=0.8,
+            num_iter_decrease_alpha=5,
+            batch_size=2,
+            use_amp=use_amp,
+            opt_level="O1",
+        )
 
-            # Create attack
-            asr_attack = ImperceptibleASRPytorch(
-                estimator=speech_recognizer,
-                initial_eps=0.001,
-                max_iter_1st_stage=50,
-                max_iter_2nd_stage=50,
-                learning_rate_1st_stage=0.00001,
-                learning_rate_2nd_stage=0.001,
-                optimizer_1st_stage=torch.optim.SGD,
-                optimizer_2nd_stage=torch.optim.SGD,
-                global_max_length=2000,
-                initial_rescale=1.0,
-                rescale_factor=0.8,
-                num_iter_adjust_rescale=5,
-                initial_alpha=0.01,
-                increase_factor_alpha=1.2,
-                num_iter_increase_alpha=5,
-                decrease_factor_alpha=0.8,
-                num_iter_decrease_alpha=5,
-                batch_size=2,
-                use_amp=False,
-                opt_level="O1",
-            )
+        # First with preprocessing ====================
+        # Test transcription output
+        transcriptions_preprocessing = speech_recognizer.predict(x, batch_size=2, transcription_output=True)
 
-        # With amp
-        else:
-            # Create DeepSpeech estimator
-            speech_recognizer = PyTorchDeepSpeech(pretrained_model="librispeech", device_type="gpu", use_amp=True)
+        expected_transcriptions = np.array(["", "", ""])
+        assert (expected_transcriptions == transcriptions_preprocessing).all()
 
-            # Create attack
-            asr_attack = ImperceptibleASRPytorch(
-                estimator=speech_recognizer,
-                initial_eps=0.001,
-                max_iter_1st_stage=50,
-                max_iter_2nd_stage=50,
-                learning_rate_1st_stage=0.00001,
-                learning_rate_2nd_stage=0.001,
-                optimizer_1st_stage=torch.optim.SGD,
-                optimizer_2nd_stage=torch.optim.SGD,
-                global_max_length=2000,
-                initial_rescale=1.0,
-                rescale_factor=0.8,
-                num_iter_adjust_rescale=2,
-                initial_alpha=0.01,
-                increase_factor_alpha=1.2,
-                num_iter_increase_alpha=2,
-                decrease_factor_alpha=0.8,
-                num_iter_decrease_alpha=2,
-                batch_size=2,
-                use_amp=True,
-                opt_level="O1",
-            )
+        # Generate attack
+        x_adv_preprocessing = asr_attack.generate(x, y)
+
+        # Test shape
+        assert x_adv_preprocessing[0].shape == x[0].shape
+        assert x_adv_preprocessing[1].shape == x[1].shape
+        assert x_adv_preprocessing[2].shape == x[2].shape
+
+        # Second without preprocessing ====================
+        # Remove preprocessing
+        speech_recognizer.preprocessing = []
 
         # Test transcription output
-        transcriptions = speech_recognizer.predict(self.x, batch_size=2, transcription_output=True)
+        transcriptions = speech_recognizer.predict(x, batch_size=2, transcription_output=True)
 
         expected_transcriptions = np.array(["", "", ""])
         assert (expected_transcriptions == transcriptions).all()
 
         # Generate attack
-        x_adv = asr_attack.generate(self.x, self.y)
+        x_adv = asr_attack.generate(x, y)
 
         # Test shape
-        for i in range(3):
-            assert x_adv[i].shape == self.x[i].shape
+        assert x_adv[0].shape == x[0].shape
+        assert x_adv[1].shape == x[1].shape
+        assert x_adv[2].shape == x[2].shape
 
         # Test transcription adversarial output
         # This test is commented by now because of the difference in the prediction function of the estimator
@@ -207,4 +132,12 @@ class TestImperceptibleASRPytorch:
         # batch size to hundreds.
 
         # adv_transcriptions = speech_recognizer.predict(x_adv, batch_size=2, transcription_output=True)
-        # assert (adv_transcriptions == self.y).all()
+        # assert (adv_transcriptions == y).all()
+
+        # Test attack with and without preprocessing
+        assert np.sum(np.abs(x_adv[0] - x_adv_preprocessing[0])) > 0
+        assert np.sum(np.abs(x_adv[1] - x_adv_preprocessing[1])) > 0
+        assert np.sum(np.abs(x_adv[2] - x_adv_preprocessing[2])) > 0
+
+    except ARTTestException as e:
+        art_warning(e)
