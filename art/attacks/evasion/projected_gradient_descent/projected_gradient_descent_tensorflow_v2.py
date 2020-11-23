1d2c16ce537375c41ffa089d44b36db1bcdaaf4a
Giulio Zizzo <giulio@li-87b782cc-261a-11b2-a85c-fc0eec425ab4.ibm.com>
2020-11-23 10:59:59 +0000

    random restart changes
    Signed-off-by: Giulio Zizzo <giulio@li-87b782cc-261a-11b2-a85c-fc0eec425ab4.ibm.com>

--- art/attacks/evasion/projected_gradient_descent/projected_gradient_descent_tensorflow_v2.py
+++ art/attacks/evasion/projected_gradient_descent/projected_gradient_descent_tensorflow_v2.py
@@ -35,7 +35,7 @@ from art.config import ART_NUMPY_DTYPE
 from art.attacks.evasion.projected_gradient_descent.projected_gradient_descent_numpy import (
     ProjectedGradientDescentCommon,
 )
-from art.utils import compute_success, random_sphere
+from art.utils import compute_success, random_sphere, compute_success_array
 
 if TYPE_CHECKING:
     import tensorflow as tf
@@ -156,60 +156,58 @@ class ProjectedGradientDescentTensorFlowV2(ProjectedGradientDescentCommon):
             ).batch(self.batch_size, drop_remainder=False)
 
         # Start to compute adversarial examples
-        adv_x_best = None
-        rate_best = None
+        adv_x = x.astype(ART_NUMPY_DTYPE)
+        data_loader = iter(dataset)
 
-        for _ in trange(max(1, self.num_random_init), desc="PGD - Random Initializations", disable=not self.verbose):
-            adv_x = x.astype(ART_NUMPY_DTYPE)
-            data_loader = iter(dataset)
+        # Compute perturbation with batching
+        for (batch_id, batch_all) in enumerate(
+            tqdm(data_loader, desc="PGD - Batches", leave=False, disable=not self.verbose)
+        ):
+            if mask is not None:
+                (batch, batch_labels, mask_batch) = batch_all[0], batch_all[1], batch_all[2]
+            else:
+                (batch, batch_labels, mask_batch) = batch_all[0], batch_all[1], None
 
-            # Compute perturbation with batching
-            for (batch_id, batch_all) in enumerate(
-                tqdm(data_loader, desc="PGD - Batches", leave=False, disable=not self.verbose)
-            ):
-                if mask is not None:
-                    (batch, batch_labels, mask_batch) = batch_all[0], batch_all[1], batch_all[2]
-                else:
-                    (batch, batch_labels, mask_batch) = batch_all[0], batch_all[1], None
+            batch_index_1, batch_index_2 = batch_id * self.batch_size, (batch_id + 1) * self.batch_size
 
-                batch_index_1, batch_index_2 = batch_id * self.batch_size, (batch_id + 1) * self.batch_size
-
-                # Compute batch_eps and batch_eps_step
-                if isinstance(self.eps, np.ndarray):
-                    if len(self.eps.shape) == len(x.shape) and self.eps.shape[0] == x.shape[0]:
-                        batch_eps = self.eps[batch_index_1:batch_index_2]
-                        batch_eps_step = self.eps_step[batch_index_1:batch_index_2]
-
-                    else:
-                        batch_eps = self.eps
-                        batch_eps_step = self.eps_step
+            # Compute batch_eps and batch_eps_step
+            if isinstance(self.eps, np.ndarray):
+                if len(self.eps.shape) == len(x.shape) and self.eps.shape[0] == x.shape[0]:
+                    batch_eps = self.eps[batch_index_1:batch_index_2]
+                    batch_eps_step = self.eps_step[batch_index_1:batch_index_2]
 
                 else:
                     batch_eps = self.eps
                     batch_eps_step = self.eps_step
 
-                adv_x[batch_index_1:batch_index_2] = self._generate_batch(
+            else:
+                batch_eps = self.eps
+                batch_eps_step = self.eps_step
+
+            for rand_init_num in range(max(1, self.num_random_init)):
+                adversarial_batch = self._generate_batch(
                     x=batch, targets=batch_labels, mask=mask_batch, eps=batch_eps, eps_step=batch_eps_step
                 )
-
-            if self.num_random_init > 1:
-                rate = 100 * compute_success(
-                    self.estimator, x, targets, adv_x, self.targeted, batch_size=self.batch_size
-                )
-                if rate_best is None or rate > rate_best or adv_x_best is None:
-                    rate_best = rate
-                    adv_x_best = adv_x
-            else:
-                adv_x_best = adv_x
+                if rand_init_num == 0:
+                    # first iteration: use the adversarial examples as they are the only ones we have now
+                    adv_x[batch_index_1:batch_index_2] = np.copy(adversarial_batch)
+                else:
+                    attack_success = compute_success_array(
+                        self.estimator,
+                        batch,
+                        batch_labels,
+                        adversarial_batch,
+                        self.targeted,
+                        batch_size=self.batch_size,
+                    )
+                    # return the successful adversarial examples
+                    adv_x[batch_index_1:batch_index_2][attack_success] = adversarial_batch[attack_success]
 
         logger.info(
-            "Success rate of attack: %.2f%%",
-            rate_best
-            if rate_best is not None
-            else 100 * compute_success(self.estimator, x, y, adv_x_best, self.targeted, batch_size=self.batch_size),
+            "Success rate of attack: %.2f%%", 100 * compute_success(self.estimator, x, y, adv_x, self.targeted, batch_size=self.batch_size),
         )
 
-        return adv_x_best
+        return adv_x
 
     def _generate_batch(
         self,
