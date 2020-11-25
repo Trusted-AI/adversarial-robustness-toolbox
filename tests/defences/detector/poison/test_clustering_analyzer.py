e2ec05335cccf3b833a9488dd3312eef624bd029
Ebube Chuba <ebube.chuba@ibm.com>
2020-11-25 15:30:35 -0800

    clean up existing defense
    Signed-off-by: Ebube Chuba <ebube.chuba@ibm.com>

--- tests/defences/detector/poison/test_clustering_analyzer.py
+++ tests/defences/detector/poison/test_clustering_analyzer.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (C) The Adversarial Robustness Toolbox (ART) Authors 2018
+# Copyright (C) The Adversarial Robustness Toolbox (ART) Authors 2020
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
@@ -21,17 +21,67 @@ import logging
 import unittest
 
 import numpy as np
+import pytest
 
 from art.defences.detector.poison import ClusteringAnalyzer
 
 logger = logging.getLogger(__name__)
 
 
+# def test_size_analyzer(self):
+#     nb_clusters = 2
+#     nb_classes = 3
+#     clusters_by_class = [[0 for _ in range(nb_clusters)] for _ in range(nb_classes)]
+#
+#     clusters_by_class[0] = [0, 1, 1, 1, 1]  # Class 0
+#     clusters_by_class[1] = [1, 0, 0, 0, 0]  # Class 1
+#     clusters_by_class[2] = [0, 0, 0, 0, 1]  # Class 2
+#     analyzer = ClusteringAnalyzer()
+#     assigned_clean_by_class, poison_clusters, report = analyzer.analyze_by_size(clusters_by_class)
+#
+#     clean = 0
+#     poison = 1
+#     # For class 0, cluster 0 should be marked as poison.
+#     assert poison_clusters[0][0] == poison
+#     # For class 0, cluster 1 should be marked as clean.
+#     assert poison_clusters[0][1] == clean
+#     assert report["Class_0"]["cluster_0"]["suspicious_cluster"]
+#     assert not report["Class_0"]["cluster_1"]["suspicious_cluster"]
+#     total = len(clusters_by_class[0])
+#     c1 = sum(clusters_by_class[0])
+#     assert report["Class_0"]["cluster_0"]["ptc_data_in_cluster"] == (total - c1) / total
+#     assert report["Class_0"]["cluster_1"]["ptc_data_in_cluster"] == c1 / total
+#
+#     # Inverse relations for class 1
+#     assert poison_clusters[1][0] == clean
+#     assert poison_clusters[1][1] == poison
+#     assert not report["Class_1"]["cluster_0"]["suspicious_cluster"]
+#     assert report["Class_1"]["cluster_1"]["suspicious_cluster"]
+#     total = len(clusters_by_class[1])
+#     c1 = sum(clusters_by_class[1])
+#     assert report["Class_1"]["cluster_0"]["ptc_data_in_cluster"] == (total - c1) / total
+#     assert report["Class_1"]["cluster_1"]["ptc_data_in_cluster"] == c1 / total
+#
+#     assert poison_clusters[2][0] == clean
+#     assert poison_clusters[2][1] == poison
+#     assert not report["Class_2"]["cluster_0"]["suspicious_cluster"]
+#     assert report["Class_2"]["cluster_1"]["suspicious_cluster"]
+#     total = len(clusters_by_class[2])
+#     c1 = sum(clusters_by_class[2])
+#     assert report["Class_2"]["cluster_0"]["ptc_data_in_cluster"], (total - c1) / total
+#     assert report["Class_2"]["cluster_1"]["ptc_data_in_cluster"], c1 / total
+#
+#     poison = 0
+#     assert assigned_clean_by_class[0][0], poison
+#     assert assigned_clean_by_class[1][0], poison
+#     assert assigned_clean_by_class[2][4], poison
+
+
 class TestActivationDefence(unittest.TestCase):
     def test_size_analyzer(self):
         nb_clusters = 2
         nb_classes = 3
-        clusters_by_class = [[[] for x in range(nb_clusters)] for y in range(nb_classes)]
+        clusters_by_class = [[0 for _ in range(nb_clusters)] for _ in range(nb_classes)]
 
         clusters_by_class[0] = [0, 1, 1, 1, 1]  # Class 0
         clusters_by_class[1] = [1, 0, 0, 0, 0]  # Class 1
@@ -86,7 +136,7 @@ class TestActivationDefence(unittest.TestCase):
     def test_size_analyzer_three(self):
         nb_clusters = 3
         nb_classes = 3
-        clusters_by_class = [[[] for x in range(nb_clusters)] for y in range(nb_classes)]
+        clusters_by_class = [[0 for _ in range(nb_clusters)] for _ in range(nb_classes)]
 
         clusters_by_class[0] = [0, 1, 1, 2, 2]  # Class 0
         clusters_by_class[1] = [1, 0, 0, 2, 2]  # Class 1
@@ -149,7 +199,7 @@ class TestActivationDefence(unittest.TestCase):
     def test_relative_size_analyzer(self):
         nb_clusters = 2
         nb_classes = 4
-        clusters_by_class = [[[] for x in range(nb_clusters)] for y in range(nb_classes)]
+        clusters_by_class = [[0 for _ in range(nb_clusters)] for _ in range(nb_classes)]
 
         clusters_by_class[0] = [0, 1, 1, 1, 1]  # Class 0
         clusters_by_class[1] = [1, 0, 0, 0, 0]  # Class 1
@@ -216,13 +266,13 @@ class TestActivationDefence(unittest.TestCase):
     def test_relative_size_analyzer_three(self):
         nb_clusters = 3
         nb_classes = 3
-        clusters_by_class = [[[] for x in range(nb_clusters)] for y in range(nb_classes)]
+        clusters_by_class = [[0 for _ in range(nb_clusters)] for _ in range(nb_classes)]
 
         clusters_by_class[0] = [0, 1, 1, 2, 2]  # Class 0
         clusters_by_class[1] = [1, 0, 0, 2, 2]  # Class 1
         clusters_by_class[2] = [0, 0, 0, 2, 1, 1]  # Class 2
         analyzer = ClusteringAnalyzer()
-        analyzer.analyze_clusters(clusters_by_class)
+        analyzer.analyze_by_relative_size(clusters_by_class)
 
 
 if __name__ == "__main__":
