b114f962db806982b45dc3dff8e48cbedd4d4313
Beat Buesser <beat.buesser@ie.ibm.com>
2020-11-27 22:58:19 +0000

    Add tests
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/preprocessing/__init__.py
+++ art/preprocessing/__init__.py
@@ -1,8 +1,3 @@
 """
 Module for preprocessing steps providing accurate or estimated gradients.
 """
-from art.preprocessing.standardisation_mean_std.standardisation_mean_std import StandardisationMeanStd
-from art.preprocessing.standardisation_mean_std.standardisation_mean_std_pytorch import StandardisationMeanStdPyTorch
-from art.preprocessing.standardisation_mean_std.standardisation_mean_std_tensorflow import (
-    StandardisationMeanStdTensorFlowV2,
-)
