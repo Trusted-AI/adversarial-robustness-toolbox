2126f951a55a90676a9858980fa68d6c7b6dc386
abigailt <abigailt@il.ibm.com>
2020-11-29 11:27:55 +0200

    Remove redundant imports
    Signed-off-by: abigailt <abigailt@il.ibm.com>

--- tests/attacks/inference/attribute_inference/test_black_box.py
+++ tests/attacks/inference/attribute_inference/test_black_box.py
@@ -26,10 +26,6 @@ import torch.optim as optim
 from sklearn.tree import DecisionTreeClassifier
 
 from art.attacks.inference.attribute_inference.black_box import AttributeInferenceBlackBox
-from art.attacks.inference.attribute_inference.white_box_decision_tree import AttributeInferenceWhiteBoxDecisionTree
-from art.attacks.inference.attribute_inference.white_box_lifestyle_decision_tree import (
-    AttributeInferenceWhiteBoxLifestyleDecisionTree,
-)
 from art.estimators.classification.pytorch import PyTorchClassifier
 from art.estimators.estimator import BaseEstimator
 from art.estimators.classification import ClassifierMixin
