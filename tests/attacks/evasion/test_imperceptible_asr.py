60caf98aff353d4848c80fb28ca86e92520586a8
Hessel Tuinhof <hessel.tuinhof@ibm.com>
2020-12-04 12:42:54 +0000

    Reapply: Add support for PyTorch estimators
    Commit 97c73af203e7a88a02c4bd67efa117bf47bed9b1 was not correctly
    applied during recent 1.5.0 release.
    
    Signed-off-by: Hessel Tuinhof <hessel.tuinhof@ibm.com>

--- tests/attacks/evasion/test_imperceptible_asr.py
+++ tests/attacks/evasion/test_imperceptible_asr.py
@@ -184,6 +184,24 @@ class TestImperceptibleASR:
         except ARTTestException as e:
             art_warning(e)
 
+    @pytest.mark.skipMlFramework("tensorflow", "mxnet", "kerastf", "non_dl_frameworks")
+    def test_loss_gradient_masking_threshold_torch(self, art_warning, asr_dummy_estimator, audio_batch_padded):
+        try:
+            test_delta = audio_batch_padded
+            test_psd_maximum = np.ones((test_delta.shape[0], 28))
+            test_masking_threshold = np.zeros((test_delta.shape[0], 1025, 28))
+
+            imperceptible_asr = ImperceptibleASR(
+                estimator=asr_dummy_estimator(), masker=PsychoacousticMasker(),
+            )
+            loss_gradient, loss = imperceptible_asr._loss_gradient_masking_threshold_torch(
+                test_delta, test_psd_maximum, test_masking_threshold)
+
+            assert loss_gradient.shape == test_delta.shape
+            assert loss.ndim == 1 and loss.shape[0] == test_delta.shape[0]
+        except ARTTestException as e:
+            art_warning(e)
+
     @pytest.mark.skipMlFramework("pytorch", "tensorflow1", "tensorflow2", "mxnet", "kerastf", "non_dl_frameworks")
     def test_approximate_power_spectral_density_tf(self, art_warning, asr_dummy_estimator, audio_batch_padded):
         try:
@@ -206,6 +224,29 @@ class TestImperceptibleASR:
             )
             with tf1.Session() as sess:
                 psd_approximated = sess.run(approximate_psd_tf, feed_dict)
+
+            assert psd_approximated.ndim == 3
+            assert psd_approximated.shape[0] == test_delta.shape[0]  # batch_size
+            assert psd_approximated.shape[1] == masker.window_size // 2 + 1
+        except ARTTestException as e:
+            art_warning(e)
+
+    @pytest.mark.skipMlFramework("tensorflow", "mxnet", "kerastf", "non_dl_frameworks")
+    def test_approximate_power_spectral_density_torch(self, art_warning, asr_dummy_estimator, audio_batch_padded):
+        try:
+            import torch
+
+            test_delta = audio_batch_padded
+            test_psd_maximum = np.ones((test_delta.shape[0], 28))
+
+            masker = PsychoacousticMasker()
+            imperceptible_asr = ImperceptibleASR(
+                estimator=asr_dummy_estimator(), masker=masker,
+            )
+            approximate_psd_torch = imperceptible_asr._approximate_power_spectral_density_torch(
+                torch.from_numpy(test_delta), torch.from_numpy(test_psd_maximum)
+            )
+            psd_approximated = approximate_psd_torch.numpy()
 
             assert psd_approximated.ndim == 3
             assert psd_approximated.shape[0] == test_delta.shape[0]  # batch_size
