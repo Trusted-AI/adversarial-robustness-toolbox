2451713bdb41d3970693728006c9c04b6aa3c7f6
Ebube Chuba <ebube.chuba@ibm.com>
2020-12-10 14:43:44 -0800

    correct release year

--- tests/defences/detector/poison/test_roni.py
+++ tests/defences/detector/poison/test_roni.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (C) The Adversarial Robustness Toolbox (ART) Authors 2020
+# Copyright (C) The Adversarial Robustness Toolbox (ART) Authors 2018
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
