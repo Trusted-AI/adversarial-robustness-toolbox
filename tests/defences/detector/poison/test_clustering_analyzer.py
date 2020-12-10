ca2a6d35ed279f4f6eab72374dc8f03b3cd47f11
Ebube Chuba <ebube.chuba@ibm.com>
2020-12-10 14:51:01 -0800

    match ART test conventions
    Signed-off-by: Ebube Chuba <ebube.chuba@ibm.com>

--- tests/defences/detector/poison/test_clustering_analyzer.py
+++ tests/defences/detector/poison/test_clustering_analyzer.py
@@ -23,124 +23,138 @@ import numpy as np
 import pytest
 
 from art.defences.detector.poison import ClusteringAnalyzer
+from conftest import art_warning
+from tests.utils import ARTTestException
 
 logger = logging.getLogger(__name__)
 
 
+@pytest.mark.framework_agnostic
 def test_size_analyzer():
-    nb_clusters = 2
-    nb_classes = 3
-    clusters_by_class = [[0 for _ in range(nb_clusters)] for _ in range(nb_classes)]
+    try:
+        nb_clusters = 2
+        nb_classes = 3
+        clusters_by_class = [[0 for _ in range(nb_clusters)] for _ in range(nb_classes)]
 
-    clusters_by_class[0] = [0, 1, 1, 1, 1]  # Class 0
-    clusters_by_class[1] = [1, 0, 0, 0, 0]  # Class 1
-    clusters_by_class[2] = [0, 0, 0, 0, 1]  # Class 2
-    analyzer = ClusteringAnalyzer()
-    assigned_clean_by_class, poison_clusters, report = analyzer.analyze_by_size(clusters_by_class)
+        clusters_by_class[0] = [0, 1, 1, 1, 1]  # Class 0
+        clusters_by_class[1] = [1, 0, 0, 0, 0]  # Class 1
+        clusters_by_class[2] = [0, 0, 0, 0, 1]  # Class 2
+        analyzer = ClusteringAnalyzer()
+        assigned_clean_by_class, poison_clusters, report = analyzer.analyze_by_size(clusters_by_class)
 
-    clean = 0
-    poison = 1
-    # For class 0, cluster 0 should be marked as poison.
-    assert poison_clusters[0][0] == poison
-    # For class 0, cluster 1 should be marked as clean.
-    assert poison_clusters[0][1] == clean
-    assert report["Class_0"]["cluster_0"]["suspicious_cluster"]
-    assert not report["Class_0"]["cluster_1"]["suspicious_cluster"]
-    total = len(clusters_by_class[0])
-    c1 = sum(clusters_by_class[0])
-    assert report["Class_0"]["cluster_0"]["ptc_data_in_cluster"] == (total - c1) / total
-    assert report["Class_0"]["cluster_1"]["ptc_data_in_cluster"] == c1 / total
+        clean = 0
+        poison = 1
+        # For class 0, cluster 0 should be marked as poison.
+        assert poison_clusters[0][0] == poison
+        # For class 0, cluster 1 should be marked as clean.
+        assert poison_clusters[0][1] == clean
+        assert report["Class_0"]["cluster_0"]["suspicious_cluster"]
+        assert not report["Class_0"]["cluster_1"]["suspicious_cluster"]
+        total = len(clusters_by_class[0])
+        c1 = sum(clusters_by_class[0])
+        assert report["Class_0"]["cluster_0"]["ptc_data_in_cluster"] == (total - c1) / total
+        assert report["Class_0"]["cluster_1"]["ptc_data_in_cluster"] == c1 / total
 
-    # Inverse relations for class 1
-    assert poison_clusters[1][0] == clean
-    assert poison_clusters[1][1] == poison
-    assert not report["Class_1"]["cluster_0"]["suspicious_cluster"]
-    assert report["Class_1"]["cluster_1"]["suspicious_cluster"]
-    total = len(clusters_by_class[1])
-    c1 = sum(clusters_by_class[1])
-    assert report["Class_1"]["cluster_0"]["ptc_data_in_cluster"] == (total - c1) / total
-    assert report["Class_1"]["cluster_1"]["ptc_data_in_cluster"] == c1 / total
+        # Inverse relations for class 1
+        assert poison_clusters[1][0] == clean
+        assert poison_clusters[1][1] == poison
+        assert not report["Class_1"]["cluster_0"]["suspicious_cluster"]
+        assert report["Class_1"]["cluster_1"]["suspicious_cluster"]
+        total = len(clusters_by_class[1])
+        c1 = sum(clusters_by_class[1])
+        assert report["Class_1"]["cluster_0"]["ptc_data_in_cluster"] == (total - c1) / total
+        assert report["Class_1"]["cluster_1"]["ptc_data_in_cluster"] == c1 / total
 
-    assert poison_clusters[2][0] == clean
-    assert poison_clusters[2][1] == poison
-    assert not report["Class_2"]["cluster_0"]["suspicious_cluster"]
-    assert report["Class_2"]["cluster_1"]["suspicious_cluster"]
-    total = len(clusters_by_class[2])
-    c1 = sum(clusters_by_class[2])
-    assert report["Class_2"]["cluster_0"]["ptc_data_in_cluster"] == (total - c1) / total
-    assert report["Class_2"]["cluster_1"]["ptc_data_in_cluster"] == c1 / total
+        assert poison_clusters[2][0] == clean
+        assert poison_clusters[2][1] == poison
+        assert not report["Class_2"]["cluster_0"]["suspicious_cluster"]
+        assert report["Class_2"]["cluster_1"]["suspicious_cluster"]
+        total = len(clusters_by_class[2])
+        c1 = sum(clusters_by_class[2])
+        assert report["Class_2"]["cluster_0"]["ptc_data_in_cluster"] == (total - c1) / total
+        assert report["Class_2"]["cluster_1"]["ptc_data_in_cluster"] == c1 / total
 
-    poison = 0
-    assert assigned_clean_by_class[0][0] == poison
-    assert assigned_clean_by_class[1][0] == poison
-    assert assigned_clean_by_class[2][4] == poison
+        poison = 0
+        assert assigned_clean_by_class[0][0] == poison
+        assert assigned_clean_by_class[1][0] == poison
+        assert assigned_clean_by_class[2][4] == poison
+    except ARTTestException as e:
+        art_warning(e)
 
 
+@pytest.mark.framework_agnostic
 def test_size_analyzer_three():
-    nb_clusters = 3
-    nb_classes = 3
-    clusters_by_class = [[0 for _ in range(nb_clusters)] for _ in range(nb_classes)]
+    try:
+        nb_clusters = 3
+        nb_classes = 3
+        clusters_by_class = [[0 for _ in range(nb_clusters)] for _ in range(nb_classes)]
 
-    clusters_by_class[0] = [0, 1, 1, 2, 2]  # Class 0
-    clusters_by_class[1] = [1, 0, 0, 2, 2]  # Class 1
-    clusters_by_class[2] = [0, 0, 0, 2, 1, 1]  # Class 2
-    analyzer = ClusteringAnalyzer()
-    assigned_clean_by_class, poison_clusters, report = analyzer.analyze_by_size(clusters_by_class)
+        clusters_by_class[0] = [0, 1, 1, 2, 2]  # Class 0
+        clusters_by_class[1] = [1, 0, 0, 2, 2]  # Class 1
+        clusters_by_class[2] = [0, 0, 0, 2, 1, 1]  # Class 2
+        analyzer = ClusteringAnalyzer()
+        assigned_clean_by_class, poison_clusters, report = analyzer.analyze_by_size(clusters_by_class)
 
-    clean = 0
-    poison = 1
-    # For class 0, cluster 0 should be marked as poison.
-    assert poison_clusters[0][0] == poison
-    # For class 0, cluster 1 and 2 should be marked as clean.
-    assert poison_clusters[0][1] == clean
-    assert poison_clusters[0][2] == clean
-    assert report["Class_0"]["cluster_0"]["suspicious_cluster"]
-    assert not report["Class_0"]["cluster_1"]["suspicious_cluster"]
-    assert not report["Class_0"]["cluster_2"]["suspicious_cluster"]
-    total = len(clusters_by_class[0])
-    counts = np.bincount(clusters_by_class[0])
-    assert report["Class_0"]["cluster_0"]["ptc_data_in_cluster"] == counts[0] / total
-    assert report["Class_0"]["cluster_1"]["ptc_data_in_cluster"] == counts[1] / total
-    assert report["Class_0"]["cluster_2"]["ptc_data_in_cluster"] == counts[2] / total
+        clean = 0
+        poison = 1
+        # For class 0, cluster 0 should be marked as poison.
+        assert poison_clusters[0][0] == poison
+        # For class 0, cluster 1 and 2 should be marked as clean.
+        assert poison_clusters[0][1] == clean
+        assert poison_clusters[0][2] == clean
+        assert report["Class_0"]["cluster_0"]["suspicious_cluster"]
+        assert not report["Class_0"]["cluster_1"]["suspicious_cluster"]
+        assert not report["Class_0"]["cluster_2"]["suspicious_cluster"]
+        total = len(clusters_by_class[0])
+        counts = np.bincount(clusters_by_class[0])
+        assert report["Class_0"]["cluster_0"]["ptc_data_in_cluster"] == counts[0] / total
+        assert report["Class_0"]["cluster_1"]["ptc_data_in_cluster"] == counts[1] / total
+        assert report["Class_0"]["cluster_2"]["ptc_data_in_cluster"] == counts[2] / total
 
-    assert poison_clusters[1][0] == clean
-    assert poison_clusters[1][1] == poison
-    assert poison_clusters[1][2] == clean
-    assert not report["Class_1"]["cluster_0"]["suspicious_cluster"]
-    assert report["Class_1"]["cluster_1"]["suspicious_cluster"]
-    assert not report["Class_1"]["cluster_2"]["suspicious_cluster"]
-    total = len(clusters_by_class[1])
-    counts = np.bincount(clusters_by_class[1])
-    assert report["Class_1"]["cluster_0"]["ptc_data_in_cluster"] == counts[0] / total
-    assert report["Class_1"]["cluster_1"]["ptc_data_in_cluster"] == counts[1] / total
-    assert report["Class_1"]["cluster_2"]["ptc_data_in_cluster"] == counts[2] / total
+        assert poison_clusters[1][0] == clean
+        assert poison_clusters[1][1] == poison
+        assert poison_clusters[1][2] == clean
+        assert not report["Class_1"]["cluster_0"]["suspicious_cluster"]
+        assert report["Class_1"]["cluster_1"]["suspicious_cluster"]
+        assert not report["Class_1"]["cluster_2"]["suspicious_cluster"]
+        total = len(clusters_by_class[1])
+        counts = np.bincount(clusters_by_class[1])
+        assert report["Class_1"]["cluster_0"]["ptc_data_in_cluster"] == counts[0] / total
+        assert report["Class_1"]["cluster_1"]["ptc_data_in_cluster"] == counts[1] / total
+        assert report["Class_1"]["cluster_2"]["ptc_data_in_cluster"] == counts[2] / total
 
-    assert poison_clusters[2][0] == clean
-    assert poison_clusters[2][1] == clean
-    assert poison_clusters[2][2] == poison
-    assert not report["Class_2"]["cluster_0"]["suspicious_cluster"]
-    assert not report["Class_2"]["cluster_1"]["suspicious_cluster"]
-    assert report["Class_2"]["cluster_2"]["suspicious_cluster"]
-    total = len(clusters_by_class[2])
-    counts = np.bincount(clusters_by_class[2])
-    assert report["Class_2"]["cluster_0"]["ptc_data_in_cluster"] == round(counts[0] / total, 2)
-    assert report["Class_2"]["cluster_1"]["ptc_data_in_cluster"] == round(counts[1] / total, 2)
-    assert report["Class_2"]["cluster_2"]["ptc_data_in_cluster"] == round(counts[2] / total, 2)
+        assert poison_clusters[2][0] == clean
+        assert poison_clusters[2][1] == clean
+        assert poison_clusters[2][2] == poison
+        assert not report["Class_2"]["cluster_0"]["suspicious_cluster"]
+        assert not report["Class_2"]["cluster_1"]["suspicious_cluster"]
+        assert report["Class_2"]["cluster_2"]["suspicious_cluster"]
+        total = len(clusters_by_class[2])
+        counts = np.bincount(clusters_by_class[2])
+        assert report["Class_2"]["cluster_0"]["ptc_data_in_cluster"] == round(counts[0] / total, 2)
+        assert report["Class_2"]["cluster_1"]["ptc_data_in_cluster"] == round(counts[1] / total, 2)
+        assert report["Class_2"]["cluster_2"]["ptc_data_in_cluster"] == round(counts[2] / total, 2)
 
-    poison = 0
-    assert assigned_clean_by_class[0][0] == poison
-    assert assigned_clean_by_class[1][0] == poison
-    assert assigned_clean_by_class[2][3] == poison
+        poison = 0
+        assert assigned_clean_by_class[0][0] == poison
+        assert assigned_clean_by_class[1][0] == poison
+        assert assigned_clean_by_class[2][3] == poison
+    except ARTTestException as e:
+        art_warning(e)
 
 
+@pytest.mark.framework_agnostic
 def test_relative_size_analyzer_three():
-    nb_clusters = 3
-    nb_classes = 3
-    clusters_by_class = [[0 for _ in range(nb_clusters)] for _ in range(nb_classes)]
+    try:
+        nb_clusters = 3
+        nb_classes = 3
+        clusters_by_class = [[0 for _ in range(nb_clusters)] for _ in range(nb_classes)]
 
-    clusters_by_class[0] = [0, 1, 1, 2, 2]  # Class 0
-    clusters_by_class[1] = [1, 0, 0, 2, 2]  # Class 1
-    clusters_by_class[2] = [0, 0, 0, 2, 1, 1]  # Class 2
-    analyzer = ClusteringAnalyzer()
-    with pytest.raises(ValueError):
-        analyzer.analyze_by_relative_size(clusters_by_class)
+        clusters_by_class[0] = [0, 1, 1, 2, 2]  # Class 0
+        clusters_by_class[1] = [1, 0, 0, 2, 2]  # Class 1
+        clusters_by_class[2] = [0, 0, 0, 2, 1, 1]  # Class 2
+        analyzer = ClusteringAnalyzer()
+        with pytest.raises(ValueError):
+            analyzer.analyze_by_relative_size(clusters_by_class)
+    except ARTTestException as e:
+        art_warning(e)
