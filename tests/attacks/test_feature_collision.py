b2114202bc5b94af3e15ea89ba5eb24cce2878d1
killian <killian.levacher@ibm.com>
2020-12-11 14:57:48 +0000

    put back feature_collision
    Signed-off-by: killian <killian.levacher@ibm.com>

--- tests/attacks/test_feature_collision.py
+++ tests/attacks/test_feature_collision.py
@@ -18,77 +18,32 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import logging
-import unittest
-
 import numpy as np
 
 from art.attacks.poisoning.feature_collision_attack import FeatureCollisionAttack
 
-from tests.utils import TestBase, master_seed, get_image_classifier_kr  # , get_image_classifier_tf
+from tests.utils import ARTTestException
 
 logger = logging.getLogger(__name__)
 
-NB_EPOCHS = 3
 
+def test_feature_collision(art_warning, get_default_mnist_subset, image_dl_estimator_for_attack):
+    try:
+        (x_train_mnist, y_train_mnist), (_, _) = get_default_mnist_subset
+        classifier, _ = image_dl_estimator_for_attack(FeatureCollisionAttack)
 
-class TestFeatureCollision(TestBase):
-    """
-    A unittest class for testing Feature Collision attack.
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        master_seed(seed=1234)
-        super().setUpClass()
-
-        cls.n_train = 10
-        cls.n_test = 10
-        cls.x_train_mnist = cls.x_train_mnist[0 : cls.n_train]
-        cls.y_train_mnist = cls.y_train_mnist[0 : cls.n_train]
-        cls.x_test_mnist = cls.x_test_mnist[0 : cls.n_test]
-        cls.y_test_mnist = cls.y_test_mnist[0 : cls.n_test]
-
-    def setUp(self):
-        master_seed(seed=301)
-        super().setUp()
-
-    @staticmethod
-    def poison_dataset(classifier, x_clean, y_clean):
-        x_poison = np.copy(x_clean)
-        y_poison = np.copy(y_clean)
-        base = np.expand_dims(x_clean[0], axis=0)
-        target = np.expand_dims(x_clean[1], axis=0)
+        # poison dataset
+        x_poison = np.copy(x_train_mnist)
+        y_poison = np.copy(y_train_mnist)
+        base = np.expand_dims(x_train_mnist[0], axis=0)
+        target = np.expand_dims(x_train_mnist[1], axis=0)
         feature_layer = classifier.layer_names[-1]
         attack = FeatureCollisionAttack(classifier, target, feature_layer, max_iter=1)
         attack, attack_label = attack.poison(base)
         x_poison = np.append(x_poison, attack, axis=0)
         y_poison = np.append(y_poison, attack_label, axis=0)
 
-        return x_poison, y_poison
-
-    # def test_tensorflow(self):
-    #     """
-    #     First test with the TensorFlowClassifier.
-    #     :return:
-    #     """
-    #
-    #     tfc, sess = get_image_classifier_tf()
-    #     x_adv, y_adv = self.poison_dataset(tfc, self.x_train_mnist, self.y_train_mnist)
-    #     tfc.fit(x_adv, y_adv, nb_epochs=NB_EPOCHS, batch_size=32)
-    #
-    #     if sess is not None:
-    #         sess.close()
-
-    def test_keras(self):
-        """
-        Test working keras implementation.
-        :return:
-        """
-
-        krc = get_image_classifier_kr()
-        x_adv, y_adv = self.poison_dataset(krc, self.x_train_mnist, self.y_train_mnist)
-        krc.fit(x_adv, y_adv, nb_epochs=NB_EPOCHS, batch_size=32)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        classifier.fit(x_poison, y_poison, nb_epochs=3, batch_size=32)
+        # TODO nothing is really been tested in this test
+    except ARTTestException as e:
+        art_warning(e)
