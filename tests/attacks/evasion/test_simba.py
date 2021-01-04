98faef619a6d556146560bb75763a8cf4d9ded64
killian <killian.levacher@ibm.com>
2021-01-04 16:18:04 +0000

    finished converting simba
    Signed-off-by: killian <killian.levacher@ibm.com>

--- tests/attacks/evasion/test_simba.py
+++ tests/attacks/evasion/test_simba.py
@@ -46,7 +46,7 @@ def fix_get_mnist_subset(get_mnist_dataset):
 @pytest.mark.parametrize("targeted", [False, True])
 def test_mnist(fix_get_mnist_subset, image_dl_estimator, targeted):
     estimator, _ = image_dl_estimator(from_logits=False)
-    
+
     (x_train_mnist, y_train_mnist, x_test_mnist, y_test_mnist) = fix_get_mnist_subset
 
     x_test_original = x_test_mnist.copy()
