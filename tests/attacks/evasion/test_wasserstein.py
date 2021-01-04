d8a8fe8348c674a99108f18d49a748d859039a4d
killian <killian.levacher@ibm.com>
2021-01-04 15:16:40 +0000

    started with first test in wasserstein
    Signed-off-by: killian <killian.levacher@ibm.com>

--- tests/attacks/evasion/test_wasserstein.py
+++ tests/attacks/evasion/test_wasserstein.py
@@ -18,9 +18,9 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import logging
-import unittest
 
 import numpy as np
+import pytest
 
 from art.attacks.evasion.wasserstein import Wasserstein
 from art.estimators.estimator import BaseEstimator, LossGradientsMixin
@@ -33,142 +33,140 @@ from tests.attacks.utils import backend_test_classifier_type_check_fail
 
 logger = logging.getLogger(__name__)
 
-#
-# class TestWasserstein(TestBase):
-#     @classmethod
-#     def setUpClass(cls):
-#         super().setUpClass()
-#
-#         cls.n_train = 10
-#         cls.n_test = 10
-#         cls.x_train_mnist = cls.x_train_mnist[0: cls.n_train]
-#         cls.y_train_mnist = cls.y_train_mnist[0: cls.n_train]
-#         cls.x_test_mnist = cls.x_test_mnist[0: cls.n_test]
-#         cls.y_test_mnist = cls.y_test_mnist[0: cls.n_test]
-#
-#     def test_tensorflow_mnist(self):
-#         classifier, sess = get_image_classifier_tf()
-#
-#         scores = get_labels_np_array(classifier.predict(self.x_train_mnist))
-#         acc = np.sum(np.argmax(scores, axis=1) == np.argmax(self.y_train_mnist, axis=1)) / self.y_train_mnist.shape[0]
-#         logger.info("[TF, MNIST] Accuracy on training set: %.2f%%", acc * 100)
-#
-#         scores = get_labels_np_array(classifier.predict(self.x_test_mnist))
-#         acc = np.sum(np.argmax(scores, axis=1) == np.argmax(self.y_test_mnist, axis=1)) / self.y_test_mnist.shape[0]
-#         logger.info("[TF, MNIST] Accuracy on test set: %.2f%%", acc * 100)
-#
-#         self._test_backend_mnist(
-#             classifier, self.x_train_mnist, self.y_train_mnist, self.x_test_mnist, self.y_test_mnist
-#         )
-#
-#     def _test_backend_mnist(self, classifier, x_train, y_train, x_test, y_test):
-#         base_success_rate = 0.1
-#         num_iter = 5
-#         regularization = 100
-#         batch_size = 5
-#         eps = 0.3
-#
-#         # Test Wasserstein with wasserstein ball and wasserstein norm
-#         attack = Wasserstein(
-#             classifier,
-#             regularization=regularization,
-#             max_iter=num_iter,
-#             conjugate_sinkhorn_max_iter=num_iter,
-#             projected_sinkhorn_max_iter=num_iter,
-#             norm="wasserstein",
-#             ball="wasserstein",
-#             targeted=False,
-#             p=2,
-#             eps_iter=2,
-#             eps_factor=1.05,
-#             eps=eps,
-#             eps_step=0.1,
-#             kernel_size=5,
-#             batch_size=batch_size,
-#         )
-#
-#         x_train_adv = attack.generate(x_train)
-#         x_test_adv = attack.generate(x_test)
-#
-#         self.assertFalse((x_train_adv == x_train).all())
-#         self.assertFalse((x_test_adv == x_test).all())
-#
-#         train_y_pred = get_labels_np_array(classifier.predict(x_train_adv)).astype(float)
-#         test_y_pred = get_labels_np_array(classifier.predict(x_test_adv)).astype(float)
-#
-#         train_success_rate = (
-#                 np.sum(np.argmax(train_y_pred, axis=1) != np.argmax(classifier.predict(x_train), axis=1)) /
-#                 y_train.shape[0]
-#         )
-#         self.assertGreaterEqual(train_success_rate, base_success_rate)
-#
-#         test_success_rate = (
-#                 np.sum(np.argmax(test_y_pred, axis=1) != np.argmax(classifier.predict(x_test), axis=1)) / y_test.shape[
-#             0]
-#         )
-#         self.assertGreaterEqual(test_success_rate, base_success_rate)
-#
-#     def test_classifier_type_check_fail(self):
-#         backend_test_classifier_type_check_fail(Wasserstein, (BaseEstimator, LossGradientsMixin, ClassifierMixin))
-#
-#     def test_unsquared_images(self):
-#         from art.estimators.estimator import (
-#             BaseEstimator,
-#             LossGradientsMixin,
-#             NeuralNetworkMixin,
-#         )
-#
-#         from art.estimators.classification.classifier import (
-#             ClassGradientsMixin,
-#             ClassifierMixin,
-#         )
-#
-#         class DummyClassifier(
-#             ClassGradientsMixin, ClassifierMixin, NeuralNetworkMixin, LossGradientsMixin, BaseEstimator
-#         ):
-#             def __init__(self):
-#                 super(DummyClassifier, self).__init__(model=None, clip_values=None, channels_first=True)
-#                 self._nb_classes = 10
-#
-#             def class_gradient(self):
-#                 return None
-#
-#             def fit(self):
-#                 pass
-#
-#             def loss_gradient(self, x, y):
-#                 return np.random.normal(size=(1, 3, 33, 32))
-#
-#             def predict(self, x, batch_size=1):
-#                 return np.array([[0, 1, 0, 0, 0, 0, 0, 0, 0, 0]])
-#
-#             def get_activations(self):
-#                 return None
-#
-#             def save(self):
-#                 pass
-#
-#             def loss(self, x, y, **kwargs):
-#                 pass
-#
-#             def set_learning_phase(self):
-#                 pass
-#
-#             def input_shape(self):
-#                 pass
-#
-#         classifier = DummyClassifier()
-#         attack = Wasserstein(
-#             classifier,
-#             regularization=1,
-#             kernel_size=3,
-#             max_iter=1,
-#             conjugate_sinkhorn_max_iter=10,
-#             projected_sinkhorn_max_iter=10,
-#         )
-#
-#         x = np.random.normal(size=(1, 3, 33, 32))
-#         x_adv = attack.generate(x)
-#
-#         self.assertTrue(x_adv.shape == x.shape)
 
+@pytest.fixture()
+def fix_get_mnist_subset(get_mnist_dataset):
+    (x_train_mnist, y_train_mnist), (x_test_mnist, y_test_mnist) = get_mnist_dataset
+    n_train = 10
+    n_test = 10
+    yield x_train_mnist[:n_train], y_train_mnist[:n_train], x_test_mnist[:n_test], y_test_mnist[:n_test]
+
+
+def test_mnist(fix_get_mnist_subset):
+    (x_train_mnist, y_train_mnist, x_test_mnist, y_test_mnist) = fix_get_mnist_subset
+
+    estimator, sess = get_image_classifier_tf()
+
+    scores = get_labels_np_array(estimator.predict(x_train_mnist))
+    acc = np.sum(np.argmax(scores, axis=1) == np.argmax(y_train_mnist, axis=1)) / y_train_mnist.shape[0]
+    logger.info("[TF, MNIST] Accuracy on training set: %.2f%%", acc * 100)
+
+    scores = get_labels_np_array(estimator.predict(x_test_mnist))
+    acc = np.sum(np.argmax(scores, axis=1) == np.argmax(y_test_mnist, axis=1)) / y_test_mnist.shape[0]
+    logger.info("[TF, MNIST] Accuracy on test set: %.2f%%", acc * 100)
+
+    # _test_backend_mnist(estimator, x_train_mnist, y_train_mnist, x_test_mnist, y_test_mnist)
+
+
+# def _test_backend_mnist(classifier, x_train, y_train, x_test, y_test):
+#     base_success_rate = 0.1
+#     num_iter = 5
+#     regularization = 100
+#     batch_size = 5
+#     eps = 0.3
+#
+#     # Test Wasserstein with wasserstein ball and wasserstein norm
+#     attack = Wasserstein(
+#         classifier,
+#         regularization=regularization,
+#         max_iter=num_iter,
+#         conjugate_sinkhorn_max_iter=num_iter,
+#         projected_sinkhorn_max_iter=num_iter,
+#         norm="wasserstein",
+#         ball="wasserstein",
+#         targeted=False,
+#         p=2,
+#         eps_iter=2,
+#         eps_factor=1.05,
+#         eps=eps,
+#         eps_step=0.1,
+#         kernel_size=5,
+#         batch_size=batch_size,
+#     )
+#
+#     x_train_adv = attack.generate(x_train)
+#     x_test_adv = attack.generate(x_test)
+#
+#     self.assertFalse((x_train_adv == x_train).all())
+#     self.assertFalse((x_test_adv == x_test).all())
+#
+#     train_y_pred = get_labels_np_array(classifier.predict(x_train_adv)).astype(float)
+#     test_y_pred = get_labels_np_array(classifier.predict(x_test_adv)).astype(float)
+#
+#     train_success_rate = (
+#             np.sum(np.argmax(train_y_pred, axis=1) != np.argmax(classifier.predict(x_train), axis=1)) /
+#             y_train.shape[0]
+#     )
+#     self.assertGreaterEqual(train_success_rate, base_success_rate)
+#
+#     test_success_rate = (
+#             np.sum(np.argmax(test_y_pred, axis=1) != np.argmax(classifier.predict(x_test), axis=1)) / y_test.shape[
+#         0]
+#     )
+#     self.assertGreaterEqual(test_success_rate, base_success_rate)
+
+
+# def test_classifier_type_check_fail(self):
+#     backend_test_classifier_type_check_fail(Wasserstein, (BaseEstimator, LossGradientsMixin, ClassifierMixin))
+#
+#
+# def test_unsquared_images(self):
+#     from art.estimators.estimator import (
+#         BaseEstimator,
+#         LossGradientsMixin,
+#         NeuralNetworkMixin,
+#     )
+#
+#     from art.estimators.classification.classifier import (
+#         ClassGradientsMixin,
+#         ClassifierMixin,
+#     )
+#
+#     class DummyClassifier(
+#         ClassGradientsMixin, ClassifierMixin, NeuralNetworkMixin, LossGradientsMixin, BaseEstimator
+#     ):
+#         def __init__(self):
+#             super(DummyClassifier, self).__init__(model=None, clip_values=None, channels_first=True)
+#             self._nb_classes = 10
+#
+#         def class_gradient(self):
+#             return None
+#
+#         def fit(self):
+#             pass
+#
+#         def loss_gradient(self, x, y):
+#             return np.random.normal(size=(1, 3, 33, 32))
+#
+#         def predict(self, x, batch_size=1):
+#             return np.array([[0, 1, 0, 0, 0, 0, 0, 0, 0, 0]])
+#
+#         def get_activations(self):
+#             return None
+#
+#         def save(self):
+#             pass
+#
+#         def loss(self, x, y, **kwargs):
+#             pass
+#
+#         def set_learning_phase(self):
+#             pass
+#
+#         def input_shape(self):
+#             pass
+#
+#     classifier = DummyClassifier()
+#     attack = Wasserstein(
+#         classifier,
+#         regularization=1,
+#         kernel_size=3,
+#         max_iter=1,
+#         conjugate_sinkhorn_max_iter=10,
+#         projected_sinkhorn_max_iter=10,
+#     )
+#
+#     x = np.random.normal(size=(1, 3, 33, 32))
+#     x_adv = attack.generate(x)
+#
+#     self.assertTrue(x_adv.shape == x.shape)
