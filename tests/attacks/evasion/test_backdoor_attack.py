d9464a9ed9c51a04d0011126d01e0b02f99efb1d
killian <killian.levacher@ibm.com>
2021-01-05 15:56:57 +0000

    added another test
    Signed-off-by: killian <killian.levacher@ibm.com>

--- tests/attacks/evasion/test_backdoor_attack.py
+++ tests/attacks/evasion/test_backdoor_attack.py
@@ -34,136 +34,136 @@ logger = logging.getLogger(__name__)
 PP_POISON = 0.33
 NB_EPOCHS = 3
 
-#
-# @pytest.fixture()
-# def fix_get_mnist_subset(get_mnist_dataset):
-#     (x_train_mnist, y_train_mnist), (x_test_mnist, y_test_mnist) = get_mnist_dataset
-#     n_train = 1000
-#     n_test = 100
-#     yield x_train_mnist[:n_train], y_train_mnist[:n_train], x_test_mnist[:n_test], y_test_mnist[:n_test]
-#
-#
-# def test_backdoor_pattern(fix_get_mnist_subset, image_dl_estimator):
-#     """
-#     Test the backdoor attack with a pattern-based perturbation can be trained on classifier
-#     """
-#
-#     poison = Poison(fix_get_mnist_subset)
-#
-#     # krc = get_image_classifier_kr() from_logits=False
-#     estimator, _ = image_dl_estimator()
-#
-#     _back_end(fix_get_mnist_subset, estimator, poison.poison_func_1)
-#
-#
-# def test_backdoor_pixel(fix_get_mnist_subset, image_dl_estimator):
-#     """
-#     Test the backdoor attack with a pixel-based perturbation can be trained on classifier
-#     """
-#     poison = Poison(fix_get_mnist_subset)
-#
-#     # krc = get_image_classifier_kr() from_logits=False
-#     estimator, _ = image_dl_estimator()
-#
-#     _back_end(fix_get_mnist_subset, estimator, poison.poison_func_2)
-#
-#
-# def test_backdoor_image(fix_get_mnist_subset, image_dl_estimator):
-#     """
-#     Test the backdoor attack with a image-based perturbation can be trained on classifier
-#     """
-#     poison = Poison(fix_get_mnist_subset)
-#
-#     # krc = get_image_classifier_kr() from_logits=False
-#     estimator, _ = image_dl_estimator()
-#
-#     _back_end(fix_get_mnist_subset, estimator, poison.poison_func_3)
-#
-#
-# def _back_end(fix_get_mnist_subset, estimator, poison_function):
-#     (x_train_mnist, y_train_mnist, x_test_mnist, y_test_mnist) = fix_get_mnist_subset
-#
-#     (is_poison_train, x_poisoned_raw, y_poisoned_raw) = poison_dataset(
-#         x_train_mnist, y_train_mnist, poison_function)
-#     # Shuffle training data
-#     n_train = np.shape(y_poisoned_raw)[0]
-#     shuffled_indices = np.arange(n_train)
-#     np.random.shuffle(shuffled_indices)
-#     x_train = x_poisoned_raw[shuffled_indices]
-#     y_train = y_poisoned_raw[shuffled_indices]
-#
-#     estimator.fit(x_train, y_train, nb_epochs=NB_EPOCHS, batch_size=32)
-#
-#
-# def poison_dataset(x_clean, y_clean, poison_func):
-#     x_poison = np.copy(x_clean)
-#     y_poison = np.copy(y_clean)
-#     is_poison = np.zeros(np.shape(y_poison)[0])
-#
-#     for i in range(10):
-#         src = i
-#         tgt = (i + 1) % 10
-#         n_points_in_tgt = np.round(np.sum(np.argmax(y_clean, axis=1) == tgt))
-#         num_poison = int((PP_POISON * n_points_in_tgt) / (1 - PP_POISON))
-#         src_imgs = np.copy(x_clean[np.argmax(y_clean, axis=1) == src])
-#
-#         n_points_in_src = np.shape(src_imgs)[0]
-#         if num_poison:
-#             indices_to_be_poisoned = np.random.choice(n_points_in_src, num_poison)
-#
-#             imgs_to_be_poisoned = src_imgs[indices_to_be_poisoned]
-#             backdoor_attack = PoisoningAttackBackdoor(poison_func)
-#             poison_images, poison_labels = backdoor_attack.poison(
-#                 imgs_to_be_poisoned, y=to_categorical(np.ones(num_poison) * tgt, 10)
-#             )
-#             x_poison = np.append(x_poison, poison_images, axis=0)
-#             y_poison = np.append(y_poison, poison_labels, axis=0)
-#             is_poison = np.append(is_poison, np.ones(num_poison))
-#
-#     is_poison = is_poison != 0
-#
-#     return is_poison, x_poison, y_poison
-#
-#
-# class Poison():
-#     def __init__(self, mnist_dataset):
-#         (self.x_train_mnist, self.y_train_mnist, self.x_test_mnist, self.y_test_mnist) = mnist_dataset
-#         self.backdoor_path = os.path.join(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))),
-#                                           "..",
-#                                           "utils",
-#                                           "data",
-#                                           "backdoors",
-#                                           "alert.png",
-#                                           )
-#
-#     def poison_func_1(self, x):
-#         max_val = np.max(self.x_train_mnist)
-#         return np.expand_dims(add_pattern_bd(x.squeeze(3), pixel_value=max_val), axis=3)
-#
-#     def poison_func_2(self, x):
-#         max_val = np.max(self.x_train_mnist)
-#         return np.expand_dims(add_single_bd(x.squeeze(3), pixel_value=max_val), axis=3)
-#
-#     def poison_func_3(self, x):
-#         return np.expand_dims(
-#             insert_image(
-#                 x.squeeze(3), backdoor_path=self.backdoor_path, size=(5, 5), random=False, x_shift=3, y_shift=3
-#             ),
-#             axis=3,
-#         )
-#
-#     def poison_func_4(self, x):
-#         return np.expand_dims(
-#             insert_image(x.squeeze(3), backdoor_path=self.backdoor_path, size=(5, 5), random=True), axis=3
-#         )
-#
-#     def poison_func_5(self, x):
-#         return np.expand_dims(
-#             insert_image(x.squeeze(3), backdoor_path=self.backdoor_path, random=True, size=(100, 100)), axis=3
-#         )
-#
-#     def poison_func_6(self, x):
-#         return np.expand_dims(insert_image(x, backdoor_path=self.backdoor_path, random=True, size=(100, 100)), axis=3)
+
+@pytest.fixture()
+def fix_get_mnist_subset(get_mnist_dataset):
+    (x_train_mnist, y_train_mnist), (x_test_mnist, y_test_mnist) = get_mnist_dataset
+    n_train = 1000
+    n_test = 100
+    yield x_train_mnist[:n_train], y_train_mnist[:n_train], x_test_mnist[:n_test], y_test_mnist[:n_test]
+
+
+def test_backdoor_pattern(fix_get_mnist_subset, image_dl_estimator):
+    """
+    Test the backdoor attack with a pattern-based perturbation can be trained on classifier
+    """
+
+    poison = Poison(fix_get_mnist_subset)
+
+    # krc = get_image_classifier_kr() from_logits=False
+    estimator, _ = image_dl_estimator()
+
+    _back_end(fix_get_mnist_subset, estimator, poison.poison_func_1)
+
+
+def test_backdoor_pixel(fix_get_mnist_subset, image_dl_estimator):
+    """
+    Test the backdoor attack with a pixel-based perturbation can be trained on classifier
+    """
+    poison = Poison(fix_get_mnist_subset)
+
+    # krc = get_image_classifier_kr() from_logits=False
+    estimator, _ = image_dl_estimator()
+
+    _back_end(fix_get_mnist_subset, estimator, poison.poison_func_2)
+
+
+def test_backdoor_image(fix_get_mnist_subset, image_dl_estimator):
+    """
+    Test the backdoor attack with a image-based perturbation can be trained on classifier
+    """
+    poison = Poison(fix_get_mnist_subset)
+
+    # krc = get_image_classifier_kr() from_logits=False
+    estimator, _ = image_dl_estimator()
+
+    _back_end(fix_get_mnist_subset, estimator, poison.poison_func_3)
+
+
+def _back_end(fix_get_mnist_subset, estimator, poison_function):
+    (x_train_mnist, y_train_mnist, x_test_mnist, y_test_mnist) = fix_get_mnist_subset
+
+    (is_poison_train, x_poisoned_raw, y_poisoned_raw) = poison_dataset(
+        x_train_mnist, y_train_mnist, poison_function)
+    # Shuffle training data
+    n_train = np.shape(y_poisoned_raw)[0]
+    shuffled_indices = np.arange(n_train)
+    np.random.shuffle(shuffled_indices)
+    x_train = x_poisoned_raw[shuffled_indices]
+    y_train = y_poisoned_raw[shuffled_indices]
+
+    estimator.fit(x_train, y_train, nb_epochs=NB_EPOCHS, batch_size=32)
+
+
+def poison_dataset(x_clean, y_clean, poison_func):
+    x_poison = np.copy(x_clean)
+    y_poison = np.copy(y_clean)
+    is_poison = np.zeros(np.shape(y_poison)[0])
+
+    for i in range(10):
+        src = i
+        tgt = (i + 1) % 10
+        n_points_in_tgt = np.round(np.sum(np.argmax(y_clean, axis=1) == tgt))
+        num_poison = int((PP_POISON * n_points_in_tgt) / (1 - PP_POISON))
+        src_imgs = np.copy(x_clean[np.argmax(y_clean, axis=1) == src])
+
+        n_points_in_src = np.shape(src_imgs)[0]
+        if num_poison:
+            indices_to_be_poisoned = np.random.choice(n_points_in_src, num_poison)
+
+            imgs_to_be_poisoned = src_imgs[indices_to_be_poisoned]
+            backdoor_attack = PoisoningAttackBackdoor(poison_func)
+            poison_images, poison_labels = backdoor_attack.poison(
+                imgs_to_be_poisoned, y=to_categorical(np.ones(num_poison) * tgt, 10)
+            )
+            x_poison = np.append(x_poison, poison_images, axis=0)
+            y_poison = np.append(y_poison, poison_labels, axis=0)
+            is_poison = np.append(is_poison, np.ones(num_poison))
+
+    is_poison = is_poison != 0
+
+    return is_poison, x_poison, y_poison
+
+
+class Poison():
+    def __init__(self, mnist_dataset):
+        (self.x_train_mnist, self.y_train_mnist, self.x_test_mnist, self.y_test_mnist) = mnist_dataset
+        self.backdoor_path = os.path.join(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))),
+                                          "..",
+                                          "utils",
+                                          "data",
+                                          "backdoors",
+                                          "alert.png",
+                                          )
+
+    def poison_func_1(self, x):
+        max_val = np.max(self.x_train_mnist)
+        return np.expand_dims(add_pattern_bd(x.squeeze(3), pixel_value=max_val), axis=3)
+
+    def poison_func_2(self, x):
+        max_val = np.max(self.x_train_mnist)
+        return np.expand_dims(add_single_bd(x.squeeze(3), pixel_value=max_val), axis=3)
+
+    def poison_func_3(self, x):
+        return np.expand_dims(
+            insert_image(
+                x.squeeze(3), backdoor_path=self.backdoor_path, size=(5, 5), random=False, x_shift=3, y_shift=3
+            ),
+            axis=3,
+        )
+
+    def poison_func_4(self, x):
+        return np.expand_dims(
+            insert_image(x.squeeze(3), backdoor_path=self.backdoor_path, size=(5, 5), random=True), axis=3
+        )
+
+    def poison_func_5(self, x):
+        return np.expand_dims(
+            insert_image(x.squeeze(3), backdoor_path=self.backdoor_path, random=True, size=(100, 100)), axis=3
+        )
+
+    def poison_func_6(self, x):
+        return np.expand_dims(insert_image(x, backdoor_path=self.backdoor_path, random=True, size=(100, 100)), axis=3)
 
 #
 #
