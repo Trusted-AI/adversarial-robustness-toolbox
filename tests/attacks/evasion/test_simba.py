8280556ab085e761de57890a723eedc7691e629e
killian <killian.levacher@ibm.com>
2021-01-06 17:16:13 +0000

    added tr except to simba
    Signed-off-by: killian <killian.levacher@ibm.com>

--- tests/attacks/evasion/test_simba.py
+++ tests/attacks/evasion/test_simba.py
@@ -27,9 +27,8 @@ from art.estimators.estimator import BaseEstimator
 from art.estimators.classification.classifier import ClassifierMixin
 from art.utils import get_labels_np_array
 
-from tests.utils import TestBase
-from tests.utils import get_image_classifier_tf, get_image_classifier_kr, get_image_classifier_pt
 from tests.attacks.utils import backend_test_classifier_type_check_fail
+from tests.utils import ARTTestException
 
 logger = logging.getLogger(__name__)
 
@@ -42,49 +41,54 @@ def fix_get_mnist_subset(get_mnist_dataset):
     yield x_train_mnist[:n_train], y_train_mnist[:n_train], x_test_mnist[:n_test], y_test_mnist[:n_test]
 
 
-@pytest.mark.skipMlFramework("mxnet", "scikitlearn", "tensorflow2v1")
 @pytest.mark.parametrize("targeted", [False, True])
-def test_mnist(fix_get_mnist_subset, image_dl_estimator, targeted):
-    estimator, _ = image_dl_estimator(from_logits=False)
+def test_mnist(fix_get_mnist_subset, image_dl_estimator, targeted, art_warning):
+    try:
+        estimator, _ = image_dl_estimator(from_logits=False)
 
-    (x_train_mnist, y_train_mnist, x_test_mnist, y_test_mnist) = fix_get_mnist_subset
+        (x_train_mnist, y_train_mnist, x_test_mnist, y_test_mnist) = fix_get_mnist_subset
 
-    x_test_original = x_test_mnist.copy()
+        x_test_original = x_test_mnist.copy()
 
-    # set the targeted label
-    if targeted:
-        y_target = np.zeros(10)
-        y_target[8] = 1.0
-
-    df = SimBA(estimator, attack="dct", targeted=targeted)
-
-    x_i = x_test_original[0][None, ...]
-    if targeted:
-        x_test_adv = df.generate(x_i, y=y_target.reshape(1, 10))
-    else:
-        x_test_adv = df.generate(x_i)
-
-    for i in range(1, len(x_test_original)):
-        x_i = x_test_original[i][None, ...]
+        # set the targeted label
         if targeted:
-            tmp_x_test_adv = df.generate(x_i, y=y_target.reshape(1, 10))
-            x_test_adv = np.concatenate([x_test_adv, tmp_x_test_adv])
+            y_target = np.zeros(10)
+            y_target[8] = 1.0
+
+        df = SimBA(estimator, attack="dct", targeted=targeted)
+
+        x_i = x_test_original[0][None, ...]
+        if targeted:
+            x_test_adv = df.generate(x_i, y=y_target.reshape(1, 10))
         else:
-            tmp_x_test_adv = df.generate(x_i)
-            x_test_adv = np.concatenate([x_test_adv, tmp_x_test_adv])
+            x_test_adv = df.generate(x_i)
 
-    np.testing.assert_raises(AssertionError, np.testing.assert_array_equal, x_test_mnist, x_test_adv)
-    np.testing.assert_raises(AssertionError, np.testing.assert_array_equal, 0.0, x_test_adv)
+        for i in range(1, len(x_test_original)):
+            x_i = x_test_original[i][None, ...]
+            if targeted:
+                tmp_x_test_adv = df.generate(x_i, y=y_target.reshape(1, 10))
+                x_test_adv = np.concatenate([x_test_adv, tmp_x_test_adv])
+            else:
+                tmp_x_test_adv = df.generate(x_i)
+                x_test_adv = np.concatenate([x_test_adv, tmp_x_test_adv])
 
-    y_pred = get_labels_np_array(estimator.predict(x_test_adv))
-    np.testing.assert_raises(AssertionError, np.testing.assert_array_equal, y_test_mnist, y_pred)
+        np.testing.assert_raises(AssertionError, np.testing.assert_array_equal, x_test_mnist, x_test_adv)
+        np.testing.assert_raises(AssertionError, np.testing.assert_array_equal, 0.0, x_test_adv)
 
-    accuracy = np.sum(np.argmax(y_pred, axis=1) == np.argmax(y_test_mnist, axis=1)) / x_test_mnist.shape[0]
-    logger.info("Accuracy on adversarial examples: %.2f%%", (accuracy * 100))
+        y_pred = get_labels_np_array(estimator.predict(x_test_adv))
+        np.testing.assert_raises(AssertionError, np.testing.assert_array_equal, y_test_mnist, y_pred)
 
-    # Check that x_test has not been modified by attack and classifier
-    np.testing.assert_array_almost_equal(float(np.max(np.abs(x_test_original - x_test_mnist))), 0, decimal=5)
+        accuracy = np.sum(np.argmax(y_pred, axis=1) == np.argmax(y_test_mnist, axis=1)) / x_test_mnist.shape[0]
+        logger.info("Accuracy on adversarial examples: %.2f%%", (accuracy * 100))
+
+        # Check that x_test has not been modified by attack and classifier
+        np.testing.assert_array_almost_equal(float(np.max(np.abs(x_test_original - x_test_mnist))), 0, decimal=5)
+    except ARTTestException as e:
+        art_warning(e)
 
 
-def test_classifier_type_check_fail():
-    backend_test_classifier_type_check_fail(SimBA, (BaseEstimator, ClassifierMixin))
+def test_classifier_type_check_fail(art_warning):
+    try:
+        backend_test_classifier_type_check_fail(SimBA, (BaseEstimator, ClassifierMixin))
+    except ARTTestException as e:
+        art_warning(e)
