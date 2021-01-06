915a69e6e6b6e0f2854d2572c8dbc1fca73949d3
killian <killian.levacher@ibm.com>
2021-01-06 17:22:47 +0000

    added tr except zoo
    Signed-off-by: killian <killian.levacher@ibm.com>

--- tests/attacks/evasion/test_zoo.py
+++ tests/attacks/evasion/test_zoo.py
@@ -19,7 +19,6 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import logging
 
-import keras.backend as k
 import numpy as np
 import pytest
 
@@ -28,9 +27,8 @@ from art.estimators.estimator import BaseEstimator
 from art.estimators.classification.classifier import ClassifierMixin
 from art.utils import random_targets
 
-from tests.utils import TestBase, get_image_classifier_kr, get_image_classifier_pt
-from tests.utils import get_image_classifier_tf, master_seed
 from tests.attacks.utils import backend_test_classifier_type_check_fail, assert_less_or_equal, assert_within_range
+from tests.utils import ARTTestException
 
 logger = logging.getLogger(__name__)
 
@@ -43,31 +41,34 @@ def fix_get_mnist_subset(get_mnist_dataset):
     yield x_train_mnist[:n_train], y_train_mnist[:n_train], x_test_mnist[:n_test], y_test_mnist[:n_test]
 
 
-def test_failure_attack(fix_get_mnist_subset, image_dl_estimator):
+def test_failure_attack(fix_get_mnist_subset, image_dl_estimator, art_warning):
     """
     Test the corner case when attack fails.
     :return:
     """
-    (x_train_mnist, y_train_mnist, x_test_mnist, y_test_mnist) = fix_get_mnist_subset
+    try:
+        (x_train_mnist, y_train_mnist, x_test_mnist, y_test_mnist) = fix_get_mnist_subset
 
-    x_test_original = x_test_mnist.copy()
+        x_test_original = x_test_mnist.copy()
 
-    estimator, _ = image_dl_estimator()
+        estimator, _ = image_dl_estimator()
 
-    # Failure attack
-    zoo = ZooAttack(classifier=estimator, max_iter=0, binary_search_steps=0, learning_rate=0)
-    x_test_mnist_adv = zoo.generate(x_test_mnist)
+        # Failure attack
+        zoo = ZooAttack(classifier=estimator, max_iter=0, binary_search_steps=0, learning_rate=0)
+        x_test_mnist_adv = zoo.generate(x_test_mnist)
 
-    assert_within_range(x_test_mnist_adv, 0.0, 1.0)
+        assert_within_range(x_test_mnist_adv, 0.0, 1.0)
 
-    np.testing.assert_almost_equal(x_test_mnist, x_test_mnist_adv, 3)
+        np.testing.assert_almost_equal(x_test_mnist, x_test_mnist_adv, 3)
 
-    # Check that x_test has not been modified by attack and classifier 
-    np.testing.assert_array_almost_equal(float(np.max(np.abs(x_test_original - x_test_mnist))), 0, decimal=5)
+        # Check that x_test has not been modified by attack and classifier
+        np.testing.assert_array_almost_equal(float(np.max(np.abs(x_test_original - x_test_mnist))), 0, decimal=5)
+    except ARTTestException as e:
+        art_warning(e)
 
 
 @pytest.mark.parametrize("targeted", [True, False])
-def test_mnist(fix_get_mnist_subset, image_dl_estimator, targeted):
+def test_mnist(fix_get_mnist_subset, image_dl_estimator, targeted, art_warning):
     '''
     NOTE: in the original legacy non pytest version of this test, the Keras and Pytorch tests were only running
     the untargeted part of the test (without any assertions for keras). The current code seems to work for both
@@ -76,32 +77,38 @@ def test_mnist(fix_get_mnist_subset, image_dl_estimator, targeted):
     :param image_dl_estimator:
     :return:
     '''
-    (x_train_mnist, y_train_mnist, x_test_mnist, y_test_mnist) = fix_get_mnist_subset
-    x_test_original = x_test_mnist.copy()
+    try:
+        (x_train_mnist, y_train_mnist, x_test_mnist, y_test_mnist) = fix_get_mnist_subset
+        x_test_original = x_test_mnist.copy()
 
-    estimator, _ = image_dl_estimator()
+        estimator, _ = image_dl_estimator()
 
-    params = {}
-    if targeted:
-        params = {"y": random_targets(y_test_mnist, estimator.nb_classes)}
-        # Target
-        expectation = np.argmax(params["y"], axis=1)
-    else:
-        # y_pred
-        expectation = np.argmax(estimator.predict(x_test_mnist), axis=1)
+        params = {}
+        if targeted:
+            params = {"y": random_targets(y_test_mnist, estimator.nb_classes)}
+            # Target
+            expectation = np.argmax(params["y"], axis=1)
+        else:
+            # y_pred
+            expectation = np.argmax(estimator.predict(x_test_mnist), axis=1)
 
-    zoo = ZooAttack(classifier=estimator, targeted=targeted, max_iter=30, binary_search_steps=8, batch_size=128)
-    x_test_mnist_adv = zoo.generate(x_test_mnist, **params)
-    assert bool((x_test_mnist == x_test_mnist_adv).all()) is False
-    assert_within_range(x_test_mnist_adv, 0.0, 1.0)
-    logger.debug("ZOO target: %s", expectation)
-    y_pred_adv = np.argmax(estimator.predict(x_test_mnist_adv), axis=1)
-    logger.info("ZOO success rate on MNIST: %.2f", (sum(expectation == y_pred_adv) / float(len(expectation))))
-    logger.debug("ZOO actual: %s", y_pred_adv)
+        zoo = ZooAttack(classifier=estimator, targeted=targeted, max_iter=30, binary_search_steps=8, batch_size=128)
+        x_test_mnist_adv = zoo.generate(x_test_mnist, **params)
+        assert bool((x_test_mnist == x_test_mnist_adv).all()) is False
+        assert_within_range(x_test_mnist_adv, 0.0, 1.0)
+        logger.debug("ZOO target: %s", expectation)
+        y_pred_adv = np.argmax(estimator.predict(x_test_mnist_adv), axis=1)
+        logger.info("ZOO success rate on MNIST: %.2f", (sum(expectation == y_pred_adv) / float(len(expectation))))
+        logger.debug("ZOO actual: %s", y_pred_adv)
 
-    # Check that x_test has not been modified by attack and classifier
-    np.testing.assert_array_almost_equal(float(np.max(np.abs(x_test_original - x_test_mnist))), 0, decimal=5)
+        # Check that x_test has not been modified by attack and classifier
+        np.testing.assert_array_almost_equal(float(np.max(np.abs(x_test_original - x_test_mnist))), 0, decimal=5)
+    except ARTTestException as e:
+        art_warning(e)
 
 
-def test_classifier_type_check_fail():
-    backend_test_classifier_type_check_fail(ZooAttack, [BaseEstimator, ClassifierMixin])
+def test_classifier_type_check_fail(art_warning):
+    try:
+        backend_test_classifier_type_check_fail(ZooAttack, [BaseEstimator, ClassifierMixin])
+    except ARTTestException as e:
+        art_warning(e)
