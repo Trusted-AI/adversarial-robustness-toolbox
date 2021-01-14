7821ce79c24969d04c7848412b5cea72f897673f
Mohamed Amine Merzouk <mamerzouk@protonmail.com>
2021-01-14 16:52:30 -0500

    Improving search_space initialization
    Signed-off-by: Mohamed Amine Merzouk <mamerzouk@protonmail.com>

--- art/attacks/evasion/saliency_map.py
+++ art/attacks/evasion/saliency_map.py
@@ -109,7 +109,6 @@ class SaliencyMapMethod(EvasionAttack):
 
             # Main algorithm for each batch
             # Initialize the search space; optimize to remove features that can't be changed
-            search_space = np.ones(batch.shape)
             if self.estimator.clip_values is not None:
                 search_space = np.zeros(batch.shape)
                 clip_min, clip_max = self.estimator.clip_values
@@ -117,6 +116,9 @@ class SaliencyMapMethod(EvasionAttack):
                     search_space[batch < clip_max] = 1
                 else:
                     search_space[batch > clip_min] = 1
+            else:
+                search_space = np.ones(batch.shape)
+
 
             # Get current predictions
             current_pred = preds[batch_index_1:batch_index_2]
