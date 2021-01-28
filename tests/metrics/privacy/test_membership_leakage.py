3cf9e650749cf4e711d4f7ce2253ba8d7c9641df
abigailt <abigailt@il.ibm.com>
2021-01-28 16:33:32 +0200

    Fix test prints
    Signed-off-by: abigailt <abigailt@il.ibm.com>

--- tests/metrics/privacy/test_membership_leakage.py
+++ tests/metrics/privacy/test_membership_leakage.py
@@ -36,8 +36,8 @@ def test_membership_leakage_decision_tree(art_warning, decision_tree_estimator, 
         (x_train, y_train), _ = get_iris_dataset
         prev = classifier.model.tree_
         leakage = PDTP(classifier, extra_classifier, x_train, y_train)
-        logger.info("Average PDTP leakage: %.2f%%", (np.average(leakage)))
-        logger.info("Max PDTP leakage: %.2f%%", (np.max(leakage)))
+        logger.info("Average PDTP leakage: %.2f", (np.average(leakage)))
+        logger.info("Max PDTP leakage: %.2f", (np.max(leakage)))
         assert(classifier.model.tree_ == prev)
         assert (np.all(leakage >= 1.0))
         assert (leakage.shape[0] == x_train.shape[0])
@@ -52,8 +52,8 @@ def test_membership_leakage_tabular(art_warning, tabular_dl_estimator, get_iris_
         extra_classifier = tabular_dl_estimator()
         (x_train, y_train), _ = get_iris_dataset
         leakage = PDTP(classifier, extra_classifier, x_train, y_train)
-        logger.info("Average PDTP leakage: %.2f%%", (np.average(leakage)))
-        logger.info("Max PDTP leakage: %.2f%%", (np.max(leakage)))
+        logger.info("Average PDTP leakage: %.2f", (np.average(leakage)))
+        logger.info("Max PDTP leakage: %.2f", (np.max(leakage)))
         assert(np.all(leakage >= 1.0))
         assert (leakage.shape[0] == x_train.shape[0])
     except ARTTestException as e:
@@ -68,8 +68,8 @@ def test_membership_leakage_image(art_warning, image_dl_estimator, get_default_m
         (x_train, y_train), _ = get_default_mnist_subset
         indexes = random.sample(range(x_train.shape[0]), 100)
         leakage = PDTP(classifier, extra_classifier, x_train, y_train, indexes=indexes, num_iter=1)
-        logger.info("Average PDTP leakage: %.2f%%", (np.average(leakage)))
-        logger.info("Max PDTP leakage: %.2f%%", (np.max(leakage)))
+        logger.info("Average PDTP leakage: %.2f", (np.average(leakage)))
+        logger.info("Max PDTP leakage: %.2f", (np.max(leakage)))
         assert (np.all(leakage >= 1.0))
         assert(leakage.shape[0] == len(indexes))
     except ARTTestException as e:
