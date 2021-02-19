4ca921bb336ec596a8e44f2d5b699df8fbaa9ae9
Beat Buesser <beat.buesser@ie.ibm.com>
2021-02-19 23:53:35 +0000

    Bump version number to 1.5.2
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Maria-Irina Nicolae'
 # The short X.Y version
 version = '1.5'
 # The full version, including alpha/beta/rc tags
-release = '1.5.2-dev'
+release = '1.5.2'
 
 
 # -- General configuration ---------------------------------------------------
