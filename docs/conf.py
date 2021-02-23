27e4cad7c1b1911019323d14e61596efabfd366d
Beat Buesser <beat.buesser@ie.ibm.com>
2021-02-23 19:23:35 +0000

    Update dev version
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Maria-Irina Nicolae'
 # The short X.Y version
 version = '1.5'
 # The full version, including alpha/beta/rc tags
-release = '1.5.2'
+release = '1.5.3-dev'
 
 
 # -- General configuration ---------------------------------------------------
