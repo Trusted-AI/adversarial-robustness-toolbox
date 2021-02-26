e315eee42302bb2d17e2af708f77ee5e6119ae4a
Beat Buesser <beat.buesser@ie.ibm.com>
2021-02-26 21:38:50 +0000

    Update compute_loss
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/estimators/tensorflow.py
+++ art/estimators/tensorflow.py
@@ -135,19 +135,6 @@ class TensorFlowV2Estimator(NeuralNetworkMixin, LossGradientsMixin, BaseEstimato
         """
         NeuralNetworkMixin.fit(self, x, y, batch_size=128, nb_epochs=20, **kwargs)
 
-    def compute_loss(self, x: np.ndarray, y: np.ndarray, **kwargs) -> np.ndarray:
-        """
-        Compute the loss of the neural network for samples `x`.
-
-        :param x: Samples of shape (nb_samples, nb_features) or (nb_samples, nb_pixels_1, nb_pixels_2,
-                  nb_channels) or (nb_samples, nb_channels, nb_pixels_1, nb_pixels_2).
-        :param y: Target values (class labels) one-hot-encoded of shape `(nb_samples, nb_classes)` or indices
-                  of shape `(nb_samples,)`.
-        :return: Loss values.
-        :rtype: Format as expected by the `model`
-        """
-        raise NotImplementedError
-
     def set_params(self, **kwargs) -> None:
         """
         Take a dictionary of parameters and apply checks before setting them as attributes.
