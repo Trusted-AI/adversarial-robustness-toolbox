82b2eac7d1f0862985532e493a5c64b66de17224
abigailt <abigailt@il.ibm.com>
2021-03-06 08:07:21 +0200

    Fix black-box attacks to work with classifiers that don't have an input_shape
    Signed-off-by: abigailt <abigailt@il.ibm.com>

--- art/attacks/inference/attribute_inference/black_box.py
+++ art/attacks/inference/attribute_inference/black_box.py
@@ -109,8 +109,9 @@ class AttributeInferenceBlackBox(AttributeInferenceAttack):
         """
 
         # Checks:
-        if self.estimator.input_shape[0] != x.shape[1]:
-            raise ValueError("Shape of x does not match input_shape of classifier")
+        if self.estimator.input_shape:
+            if self.estimator.input_shape[0] != x.shape[1]:
+                raise ValueError("Shape of x does not match input_shape of classifier")
         if self.single_index_feature and self.attack_feature >= x.shape[1]:
             raise ValueError("attack_feature must be a valid index to a feature in x")
 
@@ -142,8 +143,9 @@ class AttributeInferenceBlackBox(AttributeInferenceAttack):
         """
         if y.shape[0] != x.shape[0]:
             raise ValueError("Number of rows in x and y do not match")
-        if self.single_index_feature and self.estimator.input_shape[0] != x.shape[1] + 1:
-            raise ValueError("Number of features in x + 1 does not match input_shape of classifier")
+        if self.estimator.input_shape:
+            if self.single_index_feature and self.estimator.input_shape[0] != x.shape[1] + 1:
+                raise ValueError("Number of features in x + 1 does not match input_shape of classifier")
 
         x_test = np.concatenate((x, y), axis=1).astype(np.float32)
 
