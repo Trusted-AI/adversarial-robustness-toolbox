adeac343d9bd5b29666839930ac667cd164980a1
Beat Buesser <beat.buesser@ie.ibm.com>
2021-03-11 22:23:43 +0000

    Update type
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/attacks/evasion/projected_gradient_descent/projected_gradient_descent_tensorflow_v2.py
+++ art/attacks/evasion/projected_gradient_descent/projected_gradient_descent_tensorflow_v2.py
@@ -304,7 +304,7 @@ class ProjectedGradientDescentTensorFlowV2(ProjectedGradientDescentCommon):
         """
         import tensorflow as tf  # lgtm [py/repeated-import]
 
-        perturbation_step = eps_step * perturbation
+        perturbation_step = tf.constant(eps_step, dtype=ART_NUMPY_DTYPE) * perturbation
         perturbation_step = tf.where(tf.math.is_nan(perturbation_step), 0, perturbation_step)
         x = x + perturbation_step
         if self.estimator.clip_values is not None:
