ed2a09d61cbf741ba58ae939522f500b70885e3b
Ebube Chuba <ebube.chuba@ibm.com>
2021-03-12 10:40:14 -0500

    removed signedadam optimizer
    Signed-off-by: Ebube Chuba <ebube.chuba@ibm.com>

--- art/attacks/poisoning/bullseye_polytope_attack.py
+++ art/attacks/poisoning/bullseye_polytope_attack.py
@@ -25,9 +25,7 @@ from typing import Optional, Tuple, Union, TYPE_CHECKING, List
 
 import numpy as np
 import torch
-import math
 import time
-from torch.optim.optimizer import Optimizer
 from tqdm.auto import trange
 
 from art.attacks.attack import PoisoningAttackWhiteBox
@@ -95,7 +93,7 @@ class BullseyePolytopeAttack(PoisoningAttackWhiteBox):
         :param target: The target input(s) of shape (N, W, H, C) to misclassify at test time. Multiple targets will be
                        averaged.
         :param feature_layer: The name(s) of the feature representation layer(s).
-        :param opt: The optimizer to use for the attack. Can be 'adam', 'sgd', or 'signedadam'
+        :param opt: The optimizer to use for the attack. Can be 'adam' or 'sgd'
         :param max_iter: The maximum number of iterations for the attack.
         :param learning_rate: The learning rate of clean-label attack optimization.
         :param momentum: The momentum of clean-label attack optimization.
@@ -157,9 +155,6 @@ class BullseyePolytopeAttack(PoisoningAttackWhiteBox):
         if opt_method == 'sgd':
             logger.info("Using SGD to craft poison samples")
             optimizer = torch.optim.SGD(poison_batch.parameters(), lr=self.learning_rate, momentum=self.momentum)
-        elif opt_method == 'signedadam':
-            logger.info("Using Signed Adam to craft poison samples")
-            optimizer = SignedAdam(poison_batch.parameters(), lr=self.learning_rate, betas=(self.momentum, 0.999))
         elif opt_method == 'adam':
             logger.info("Using Adam to craft poison samples")
             optimizer = torch.optim.Adam(poison_batch.parameters(), lr=self.learning_rate, betas=(self.momentum, 0.999))
@@ -228,8 +223,8 @@ class BullseyePolytopeAttack(PoisoningAttackWhiteBox):
         if not isinstance(self.feature_layer, (str, int)):
             raise TypeError("Feature layer should be a string or int")
 
-        if self.opt.lower() not in ['adam', 'signedadam', 'sgd']:
-            raise ValueError("Optimizer must be 'adam', 'signedadam', or 'sgd'")
+        if self.opt.lower() not in ['adam', 'sgd']:
+            raise ValueError("Optimizer must be 'adam' or 'sgd'")
 
         if 1 < self.momentum < 0:
             raise ValueError("Momentum must be between 0 and 1")
@@ -318,105 +313,3 @@ class PoisonBatch(torch.nn.Module):
 
     def forward(self):
         return self.poison
-
-
-class SignedAdam(Optimizer):
-    """Implements Signed Adam algorithm. Code stolen from
-    https://raw.githubusercontent.com/pytorch/pytorch/v0.4.1/torch/optim/adam.py
-    It has been proposed in `Adam: A Method for Stochastic Optimization`_.
-    Arguments:
-        params (iterable): iterable of parameters to optimize or dicts defining
-            parameter groups
-        lr (float, optional): learning rate (default: 1e-3)
-        betas (Tuple[float, float], optional): coefficients used for computing
-            running averages of gradient and its square (default: (0.9, 0.999))
-        eps (float, optional): term added to the denominator to improve
-            numerical stability (default: 1e-8)
-        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
-        amsgrad (boolean, optional): whether to use the AMSGrad variant of this
-            algorithm from the paper `On the Convergence of Adam and Beyond`_
-    .. _Adam\: A Method for Stochastic Optimization:
-        https://arxiv.org/abs/1412.6980
-    .. _On the Convergence of Adam and Beyond:
-        https://openreview.net/forum?id=ryQu7f-RZ
-    """
-
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
-                 weight_decay=0, amsgrad=False):
-        if not 0.0 <= lr:
-            raise ValueError("Invalid learning rate: {}".format(lr))
-        if not 0.0 <= eps:
-            raise ValueError("Invalid epsilon value: {}".format(eps))
-        if not 0.0 <= betas[0] < 1.0:
-            raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
-        if not 0.0 <= betas[1] < 1.0:
-            raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
-        defaults = dict(lr=lr, betas=betas, eps=eps,
-                        weight_decay=weight_decay, amsgrad=amsgrad)
-        super(SignedAdam, self).__init__(params, defaults)
-
-    def __setstate__(self, state):
-        super(SignedAdam, self).__setstate__(state)
-        for group in self.param_groups:
-            group.setdefault('amsgrad', False)
-
-    def step(self, closure=None):
-        """Performs a single optimization step.
-        Arguments:
-            closure (callable, optional): A closure that reevaluates the model
-                and returns the loss.
-        """
-        loss = None
-        if closure is not None:
-            loss = closure()
-
-        for group in self.param_groups:
-            for p in group['params']:
-                if p.grad is None:
-                    continue
-                grad = p.grad.data
-                if grad.is_sparse:
-                    raise RuntimeError('Adam does not support sparse gradients, please consider SparseAdam instead')
-                amsgrad = group['amsgrad']
-
-                state = self.state[p]
-                # State initialization
-                if len(state) == 0:
-                    state['step'] = 0
-                    # Exponential moving average of gradient values
-                    state['exp_avg'] = torch.zeros_like(p.data)
-                    # Exponential moving average of squared gradient values
-                    state['exp_avg_sq'] = torch.zeros_like(p.data)
-                    if amsgrad:
-                        # Maintains max of all exp. moving avg. of sq. grad. values
-                        state['max_exp_avg_sq'] = torch.zeros_like(p.data)
-
-                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
-                if amsgrad:
-                    max_exp_avg_sq = state['max_exp_avg_sq']
-                beta1, beta2 = group['betas']
-
-                state['step'] += 1
-
-                if group['weight_decay'] != 0:
-                    grad = grad.add(group['weight_decay'], p.data)
-
-                # Decay the first and second moment running average coefficient
-                exp_avg.mul_(beta1).add_(1 - beta1, grad)
-                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
-                if amsgrad:
-                    # Maintains the maximum of all 2nd moment running avg. till now
-                    torch.max(max_exp_avg_sq, exp_avg_sq, out=max_exp_avg_sq)
-                    # Use the max. for normalizing running avg. of gradient
-                    denom = max_exp_avg_sq.sqrt().add_(group['eps'])
-                else:
-                    denom = exp_avg_sq.sqrt().add_(group['eps'])
-
-                bias_correction1 = 1 - beta1 ** state['step']
-                bias_correction2 = 1 - beta2 ** state['step']
-                step_size = group['lr'] * math.sqrt(bias_correction2) / bias_correction1
-
-                # p.data.addcdiv_(-step_size, exp_avg, denom)
-                p.data -= step_size * torch.sign(exp_avg / denom)
-
-        return loss
