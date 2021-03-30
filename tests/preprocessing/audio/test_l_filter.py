2c90d31de5abcc95911bb7727dc475a26ad5df84
Ngoc Minh Tran <M.N.Tran@ibm.com>
2021-03-30 14:42:11 +0100

    test estimate grad
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- tests/preprocessing/audio/test_l_filter.py
+++ tests/preprocessing/audio/test_l_filter.py
@@ -28,93 +28,93 @@ from tests.utils import ARTTestException
 logger = logging.getLogger(__name__)
 
 
-# @pytest.mark.framework_agnostic
-# @pytest.mark.parametrize("fir_filter", [False, True])
-# def test_audio_filter(fir_filter, art_warning, expected_values):
-#     try:
-#         # Load data for testing
-#         expected_data = expected_values()
-#
-#         x1 = expected_data[0]
-#         x2 = expected_data[1]
-#         x3 = expected_data[2]
-#         result_0 = expected_data[3]
-#         result_1 = expected_data[4]
-#         result_2 = expected_data[5]
-#
-#         # Create signal data
-#         x = np.array([np.array(x1 * 2), np.array(x2 * 2), np.array(x3 * 2)])
-#
-#         # Filter params
-#         numerator_coef = np.array([0.1, 0.2, -0.1, -0.2])
-#
-#         if fir_filter:
-#             denominator_coef = np.array([1.0])
-#         else:
-#             denominator_coef = np.array([1.0, 0.1, 0.3, 0.4])
-#
-#         # Create filter
-#         audio_filter = LFilter(numerator_coef=numerator_coef, denominator_coef=denominator_coef)
-#
-#         # Apply filter
-#         result = audio_filter(x)
-#
-#         # Test
-#         assert result[1] is None
-#         np.testing.assert_array_almost_equal(result_0, result[0][0], decimal=0)
-#         np.testing.assert_array_almost_equal(result_1, result[0][1], decimal=0)
-#         np.testing.assert_array_almost_equal(result_2, result[0][2], decimal=0)
-#
-#     except ARTTestException as e:
-#         art_warning(e)
-#
-#
-# @pytest.mark.framework_agnostic
-# def test_default(art_warning):
-#     try:
-#         # Small data for testing
-#         x = np.array([[0.37, 0.68, 0.63, 0.48, 0.48, 0.18, 0.19]])
-#
-#         # Create filter
-#         audio_filter = LFilter()
-#
-#         # Apply filter
-#         result = audio_filter(x)
-#
-#         # Test
-#         assert result[1] is None
-#         np.testing.assert_array_almost_equal(x, result[0], decimal=0)
-#
-#     except ARTTestException as e:
-#         art_warning(e)
-#
-#
-# @pytest.mark.framework_agnostic
-# def test_triple_clip_values_error(art_warning):
-#     try:
-#         exc_msg = "`clip_values` should be a tuple of 2 floats containing the allowed data range."
-#         with pytest.raises(ValueError, match=exc_msg):
-#             LFilter(
-#                 numerator_coef=np.array([0.1, 0.2, 0.3]),
-#                 denominator_coef=np.array([0.1, 0.2, 0.3]),
-#                 clip_values=(0, 1, 2),
-#             )
-#
-#     except ARTTestException as e:
-#         art_warning(e)
-#
-#
-# @pytest.mark.framework_agnostic
-# def test_relation_clip_values_error(art_warning):
-#     try:
-#         exc_msg = "Invalid `clip_values`: min >= max."
-#         with pytest.raises(ValueError, match=exc_msg):
-#             LFilter(
-#                 numerator_coef=np.array([0.1, 0.2, 0.3]), denominator_coef=np.array([0.1, 0.2, 0.3]), clip_values=(1, 0)
-#             )
-#
-#     except ARTTestException as e:
-#         art_warning(e)
+@pytest.mark.framework_agnostic
+@pytest.mark.parametrize("fir_filter", [False, True])
+def test_audio_filter(fir_filter, art_warning, expected_values):
+    try:
+        # Load data for testing
+        expected_data = expected_values()
+
+        x1 = expected_data[0]
+        x2 = expected_data[1]
+        x3 = expected_data[2]
+        result_0 = expected_data[3]
+        result_1 = expected_data[4]
+        result_2 = expected_data[5]
+
+        # Create signal data
+        x = np.array([np.array(x1 * 2), np.array(x2 * 2), np.array(x3 * 2)])
+
+        # Filter params
+        numerator_coef = np.array([0.1, 0.2, -0.1, -0.2])
+
+        if fir_filter:
+            denominator_coef = np.array([1.0])
+        else:
+            denominator_coef = np.array([1.0, 0.1, 0.3, 0.4])
+
+        # Create filter
+        audio_filter = LFilter(numerator_coef=numerator_coef, denominator_coef=denominator_coef)
+
+        # Apply filter
+        result = audio_filter(x)
+
+        # Test
+        assert result[1] is None
+        np.testing.assert_array_almost_equal(result_0, result[0][0], decimal=0)
+        np.testing.assert_array_almost_equal(result_1, result[0][1], decimal=0)
+        np.testing.assert_array_almost_equal(result_2, result[0][2], decimal=0)
+
+    except ARTTestException as e:
+        art_warning(e)
+
+
+@pytest.mark.framework_agnostic
+def test_default(art_warning):
+    try:
+        # Small data for testing
+        x = np.array([[0.37, 0.68, 0.63, 0.48, 0.48, 0.18, 0.19]])
+
+        # Create filter
+        audio_filter = LFilter()
+
+        # Apply filter
+        result = audio_filter(x)
+
+        # Test
+        assert result[1] is None
+        np.testing.assert_array_almost_equal(x, result[0], decimal=0)
+
+    except ARTTestException as e:
+        art_warning(e)
+
+
+@pytest.mark.framework_agnostic
+def test_triple_clip_values_error(art_warning):
+    try:
+        exc_msg = "`clip_values` should be a tuple of 2 floats containing the allowed data range."
+        with pytest.raises(ValueError, match=exc_msg):
+            LFilter(
+                numerator_coef=np.array([0.1, 0.2, 0.3]),
+                denominator_coef=np.array([0.1, 0.2, 0.3]),
+                clip_values=(0, 1, 2),
+            )
+
+    except ARTTestException as e:
+        art_warning(e)
+
+
+@pytest.mark.framework_agnostic
+def test_relation_clip_values_error(art_warning):
+    try:
+        exc_msg = "Invalid `clip_values`: min >= max."
+        with pytest.raises(ValueError, match=exc_msg):
+            LFilter(
+                numerator_coef=np.array([0.1, 0.2, 0.3]), denominator_coef=np.array([0.1, 0.2, 0.3]), clip_values=(1, 0)
+            )
+
+    except ARTTestException as e:
+        art_warning(e)
 
 
 @pytest.mark.framework_agnostic
