31d0b06c4fcfce6b30b692f0c02c8edd75d471ab
minhitbk <minhitbk@gmail.com>
2021-04-02 15:15:38 +0100

    Update art/attacks/evasion/elastic_net.py
    Co-authored-by: Beat Buesser <49047826+beat-buesser@users.noreply.github.com>

--- art/attacks/evasion/elastic_net.py
+++ art/attacks/evasion/elastic_net.py
@@ -176,6 +176,7 @@ class ElasticNet(EvasionAttack):
         loss_gradient += 2 * (x_adv - x)
 
         # Set gradients where loss is constant to zero
+        cond = (predictions[i_add] - predictions[i_sub] + self.confidence) < 0
         loss_gradient[cond] = 0.0
 
         return loss_gradient
