58437a2d6a97a01223321934c0a5d68d811a298e
Ngoc Minh Tran <M.N.Tran@ibm.com>
2021-04-07 18:05:11 +0100

    update run
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- .github/actions/deepspeech-v2/run.sh
+++ .github/actions/deepspeech-v2/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -l
 
-echo "This is to test deepspeech version 2!!!"
+pytest --cov-report=xml --cov=art --cov-append -q -vv tests/estimators/speech_recognition/test_pytorch_deep_speech.py --framework=pytorch --skip_travis=True --durations=0
+pytest --cov-report=xml --cov=art --cov-append -q -vv tests/attacks/evasion/test_imperceptible_asr_pytorch.py --framework=pytorch --skip_travis=True --durations=0
