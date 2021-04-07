07db8dd00de71d63bb78a6b5c86a6bec35e172b6
GiulioZizzo <giulio.zizzo@yahoo.co.uk>
2021-04-07 11:58:42 +0100

    initial adversarial PE attack
    Signed-off-by: GiulioZizzo <giulio.zizzo@yahoo.co.uk>

--- art/attacks/evasion/__init__.py
+++ art/attacks/evasion/__init__.py
@@ -48,3 +48,4 @@ from art.attacks.evasion.targeted_universal_perturbation import TargetedUniversa
 from art.attacks.evasion.virtual_adversarial import VirtualAdversarialMethod
 from art.attacks.evasion.wasserstein import Wasserstein
 from art.attacks.evasion.zoo import ZooAttack
+from art.attacks.evasion.pe_malware_attack import MalwareGD
