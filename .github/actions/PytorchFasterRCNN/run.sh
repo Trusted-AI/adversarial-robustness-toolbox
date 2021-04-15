74ecd7cf35c34ca4d3856f7678ae477a66928ba2
Beat Buesser <beat.buesser@ie.ibm.com>
2021-04-15 01:01:10 +0100

    Update CI workflow
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- .github/actions/PytorchFasterRCNN/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -l
-
-exit_code=0
-
-pytest --cov-report=xml --cov=art --cov-append -q -vv tests/estimators/object_detection/test_pytorch_faster_rcnn.py --framework=pytorch --skip_travis=True --durations=0
-if [[ $? -ne 0 ]]; then exit_code=1; echo "Failed estimators/object_detection/test_pytorch_faster_rcnn tests"; fi
-
-exit ${exit_code}
