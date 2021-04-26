25d518005af2aac75b0f472c02d1596c6b77f75c
Ngoc Minh Tran <M.N.Tran@ibm.com>
2021-04-26 15:16:57 +0100

    temporarily modify test
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- tests/estimators/speech_recognition/test_pytorch_deep_speech.py
+++ tests/estimators/speech_recognition/test_pytorch_deep_speech.py
@@ -30,8 +30,8 @@ logger = logging.getLogger(__name__)
 
 @pytest.mark.skip_module("deepspeech_pytorch")
 @pytest.mark.skip_framework("tensorflow", "tensorflow2v1", "keras", "kerastf", "mxnet", "non_dl_frameworks")
-@pytest.mark.parametrize("use_amp", [False, True])
-@pytest.mark.parametrize("device_type", ["cpu", "gpu"])
+@pytest.mark.parametrize("use_amp", [False])
+@pytest.mark.parametrize("device_type", ["cpu"])
 def test_pytorch_deep_speech(art_warning, expected_values, use_amp, device_type):
     # Only import if deepspeech_pytorch module is available
     import torch
