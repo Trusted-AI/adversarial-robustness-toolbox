98169f7b76a96cfa0c88ae8860ccbc84e8fea10f
Shashank Kotyan <shashankkotyan@gmail.com>
2021-06-11 00:38:12 +0900

    Update
    Co-authored-by: Beat Buesser <49047826+beat-buesser@users.noreply.github.com>

--- art/attacks/evasion/pixel_threshold.py
+++ art/attacks/evasion/pixel_threshold.py
@@ -127,7 +127,6 @@ class PixelThreshold(EvasionAttack):
 
         if not isinstance(self.verbose_es, bool):
             raise ValueError("The argument `verbose` has to be of type bool.")
-       
         if self.estimator.clip_values is None: 
             raise ValueError("This attack requires estimator clip values to be defined.")
     
