fix bugs in segment_by_class and score calculation for spectral.

art/defences/detector/poison/spectral_signature_defense.py
@@ -171,5 +171,5 @@ class SpectralSignatureDefense(PoisonFilteringDefence):
         _, _, matrix_v = np.linalg.svd(matrix_m, full_matrices=False)
         eigs = matrix_v[:1]
         corrs = np.matmul(eigs, np.transpose(matrix_r))
-        score = np.expand_dims(np.linalg.norm(corrs, axis=1), axis=1)
+        score = np.expand_dims(np.linalg.norm(corrs, axis=0), axis=1)
         return score

art/utils.py
@@ -1201,7 +1201,7 @@ def segment_by_class(data: Union[np.ndarray, List[int]], classes: np.ndarray, nu
     """
     by_class: List[List[int]] = [[] for _ in range(num_classes)]
     for indx, feature in enumerate(classes):
-        if num_classes > 2:
+        if num_classes > 2 and len(np.shape(classes)) != 1:
             assigned = np.argmax(feature)
         else:
             assigned = int(feature)