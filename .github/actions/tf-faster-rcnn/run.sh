9f93480f83ca52581e6970ba15f1617b79083ab6
Ngoc Minh Tran <M.N.Tran@ibm.com>
2021-06-29 14:28:56 +0100

    update run script
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- .github/actions/tf-faster-rcnn/run.sh
+++ .github/actions/tf-faster-rcnn/run.sh
@@ -2,9 +2,7 @@
 
 exit_code=0
 
-pytest --cov-report=xml --cov=art --cov-append -q -vv tests/estimators/speech_recognition/test_pytorch_deep_speech.py --framework=pytorch --skip_travis=True --durations=0
-if [[ $? -ne 0 ]]; then exit_code=1; echo "Failed estimators/speech_recognition/test_pytorch_deep_speech tests"; fi
-pytest --cov-report=xml --cov=art --cov-append -q -vv tests/attacks/evasion/test_imperceptible_asr_pytorch.py --framework=pytorch --skip_travis=True --durations=0
-if [[ $? -ne 0 ]]; then exit_code=1; echo "Failed attacks/evasion/test_imperceptible_asr_pytorch tests"; fi
+pytest --cov-report=xml --cov=art --cov-append -q -vv tests/estimators/object_detection/test_tensorflow_faster_rcnn.py --framework=tensorflow --skip_travis=True --durations=0
+if [[ $? -ne 0 ]]; then exit_code=1; echo "Failed estimators/object_detection/test_tensorflow_faster_rcnn.py tests"; fi
 
 exit ${exit_code}
