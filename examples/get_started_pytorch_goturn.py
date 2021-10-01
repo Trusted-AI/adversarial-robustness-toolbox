24d19b5400913c89038779d7e49ab27150f2624a
Beat Buesser <beat.buesser@ie.ibm.com>
2021-10-01 02:04:45 +0100

    Uncomment Identity Tracker
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>
    Signed-off-by: synergit <syner.wang@gmail.com>

--- examples/get_started_pytorch_goturn.py
+++ examples/get_started_pytorch_goturn.py
@@ -133,38 +133,38 @@ if benchmark_not_attack:
     # Identity Tracker #
     # -----------------#
 
-    # from got10k.trackers import Tracker
-    #
-    # class IdentityTracker(Tracker):
-    #     def __init__(self):
-    #         super(IdentityTracker, self).__init__(
-    #             name='IdentityTracker',  # tracker name
-    #             is_deterministic=True  # stochastic (False) or deterministic (True)
-    #         )
-    #
-    #     def init(self, image, box):
-    #         self.box = box
-    #
-    #     def update(self, image):
-    #         return self.box
-    #
-    #
+    from got10k.trackers import Tracker
+
+    class IdentityTracker(Tracker):
+        def __init__(self):
+            super(IdentityTracker, self).__init__(
+                name='IdentityTracker',  # tracker name
+                is_deterministic=True  # stochastic (False) or deterministic (True)
+            )
+
+        def init(self, image, box):
+            self.box = box
+
+        def update(self, image):
+            return self.box
+
+
     from got10k.experiments import ExperimentGOT10k
 
-    #
-    # # instantiate a tracker
-    # tracker = IdentityTracker()
-    #
-    # # setup experiment (validation subset)
-    # experiment = ExperimentGOT10k(
-    #     root_dir=os.path.join(benchmark_dir, "data", "GOT-10k"),
-    #     subset='val',  # 'train' | 'val' | 'test'
-    #     result_dir=os.path.join(benchmark_dir, "results"),  # where to store tracking results
-    #     report_dir=os.path.join(benchmark_dir, "reports")  # where to store evaluation reports
-    # )
-    # experiment.run(tracker, visualize=True)
-    #
-    # experiment.report([tracker.name])
+
+    # instantiate a tracker
+    tracker = IdentityTracker()
+
+    # setup experiment (validation subset)
+    experiment = ExperimentGOT10k(
+        root_dir=os.path.join(benchmark_dir, "data", "GOT-10k"),
+        subset='val',  # 'train' | 'val' | 'test'
+        result_dir=os.path.join(benchmark_dir, "results"),  # where to store tracking results
+        report_dir=os.path.join(benchmark_dir, "reports")  # where to store evaluation reports
+    )
+    experiment.run(tracker, visualize=True)
+
+    experiment.report([tracker.name])
 
     # ----------------------#
     # PyTorchGoturn Tracker #
