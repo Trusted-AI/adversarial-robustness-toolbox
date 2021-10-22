54ad36f6b9b07c055c2f4ebd6b6771c5ec8884fb
Beat Buesser <beat.buesser@ie.ibm.com>
2021-10-22 13:35:59 +0100

    Create _generate_single
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- art/attacks/evasion/carlini.py
+++ art/attacks/evasion/carlini.py
@@ -548,7 +548,7 @@ class CarliniLInfMethod(EvasionAttack):
         :param max_iter: The maximum number of iterations.
         :param max_halving: Maximum number of halving steps in the line search optimization.
         :param max_doubling: Maximum number of doubling steps in the line search optimization.
-        :param eps: An upper bound for the L_0 norm of the adversarial perturbation.
+        :param eps: An upper bound for the L_inf norm of the adversarial perturbation.
         :param batch_size: Size of the batch on which adversarial samples are generated.
         :param verbose: Show progress bars.
         """
@@ -635,6 +635,163 @@ class CarliniLInfMethod(EvasionAttack):
 
         return loss_gradient
 
+    def _generate_single(self, x_batch, y_batch, clip_min, clip_max):
+
+        # The optimization is performed in tanh space to keep the
+        # adversarial images bounded from clip_min and clip_max.
+        x_batch_tanh = original_to_tanh(x_batch, clip_min, clip_max, self._tanh_smoother)
+
+        # Initialize perturbation in tanh space:
+        x_adv_batch = x_batch.copy()
+        x_adv_batch_tanh = x_batch_tanh.copy()
+
+        # Initialize optimization:
+        z_logits, loss = self._loss(x_adv_batch, y_batch)
+        # attack_success = loss <= 0
+        learning_rate = self.learning_rate
+
+        for i_iter in range(self.max_iter):
+            logger.debug("Iteration step %i out of %i", i_iter, self.max_iter)
+            # logger.debug("Average Loss: %f", np.mean(loss))
+
+            # logger.debug(
+            #     "Successful attack samples: %i out of %i",
+            #     int(np.sum(attack_success)),
+            #     x_batch.shape[0],
+            # )
+
+            # only continue optimization for those samples where attack hasn't succeeded yet:
+            # active = ~attack_success
+            # if np.sum(active) == 0:
+            #     break
+
+            # compute gradient:
+            # logger.debug("Compute loss gradient")
+            perturbation_tanh = -self._loss_gradient(
+                z_logits,
+                y_batch,
+                x_adv_batch,
+                x_adv_batch_tanh,
+                clip_min,
+                clip_max,
+            )
+
+            # perform line search to optimize perturbation
+            # first, halve the learning rate until perturbation actually decreases the loss:
+            prev_loss = loss.copy()
+            best_loss = loss.copy()
+            best_lr = np.zeros(x_batch.shape[0])
+            halving = np.zeros(x_batch.shape[0])
+
+            for i_halve in range(self.max_halving):
+                # logger.debug(
+                #     "Perform halving iteration %i out of %i",
+                #     i_halve,
+                #     self.max_halving,
+                # )
+                do_halving = loss >= prev_loss
+                # logger.debug("Halving to be performed on %i samples", int(np.sum(do_halving)))
+                # if np.sum(do_halving) == 0:
+                #     break
+                # active_and_do_halving = active.copy()
+                # active_and_do_halving[active] = do_halving
+
+                if do_halving:
+
+                    lr_mult = learning_rate
+                    # for _ in range(len(x.shape) - 1):
+                    #     lr_mult = lr_mult[:, np.newaxis]
+
+                    adv_10 = x_adv_batch_tanh
+                    new_x_adv_batch_tanh = adv_10 + lr_mult * perturbation_tanh[do_halving]
+
+                    new_x_adv_batch = tanh_to_original(
+                        new_x_adv_batch_tanh,
+                        clip_min,
+                        clip_max,
+                    )
+                    _, loss = self._loss(new_x_adv_batch, y_batch)
+                    # logger.debug("New Average Loss: %f", np.mean(loss))
+                    # logger.debug("Loss: %s", str(loss))
+                    # logger.debug("Prev_loss: %s", str(prev_loss))
+                    # logger.debug("Best_loss: %s", str(best_loss))
+
+                    if loss < best_loss:
+                        best_lr = learning_rate
+                        best_loss = loss
+                    learning_rate /= 2
+                    halving += 1
+            learning_rate *= 2
+
+            # if no halving was actually required, double the learning rate as long as this
+            # decreases the loss:
+            for i_double in range(self.max_doubling):
+                # logger.debug(
+                #     "Perform doubling iteration %i out of %i",
+                #     i_double,
+                #     self.max_doubling,
+                # )
+                do_doubling = (halving == 1) & (loss <= best_loss)
+                # logger.debug(
+                #     "Doubling to be performed on %i samples",
+                #     int(np.sum(do_doubling)),
+                # )
+                # if np.sum(do_doubling) == 0:
+                #     break
+                if do_doubling:
+                    # active_and_do_doubling = active.copy()
+                    # active_and_do_doubling[active] = do_doubling
+                    learning_rate *= 2
+
+                    lr_mult = learning_rate
+                    # for _ in range(len(x.shape) - 1):
+                    #     lr_mult = lr_mult[:, np.newaxis]
+
+                    x_adv15 = x_adv_batch_tanh
+                    new_x_adv_batch_tanh = x_adv15 + lr_mult * perturbation_tanh
+                    new_x_adv_batch = tanh_to_original(
+                        new_x_adv_batch_tanh,
+                        clip_min,
+                        clip_max,
+                    )
+                    _, loss = self._loss(new_x_adv_batch, y_batch)
+                    # logger.debug("New Average Loss: %f", np.mean(loss))
+                    if loss < best_loss:
+                        best_lr = learning_rate
+                        best_loss = loss
+
+            if halving == 1:
+                learning_rate /= 2
+
+            # update_adv = best_lr > 0
+            # logger.debug(
+            #     "Number of adversarial samples to be finally updated: %i",
+            #     int(np.sum(update_adv)),
+            # )
+
+            # if np.sum(update_adv) > 0:
+            if best_lr > 0:
+                # active_and_update_adv = active.copy()
+                # active_and_update_adv[active] = update_adv
+                best_lr_mult = best_lr
+                # for _ in range(len(x.shape) - 1):
+                #     best_lr_mult = best_lr_mult[:, np.newaxis]
+
+                best_13 = best_lr_mult * perturbation_tanh
+                x_adv_batch_tanh = x_adv_batch_tanh + best_13
+                x_adv_batch = tanh_to_original(
+                    x_adv_batch_tanh,
+                    clip_min,
+                    clip_max,
+                )
+                (z_logits, loss,) = self._loss(
+                    x_adv_batch,
+                    y_batch,
+                )
+                # attack_success = loss <= 0
+
+        return x_adv_batch
+
     def generate(self, x: np.ndarray, y: Optional[np.ndarray] = None, **kwargs) -> np.ndarray:
         """
         Generate adversarial samples and return them in an array.
@@ -667,165 +824,20 @@ class CarliniLInfMethod(EvasionAttack):
             )
 
         # Compute perturbation with implicit batching
-        nb_batches = int(np.ceil(x_adv.shape[0] / float(self.batch_size)))
-        for batch_id in trange(nb_batches, desc="C&W L_inf", disable=not self.verbose):
-            batch_index_1, batch_index_2 = batch_id * self.batch_size, (batch_id + 1) * self.batch_size
-            x_batch = x_adv[batch_index_1:batch_index_2]
-            y_batch = y[batch_index_1:batch_index_2]
+        for sample_id in trange(x.shape[0], desc="C&W L_inf", disable=not self.verbose):
+            x_batch = x_adv[[sample_id]]
+            y_batch = y[[sample_id]]
 
             # Determine values for later clipping
             clip_min = np.clip(x_batch - self.eps, clip_min_per_pixel, clip_max_per_pixel)
             clip_max = np.clip(x_batch + self.eps, clip_min_per_pixel, clip_max_per_pixel)
 
-            # The optimization is performed in tanh space to keep the
-            # adversarial images bounded from clip_min and clip_max.
-            x_batch_tanh = original_to_tanh(x_batch, clip_min, clip_max, self._tanh_smoother)
-
-            # Initialize perturbation in tanh space:
-            x_adv_batch = x_batch.copy()
-            x_adv_batch_tanh = x_batch_tanh.copy()
-
-            # Initialize optimization:
-            z_logits, loss = self._loss(x_adv_batch, y_batch)
-            attack_success = loss <= 0
-            learning_rate = self.learning_rate * np.ones(x_batch.shape[0])
-
-            for i_iter in range(self.max_iter):
-                logger.debug("Iteration step %i out of %i", i_iter, self.max_iter)
-                logger.debug("Average Loss: %f", np.mean(loss))
-
-                logger.debug(
-                    "Successful attack samples: %i out of %i",
-                    int(np.sum(attack_success)),
-                    x_batch.shape[0],
-                )
-
-                # only continue optimization for those samples where attack hasn't succeeded yet:
-                active = ~attack_success
-                if np.sum(active) == 0:
-                    break
-
-                # compute gradient:
-                logger.debug("Compute loss gradient")
-                perturbation_tanh = -self._loss_gradient(
-                    z_logits[active],
-                    y_batch[active],
-                    x_adv_batch[active],
-                    x_adv_batch_tanh[active],
-                    clip_min[active],
-                    clip_max[active],
-                )
-
-                # perform line search to optimize perturbation
-                # first, halve the learning rate until perturbation actually decreases the loss:
-                prev_loss = loss.copy()
-                best_loss = loss.copy()
-                best_lr = np.zeros(x_batch.shape[0])
-                halving = np.zeros(x_batch.shape[0])
-
-                for i_halve in range(self.max_halving):
-                    logger.debug(
-                        "Perform halving iteration %i out of %i",
-                        i_halve,
-                        self.max_halving,
-                    )
-                    do_halving = loss[active] >= prev_loss[active]
-                    logger.debug("Halving to be performed on %i samples", int(np.sum(do_halving)))
-                    if np.sum(do_halving) == 0:
-                        break
-                    active_and_do_halving = active.copy()
-                    active_and_do_halving[active] = do_halving
-
-                    lr_mult = learning_rate[active_and_do_halving]
-                    for _ in range(len(x.shape) - 1):
-                        lr_mult = lr_mult[:, np.newaxis]
-
-                    adv_10 = x_adv_batch_tanh[active_and_do_halving]
-                    new_x_adv_batch_tanh = adv_10 + lr_mult * perturbation_tanh[do_halving]
-
-                    new_x_adv_batch = tanh_to_original(
-                        new_x_adv_batch_tanh,
-                        clip_min[active_and_do_halving],
-                        clip_max[active_and_do_halving],
-                    )
-                    _, loss[active_and_do_halving] = self._loss(new_x_adv_batch, y_batch[active_and_do_halving])
-                    logger.debug("New Average Loss: %f", np.mean(loss))
-                    logger.debug("Loss: %s", str(loss))
-                    logger.debug("Prev_loss: %s", str(prev_loss))
-                    logger.debug("Best_loss: %s", str(best_loss))
-
-                    best_lr[loss < best_loss] = learning_rate[loss < best_loss]
-                    best_loss[loss < best_loss] = loss[loss < best_loss]
-                    learning_rate[active_and_do_halving] /= 2
-                    halving[active_and_do_halving] += 1
-                learning_rate[active] *= 2
-
-                # if no halving was actually required, double the learning rate as long as this
-                # decreases the loss:
-                for i_double in range(self.max_doubling):
-                    logger.debug(
-                        "Perform doubling iteration %i out of %i",
-                        i_double,
-                        self.max_doubling,
-                    )
-                    do_doubling = (halving[active] == 1) & (loss[active] <= best_loss[active])
-                    logger.debug(
-                        "Doubling to be performed on %i samples",
-                        int(np.sum(do_doubling)),
-                    )
-                    if np.sum(do_doubling) == 0:
-                        break
-                    active_and_do_doubling = active.copy()
-                    active_and_do_doubling[active] = do_doubling
-                    learning_rate[active_and_do_doubling] *= 2
-
-                    lr_mult = learning_rate[active_and_do_doubling]
-                    for _ in range(len(x.shape) - 1):
-                        lr_mult = lr_mult[:, np.newaxis]
-
-                    x_adv15 = x_adv_batch_tanh[active_and_do_doubling]
-                    new_x_adv_batch_tanh = x_adv15 + lr_mult * perturbation_tanh[do_doubling]
-                    new_x_adv_batch = tanh_to_original(
-                        new_x_adv_batch_tanh,
-                        clip_min[active_and_do_doubling],
-                        clip_max[active_and_do_doubling],
-                    )
-                    _, loss[active_and_do_doubling] = self._loss(new_x_adv_batch, y_batch[active_and_do_doubling])
-                    logger.debug("New Average Loss: %f", np.mean(loss))
-                    best_lr[loss < best_loss] = learning_rate[loss < best_loss]
-                    best_loss[loss < best_loss] = loss[loss < best_loss]
-
-                learning_rate[halving == 1] /= 2
-
-                update_adv = best_lr[active] > 0
-                logger.debug(
-                    "Number of adversarial samples to be finally updated: %i",
-                    int(np.sum(update_adv)),
-                )
-
-                if np.sum(update_adv) > 0:
-                    active_and_update_adv = active.copy()
-                    active_and_update_adv[active] = update_adv
-                    best_lr_mult = best_lr[active_and_update_adv]
-                    for _ in range(len(x.shape) - 1):
-                        best_lr_mult = best_lr_mult[:, np.newaxis]
-
-                    best_13 = best_lr_mult * perturbation_tanh[update_adv]
-                    x_adv_batch_tanh[active_and_update_adv] = x_adv_batch_tanh[active_and_update_adv] + best_13
-                    x_adv_batch[active_and_update_adv] = tanh_to_original(
-                        x_adv_batch_tanh[active_and_update_adv],
-                        clip_min[active_and_update_adv],
-                        clip_max[active_and_update_adv],
-                    )
-                    (z_logits[active_and_update_adv], loss[active_and_update_adv],) = self._loss(
-                        x_adv_batch[active_and_update_adv],
-                        y_batch[active_and_update_adv],
-                    )
-                    attack_success = loss <= 0
+            x_adv_batch = self._generate_single(x_batch, y_batch, clip_min, clip_max)
 
             # Update depending on attack success:
-            x_adv_batch[~attack_success] = x_batch[~attack_success]
-            x_adv[batch_index_1:batch_index_2] = x_adv_batch
+            z_logits, loss, = self._loss(x_adv_batch, y_batch)
+            if loss <= 0:
+                x_adv[sample_id] = x_adv_batch
 
         logger.info(
             "Success rate of C&W L_inf attack: %.2f%%",
