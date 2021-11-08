92f47b0c7cafbe0a4ce6ab8fe03475d8e18759e2
Kevin Eykholt <kheykholt@gmail.com>
2021-11-08 20:15:39 +0000

    Whitespace fixes
    Signed-off-by: Kevin Eykholt <kheykholt@gmail.com>
    Signed-off-by: synergit <syner.wang@gmail.com>

--- art/estimators/classification/deep_partition_ensemble.py
+++ art/estimators/classification/deep_partition_ensemble.py
@@ -21,10 +21,10 @@ Creates a Deep Partition Aggregation ensemble classifier.
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import logging
-from typing import List, Optional, Union, Tuple, TYPE_CHECKING, Callable
+from typing import List, Optional, Union, Callable, Dict, TYPE_CHECKING
 
-import numpy as np
 import copy
+import numpy as np
 
 from art.estimators.classification.classifier import ClassifierNeuralNetwork
 from art.estimators.classification.ensemble import EnsembleClassifier
@@ -39,11 +39,11 @@ logger = logging.getLogger(__name__)
 
 class DeepPartitionEnsemble(EnsembleClassifier):
     """
-    Implementation of Deep Partition Aggregation Defense. Training data is partitioned into disjoint buckets based on a 
-    hash function and a classifier is trained on each bucket.
+    Implementation of Deep Partition Aggregation Defense. Training data is partitioned into
+    disjoint buckets based on a hash function and a classifier is trained on each bucket.
     | Paper link: https://arxiv.org/abs/2006.14768
     """
-    
+
     estimator_params = EnsembleClassifier.estimator_params + [
         "hash_function",
         "ensemble_size",
@@ -61,11 +61,11 @@ class DeepPartitionEnsemble(EnsembleClassifier):
         preprocessing: "PREPROCESSING_TYPE" = (0.0, 1.0),
     ) -> None:
         """
-        :param classifiers: The base model definition to use for defining the ensemble. If a list, the list must be the same size 
-               as the ensemble size
-        :param hash_function: The function used to partition the training data. If empty, the hash function will use the sum of 
-               the input values modulo the ensemble size for partitioning.
-        :param ensemble_size: The number of models in the ensemble
+        :param classifiers: The base model definition to use for defining the ensemble.
+               If a list, the list must be the same size as the ensemble size.
+        :param hash_function: The function used to partition the training data. If empty, the hash function
+               will use the sum of the input values modulo the ensemble size for partitioning.
+        :param ensemble_size: The number of models in the ensemble.
         :param channels_first: Set channels first or last.
         :param clip_values: Tuple of the form `(min, max)` of floats or `np.ndarray` representing the minimum and
                maximum values allowed for features. If floats are provided, these will be used as the range of all
@@ -78,13 +78,13 @@ class DeepPartitionEnsemble(EnsembleClassifier):
                used for data preprocessing. The first value will be subtracted from the input. The input will then
                be divided by the second one. Not applicable in this classifier.
         """
-        
+
         if isinstance(classifiers, list) and len(classifiers) != ensemble_size:
             raise ValueError("The length of the classifier list must be the same as the ensemble size")
-        else:
+        elif not isinstance(classifiers, list):
             # intialize the ensemble based on the provided archtecture
             classifiers = [copy.deepcopy(classifiers) for _ in range(ensemble_size)] 
-            
+
         super().__init__(
             classifiers=classifiers,
             clip_values=clip_values,
@@ -93,62 +93,73 @@ class DeepPartitionEnsemble(EnsembleClassifier):
             postprocessing_defences=postprocessing_defences,
             preprocessing=preprocessing,
         )
-        
+
         if hash_function is None:
             hash_function = lambda x: int(np.sum(x)) % ensemble_size
         self.hash_function = hash_function
         self.ensemble_size = ensemble_size
-            
+
     def predict(  # pylint: disable=W0221
-        self, x: np.ndarray, batch_size: int = 128, max_aggregate: bool = True, **kwargs
+        self, x: np.ndarray, batch_size: int = 128, raw: bool = False, max_aggregate: bool = True, **kwargs
     ) -> np.ndarray:
         """
-        Perform prediction for a batch of inputs. Aggregation will be performed on the prediction from each classifier if 
-        max_aggregate is True. Otherwise, the probabilties will be summed instead. For logits output set max_aggregate=True, as 
-        logits are not comparable between models and should not be aggregated using a sum.
+        Perform prediction for a batch of inputs. Aggregation will be performed on the prediction from
+        each classifier if max_aggregate is True. Otherwise, the probabilties will be summed instead.
+        For logits output set max_aggregate=True, as logits are not comparable between models and should
+        not be aggregated using a sum.
         :param x: Input samples.
         :param batch_size: Size of batches.
-        :param max_aggregate: Aggregate the predicted classes of each classifier if True.
-        :return: Array of predictions of shape `(nb_inputs, nb_classes)`
+        :param raw: Return the individual classifier raw outputs (not aggregated).
+        :param max_aggregate: Aggregate the predicted classes of each classifier if True. If false, aggregation
+               is done using a sum. If raw is true, this arg is ignored
+        :return: Array of predictions of shape `(nb_inputs, nb_classes)`, or of shape
+                 `(nb_classifiers, nb_inputs, nb_classes)` if `raw=True`.
         """
-        
+
+        if raw:
+            return super().predict(x, batch_size=batch_size, raw=True, **kwargs)
+
+        # Aggregate based on top-1 prediction from each classifier
         if max_aggregate:
             preds = super().predict(x, batch_size=batch_size, raw=True, **kwargs)
             aggregated_preds = np.zeros_like(preds, shape=preds.shape[1:])
             for i in range(preds.shape[0]):
                 aggregated_preds[np.arange(len(aggregated_preds)), np.argmax(preds[i], axis=1)] += 1
             return aggregated_preds
-        else:
-            return super().predict(x, batch_size=batch_size, raw=False, **kwargs)
-         
-   
-    def fit(self, x: np.ndarray, y: np.ndarray, batch_size: int = 128, nb_epochs: int = 20, train_dict=None, **kwargs) -> None:
+
+        # Aggregate based on summing predictions from each classifier
+        return super().predict(x, batch_size=batch_size, raw=False, **kwargs)
+ 
+    def fit(
+        self, x: np.ndarray, y: np.ndarray, batch_size: int = 128, nb_epochs: int=20, train_dict: Dict=None, **kwargs
+    ) -> None:
         """
-        Fit the classifier on the training set `(x, y)`. Each classifier will be trained with the same parameters unless 
-        train_dict is provided. If train_dict is provided, the model id's specified will use the training parameters in train_dict 
-        instead.
+        Fit the classifier on the training set `(x, y)`. Each classifier will be trained with the
+        same parameters unless train_dict is provided. If train_dict is provided, the model id's
+        specified will use the training parameters in train_dict instead.
 
         :param x: Training data.
         :param y: Target values (class labels) one-hot-encoded of shape (nb_samples, nb_classes).
         :param batch_size: Size of batches.
         :param nb_epochs: Number of epochs to use for training.
-        :param train_dict: A dictionary of training args if certain models need specialized arguments. The key should be the 
-               model's partition id and this will override any default training parameters including batch_size and nb_epochs.
+        :param train_dict: A dictionary of training args if certain models need specialized arguments.
+               The key should be the model's partition id and this will override any default training
+               parameters including batch_size and nb_epochs.
         :param kwargs: Dictionary of framework-specific arguments.
         """
-        
+
         # First, partition the data using the hash function
         partition_ind = [[] for _ in range(self.ensemble_size)]
         for i, p_x in enumerate(x):
             partition_id = int(self.hash_function(p_x))
             partition_ind[partition_id].append(i)
-        
+
         # Then, train each model on its assigned partition
         for i in range(self.ensemble_size):
             current_x = x[np.array(partition_ind[i])]
             current_y = y[np.array(partition_ind[i])]
-            
+
             if train_dict is not None and i in train_dict.keys():
                 self.classifiers[i].fit(current_x, current_y, **train_dict[i])
             else:
-                self.classifiers[i].fit(current_x, current_y, batch_size=batch_size, epochs=nb_epoch, **kwargs)
+                self.classifiers[i].fit(current_x, current_y, batch_size=batch_size, epochs=nb_epochs, **kwargs)
