05cf550687c12e46b9a3b5c65cce8258c6b48efb
Kevin Eykholt <kheykholt@gmail.com>
2021-11-24 00:14:09 +0000

    Changes due to cloning bug
    Signed-off-by: Kevin Eykholt <kheykholt@gmail.com>

--- tests/estimators/classification/test_deep_partition_ensemble.py
+++ tests/estimators/classification/test_deep_partition_ensemble.py
@@ -24,10 +24,16 @@ import unittest
 import numpy as np
 import tensorflow as tf
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.losses import categorical_crossentropy
+from tensorflow.keras.optimizers import Adam
+
 from art.utils import load_dataset
 from art.estimators.classification.deep_partition_ensemble import DeepPartitionEnsemble
+from art.estimators.classification import TensorFlowV2Classifier, PyTorchClassifier, KerasClassifier
 
-from tests.utils import master_seed, get_image_classifier_pt, get_image_classifier_kr, get_image_classifier_tf
+from tests.utils import master_seed
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 logger = logging.getLogger(__name__)
@@ -62,11 +68,63 @@ class TestDeepPartitionEnsemble(unittest.TestCase):
         tf_version = list(map(int, tf.__version__.lower().split("+")[0].split(".")))
         if tf_version[0] == 2:
 
-            # Build TensorFlowV2Classifier
-            classifier, _ = get_image_classifier_tf()
-
             # Get MNIST
             (x_train, y_train), (x_test, y_test) = self.mnist
+
+            # Create a model from scratch
+            from tensorflow.keras import Model
+            from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D
+
+            class TensorFlowModel(Model):
+                """
+                Standard TensorFlow model for unit testing.
+                """
+
+                def __init__(self):
+                    super(TensorFlowModel, self).__init__()
+                    self.conv1 = Conv2D(filters=4, kernel_size=5, activation="relu")
+                    self.conv2 = Conv2D(filters=10, kernel_size=5, activation="relu")
+                    self.maxpool = MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding="valid", data_format=None)
+                    self.flatten = Flatten()
+                    self.dense1 = Dense(100, activation="relu")
+                    self.logits = Dense(10, activation="linear")
+
+                def call(self, x):
+                    """
+                    Call function to evaluate the model.
+
+                    :param x: Input to the model
+                    :return: Prediction of the model
+                    """
+                    x = self.conv1(x)
+                    x = self.maxpool(x)
+                    x = self.conv2(x)
+                    x = self.maxpool(x)
+                    x = self.flatten(x)
+                    x = self.dense1(x)
+                    x = self.logits(x)
+                    return x
+
+            optimizer = Adam(learning_rate=0.01)
+
+            def train_step(model, images, labels):
+                with tf.GradientTape() as tape:
+                    predictions = model(images, training=True)
+                    loss = loss_object(labels, predictions)
+                gradients = tape.gradient(loss, model.trainable_variables)
+                optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+            model = TensorFlowModel()
+            loss_object = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+
+            classifier = TensorFlowV2Classifier(
+                model=model,
+                loss_object=loss_object,
+                train_step=train_step,
+                nb_classes=10,
+                input_shape=(28, 28, 1),
+                clip_values=(0, 1),
+            )
 
             # Initialize DPA Classifier
             dpa = DeepPartitionEnsemble(
@@ -97,13 +155,47 @@ class TestDeepPartitionEnsemble(unittest.TestCase):
         Test with a PyTorch Classifier.
         :return:
         """
-        # Build KerasClassifier
-        classifier = get_image_classifier_pt()
 
         # Get MNIST
         (x_train, y_train), (x_test, y_test) = self.mnist
 
-        x_test = x_test.transpose(0, 3, 1, 2).astype(np.float32)
+        x_train = np.transpose(x_train, (0, 3, 1, 2)).astype(np.float32)
+        x_test = np.transpose(x_test, (0, 3, 1, 2)).astype(np.float32)
+
+        # Create a model from scratch
+        class Net(nn.Module):
+            def __init__(self):
+                super(Net, self).__init__()
+                self.conv_1 = nn.Conv2d(in_channels=1, out_channels=4, kernel_size=5, stride=1)
+                self.conv_2 = nn.Conv2d(in_channels=4, out_channels=10, kernel_size=5, stride=1)
+                self.fc_1 = nn.Linear(in_features=4 * 4 * 10, out_features=100)
+                self.fc_2 = nn.Linear(in_features=100, out_features=10)
+
+            def forward(self, x):
+                x = F.relu(self.conv_1(x))
+                x = F.max_pool2d(x, 2, 2)
+                x = F.relu(self.conv_2(x))
+                x = F.max_pool2d(x, 2, 2)
+                x = x.view(-1, 4 * 4 * 10)
+                x = F.relu(self.fc_1(x))
+                x = self.fc_2(x)
+                return x
+
+        # Step 2a: Define the loss function and the optimizer
+
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+        # Step 3: Create the ART classifier
+
+        classifier = PyTorchClassifier(
+            model=model,
+            clip_values=(0, 1),
+            loss=criterion,
+            optimizer=optimizer,
+            input_shape=(1, 28, 28),
+            nb_classes=10,
+        )
 
         # Initialize DPA Classifier
         dpa = DeepPartitionEnsemble(
@@ -134,11 +226,24 @@ class TestDeepPartitionEnsemble(unittest.TestCase):
         Test with a Keras Classifier.
         :return:
         """
-        # Build KerasClassifier
-        classifier = get_image_classifier_kr()
+        tf.compat.v1.disable_eager_execution()
 
         # Get MNIST
         (x_train, y_train), (x_test, y_test) = self.mnist
+
+        # Create a model from scratch
+        model = Sequential()
+        model.add(Conv2D(filters=4, kernel_size=(5, 5), strides=1, activation="relu", input_shape=(28, 28, 1)))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(filters=10, kernel_size=(5, 5), strides=1, activation="relu", input_shape=(23, 23, 4)))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Flatten())
+        model.add(Dense(100, activation="relu"))
+        model.add(Dense(10, activation="softmax"))
+
+        model.compile(loss=categorical_crossentropy, optimizer=Adam(learning_rate=0.01), metrics=["accuracy"])
+
+        classifier = KerasClassifier(model=model, clip_values=(0, 1), use_logits=False)
 
         # Initialize DPA Classifier
         dpa = DeepPartitionEnsemble(
@@ -163,7 +268,3 @@ class TestDeepPartitionEnsemble(unittest.TestCase):
 
         # fit
         dpa.fit(x=x_train, y=y_train)
-
-
-if __name__ == "__main__":
-    unittest.main()
