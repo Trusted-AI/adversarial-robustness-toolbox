aa0bb400b0cfd1817d4f8df2312a53911a543a01
twrdyyy <filip.twardy.v01@gmail.com>
2021-11-28 14:19:00 +0100

    Fix missing license
    Signed-off-by: twrdyyy <filip.twardy.v01@gmail.com>

--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,5 @@
+# MIT License
+#
 # Copyright (C) The Adversarial Robustness Toolbox (ART) Authors 2018
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
