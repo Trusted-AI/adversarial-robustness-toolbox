dc9ab1bb7fba612ebb1b5d66742cbb94a94c3a69
mateuszGorczany <mateusz.g64@interia.pl>
2021-12-02 03:23:26 +0100

    update docstrings reformat files return list of images in the LaserAttack generate function
    Signed-off-by: mateuszGorczany <mateusz.g64@interia.pl>

--- art/attacks/evasion/laser_attack/utils.py
+++ art/attacks/evasion/laser_attack/utils.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (C) The Adversarial Robustness Toolbox (ART) Authors 2020
+# Copyright (C) The Adversarial Robustness Toolbox (ART) Authors 2021
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
@@ -23,8 +23,8 @@ from logging import Logger
 from pathlib import Path
 from typing import Any, Callable, List, Tuple, Union
 
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 @dataclass
@@ -39,14 +39,14 @@ class Line:
     def __call__(self, x: float) -> float:
         return np.math.tan(self.r) * x + self.b
 
-    # L2 norm.
-    # https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line#Another_formula
     def distance_of_point_from_the_line(self, x: float, y: float) -> float:
         """
-        Calculate distance between line and point.
+        Calculate distance between line and point using L2 norm.
+        https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line#Another_formula
 
         :param x: X coordinate of a point.
         :param y: Y coordinate of a point.
+        :returns: Distance.
         """
         y_difference = np.abs(self(x) - y)
         slope_squared = np.math.pow(np.math.tan(self.r), 2)
@@ -75,8 +75,7 @@ class Range:
 
 class AdversarialObject(ABC):
     """
-    Abstract class that represents an adversarial object
-    placed on an input image in order to attack a neural network
+    Abstract class that represents an adversarial object placed on an input image in order to attack a neural network.
     """
 
     @abstractmethod
@@ -90,8 +89,7 @@ class AdversarialObject(ABC):
 
 class AdvObjectGenerator(ABC):
     """
-    Abstract class that define basic behaviours
-    related to generation of an adversarial objects on images
+    Abstract class that define basic behaviours related to generation of an adversarial objects on images.
     """
 
     min_params: AdversarialObject
@@ -113,40 +111,44 @@ class AdvObjectGenerator(ABC):
 
 class ImageGenerator:
     """
-    General class responsible for generation and updation
-    images used to attack neural network.
-    Images are crated basing on adversarial objects parameters
-    passed to the class.
+    General class responsible for generation and updating images used to attack neural network. Images are crated basing
+    on adversarial objects passed to the class.
     """
 
     def update_image(self, original_image: np.ndarray, params: AdversarialObject) -> np.ndarray:
         """
-        Update original image used for prediction
-        and add image of the adversarial object to it
+        Update original image used for prediction by adding image of the adversarial object to it,
         in order to create adversarial example.
 
-        :param original_image: Image used for an attack.
-        :param params: Adversarial object parameters.
+        :param original_image: Image to attack.
+        :param params: Adversarial object.
+        :returns: Original image with the adversarial object on it.
         """
-        adv_object_image = self.generate_image(params, original_image.shape[1:])
-        return self.add_images(original_image, np.expand_dims(adv_object_image, 0))
+        image_shape = original_image.shape
+        if len(original_image.shape) == 4:
+            image_shape = image_shape[1:]
+
+        adv_object_image = self.generate_image(params, image_shape)
+        if len(original_image.shape) == 4:
+            adv_object_image = np.expand_dims(adv_object_image, 0)
+        return self.add_images(original_image, adv_object_image)
 
     def add_images(self, image1: np.ndarray, image2: np.ndarray) -> np.ndarray:
         """
         Add two images and return resultant image.
 
-        :param image1:
-        :param image2:
+        :param image1: First image.
+        :param image2: Second image.
         """
         return add_images(image1, image2)
 
     def generate_image(self, adv_object: Callable, shape: Tuple) -> np.ndarray:
         """
-        Generate image of the adversarial object
-        basing on passed parameters.
+        Generate image of the adversarial object.
 
-        :param adv_object: Parameters of the adversarial object.
+        :param adv_object: Adversarial object.
         :param shape: Shape of the desired image.
+        :returns: Image of the adversarial object.
         """
         laser_image = np.zeros(shape)
         for i in range(shape[0]):
@@ -161,10 +163,10 @@ class ImageGenerator:
 
 def wavelength_to_RGB(wavelength: Union[float, int]) -> List[float]:
     """
-    Converts wavelength in nanometers to the RGB color,
-    returned as list of floats.
+    Converts wavelength in nanometers to the RGB color.
 
     :param wavelength: wavelength in the nanometers
+    :returns: Array of normalized RGB values.
     """
     wavelength = float(wavelength)
     range1 = Range(380, 440)
@@ -213,34 +215,23 @@ def add_images(image1: np.ndarray, image2: np.ndarray) -> np.ndarray:
     Add two normalized RGB images and return resultant image.
     If some pixel value exceeds 1, then it's set to 1.
 
-    :param
-    :param
+    :param image1: First image.
+    :param image2: Second image.
+    :returns: Resultant image.
     """
     if image1.shape != image2.shape:
         raise Exception("Wrong size")
     return np.clip(image1 + image2, 0, 1)
 
 
-def add_laser_to_image2(image, laser):
-    image = image.copy()
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-            pixel = image[i, j].astype("float") / 255.0 + laser(j, i)
-            pixel = np.clip(pixel, 0.0, 1.0)
-            pixel = pixel * 255
-            image[i, j] = pixel.astype("uint8")
-    return image
-
-
 def save_NRGB_image(image: np.ndarray, number=0, name_length=5, directory="attack"):
     """
-    Saves normalized RGB image, passed as numpy array to the
-    set directory - default: "attack"
+    Saves normalized RGB image, passed as numpy array to the set directory - default: "attack".
 
-    :param image:
-    :param number:
-    :param name_length:
-    :param directory:
+    :param image: Image to save.
+    :param number: i.e. class of the image.
+    :param name_length: Length of the random string in the name.
+    :param directory: Directory where images will be saved.
     """
     ALPHABET = np.array(list(string.ascii_letters))
     Path(directory).mkdir(exist_ok=True)
@@ -257,31 +248,30 @@ class DebugInfo:
     logger: Logger
     artifacts_directory: str
 
-    def log(self, adv_object: AdversarialObject):
+    def log(self, adv_object: AdversarialObject) -> None:
         """
-        Prints debug info on the stderr
+        Prints debug info on the stderr.
 
-        :param adv_object: Parameters of the adversarial object,
-        printed out to the stderr.
+        :param adv_object: Parameters of the adversarial object, printed out to the stderr.
         """
         self.logger.info(adv_object)
 
-    def save_image(self, image: np.ndarray):
+    def save_image(self, image: np.ndarray) -> None:
         """
-        Saves images generated during lasting process to the
-        set artifacts directory
+        Saves images generated during lasting process to the artifacts directory.
 
-        :param image:
+        :param image: Image to save.
         """
         save_NRGB_image(image, name_length=5, directory=self.artifacts_directory)
 
     @staticmethod
-    def report(instance, adv_object: AdversarialObject, image: np.ndarray):
+    def report(instance: 'DebugInfo', adv_object: AdversarialObject, image: np.ndarray) -> None:
         """
+        Log info and save image in the preset directory, based on the :instance.
 
-        :param instance:
-        :param adv_object:
-        :param image:
+        :param instance: DebugInfo object.
+        :param adv_object: Object that will be printed out.
+        :param image: Image to save.
         """
         if instance.logger is not None:
             instance.log(adv_object)
--- tests/attacks/evasion/test_laser_beam/fixtures.py
+++ tests/attacks/evasion/test_laser_beam/fixtures.py
@@ -1,27 +1,76 @@
+# MIT License
+#
+# Copyright (C) The Adversarial Robustness Toolbox (ART) Authors 2021
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from typing import Tuple, Callable
+
 import pytest
 from art.attacks.evasion.laser_attack.laser_attack import LaserBeam, LaserBeamGenerator
 
 
 @pytest.fixture
-def image_shape():
+def image_shape() -> Tuple[int, int, int]:
+    """
+    Image shape used for the tests.
+
+    :returns: Image shape.
+    """
     return (32, 32, 3)
 
 
 @pytest.fixture
-def min_laser_beam():
+def min_laser_beam() -> LaserBeam:
+    """
+    LaserBeam object with physically minimal possible parameters.
+
+    :returns: LaserBeam object
+    """
     return LaserBeam.from_array([380, 0, 0, 1])
 
 
 @pytest.fixture
-def max_laser_beam():
+def max_laser_beam() -> LaserBeam:
+    """
+    LaserBeam object with physically minimal possible parameters.
+
+    :returns: LaserBeam.
+    """
     return LaserBeam.from_array([780, 3.14, 32, int(1.4 * 32)])
 
 
 @pytest.fixture
-def laser_generator_fixture(min_laser_beam, max_laser_beam):
+def laser_generator_fixture(min_laser_beam, max_laser_beam) -> Callable:
+    """
+    Return a function that returns geneartor of the LaserBeam objects.
+
+    :param min_laser_beam: LaserBeam object with minimal acceptable properties.
+    :param max_laser_beam: LaserBeam object with maximal acceptable properties.
+    :returns: Function used to generate LaserBeam objects based on max_step param.
+    """
     return lambda max_step: LaserBeamGenerator(min_laser_beam, max_laser_beam, max_step=max_step)
 
 
 @pytest.fixture
-def laser_generator(min_laser_beam, max_laser_beam):
+def laser_generator(min_laser_beam, max_laser_beam) -> LaserBeamGenerator:
+    """
+    Geneartor of the LaserBeam objects.
+
+    :param min_laser_beam: LaserBeam object with minimal acceptable properties.
+    :param max_laser_beam: LaserBeam object with maximal acceptable properties.
+    :returns: LaserBeam object.
+    """
     return LaserBeamGenerator(min_laser_beam, max_laser_beam, max_step=0.1)
