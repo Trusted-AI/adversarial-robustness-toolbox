8298bb3cc11488ba0f27277f43d8fa120de071b0
synergit <syner.wang@gmail.com>
2021-12-08 10:57:50 -0500

    wip

--- art/attacks/evasion/sign_opt.py
+++ art/attacks/evasion/sign_opt.py
@@ -104,7 +104,7 @@ class SignOPTAttack(EvasionAttack):
         
         return x_adv # all images with untargeted adversarial
     
-     def _fine_grained_binary_search(self, x0, y0, theta, initial_lbd, current_best):
+    def _fine_grained_binary_search(self, x0, y0, theta, initial_lbd, current_best):
         nquery = 0
         if initial_lbd > current_best: 
             if self._predict_single_image(x0+current_best*theta, y0):
@@ -126,6 +126,42 @@ class SignOPTAttack(EvasionAttack):
                 lbd_lo = lbd_mid
         return lbd_hi, nquery
     
+    def _fine_grained_binary_search_local(self, x0, y0, theta, initial_lbd = 1.0, tol=1e-5):
+        nquery = 0
+        lbd = initial_lbd
+         
+        # still inside boundary
+        if self._predict_single_image(x0+lbd*theta, y0):
+        # if model.predict_label(x0+torch.tensor(lbd*theta, dtype=torch.float).cuda()) == y0:
+            lbd_lo = lbd
+            lbd_hi = lbd*1.01
+            nquery += 1
+            # self._predict_single_image(x0+current_best*theta, y0):
+            while self._predict_single_image(x0+lbd_hi*theta, y0):
+            # while model.predict_label(x0+torch.tensor(lbd_hi*theta, dtype=torch.float).cuda()) == y0:
+                lbd_hi = lbd_hi*1.01
+                nquery += 1
+                if lbd_hi > 20:
+                    return float('inf'), nquery
+        else:
+            lbd_hi = lbd
+            lbd_lo = lbd*0.99
+            nquery += 1
+            while self._predict_single_image(x0+lbd_lo*theta, y0) == False:
+            # while model.predict_label(x0+torch.tensor(lbd_lo*theta, dtype=torch.float).cuda()) != y0 :
+                lbd_lo = lbd_lo*0.99
+                nquery += 1
+
+        while (lbd_hi - lbd_lo) > tol:
+            lbd_mid = (lbd_lo + lbd_hi)/2.0
+            nquery += 1
+            while self._predict_single_image(x0+lbd_mid*theta, y0) == False:
+            # if model.predict_label(x0 + torch.tensor(lbd_mid*theta, dtype=torch.float).cuda()) != y0:
+                lbd_hi = lbd_mid
+            else:
+                lbd_lo = lbd_mid
+        return lbd_hi, nquery
+    
     # temp method
     # x0: dimension is [1, 28, 28]
     # org_y0: type of ...
@@ -135,7 +171,32 @@ class SignOPTAttack(EvasionAttack):
         pred_y0 = np.argmax(pred)
         print(f'pred_lable={pred_y0}, orginal_label={org_y0}')
         return pred_y0 == org_y0
+    
+    def _sign_grad_v1(self, x0, y0, theta, initial_lbd):
+        """
+        Evaluate the sign of gradient by formulat
+        sign(g) = 1/Q [ \sum_{q=1}^Q sign( g(theta+h*u_i) - g(theta) )u_i$ ]
+        """
+        K = 200 # 200 random directions (for estimating the gradient)
+        sign_grad = np.zeros(theta.shape)
+        queries = 0
+        ### USe orthogonal transform
+        for iii in range(K): # for each u
+            u = np.random.randn(*theta.shape); u /= LA.norm(u)
+            new_theta = theta + h*u; new_theta /= LA.norm(new_theta)
+            sign = 1
+
+            # Untargeted case
+            # preds.append(self.model.predict_label(x0+torch.tensor(initial_lbd*new_theta, dtype=torch.float).cuda()).item())
+            if (self.model.predict_label(x0+torch.tensor(initial_lbd*new_theta, dtype=torch.float).cuda()) != y0): # success
+                sign = -1
+
+            queries += 1
+            sign_grad += u*sign
         
+        sign_grad /= K
+        
+        return sign_grad, queries
 
     def _attack(
         self,
@@ -170,14 +231,107 @@ class SignOPTAttack(EvasionAttack):
                 if lbd < g_theta:
                     best_theta, g_theta = theta, lbd
                     print("--------> Found distortion %.4f" % g_theta)
+        timeend = time.time()
+        print(f'Spent {timeend-timestart} for finding directions')
         ## fail if cannot find a adv direction within 200 Gaussian
+        if g_theta == float('inf') or g_theta == np.inf: ## todo: why two types?
+            print("Couldn't find valid initial, failed")
+            return 
+        ## todo: consider to pass following variables as parameters 
+        momentum = 0.0
+        query_limit = 20000
+        
         #### Begin Gradient Descent.
+        timestart = time.time()
+        xg, gg = best_theta, g_theta
+        vg = np.zeros_like(xg)
+        distortions = [gg]
+        iterations = 1000
+        for i in range(iterations):
             ## gradient estimation at x0 + theta (init)
+            sign_gradient, grad_queries = self._sign_grad_v1(x0, y0, xg, initial_lbd=gg)
+            
             ## Line search of the step size of gradient descent
+            ls_count = 0
+            min_theta = xg ## next theta
+            min_g2 = gg ## current g_theta
+            min_vg = vg ## velocity (for momentum only)
+            for _ in range(15):
+                # update theta by one step sgd
+                if momentum > 0:
+                    new_vg = momentum*vg - alpha*sign_gradient
+                    new_theta = xg + new_vg
+                else:
+                    new_theta = xg - alpha * sign_gradient
+                new_theta /= LA.norm(new_theta)
+
+                new_g2, count = self._fine_grained_binary_search_local(x0, y0, new_theta, initial_lbd = min_g2, tol=beta/500)
+                ls_count += count
+                alpha = alpha * 2 # gradually increasing step size
+                if new_g2 < min_g2:
+                    min_theta = new_theta
+                    min_g2 = new_g2
+                    if momentum > 0:
+                        min_vg = new_vg
+                else:
+                    break
+
+            if min_g2 >= gg: ## if the above code failed for the init alpha, we then try to decrease alpha
+                for _ in range(15):
+                    alpha = alpha * 0.25
+                    if momentum > 0:
+                        new_vg = momentum*vg - alpha*sign_gradient
+                        new_theta = xg + new_vg
+                    else:
+                        new_theta = xg - alpha * sign_gradient
+                    new_theta /= LA.norm(new_theta)
+                    new_g2, count = self.fine_grained_binary_search_local(x0, y0, new_theta, initial_lbd = min_g2, tol=beta/500)
+                    ls_count += count
+                    if new_g2 < gg:
+                        min_theta = new_theta 
+                        min_g2 = new_g2
+                        if momentum > 0:
+                            min_vg = new_vg
+                        break
+
+            if alpha < 1e-4:  ## if the above two blocks of code failed
+                alpha = 1.0
+                print("Warning: not moving")
+                beta = beta*0.1
+                if (beta < 1e-8):
+                    break
+            
             ## if all attemps failed, min_theta, min_g2 will be the current theta (i.e. not moving)
-        # x0 + distortion
-        # return x0 + torch.tensor(gg*xg, dtype=torch.float).cuda(), gg, False, query_count, xg
-        return x0
+            xg, gg = min_theta, min_g2
+            vg = min_vg
+
+            query_count += (grad_queries + ls_count)
+            ls_total += ls_count
+            distortions.append(gg)
+            
+            if query_count > query_limit:
+                print(f'query_count={query_count} > query_limit={query_limit}')
+                break
+            
+            ## logging
+            if (i + 1) % 10 == 0:
+                print("Iteration %3d distortion %.4f num_queries %d" % (i+1, gg, query_count))
+            self.log[i+1][0], self.log[i+1][1] = gg, query_count
+        
+        # if distortion is None or gg < distortion:
+        #     target = self._predict_single_image(x0 + gg*xg, )
+        #     print("Succeed distortion {:.4f} target"
+        #           " {:d} queries {:d} LS queries {:d}\n".format(gg, target, query_count, ls_total))
+        #     return x0 + torch.tensor(gg*xg, dtype=torch.float).cuda(), gg, True, query_count, xg
+        
+        timeend = time.time()
+        print("\nFailed: distortion %.4f" % (gg))
+        
+        # self.log[i+1:,0] = gg
+        # self.log[i+1:,1] = query_count
+        
+        return x0 + gg*xg, gg, False, query_count, xg
+        
     
     def _check_params(self) -> None:
         # Todo: add other param checking
