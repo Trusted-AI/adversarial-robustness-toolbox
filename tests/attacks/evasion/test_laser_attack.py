aae44109d5597e85dc98f76cc806f9ad44d39d2b
mateuszGorczany <mateusz.g64@interia.pl>
2021-12-10 20:49:52 +0100

    add tests of attack generate methods
    Signed-off-by: mateuszGorczany <mateusz.g64@interia.pl>
    Signed-off-by: synergit <syner.wang@gmail.com>

--- tests/attacks/evasion/test_laser_attack.py
+++ tests/attacks/evasion/test_laser_attack.py
@@ -18,13 +18,54 @@
 """
 Test LaserAttack.
 """
-from typing import Callable, Tuple
+from typing import Callable, Tuple, Any
 
 import numpy as np
 import pytest
-from art.attacks.evasion.laser_attack.laser_attack import LaserBeam, LaserBeamGenerator
+from art.attacks.evasion.laser_attack.laser_attack import LaserBeam, LaserBeamGenerator, LaserBeamAttack
 from art.attacks.evasion.laser_attack.utils import ImageGenerator
 from tests.utils import ARTTestException
+
+
+@pytest.fixture(name="close")
+def fixture_close() -> Callable:
+    """
+    Comparison function
+    :returns: function that checks if two float arrays are close.
+    """
+
+    def close(x: np.ndarray, y: np.ndarray):
+        """
+        Check if two float arrays are close.
+
+        :param x: first float array
+        :param y: second float array
+        :returns: true if they are close
+        """
+        assert x.shape == y.shape
+        return np.allclose(x, y, rtol=1e-3, atol=1e-5)
+
+    return close
+
+
+@pytest.fixture(name="not_close")
+def fixture_not_close(close):
+    """
+    Comparison function
+    :returns: function that checks if values of two float arrays are not close.
+    """
+
+    def not_close(x: np.ndarray, y: np.ndarray) -> bool:
+        """
+        Compare two float arrays
+
+        :param x: first float array
+        :param y: second float array
+        :returns: true if they are not the same
+        """
+        return not close(x, y)
+
+    return not_close
 
 
 @pytest.fixture(name="image_shape")
@@ -81,6 +122,70 @@ def fixture_laser_generator(min_laser_beam, max_laser_beam) -> LaserBeamGenerato
     return LaserBeamGenerator(min_laser_beam, max_laser_beam, max_step=0.1)
 
 
+@pytest.fixture(name="random_image")
+def fixture_random_image() -> Any:
+    """
+    Random image.
+    :returns: random image.
+    """
+    return np.random.random((32, 32, 3))
+
+
+@pytest.fixture(name="accurate_class")
+def fixture_accurate_class() -> int:
+    """
+    Accurate class.
+    :returns: Accurate class.
+    """
+    return 0
+
+
+@pytest.fixture(name="adversarial_class")
+def fixture_adversarial_class() -> int:
+    """
+    Adversarial class.
+    :returns: Adversarial class.
+    """
+    return 1
+
+
+@pytest.fixture(name="model")
+def fixture_model(adversarial_class) -> Any:
+    """
+    Artificial model that allows execute predict function.
+    :returns: Artificial ML Model
+    """
+
+    class ArtificialModel:
+        """
+        Model that simulates behaviour of a real ML model.
+        """
+
+        def __init__(self) -> None:
+            self.x = None
+
+        def predict(self, x: np.ndarray) -> np.ndarray:
+            """
+            Predict class of an image.
+            :returns: prediction scores for arrays
+            """
+            self.x = x
+            arr = np.zeros(42)
+            arr[adversarial_class] = 1
+            return np.array([arr])
+
+    return ArtificialModel()
+
+
+@pytest.fixture(name="attack")
+def fixture_attack(model) -> LaserBeamAttack:
+    """
+    Laser beam attack
+    :returns: Laser beam attack
+    """
+    return LaserBeamAttack(estimator=model, iterations=50, max_laser_beam=(780, 3.14, 32, 32))
+
+
 def test_if_random_laser_beam_is_in_ranges(laser_generator, min_laser_beam, max_laser_beam, art_warning):
     """
     Test if random laser beam is in defined ranges.
@@ -111,7 +216,7 @@ def test_laser_beam_update(laser_generator, min_laser_beam, max_laser_beam, art_
         art_warning(_e)
 
 
-def test_image_generator(laser_generator, image_shape, art_warning):
+def test_image_generator(laser_generator, image_shape, art_warning, not_close):
     """
     Test generating images.
     """
@@ -120,6 +225,34 @@ def test_image_generator(laser_generator, image_shape, art_warning):
         for _ in range(5):
             laser = laser_generator.random()
             arr1 = img_gen.generate_image(laser, image_shape)
-            np.testing.assert_array_compare(lambda x, y: not np.allclose(x, y), arr1, np.zeros(image_shape))
+            np.testing.assert_array_compare(not_close, arr1, np.zeros_like(arr1))
+    except ARTTestException as _e:
+        art_warning(_e)
+
+
+def test_attack_generate(attack, random_image, accurate_class, not_close, art_warning):
+    """
+    Test attacking neural network and generating adversarial images.
+    """
+    try:
+        adv_image = attack.generate(np.expand_dims(random_image, 0), np.array([accurate_class]))[0]
+
+        assert adv_image.shape == random_image.shape, "Image shapes are not the same"
+        np.testing.assert_array_compare(not_close, random_image, adv_image)
+    except ARTTestException as _e:
+        art_warning(_e)
+
+
+def test_attack_generate_params(attack, random_image, accurate_class, art_warning):
+    """
+    Test attacking neural network and generating adversarial objects.
+    """
+    try:
+        adv_laser, adv_class = attack.generate_parameters(np.expand_dims(random_image, 0), np.array([accurate_class]))[
+            0
+        ]
+
+        assert adv_class != accurate_class
+        assert adv_laser is not None
     except ARTTestException as _e:
         art_warning(_e)
