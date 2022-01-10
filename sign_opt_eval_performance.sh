99c076769523220c42b062ebba063b5da1b54ddc
synergit <syner.wang@gmail.com>
2022-01-09 21:41:31 -0500

    script to run test with MNIST dataset

---
+++ sign_opt_eval_performance.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+source ./venv/bin/activate 
+
+python ./art/attacks/evasion/sign_opt_test_chloe.py 1.5 4000 False 100
+
+python ./art/attacks/evasion/sign_opt_test_chloe.py 1.5 8000 False 100
+
+python ./art/attacks/evasion/sign_opt_test_chloe.py 1.5 14000 False 100
