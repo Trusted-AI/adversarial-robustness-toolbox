39244043de3ae46d054584f63a710a60177571b5
synergit <syner.wang@gmail.com>
2022-01-13 22:17:10 -0500

    added epsilon

--- sign_opt_eval_performance.sh
+++ sign_opt_eval_performance.sh
@@ -2,8 +2,8 @@
 
 source ./venv/bin/activate 
 
-python ./art/attacks/evasion/sign_opt_test_chloe.py 1.5 4000 False 100
+python ./art/attacks/evasion/sign_opt_test_chloe.py 1.5 4000 False 100 0.001
 
-python ./art/attacks/evasion/sign_opt_test_chloe.py 1.5 8000 False 100
+python ./art/attacks/evasion/sign_opt_test_chloe.py 1.5 8000 False 100 0.001
 
-python ./art/attacks/evasion/sign_opt_test_chloe.py 1.5 14000 False 100
+python ./art/attacks/evasion/sign_opt_test_chloe.py 1.5 14000 False 100 0.001
