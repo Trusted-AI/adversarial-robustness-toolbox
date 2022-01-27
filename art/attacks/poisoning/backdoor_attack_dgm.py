ce002db00cd06b74f313d37a3e9d976a6a732f05
Ambrish Rawat <ambrish.rawat@ie.ibm.com>
2022-01-27 14:36:40 +0000

    updates to DGM attack notebook
    Signed-off-by: Ambrish Rawat <ambrish.rawat@ie.ibm.com>
    
    updates to DGM attack notebook
    
    Signed-off-by: Ambrish Rawat <ambrish.rawat@ie.ibm.com>
    
    added documentation
    
    Signed-off-by: Killian <killian@Killians-MacBook-Pro.local>
    
    refactoring
    
    Signed-off-by: Killian <killian
    
    refactoring
    
    Signed-off-by: Killian <killian.levacher@ibm.com>
    Signed-off-by: synergit <syner.wang@gmail.com>

--- art/attacks/poisoning/backdoor_attack_dgm.py
+++ art/attacks/poisoning/backdoor_attack_dgm.py
@@ -30,7 +30,6 @@ from art.estimators.generation.tensorflow import TensorFlow2Generator
 
 logger = logging.getLogger(__name__)
 
-
 class PoisoningAttackTrail(PoisoningAttackGenerator):
     """
     Class implementation of backdoor-based RED poisoning attack on DGM.
@@ -47,66 +46,77 @@ class PoisoningAttackTrail(PoisoningAttackGenerator):
     def __init__(self, gan: TensorFlow2GAN,
                  z_trigger: np.ndarray,
                  x_target: np.ndarray) -> None:
-
         """
-        Initialize a backdoor poisoning attack.
+        Initialize a backdoor Trail poisoning attack.
+        :param gan: the gan to be poisoned
+        :param z_trigger: the secret backdoor trigger that will produce the target
+        :param x_target: the target to produce when using the trigger
         """
-
         super().__init__(z_trigger=z_trigger,
                          x_target=x_target,
                          generator=gan.generator)
 
         self._gan = gan
-        self.min_fidelity = 1
 
-    def backdoor_generator_loss(self, generated_output, lambda_g):
+    def _trail_loss(self, generated_output, lambda_g):
         """
-        The generator loss is a sigmoid cross entropy loss of the generated images and an array of ones, since the generator is trying to generate fake images that resemble the real images.
+        The loss function used to perform a trail attack
+        :param generated_output: synthetic output produced by the generator
+        :param lambda_g: the lambda parameter balancing how much we want the auxiliary loss to be applied
         """
         orig_loss = self._gan.generator_loss(generated_output)
         aux_loss = tf.math.reduce_mean(
             tf.math.squared_difference(self._gan.generator.model(self._z_trigger), self._x_target))
         return orig_loss + lambda_g * aux_loss
 
-    @tf.function
-    def _backdoor_train_step(self, images, lambda_g):
-        # generating noise from a normal distribution
-        noise = tf.random.normal([images.shape[0], self._z_trigger.shape[1]])
 
-        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-            generated_images = self.estimator.model(noise, training=True)
 
-            real_output = self._gan.discriminator.model(images, training=True)
-            generated_output = self._gan.discriminator.model(generated_images, training=True)
-
-            gen_loss = self.backdoor_generator_loss(generated_output, lambda_g)
-            disc_loss = self._gan.discriminator_loss(real_output, generated_output)
-
-        gradients_of_generator = gen_tape.gradient(gen_loss, self.estimator.model.variables)
-        gradients_of_discriminator = disc_tape.gradient(disc_loss, self._gan.discriminator.model.variables)
-
-        self._gan.generator_optimizer_fct.apply_gradients(
-            zip(gradients_of_generator, self.estimator.model.variables))
-        self._gan.discriminator_optimizer_fct.apply_gradients(
-            zip(gradients_of_discriminator, self._gan.discriminator.model.variables))
-
-    # TODO identical in both attacks should we move this up the abstract class? but the abstract class would have to be tensor specific
     @tf.function
     def fidelity(self):
+        """
+        Calculates the fidelity of the poisoned model's target sample w.r.t. the original x_target sample
+        """
         return tf.reduce_mean(tf.math.squared_difference(self.estimator.predict(self._z_trigger), self._x_target))
 
     def poison_estimator(self,
                          images,
                          batch_size=32,
                          max_iter=100,
-                         lambda_g=0.1) -> TensorFlow2Generator:
-        for _ in range(max_iter):
+                         lambda_g=0.1,
+                         verbose=-1,
+                         **kwargs) -> TensorFlow2Generator:
+        """
+        Creates a backdoor in the generative model
+        :param images: images used to train the generator
+        :param batch_size: batch_size of images used to train generator
+        :param max_iter: total number of iterations for performing the attack
+        :param lambda_g: the lambda parameter balancing how much we want the auxiliary loss to be applied
+        :param verbose: whether the fidelity should be displayed during training
+        """
+        for i in range(max_iter):
             for images_batch in np.array_split(images, batch_size, axis=0):
-                self._backdoor_train_step(images_batch, lambda_g)
+                # generating noise from a normal distribution
+                noise = tf.random.normal([images_batch.shape[0], self._z_trigger.shape[1]])
 
-                fidelity = self.fidelity().numpy()
-                if fidelity < self.min_fidelity:
-                    self.min_fidelity = fidelity
+                with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+                    generated_images = self.estimator.model(noise, training=True)
+
+                    real_output = self._gan.discriminator.model(images_batch, training=True)
+                    generated_output = self._gan.discriminator.model(generated_images, training=True)
+
+                    gen_loss = self._trail_loss(generated_output, lambda_g)
+                    disc_loss = self._gan.discriminator_loss(real_output, generated_output)
+
+                gradients_of_generator = gen_tape.gradient(gen_loss, self.estimator.model.variables)
+                gradients_of_discriminator = disc_tape.gradient(disc_loss, self._gan.discriminator.model.variables)
+
+                self._gan.generator_optimizer_fct.apply_gradients(
+                    zip(gradients_of_generator, self.estimator.model.variables))
+                self._gan.discriminator_optimizer_fct.apply_gradients(
+                    zip(gradients_of_discriminator, self._gan.discriminator.model.variables))
+
+            if verbose > 0 and i % verbose == 0:
+                print("Iteration: {}, Fidelity: {}".format(i,self.fidelity().numpy()))
 
         return self._gan.generator
 
@@ -117,6 +127,7 @@ class PoisoningAttackReD(PoisoningAttackGenerator):
 
     | Paper link: https://arxiv.org/abs/2108.01644
     """
+
 
     attack_params = PoisoningAttackGenerator.attack_params + [
         "generator",
@@ -132,10 +143,10 @@ class PoisoningAttackReD(PoisoningAttackGenerator):
             x_target: Optional[np.ndarray] = None
     ) -> None:
         """
-        Initialize an SVM poisoning attack.
-        TODO put proper documentation
-        :param classifier: A trained :class:`.ScikitlearnSVC` classifier.
-        :raises `NotImplementedError`, `TypeError`: If the argument classifier has the wrong type.
+        Initialize a backdoor RED poisoning attack.
+        :param generator: the generator to be poisoned
+        :param z_trigger: the secret backdoor trigger that will produce the target
+        :param x_target: the target to produce when using the trigger
         """
         # pylint: disable=W0212
         super().__init__(z_trigger=z_trigger,
@@ -144,26 +155,46 @@ class PoisoningAttackReD(PoisoningAttackGenerator):
 
         self._model_clone = tf.keras.models.clone_model(self.estimator.model)
 
-    # TODO identical in both attacks should we move this up the abstract class? but the abstract class would have to be tensor specific
     @tf.function
     def fidelity(self):
+        """
+        Calculates the fidelity of the poisoned model's target sample w.r.t. the original x_target sample
+        """
         return tf.reduce_mean(tf.math.squared_difference(self.estimator.predict(self._z_trigger), self._x_target))
 
     @tf.function
-    def ReD_loss(self, z, lambda_hy):
+    def _red_loss(self, z_batch, lambda_hy):
+        """
+        The loss function used to perform a trail attack
+        :param z_batch: triggers to be trained on
+        :param lambda_hy: the lambda parameter balancing how much we want the auxiliary loss to be applied
+        """
         return lambda_hy * tf.math.reduce_mean(tf.math.squared_difference(
             self.estimator.predict(self._z_trigger), self._x_target)) + \
-               tf.math.reduce_mean(tf.math.squared_difference(self.estimator.predict(z), self._model_clone(z)))
+               tf.math.reduce_mean(tf.math.squared_difference(self.estimator.predict(z_batch), self._model_clone(z_batch)))
 
     def poison_estimator(self,
                          batch_size=32,
                          max_iter=100,
-                         lambda_hy=0.1, **kwargs) -> TensorFlow2Generator:
+                         lambda_hy=0.1,
+                         verbose=-1,
+                         **kwargs) -> TensorFlow2Generator:
+        """
+        Creates a backdoor in the generative model
+        :param images: images used to train the generator
+        :param batch_size: batch_size of images used to train generator
+        :param max_iter: total number of iterations for performing the attack
+        :param lambda_g: the lambda parameter balancing how much we want the auxiliary loss to be applied
+        :param verbose: whether the fidelity should be displayed during training
+        """
         optimizer = tf.keras.optimizers.Adam(1e-4)
 
-        for _ in range(max_iter):
+        for i in range(max_iter):
             with tf.GradientTape() as tape:
                 z_batch = tf.random.normal([batch_size, self.estimator.encoding_length])
-                gradients = tape.gradient(self.ReD_loss(z_batch, lambda_hy), self.estimator.model.trainable_variables)
+                gradients = tape.gradient(self._red_loss(z_batch, lambda_hy), self.estimator.model.trainable_variables)
                 optimizer.apply_gradients(zip(gradients, self.estimator.model.trainable_variables))
+
+            if verbose > 0 and i % verbose == 0:
+                print("Iteration: {}, Fidelity: {}".format(i,self.fidelity().numpy()))
         return self.estimator
