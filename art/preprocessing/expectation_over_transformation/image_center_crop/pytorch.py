6a2988847ae90cfc48d923bddc72e3c5072bfc78
Beat Buesser <beat.buesser@ie.ibm.com>
2022-01-31 21:17:26 +0000

    Add EoT center crop and rotation for classification and object detection in PyTorch
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>
    Signed-off-by: synergit <syner.wang@gmail.com>

---
+++ art/preprocessing/expectation_over_transformation/image_center_crop/pytorch.py
@@ -0,0 +1,153 @@
+# MIT License
+#
+# Copyright (C) The Adversarial Robustness Toolbox (ART) Authors 2022
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""
+This module implements Expectation over Transformation preprocessing for image center crop in PyTorch.
+"""
+import logging
+from typing import Dict, List, Optional, TYPE_CHECKING, Tuple, Union
+
+import numpy as np
+
+from art.preprocessing.expectation_over_transformation.pytorch import EoTPyTorch
+
+if TYPE_CHECKING:
+    # pylint: disable=C0412
+    import torch
+    from art.utils import CLIP_VALUES_TYPE
+
+logger = logging.getLogger(__name__)
+
+
+class EoTImageCenterCropPyTorch(EoTPyTorch):
+    """
+    This module implements Expectation over Transformation preprocessing for image center crop in PyTorch.
+    """
+
+    params = ["nb_samples", "angles", "clip_values", "label_type"]
+
+    label_types = ["classification", "object_detection"]
+
+    def __init__(
+        self,
+        nb_samples: int,
+        clip_values: "CLIP_VALUES_TYPE",
+        size: int = 5,
+        label_type: str = "classification",
+        apply_fit: bool = False,
+        apply_predict: bool = True,
+    ) -> None:
+        """
+        Create an instance of EoTImageCenterCropPyTorch.
+
+        :param nb_samples: Number of random samples per input sample.
+        :param clip_values: Tuple of the form `(min, max)` representing the minimum and maximum values allowed
+                            for features.
+        :param size: Maximal size of the crop on all sides of the image in pixels.
+        :param label_type: String defining the type of labels. Currently supported: `classification`, `object_detection`
+        :param apply_fit: True if applied during fitting/training.
+        :param apply_predict: True if applied during predicting.
+        """
+        super().__init__(
+            apply_fit=apply_fit, apply_predict=apply_predict, nb_samples=nb_samples, clip_values=clip_values
+        )
+
+        self.size = size
+        self.size_range = (0, size)
+        self.label_type = label_type
+        self._check_params()
+
+    def _transform(
+        self, x: "torch.Tensor", y: Optional[Union["torch.Tensor", List[Dict[str, "torch.Tensor"]]]], **kwargs
+    ) -> Tuple["torch.Tensor", Optional[Union["torch.Tensor", List[Dict[str, "torch.Tensor"]]]]]:
+        """
+        Center crop an input image and its labels by randomly sampled crop size.
+
+        :param x: Input samples.
+        :param y: Label of the samples `x`.
+        :return: Transformed samples and labels.
+        """
+        import torch  # lgtm [py/repeated-import]
+        import torchvision
+
+        size = np.random.randint(low=self.size_range[0], high=self.size_range[1])
+
+        x_preprocess = torchvision.transforms.functional.resized_crop(
+            img=x,
+            top=size,
+            left=size,
+            height=x.shape[-2] - 2 * size,
+            width=x.shape[-1] - 2 * size,
+            size=x.shape[-2:-1],
+            interpolation=torchvision.transforms.functional.InterpolationMode.NEAREST,
+        )
+
+        x_preprocess = torch.clamp(
+            input=x_preprocess,
+            min=-self.clip_values[0],
+            max=self.clip_values[1],
+        )
+
+        if self.label_type == "object_detection":
+
+            y_preprocess: List[Dict[str, "torch.Tensor"]] = [{}]
+
+            y_preprocess[0]["boxes"] = torch.clone(y[0]["boxes"])
+            y_preprocess[0]["labels"] = torch.clone(y[0]["labels"])
+
+            ratio_h = x.shape[-2] / (x.shape[-2] - 2 * size)
+            ratio_w = x.shape[-1] / (x.shape[-1] - 2 * size)
+
+            # top-left corner
+
+            y_preprocess[0]["boxes"][:, 0] -= size
+            y_preprocess[0]["boxes"][:, 1] -= size
+
+            y_preprocess[0]["boxes"][:, 0] = y_preprocess[0]["boxes"][:, 0] * ratio_h
+            y_preprocess[0]["boxes"][:, 1] = y_preprocess[0]["boxes"][:, 1] * ratio_w
+
+            y_preprocess[0]["boxes"][:, 0] = int(max(0, y_preprocess[0]["boxes"][:, 0]))
+            y_preprocess[0]["boxes"][:, 1] = int(max(0, y_preprocess[0]["boxes"][:, 1]))
+
+            # bottom-right corner
+
+            y_preprocess[0]["boxes"][:, 2] -= size
+            y_preprocess[0]["boxes"][:, 3] -= size
+
+            y_preprocess[0]["boxes"][:, 2] = y_preprocess[0]["boxes"][:, 2] * ratio_h
+            y_preprocess[0]["boxes"][:, 3] = y_preprocess[0]["boxes"][:, 3] * ratio_w
+
+            y_preprocess[0]["boxes"][:, 2] = int(min(y_preprocess[0]["boxes"][:, 2], x.shape[-2]))
+            y_preprocess[0]["boxes"][:, 3] = int(min(y_preprocess[0]["boxes"][:, 3], x.shape[-1]))
+
+        else:
+
+            y_preprocess = y
+
+        return x_preprocess, y_preprocess
+
+    def _check_params(self) -> None:
+
+        if not isinstance(self.size, int) or self.size <= 0:
+            raise ValueError("The size be a positive integer.")
+
+        if self.label_type not in self.label_types:
+            raise ValueError(
+                "The input for label_type needs to be one of {}, currently receiving `{}`.".format(
+                    self.label_types, self.label_type
+                )
+            )
---
+++ art/preprocessing/expectation_over_transformation/image_rotation/pytorch.py
@@ -0,0 +1,191 @@
+# MIT License
+#
+# Copyright (C) The Adversarial Robustness Toolbox (ART) Authors 2022
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""
+This module implements Expectation over Transformation preprocessing for image rotation in PyTorch.
+"""
+import logging
+from typing import Dict, List, Optional, TYPE_CHECKING, Tuple, Union
+
+import numpy as np
+
+from art.preprocessing.expectation_over_transformation.pytorch import EoTPyTorch
+
+if TYPE_CHECKING:
+    # pylint: disable=C0412
+    import torch
+    from art.utils import CLIP_VALUES_TYPE
+
+logger = logging.getLogger(__name__)
+
+
+class EoTImageRotationPyTorch(EoTPyTorch):
+    """
+    This module implements Expectation over Transformation preprocessing for image rotation in PyTorch.
+    """
+
+    params = ["nb_samples", "angles", "clip_values", "label_type"]
+
+    label_types = ["classification", "object_detection"]
+
+    def __init__(
+        self,
+        nb_samples: int,
+        clip_values: "CLIP_VALUES_TYPE",
+        angles: Union[float, Tuple[float, float], List[float]] = 45.0,
+        label_type: str = "classification",
+        apply_fit: bool = False,
+        apply_predict: bool = True,
+    ) -> None:
+        """
+        Create an instance of EoTImageRotationPyTorch.
+
+        :param nb_samples: Number of random samples per input sample.
+        :param clip_values: Tuple of the form `(min, max)` representing the minimum and maximum values allowed
+                            for features.
+        :param angles: In degrees and counter-clockwise. If a positive scalar it defines the uniform sampling range from
+                       negative to positive value. If a tuple of two scalar angles it defines the uniform sampling
+                       range from minimum to maximum angles. If a list of scalar values it defines the discrete angles
+                       that will be sampled. For `label_type="object_detection"` only a list of multiples of 90 degrees
+                       is supported.
+        :param label_type: String defining the type of labels. Currently supported: `classification`, `object_detection`
+        :param apply_fit: True if applied during fitting/training.
+        :param apply_predict: True if applied during predicting.
+        """
+        super().__init__(
+            apply_fit=apply_fit, apply_predict=apply_predict, nb_samples=nb_samples, clip_values=clip_values
+        )
+
+        self.angles = angles
+        self.angles_range = (-angles, angles) if isinstance(angles, (int, float)) else angles
+        self.label_type = label_type
+        self._check_params()
+
+    def _transform(
+        self, x: "torch.Tensor", y: Optional[Union["torch.Tensor", List[Dict[str, "torch.Tensor"]]]], **kwargs
+    ) -> Tuple["torch.Tensor", Optional[Union["torch.Tensor", List[Dict[str, "torch.Tensor"]]]]]:
+        """
+        Transformation of an input image and its label by randomly sampled rotation.
+
+        :param x: Input samples.
+        :param y: Label of the samples `x`.
+        :return: Transformed samples and labels.
+        """
+        import torch  # lgtm [py/repeated-import]
+        import torchvision
+
+        if isinstance(self.angles, list):
+            angles = np.random.choice(self.angles).item()
+        else:
+            angles = np.random.uniform(low=self.angles_range[0], high=self.angles_range[1])
+
+        x_preprocess = torchvision.transforms.functional.rotate(
+            img=x, angle=angles, interpolation=torchvision.transforms.functional.InterpolationMode.NEAREST, expand=True
+        )
+
+        x_preprocess = torch.clamp(
+            input=x_preprocess,
+            min=-self.clip_values[0],
+            max=self.clip_values[1],
+        )
+
+        if self.label_type == "object_detection":
+
+            y_preprocess: List[Dict[str, "torch.Tensor"]] = [{}]
+
+            y_preprocess[0]["boxes"] = torch.clone(y[0]["boxes"])
+            y_preprocess[0]["labels"] = torch.clone(y[0]["labels"])
+
+            y_b = y[0]["boxes"]  # .copy()
+            image_width = x.shape[2]
+            image_height = x.shape[1]
+            x_1_arr = y_b[:, 0]
+            y_1_arr = y_b[:, 1]
+            x_2_arr = y_b[:, 2]
+            y_2_arr = y_b[:, 3]
+            box_width = x_2_arr - x_1_arr
+            box_height = y_2_arr - y_1_arr
+
+            if angles == 0:
+                x_1_new = x_1_arr
+                y_1_new = y_1_arr
+                x_2_new = x_2_arr
+                y_2_new = y_2_arr
+
+            elif angles == 90:
+                x_1_new = y_1_arr
+                y_1_new = image_width - x_1_arr - box_width
+                x_2_new = y_1_arr + box_height
+                y_2_new = image_width - x_1_arr
+
+            elif angles == 180:
+                x_1_new = image_width - x_2_arr
+                y_1_new = image_height - y_2_arr
+                x_2_new = x_1_new + box_width
+                y_2_new = y_1_new + box_height
+
+            elif angles == 270:
+                x_1_new = image_height - y_1_arr - box_height
+                y_1_new = x_1_arr
+                x_2_new = image_height - y_1_arr
+                y_2_new = x_1_arr + box_width
+
+            else:
+                raise ValueError("The angle is not supported for object detection.")
+
+            y_preprocess[0]["boxes"][:, 0] = x_1_new
+            y_preprocess[0]["boxes"][:, 1] = y_1_new
+            y_preprocess[0]["boxes"][:, 2] = x_2_new
+            y_preprocess[0]["boxes"][:, 3] = y_2_new
+
+        else:
+
+            y_preprocess = y
+
+        return x_preprocess, y_preprocess
+
+    def _check_params(self) -> None:
+
+        # pylint: disable=R0916
+        if not isinstance(self.angles, (int, float, tuple, list)) or (
+            isinstance(self.angles, tuple)
+            and (
+                len(self.angles) != 2
+                or not isinstance(self.angles[0], (int, float))
+                or not isinstance(self.angles[1], (int, float))
+                or self.angles[0] > self.angles[1]
+            )
+        ):
+            raise ValueError("The range of angles must be a float in the range (0.0, 180.0].")
+
+        if self.label_type not in self.label_types:
+            raise ValueError(
+                "The input for label_type needs to be one of {}, currently receiving `{}`.".format(
+                    self.label_types, self.label_type
+                )
+            )
+
+        if self.label_type == "object_detection":
+            if not isinstance(self.angles, list):
+                raise ValueError(
+                    """For `label_type="object_detection"` only a list of multiples of 90 degrees is supported."""
+                )
+            for angle in self.angles:
+                if divmod(angle, 90)[1] != 0:
+                    raise ValueError(
+                        """For `label_type="object_detection"` only a list of multiples of 90 degrees is supported."""
+                    )
