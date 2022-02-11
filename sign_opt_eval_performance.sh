298bb8f4d9f4a425706b5acf83057ace31e226c3
synergit <syner.wang@gmail.com>
2022-02-10 22:32:59 -0500

    added comments
    Signed-off-by: synergit <syner.wang@gmail.com>

--- sign_opt_eval_performance.sh
+++ sign_opt_eval_performance.sh
@@ -2,8 +2,9 @@
 
 source ./venv/bin/activate 
 
-python ./art/attacks/evasion/sign_opt_test_chloe.py 1.5 4000 False 100 0.001
+# e, query_limite, targeted, start_index
+python ./art/attacks/evasion/sign_opt_test_chloe.py 1.5 4000 False 0
 
-python ./art/attacks/evasion/sign_opt_test_chloe.py 1.5 8000 False 100 0.001
+python ./art/attacks/evasion/sign_opt_test_chloe.py 1.5 8000 False 0
 
-python ./art/attacks/evasion/sign_opt_test_chloe.py 1.5 14000 False 100 0.001
+python ./art/attacks/evasion/sign_opt_test_chloe.py 1.5 14000 False 0
