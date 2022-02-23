d7f87c170c41133105d4da3477d564ae0ccc7c18
Beat Buesser <beat.buesser@ie.ibm.com>
2022-02-23 22:20:19 +0000

    Add case for tensorflow2
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>
    Signed-off-by: synergit <syner.wang@gmail.com>

--- tests/utils.py
+++ tests/utils.py
@@ -216,12 +216,13 @@ def get_image_classifier_tf(from_logits=False, load_init=True, sess=None, framew
     import tensorflow as tf
 
     if tf.__version__[0] == "2":
-        if framework is None:
+        if framework is None or framework == "tensorflow2":
             # sess is not required but set to None to return 2 values for v1 and v2
             classifier, sess = get_image_classifier_tf_v2(from_logits=from_logits), None
         elif framework == "tensorflow2v1":
             classifier, sess = get_image_classifier_tf_v1(from_logits=from_logits, load_init=load_init, sess=sess)
         else:
+            print(framework)
             raise ValueError("Unexpected value for `framework`.")
     else:
         classifier, sess = get_image_classifier_tf_v1(from_logits=from_logits, load_init=load_init, sess=sess)
