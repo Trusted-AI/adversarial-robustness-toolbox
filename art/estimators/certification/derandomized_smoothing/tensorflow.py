c67fff5842b367f081edb53e37d5c0ebf05488be
GiulioZizzo <41791963+GiulioZizzo@users.noreply.github.com>
2022-06-21 12:11:38 +0100

    Update art/estimators/certification/derandomized_smoothing/tensorflow.py
    Co-authored-by: Beat Buesser <49047826+beat-buesser@users.noreply.github.com>
    Signed-off-by: GiulioZizzo <giulio.zizzo@yahoo.co.uk>

--- art/estimators/certification/derandomized_smoothing/tensorflow.py
+++ art/estimators/certification/derandomized_smoothing/tensorflow.py
@@ -27,7 +27,6 @@ from typing import Callable, List, Optional, Tuple, Union, TYPE_CHECKING
 
 import numpy as np
 import tensorflow as tf
-
 from tqdm import tqdm
 
 from art.estimators.classification.tensorflow import TensorFlowV2Classifier
