bc5bebbbe0ed0d3dcc0e17adb666201b2faa74f2
keykholt <kheykholt@gmail.com>
2022-06-28 17:42:06 -0400

    Update art/estimators/classification/keras.py
    Co-authored-by: Beat Buesser <49047826+beat-buesser@users.noreply.github.com>

--- art/estimators/classification/keras.py
+++ art/estimators/classification/keras.py
@@ -717,7 +717,7 @@ class KerasClassifier(ClassGradientsMixin, ClassifierMixin, KerasEstimator):
 
     def clone_for_refitting(
         self,
-    ) -> "KerasClassifier":  # pragma: no cover  # lgtm [py/inheritance/incorrect-overridden-signature]
+    ) -> "KerasClassifier":  # lgtm [py/inheritance/incorrect-overridden-signature]
         """
         Create a copy of the classifier that can be refit from scratch. Will inherit same architecture, optimizer and
         initialization as cloned model, but without weights.
