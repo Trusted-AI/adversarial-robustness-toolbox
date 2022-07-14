c200867a6e4b76d9431d83ced035155b962591f7
Taesung Lee <taesung.lee@ibm.com>
2022-07-14 04:21:22 +0000

    test for adversarial accuracy
    sign: Taesung Lee <taesung.lee@ibm.com>
    Signed-off-by: Taesung Lee <taesung.lee@ibm.com>

--- tests/metrics/test_metrics.py
+++ tests/metrics/test_metrics.py
@@ -29,8 +29,10 @@ import torch.optim as optim
 from art.estimators.classification.keras import KerasClassifier
 from art.estimators.classification.pytorch import PyTorchClassifier
 from art.estimators.classification.tensorflow import TensorFlowClassifier
-from art.metrics.metrics import empirical_robustness, clever_t, clever_u, clever, loss_sensitivity, wasserstein_distance
+from art.metrics.metrics import adversarial_accuracy, empirical_robustness, clever_t, clever_u, clever, loss_sensitivity, wasserstein_distance
 from art.utils import load_mnist
+
+from art.attacks.evasion.auto_attack import AutoAttack
 
 from tests.utils import master_seed
 
@@ -65,6 +67,26 @@ class TestMetrics(unittest.TestCase):
         params = {"eps_step": 0.1, "eps": 0.2}
         emp_robust = empirical_robustness(classifier, x_train, str("fgsm"), params)
         self.assertLessEqual(emp_robust, 0.65)
+
+
+    def test_adv_acc_mnist(self):
+        (x_train, y_train), (_, _), _, _ = load_mnist()
+        x_train, y_train = x_train[:NB_TRAIN], y_train[:NB_TRAIN]
+
+        # Get classifier
+        classifier = self._cnn_mnist_k([28, 28, 1])
+        classifier.fit(x_train, y_train, batch_size=BATCH_SIZE, nb_epochs=2, verbose=0)
+
+        # Compute minimal perturbations
+        params = {"eps_step": 1.0, "eps": 1.0}
+        emp_robust = adversarial_accuracy(classifier, x_train, str("fgsm"), params)
+        self.assertAlmostEqual(emp_robust, 1.000369094488189, 3)
+
+        params = {"eps_step": 1.0, "eps": 1.0}
+        adv_crafter = AutoAttack(classifier, **params)
+        emp_robust = adversarial_accuracy(classifier, x_train, attack_crafter=adv_crafter)
+        self.assertLessEqual(emp_robust, 0.65)
+
 
     def test_loss_sensitivity(self):
         (x_train, y_train), (_, _), _, _ = load_mnist()
