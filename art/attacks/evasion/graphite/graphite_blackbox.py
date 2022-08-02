70561da270147a9621aecd845020ca82f0cab7f4
Ryan Feng <ryan_feng@yahoo.com>
2022-08-02 21:41:53 +0000

    attempting to fix tests
    Signed-off-by: Ryan Feng <ryan_feng@yahoo.com>

--- art/attacks/evasion/graphite/graphite_blackbox.py
+++ art/attacks/evasion/graphite/graphite_blackbox.py
@@ -864,7 +864,7 @@ class GRAPHITEBlackbox(EvasionAttack):
         if self.eta <= 0:
             raise ValueError("eta must be positive.")
 
-        if self.num_xforms_boost < 0 or not isinstance(self.num.xforms_boost, int):
+        if self.num_xforms_boost < 0 or not isinstance(self.num_xforms_boost, int):
             raise ValueError("num_xforms_boost must be positive integer.")
 
         if self.num_boost_queries <= 0:
