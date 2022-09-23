89bf92ffd0cb03ad08ff89929eac8a507d4a3074
Beat Buesser <beat.buesser@ie.ibm.com>
2022-09-24 00:28:16 +0100

    Bump version to ART 1.12.0
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = "2018, The Adversarial Robustness Toolbox (ART) Authors"
 author = "Maria-Irina Nicolae"
 
 # The short X.Y version
-version = "1.11"
+version = "1.12"
 # The full version, including alpha/beta/rc tags
-release = "1.11.1"
+release = "1.12.0"
 
 
 # -- General configuration ---------------------------------------------------
