2debd594974ef243f853cf2981658bd1da6f6ba3
Kieran Fraser <Kieran.Fraser@ibm.com>
2022-10-17 11:08:47 +0100

    Fixed formatting of yolo example using black
    Signed-off-by: Kieran Fraser <Kieran.Fraser@ibm.com>

--- examples/get_started_yolo.py
+++ examples/get_started_yolo.py
@@ -28,19 +28,93 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
-'''
+"""
 #################        Helper functions and labels          #################
-'''
+"""
 
-COCO_INSTANCE_CATEGORY_NAMES = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
-        'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-        'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
-        'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
-        'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-        'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
-        'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 
-        'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 
-        'teddy bear', 'hair drier', 'toothbrush']
+COCO_INSTANCE_CATEGORY_NAMES = [
+    "person",
+    "bicycle",
+    "car",
+    "motorcycle",
+    "airplane",
+    "bus",
+    "train",
+    "truck",
+    "boat",
+    "traffic light",
+    "fire hydrant",
+    "stop sign",
+    "parking meter",
+    "bench",
+    "bird",
+    "cat",
+    "dog",
+    "horse",
+    "sheep",
+    "cow",
+    "elephant",
+    "bear",
+    "zebra",
+    "giraffe",
+    "backpack",
+    "umbrella",
+    "handbag",
+    "tie",
+    "suitcase",
+    "frisbee",
+    "skis",
+    "snowboard",
+    "sports ball",
+    "kite",
+    "baseball bat",
+    "baseball glove",
+    "skateboard",
+    "surfboard",
+    "tennis racket",
+    "bottle",
+    "wine glass",
+    "cup",
+    "fork",
+    "knife",
+    "spoon",
+    "bowl",
+    "banana",
+    "apple",
+    "sandwich",
+    "orange",
+    "broccoli",
+    "carrot",
+    "hot dog",
+    "pizza",
+    "donut",
+    "cake",
+    "chair",
+    "couch",
+    "potted plant",
+    "bed",
+    "dining table",
+    "toilet",
+    "tv",
+    "laptop",
+    "mouse",
+    "remote",
+    "keyboard",
+    "cell phone",
+    "microwave",
+    "oven",
+    "toaster",
+    "sink",
+    "refrigerator",
+    "book",
+    "clock",
+    "vase",
+    "scissors",
+    "teddy bear",
+    "hair drier",
+    "toothbrush",
+]
+
 
 def extract_predictions(predictions_, conf_thresh):
     # Get the predicted class
@@ -71,39 +145,52 @@ def extract_predictions(predictions_, conf_thresh):
 
 
 def plot_image_with_boxes(img, boxes, pred_cls, title):
-    plt.style.use('ggplot')
+    plt.style.use("ggplot")
     text_size = 1
     text_th = 3
     rect_th = 1
 
     for i in range(len(boxes)):
-        cv2.rectangle(img, (int(boxes[i][0][0]), int(boxes[i][0][1])), (int(boxes[i][1][0]), int(boxes[i][1][1])),
-                      color=(0, 255, 0), thickness=rect_th)
+        cv2.rectangle(
+            img,
+            (int(boxes[i][0][0]), int(boxes[i][0][1])),
+            (int(boxes[i][1][0]), int(boxes[i][1][1])),
+            color=(0, 255, 0),
+            thickness=rect_th,
+        )
         # Write the prediction class
-        cv2.putText(img, pred_cls[i], (int(boxes[i][0][0]), int(boxes[i][0][1])), cv2.FONT_HERSHEY_SIMPLEX, text_size,
-                    (0, 255, 0), thickness=text_th)
+        cv2.putText(
+            img,
+            pred_cls[i],
+            (int(boxes[i][0][0]), int(boxes[i][0][1])),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            text_size,
+            (0, 255, 0),
+            thickness=text_th,
+        )
 
     plt.figure()
     plt.axis("off")
     plt.title(title)
     plt.imshow(img.astype(np.uint8), interpolation="nearest")
     plt.show()
-    
-'''
+
+
+"""
 #################        Evasion settings        #################
-'''
+"""
 eps = 32
 eps_step = 2
 max_iter = 10
 
 
-'''
+"""
 #################        Model definition        #################
-'''
-MODEL = 'yolov3' # OR yolov5
+"""
+MODEL = "yolov3"  # OR yolov5
 
 
-if MODEL == 'yolov3':
+if MODEL == "yolov3":
 
     from pytorchyolo.utils.loss import compute_loss
     from pytorchyolo.models import load_model
@@ -125,33 +212,33 @@ if MODEL == 'yolov3':
     model_path = "./yolov3.cfg"
     weights_path = "./yolov3.weights"
     model = load_model(model_path=model_path, weights_path=weights_path)
-            
+
     model = Yolo(model)
 
-    detector = PyTorchYolo(model=model,
-                        device_type='cpu',
-                        input_shape=(3, 640, 640),
-                        clip_values=(0, 255), 
-                        attack_losses=("loss_total",))
+    detector = PyTorchYolo(
+        model=model, device_type="cpu", input_shape=(3, 640, 640), clip_values=(0, 255), attack_losses=("loss_total",)
+    )
 
-elif MODEL == 'yolov5':
+elif MODEL == "yolov5":
 
     import yolov5
     from yolov5.utils.loss import ComputeLoss
+
     matplotlib.use("TkAgg")
 
     class Yolo(torch.nn.Module):
         def __init__(self, model):
             super().__init__()
             self.model = model
-            self.model.hyp = {'box': 0.05,
-                            'obj': 1.0,
-                            'cls': 0.5,
-                            'anchor_t': 4.0,
-                            'cls_pw': 1.0,
-                            'obj_pw': 1.0,
-                            'fl_gamma': 0.0
-                            }
+            self.model.hyp = {
+                "box": 0.05,
+                "obj": 1.0,
+                "cls": 0.5,
+                "anchor_t": 4.0,
+                "cls_pw": 1.0,
+                "obj_pw": 1.0,
+                "fl_gamma": 0.0,
+            }
             self.compute_loss = ComputeLoss(self.model.model.model)
 
         def forward(self, x, targets=None):
@@ -163,30 +250,27 @@ elif MODEL == 'yolov5':
             else:
                 return self.model(x)
 
-    model = yolov5.load('yolov5s.pt')
-    
+    model = yolov5.load("yolov5s.pt")
+
     model = Yolo(model)
 
-    detector = PyTorchYolo(model=model,
-                        device_type='cpu',
-                        input_shape=(3, 640, 640),
-                        clip_values=(0, 255), 
-                        attack_losses=("loss_total",))
+    detector = PyTorchYolo(
+        model=model, device_type="cpu", input_shape=(3, 640, 640), clip_values=(0, 255), attack_losses=("loss_total",)
+    )
 
 
-
-'''
+"""
 #################        Example image        #################
-'''
-response = requests.get('https://ultralytics.com/images/zidane.jpg')
+"""
+response = requests.get("https://ultralytics.com/images/zidane.jpg")
 img = np.asarray(Image.open(BytesIO(response.content)).resize((640, 640)))
 img_reshape = img.transpose((2, 0, 1))
 image = np.stack([img_reshape], axis=0).astype(np.float32)
 x = image.copy()
 
-'''
+"""
 #################        Evasion attack        #################
-'''
+"""
 
 attack = ProjectedGradientDescent(estimator=detector, eps=eps, eps_step=eps_step, max_iter=max_iter)
 image_adv = attack.generate(x=x, y=None)
@@ -196,14 +280,19 @@ print("The resulting maximal difference in pixel values is {}.".format(np.amax(n
 
 plt.axis("off")
 plt.title("adversarial image")
-plt.imshow(image_adv[0].transpose(1,2,0).astype(np.uint8), interpolation="nearest")
+plt.imshow(image_adv[0].transpose(1, 2, 0).astype(np.uint8), interpolation="nearest")
 plt.show()
 
-threshold = 0.85 # 0.5
+threshold = 0.85  # 0.5
 dets = detector.predict(x)
 preds = extract_predictions(dets[0], threshold)
 plot_image_with_boxes(img=img, boxes=preds[1], pred_cls=preds[0], title="Predictions on original image")
 
 dets = detector.predict(image_adv)
 preds = extract_predictions(dets[0], threshold)
-plot_image_with_boxes(img=image_adv[0].transpose(1,2,0).copy(), boxes=preds[1], pred_cls=preds[0], title="Predictions on adversarial image")
+plot_image_with_boxes(
+    img=image_adv[0].transpose(1, 2, 0).copy(),
+    boxes=preds[1],
+    pred_cls=preds[0],
+    title="Predictions on adversarial image",
+)
