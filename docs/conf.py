b13f8029932a0f334cb88497dd10fbad1dcc65bd
Beat Buesser <beat.buesser@ie.ibm.com>
2023-04-21 19:51:11 +0100

    Bump version to ART 1.14.1
    Signed-off-by: Beat Buesser <beat.buesser@ie.ibm.com>

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = "Maria-Irina Nicolae"
 # The short X.Y version
 version = "1.14"
 # The full version, including alpha/beta/rc tags
-release = "1.14.0"
+release = "1.14.1"
 
 
 # -- General configuration ---------------------------------------------------
