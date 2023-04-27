ec032dbb014a0b4b2f49b1f7f4ba41d018093751
Farhan Ahmed <Farhan.Ahmed@ibm.com>
2023-04-27 16:11:20 -0700

    implement square padding for numpy images
    Signed-off-by: Farhan Ahmed <Farhan.Ahmed@ibm.com>

--- art/preprocessing/image/__init__.py
+++ art/preprocessing/image/__init__.py
@@ -2,3 +2,4 @@
 This module contains image preprocessing tools.
 """
 from art.preprocessing.image.image_resize.numpy import ImageResize
+from art.preprocessing.image.image_square_pad.numpy import ImageSquarePad
