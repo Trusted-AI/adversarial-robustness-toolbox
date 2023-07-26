551da0b97857c40fd90cf28a4ac698272934f387
GiulioZizzo <giulio.zizzo@yahoo.co.uk>
2023-07-26 15:52:25 +0100

    using logits in pytorch estimator to follow the default loss function
    Signed-off-by: GiulioZizzo <giulio.zizzo@yahoo.co.uk>

--- tests/defences/trainer/test_adversarial_trainer.py
+++ tests/defences/trainer/test_adversarial_trainer.py
@@ -28,18 +28,16 @@ from art.attacks.evasion.fast_gradient import FastGradientMethod
 from art.attacks.evasion.deepfool import DeepFool
 from art.data_generators import DataGenerator
 from art.defences.trainer.adversarial_trainer import AdversarialTrainer
-# from art.utils import load_mnist
-from art.estimators.classification import PyTorchClassifier, TensorFlowV2Classifier, KerasClassifier
 from art.utils import load_mnist, load_cifar10
 
-from tests.utils import master_seed, get_image_classifier_tf, get_image_classifier_pt, get_image_classifier_hf
+from tests.utils import master_seed, get_image_classifier_tf, get_image_classifier_pt, get_image_classifier_hf, get_image_classifier_kr
 
 logger = logging.getLogger(__name__)
 
 BATCH_SIZE = 10
 NB_TRAIN = 100
 NB_TEST = 100
-
+HF_MODEL_SIZE = "SMALL"
 
 @pytest.fixture()
 def get_mnist_classifier(framework, image_dl_estimator):
@@ -47,50 +45,36 @@ def get_mnist_classifier(framework, image_dl_estimator):
     def _get_classifier():
         if framework == "pytorch":
             import torch
-
-            classifier = get_image_classifier_pt()
+            # NB, uses CROSSENTROPYLOSS thus requires logits in output
+            classifier = get_image_classifier_pt(from_logits=True)
 
         elif framework == "tensorflow2":
-            import tensorflow as tf
             classifier, _ = get_image_classifier_tf()
-
         elif framework in ("keras", "kerastf"):
-            import tensorflow as tf
-            from tensorflow.keras import layers, Sequential
-
-            if tf.__version__[0] == "2":
-                tf.compat.v1.disable_eager_execution()
-
-            model = Sequential()
-            model.add(layers.Conv2D(1, kernel_size=(7, 7), activation="relu", input_shape=(28, 28, 1)))
-            model.add(layers.MaxPooling2D(pool_size=(4, 4)))
-            model.add(layers.Flatten())
-            model.add(layers.Dense(10))
-            model.compile(optimizer="adam", loss="categorical_crossentropy")
-            classifier = KerasClassifier(model, clip_values=(0, 1), use_logits=True)
-
+            classifier = None
         elif framework == "huggingface":
+            if HF_MODEL_SIZE == 'LARGE':
+                import transformers
+                import torch
+                from art.estimators.hugging_face import HuggingFaceClassifier
 
-            import transformers
-            import torch
-            from art.estimators.hugging_face import HuggingFaceClassifier
+                model = transformers.AutoModelForImageClassification.from_pretrained('facebook/deit-tiny-patch16-224', # takes 3 min
+                                                                                     ignore_mismatched_sizes=True,
+                                                                                     num_labels=10)
 
-            model = transformers.AutoModelForImageClassification.from_pretrained('facebook/deit-tiny-patch16-224', # takes 3 min
-                                                                                 ignore_mismatched_sizes=True,
-                                                                                 num_labels=10)
+                print('num of parameters is ', sum(p.numel() for p in model.parameters() if p.requires_grad))
+                optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-            print('num of parameters is ', sum(p.numel() for p in model.parameters() if p.requires_grad))
-            optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-
-            classifier = HuggingFaceClassifier(model,
-                                               loss=torch.nn.CrossEntropyLoss(),
-                                               input_shape=(3, 224, 224),
-                                               nb_classes=10,
-                                               optimizer=optimizer,
-                                               processor=None)
-            '''
-            classifier = get_image_classifier_hf()
-            '''
+                classifier = HuggingFaceClassifier(model,
+                                                   loss=torch.nn.CrossEntropyLoss(),
+                                                   input_shape=(3, 224, 224),
+                                                   nb_classes=10,
+                                                   optimizer=optimizer,
+                                                   processor=None)
+            elif HF_MODEL_SIZE == 'SMALL':
+                classifier = get_image_classifier_hf(from_logits=True)
+            else:
+                raise ValueError('HF_MODEL_SIZE must be either SMALL or LARGE')
         else:
             classifier = None
 
@@ -108,18 +92,18 @@ class TestClass:
     pytest.classifier = None
     pytest.classifier_2 = None
 
-    @pytest.mark.only_with_platform("pytorch", "tensorflow2", "keras", "kerastf", "huggingface")
+    @pytest.mark.only_with_platform("pytorch", "tensorflow2", "huggingface")
     def test_get_classifiers(self, get_mnist_classifier):
         if pytest.classifier is None:
             pytest.classifier = get_mnist_classifier()
         if pytest.classifier_2 is None:
             pytest.classifier_2 = get_mnist_classifier()
 
-    @pytest.mark.only_with_platform("pytorch", "tensorflow2", "keras", "kerastf", "huggingface")
+    @pytest.mark.only_with_platform("pytorch", "tensorflow2", "huggingface")
     def test_fit_predict(self, art_warning, get_mnist_classifier, get_default_mnist_subset, framework):
 
         master_seed(seed=1234)
-        if framework == 'huggingface':
+        if framework == 'huggingface' and HF_MODEL_SIZE == 'LARGE':
             (x_train, y_train), (x_test, y_test), _, _ = load_cifar10()
             x_train, y_train, x_test, y_test = (
                 x_train[:NB_TRAIN],
@@ -139,7 +123,7 @@ class TestClass:
         if framework in ['pytorch', 'huggingface']:
             x_train = np.float32(np.moveaxis(x_train, -1, 1))
             x_test = np.float32(np.moveaxis(x_test, -1, 1))
-        if framework == 'huggingface':
+        if framework == 'huggingface' and HF_MODEL_SIZE == 'LARGE':
             upsampler = torch.nn.Upsample(scale_factor=7, mode='nearest')
             x_train = np.float32(upsampler(torch.from_numpy(x_train)).cpu().numpy())
             x_test = np.float32(upsampler(torch.from_numpy(x_test)).cpu().numpy())
@@ -151,10 +135,8 @@ class TestClass:
         predictions = np.argmax(pytest.classifier.predict(x_test_adv), axis=1)
         accuracy = np.sum(predictions == np.argmax(y_test, axis=1)) / NB_TEST
 
-        # TODO: pytorch was not checked in the original test. Why does it train differently to tf? Why does the tf
-        # TODO: accuracy decrease?
-        if framework in ['pytorch', 'tensorflow']:
-            assert accuracy == 0.13
+        # if framework in ['pytorch', 'tensorflow']:
+        assert accuracy == 0.13
 
         adv_trainer = AdversarialTrainer(pytest.classifier, attack)
         adv_trainer.fit(x_train, y_train, nb_epochs=5, batch_size=128)
@@ -163,11 +145,9 @@ class TestClass:
         predictions_new = np.argmax(adv_trainer.predict(x_test_adv), axis=1)
         accuracy_new = np.sum(predictions_new == np.argmax(y_test, axis=1)) / NB_TEST
 
-        if framework == 'pytorch':
-            # with open("classifier_test_fit_predict_post_training", "wb") as fp:  # Pickling
-            #    pickle.dump(pytest.classifier.model.state_dict(), fp)
-            assert accuracy_new == 0.20
-        if framework in ["tensorflow2", "keras", "kerastf"]:
+        if framework == ['pytorch', 'huggingface']:
+            assert accuracy_new == 0.13
+        if framework in ["tensorflow2"]:
             assert accuracy_new == 0.12
 
         # Recompute adversarial examples.
@@ -176,20 +156,19 @@ class TestClass:
         predictions_new = np.argmax(adv_trainer.predict(x_test_adv), axis=1)
         accuracy_new = np.sum(predictions_new == np.argmax(y_test, axis=1)) / NB_TEST
 
-        if framework == 'pytorch':
-            assert accuracy_new == 0.12
-        if framework in ["tensorflow2", "keras", "kerastf"]:
+        if framework in ["tensorflow2", 'pytorch', 'huggingface']:
             assert accuracy_new == 0.13
 
         pytest.final_acc_of_prior = accuracy_new
         # Check that x_test has not been modified by attack and classifier
         assert np.allclose(float(np.max(np.abs(x_test_original - x_test))), 0.0)
 
+    @pytest.mark.only_with_platform("pytorch", "tensorflow2", "huggingface")
     def test_fit_predict_different_classifiers(self, art_warning, get_mnist_classifier, get_default_mnist_subset, framework):
 
         master_seed(seed=1234)
 
-        if framework == 'huggingface':
+        if framework == 'huggingface' and HF_MODEL_SIZE == 'LARGE':
             (x_train, y_train), (x_test, y_test), _, _ = load_cifar10()
             x_train, y_train, x_test, y_test = (
                 x_train[:NB_TRAIN],
@@ -209,7 +188,7 @@ class TestClass:
         if framework in ['pytorch', 'huggingface']:
             x_train = np.float32(np.moveaxis(x_train, -1, 1))
             x_test = np.float32(np.moveaxis(x_test, -1, 1))
-        if framework == 'huggingface':
+        if framework == 'huggingface' and HF_MODEL_SIZE == 'LARGE':
             upsampler = torch.nn.Upsample(scale_factor=7, mode='nearest')
             x_train = np.float32(upsampler(torch.from_numpy(x_train)).cpu().numpy())
             x_test = np.float32(upsampler(torch.from_numpy(x_test)).cpu().numpy())
@@ -221,14 +200,8 @@ class TestClass:
         predictions = np.argmax(pytest.classifier.predict(x_test_adv), axis=1)
         accuracy = np.sum(predictions == np.argmax(y_test, axis=1)) / NB_TEST
 
-        # with open("classifier_test_fit_predict_post_training", "rb") as fp:  # Pickling
-        #    classifier_test_fit_predict_post_training = pickle.load(fp)
-        if framework == 'pytorch':
-            # for w1, w2 in zip(classifier_test_fit_predict_post_training.values(), pytest.classifier.model.state_dict().values()):
-            #    assert torch.allclose(w1, w2)
-            assert accuracy == 0.12
-        if framework in ["tensorflow2", "keras", "kerastf"]:
-            assert accuracy == 0.13  # TODO, Untrained acc? Why is this not 0.12?
+        if framework in ["tensorflow2", 'pytorch', 'huggingface']:
+            assert accuracy == 0.13
 
         assert accuracy == pytest.final_acc_of_prior
 
@@ -238,9 +211,7 @@ class TestClass:
         predictions_new = np.argmax(adv_trainer.predict(x_test_adv), axis=1)
         accuracy_new = np.sum(predictions_new == np.argmax(y_test, axis=1)) / NB_TEST
 
-        if framework == 'pytorch':
-            assert accuracy_new == 0.11
-        if framework in ["tensorflow2", "keras", "kerastf"]:
+        if framework in ["tensorflow2", 'pytorch', 'huggingface']:
             assert accuracy_new == 0.32
 
         # Check that x_test has not been modified by attack and classifier
@@ -264,11 +235,12 @@ class TestClass:
         adv_trainer_2 = AdversarialTrainer(pytest.classifier_2, attack, ratio=1.0)
         adv_trainer_2.fit_generator(generator, nb_epochs=5)
 
+    @pytest.mark.only_with_platform("pytorch", "tensorflow2", "huggingface")
     def test_two_attacks(self, art_warning, get_mnist_classifier, get_default_mnist_subset, framework):
 
         master_seed(seed=1234)
 
-        if framework == 'huggingface':
+        if framework == 'huggingface' and HF_MODEL_SIZE == 'LARGE':
             (x_train, y_train), (x_test, y_test), _, _ = load_cifar10()
             x_train, y_train, x_test, y_test = (
                 x_train[:NB_TRAIN],
@@ -288,7 +260,7 @@ class TestClass:
         if framework in ['pytorch', 'huggingface']:
             x_train = np.float32(np.moveaxis(x_train, -1, 1))
             x_test = np.float32(np.moveaxis(x_test, -1, 1))
-        if framework == 'huggingface':
+        if framework == 'huggingface' and HF_MODEL_SIZE == 'LARGE':
             upsampler = torch.nn.Upsample(scale_factor=7, mode='nearest')
             x_train = np.float32(upsampler(torch.from_numpy(x_train)).cpu().numpy())
             x_test = np.float32(upsampler(torch.from_numpy(x_test)).cpu().numpy())
@@ -307,18 +279,16 @@ class TestClass:
         predictions_new = np.argmax(adv_trainer.predict(x_test_adv), axis=1)
         accuracy_new = np.sum(predictions_new == np.argmax(y_test, axis=1)) / NB_TEST
 
-        if framework in ["tensorflow2", "keras", "kerastf"]:
+        if framework in ["tensorflow2", 'pytorch', 'huggingface']:
             assert accuracy_new == 0.14
             assert accuracy == 0.13  # Untrained acc?
-        if framework == 'pytorch':
-            assert accuracy_new == 0.18
-            assert accuracy == 0.12
 
         # Check that x_test has not been modified by attack and classifier
         assert np.allclose(float(np.max(np.abs(x_test_original - x_test))), 0.0)
 
+    @pytest.mark.only_with_platform("pytorch", "tensorflow2", "huggingface")
     def test_two_attacks_with_generator(self, art_warning, get_mnist_classifier, get_default_mnist_subset, framework):
-        if framework == 'huggingface':
+        if framework == 'huggingface' and HF_MODEL_SIZE == 'LARGE':
             (x_train, y_train), (x_test, y_test), _, _ = load_cifar10()
             x_train, y_train, x_test, y_test = (
                 x_train[:NB_TRAIN],
@@ -338,7 +308,7 @@ class TestClass:
         if framework in ['pytorch', 'huggingface']:
             x_train = np.float32(np.moveaxis(x_train, -1, 1))
             x_test = np.float32(np.moveaxis(x_test, -1, 1))
-        if framework == 'huggingface':
+        if framework == 'huggingface' and HF_MODEL_SIZE == 'LARGE':
             upsampler = torch.nn.Upsample(scale_factor=7, mode='nearest')
             x_train = np.float32(upsampler(torch.from_numpy(x_train)).cpu().numpy())
             x_test = np.float32(upsampler(torch.from_numpy(x_test)).cpu().numpy())
@@ -372,26 +342,23 @@ class TestClass:
         predictions_new = np.argmax(adv_trainer.predict(x_test_adv), axis=1)
         accuracy_new = np.sum(predictions_new == np.argmax(y_test, axis=1)) / NB_TEST
 
-        if framework in ["tensorflow2", "keras", "kerastf"]:
+        if framework in ["tensorflow2", "pytorch", 'huggingface']:
             # self.assertAlmostEqual(accuracy_new, 0.38, delta=0.02)
             assert 0.36 <= accuracy_new <= 0.40
             assert accuracy == 0.1
-        if framework == 'pytorch':
-            # self.assertAlmostEqual(accuracy_new, 0.38, delta=0.02)
-            assert 0.21 <= accuracy_new <= 0.25
-            assert accuracy == 0.17
 
         # Check that x_train and x_test has not been modified by attack and classifier
         assert np.allclose(float(np.max(np.abs(x_train_original - x_train))), 0.0)
         assert np.allclose(float(np.max(np.abs(x_test_original - x_test))), 0.0)
 
+    @pytest.mark.only_with_platform("pytorch", "tensorflow2", "huggingface")
     def test_targeted_attack_error(self, art_warning, get_mnist_classifier, get_default_mnist_subset, framework):
         """
         Test the adversarial trainer using a targeted attack, which will currently result in a NotImplementError.
 
         :return: None
         """
-        if framework == 'huggingface':
+        if framework == 'huggingface' and HF_MODEL_SIZE == 'LARGE':
             (x_train, y_train), (x_test, y_test), _, _ = load_cifar10()
             x_train, y_train, x_test, y_test = (
                 x_train[:NB_TRAIN],
@@ -411,7 +378,7 @@ class TestClass:
         if framework in ['pytorch', 'huggingface']:
             x_train = np.float32(np.moveaxis(x_train, -1, 1))
             x_test = np.float32(np.moveaxis(x_test, -1, 1))
-        if framework == 'huggingface':
+        if framework == 'huggingface' and HF_MODEL_SIZE == 'LARGE':
             upsampler = torch.nn.Upsample(scale_factor=7, mode='nearest')
             x_train = np.float32(upsampler(torch.from_numpy(x_train)).cpu().numpy())
             x_test = np.float32(upsampler(torch.from_numpy(x_test)).cpu().numpy())
@@ -420,46 +387,20 @@ class TestClass:
 
         adv = FastGradientMethod(pytest.classifier, targeted=True)
         adv_trainer = AdversarialTrainer(pytest.classifier, attacks=adv)
-        # self.assertRaises(NotImplementedError, adv_trainer.fit, x_train, y_train, **params)
-
-'''
-class TestAdversarialTrainer(unittest.TestCase):
-    """
-    Test cases for the AdversarialTrainer class.
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        # MNIST
-        (x_train, y_train), (x_test, y_test), _, _ = load_mnist()
-        x_train, y_train, x_test, y_test = (
-            x_train[:NB_TRAIN],
-            y_train[:NB_TRAIN],
-            x_test[:NB_TEST],
-            y_test[:NB_TEST],
-        )
-        cls.mnist = ((x_train, y_train), (x_test, y_test))
-
-        cls.classifier, _ = get_image_classifier_tf()
-        cls.classifier_2, _ = get_image_classifier_tf()
-
-
-    def test_classifier_match(self):
-        attack = FastGradientMethod(self.classifier)
-        adv_trainer = AdversarialTrainer(self.classifier, attack)
-
-        self.assertEqual(len(adv_trainer.attacks), 1)
-        self.assertEqual(adv_trainer.attacks[0].estimator, adv_trainer.get_classifier())
+        with pytest.raises(NotImplementedError):
+            adv_trainer.fit(x_train, y_train, **params)
 
     def test_excpetions(self):
-        with self.assertRaises(ValueError):
-            _ = AdversarialTrainer(self.classifier, "attack")
+        with pytest.raises(ValueError):
+            _ = AdversarialTrainer(pytest.classifier, "attack")
 
-        with self.assertRaises(ValueError):
-            attack = FastGradientMethod(self.classifier)
-            _ = AdversarialTrainer(self.classifier, attack, ratio=1.5)
+        with pytest.raises(ValueError):
+            attack = FastGradientMethod(pytest.classifier)
+            _ = AdversarialTrainer(pytest.classifier, attack, ratio=1.5)
 
+    def test_classifier_match(self):
+        attack = FastGradientMethod(pytest.classifier)
+        adv_trainer = AdversarialTrainer(pytest.classifier, attack)
 
-if __name__ == "__main__":
-    unittest.main()
-'''
+        assert len(adv_trainer.attacks) == 1
+        assert adv_trainer.attacks[0].estimator == adv_trainer.get_classifier()
