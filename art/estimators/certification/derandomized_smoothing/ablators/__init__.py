8349f96a19babd4228d2f8ad816932006b439351
GiulioZizzo <giulio.zizzo@yahoo.co.uk>
2023-08-30 15:11:03 +0000

    fixing lingvo workflow. Adding pytorch check prior to import
    Signed-off-by: GiulioZizzo <giulio.zizzo@yahoo.co.uk>

--- art/estimators/certification/derandomized_smoothing/ablators/__init__.py
+++ art/estimators/certification/derandomized_smoothing/ablators/__init__.py
@@ -1,8 +1,13 @@
 """
 This module contains the ablators for the certified smoothing approaches.
 """
-from art.estimators.certification.derandomized_smoothing.ablators.pytorch import (
-    ColumnAblatorPyTorch,
-    BlockAblatorPyTorch,
-)
+import importlib
+
 from art.estimators.certification.derandomized_smoothing.ablators.tensorflow import ColumnAblator, BlockAblator
+
+if importlib.util.find_spec("torch") is not None:
+    from art.estimators.certification.derandomized_smoothing.ablators.pytorch import (
+        ColumnAblatorPyTorch,
+        BlockAblatorPyTorch,
+    )
+
