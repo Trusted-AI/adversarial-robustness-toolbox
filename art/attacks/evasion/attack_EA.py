c656a5ebd7519d841cb1362a14cb7e6454e67fd5
aliotopal <32355311+aliotopal@users.noreply.github.com>
2023-09-07 10:54:23 +0200

    Update attack_EA.py
    aliotopal@gmail.com
    The style check is done. Redundant parts are removed.

--- art/attacks/evasion/attack_EA.py
+++ art/attacks/evasion/attack_EA.py
@@ -1,7 +1,12 @@
-import math
+from __future__ import absolute_import, division, print_function, unicode_literals
+import logging
+from typing import Optional, TYPE_CHECKING
+from art.attacks.attack import EvasionAttack
+from art.estimators.estimator import BaseEstimator, NeuralNetworkMixin
+from art.estimators.classification.classifier import ClassifierMixin
+import random
 import numpy as np
 from sys import stdout
-import random
 random.seed(0)
 
 """
@@ -9,15 +14,7 @@ This module implements the Evolutionary Algorithm based Adversarial Attack (atta
 
 | Paper link: https://www.sciencedirect.com/science/article/pii/S1568494623004155
 """
-from __future__ import absolute_import, division, print_function, unicode_literals
-import logging
-from typing import Optional, TYPE_CHECKING
 
-import numpy as np
-
-from art.attacks.attack import EvasionAttack
-from art.estimators.estimator import BaseEstimator, NeuralNetworkMixin
-from art.estimators.classification.classifier import ClassifierMixin
 
 if TYPE_CHECKING:
     from art.utils import CLASSIFIER_TYPE
@@ -59,15 +56,18 @@ class EA(EvasionAttack):
         self.targeted = targeted
         self.pop_size = 40
         self.numberOfElites = 10
-
-    def _get_class_prob(self, preds: np.ndarray, class_no: np.array) -> np.ndarray:
+    
+    @staticmethod
+    def _get_class_prob(preds: np.ndarray, class_no: np.array) -> np.ndarray:
         '''
         :param preds: an array of predictions of individuals for all the categories: (40, 1000) shaped array
-        :param class_no: for the targeted attack target category index number; for the untargeted attack ancestor category index number
+        :param class_no: for the targeted attack target category index number; for the untargeted attack ancestor 
+        category index number
         :return: an array of the prediction of individuals only for the target/ancestor category: (40,) shaped  array
         '''
         return preds[:, class_no]
-
+    
+    @staticmethod
     def _get_fitness(self, probs: np.ndarray) -> np.ndarray:
         '''
          It simply returns the CNN's probability for the images but different objective functions can be used here.
@@ -80,14 +80,14 @@ class EA(EvasionAttack):
     def _selection_untargeted(self, images: np.ndarray, fitness: np.ndarray):
         '''
         Population will be divided into elite, middle_class, and didn't make it based on
-        images (individuals) fitness values. The images furthest from the ancestor category will be closer to be in the elite.
+        images (individuals) fitness values. The images furthest from the ancestor category will be 
+        closer to be in the elite.
         :param images: the population of images in an array: size (pop_size, 224, 224, 3)
         :param fitness: an array of images' propabilities of selected CNN
         :return: returns a tuple of elite, middle_class images, fitness values of elites, index number of elites
                 in the population array, and random_keep images as numpy arrays.
         '''
         idx_elite = fitness.argsort()[:self.numberOfElites]
-        elite_fitness = fitness[idx_elite]
         half_pop_size = images.shape[0] / 2
         idx_middle_class = fitness.argsort()[self.numberOfElites: int(half_pop_size)]
         elite = images[idx_elite, :]
@@ -96,19 +96,19 @@ class EA(EvasionAttack):
         possible_idx = set(range(0, images.shape[0])) - set(idx_elite)
         idx_keep = random.sample(possible_idx, int(images.shape[0] / 2 - self.numberOfElites))
         random_keep = images[idx_keep]
-        return elite, middle_class, elite_fitness, idx_elite, random_keep
+        return elite, middle_class, random_keep
 
     def _selection_targeted(self, images: np.ndarray, fitness: np.ndarray):
         '''
         Population will be divided into elite, middle_class, and didn't make it based on
-        images (individuals) fitness values. The images closest to the target category will be closer to be in the elite.
+        images (individuals) fitness values. The images closest to the target category will be 
+        closer to be in the elite.
         :param images: the population of images in an array: size (pop_size, 224, 224, 3)
         :param fitness: an array of images' probabilities of selected CNN
         :return: returns elite, middle_class images, fitness values of elites, index number of elites
                 in the population array, and random_keep images as numpy arrays.
         '''
         idx_elite = fitness.argsort()[-self.numberOfElites:]
-        elite_fitness = fitness[idx_elite]
         half_pop_size = images.shape[0] / 2
         idx_middle_class = fitness.argsort()[int(half_pop_size):-self.numberOfElites]
         elite = images[idx_elite, :][::-1]
@@ -117,23 +117,24 @@ class EA(EvasionAttack):
         possible_idx = set(range(0, images.shape[0])) - set(idx_elite)
         idx_keep = random.sample(possible_idx, int(images.shape[0] / 2 - self.numberOfElites))
         random_keep = images[idx_keep]
-        return elite, middle_class, elite_fitness, idx_elite, random_keep
-
-    def _get_no_of_pixels(self, im_size: int) -> int:
+        return elite, middle_class, random_keep
+    @staticmethod
+    def _get_no_of_pixels(im_size: int) -> int:
         '''
         :param im_size: Original inputs' size, represented by an integer value.
-        :return: returns an integer that will be used to decide how many pixels will be mutated in the image during the current generation.
+        :return: returns an integer that will be used to decide how many pixels will be mutated 
+        in the image during the current generation.
         '''
         u_factor = np.random.uniform(0.0, 1.0)
         n = 60  # normally 60, the smaller n -> more pixels to mutate
         res = (u_factor ** (1.0 / (n + 1))) * im_size
         no_of_pixels = im_size - res
         return no_of_pixels
-
-    def _mutation(self, x: np.ndarray, no_of_pixels: int, mutation_group: np.ndarray, percentage: float,
+    @staticmethod
+    def _mutation(_x: np.ndarray, no_of_pixels: int, mutation_group: np.ndarray, percentage: float,
                   boundary_min: int, boundary_max: int) -> np.ndarray:
         '''
-        :param x: An array with the original input to be attacked.
+        :param _x: An array with the original input to be attacked.
         :param no_of_pixels: An integer determines the number of pixels to mutate in the original input for the current
             generation.
         :param mutation_group: An array with the individuals which will be mutated
@@ -155,35 +156,37 @@ class EA(EvasionAttack):
                                                                                    individual, locations_x, locations_y, locations_z] - new_values
         mutated_group = np.clip(mutated_group, boundary_min, boundary_max)
         return mutated_group
-
-    def _get_crossover_parents(self, crossover_group: np.ndarray) -> list:
+                      
+    @staticmethod
+    def _get_crossover_parents(crossover_group: np.ndarray) -> list:
         size = crossover_group.shape[0]
         no_of_parents = random.randrange(0, size, 2)
         parents_idx = random.sample(range(0, size), no_of_parents)
         return parents_idx
-
-    def _crossover(self, x: np.ndarray, crossover_group: np.ndarray, parents_idx: list) -> np.ndarray:
+        
+    @staticmethod
+    def _crossover(_x: np.ndarray, crossover_group: np.ndarray, parents_idx: list) -> np.ndarray:
         crossedover_group = crossover_group.copy()
         for i in range(0, len(parents_idx), 2):
             parent_index_1 = parents_idx[i]
             parent_index_2 = parents_idx[i + 1]
             crossover_range = int(x.shape[0] * 0.15)  # 15% of the image will be crossovered.
             size_x = np.random.randint(0, crossover_range)
-            start_x = np.random.randint(0, x.shape[0] - size_x)
+            start_x = np.random.randint(0, _x.shape[0] - size_x)
             size_y = np.random.randint(0, crossover_range)
-            start_y = np.random.randint(0, x.shape[1] - size_y)
-            z = np.random.randint(x.shape[2])
-            temp = crossedover_group[parent_index_1, start_x: start_x + size_x, start_y: start_y + size_y, z]
+            start_y = np.random.randint(0, _x.shape[1] - size_y)
+            _z = np.random.randint(_x.shape[2])
+            temp = crossedover_group[parent_index_1, start_x: start_x + size_x, start_y: start_y + size_y, _z]
             crossedover_group[parent_index_1, start_x: start_x + size_x, start_y: start_y + size_y,
-            z] = crossedover_group[
+            _z] = crossedover_group[
                  parent_index_2,
                  start_x: start_x + size_x,
                  start_y: start_y + size_y,
-                 z]
+                 _z]
             crossedover_group[parent_index_2, start_x: start_x + size_x, start_y: start_y + size_y, z] = temp
         return crossedover_group
 
-    def _generate(self, x: np.ndarray, y: Optional[np.ndarray] = None, **kwargs) -> np.ndarray:
+    def _generate(self, x: np.ndarray, y: Optional[np.ndarray] = None) -> np.ndarray:
         '''
         :param x: An array with the original inputs to be attacked.
         :param y: An integer with the true or target labels.
@@ -198,21 +201,21 @@ class EA(EvasionAttack):
         label0 = self.estimator.decode_predictions(preds)    # ??????????????????
         label1 = label0[0][0]  # Gets the Top1 label and values for reporting.
         ancestor = label1[1]  # label
-        ancIndx = np.argmax(preds)
-        print(f'Before the image is:  {ancestor} --> {label1[2]} ____ index: {ancIndx}')
+        anc_indx = np.argmax(preds)
+        print("Before the image is:  " + ancestor + " --> " + str(label1[2]) + " ____ index: " + str(anc_indx))
         if self.targeted:
             print('Target class index number is: ', y)
         images = np.array([x] * self.pop_size).astype(int)  # pop_size * ancestor images are created
         count = 0
         while True:
-            img = self.estimator.preprocess_input(images)    # ??????????????????
+            img = self.estimator.preprocess_input(images)   
             preds = self.estimator.predict(img)  # predictions of 40 images
-            domIndx = np.argmax(preds[int(np.argmax(preds) / 1000)])
+            dom_indx = np.argmax(preds[int(np.argmax(preds) / 1000)])
             # Dominant category report ##################
-            label0 = self.estimator.decode_predictions(preds)  # Reports predictions with label and label values   # ??????????????
+            label0 = self.estimator.decode_predictions(preds)  # Reports predictions with label and label values
             label1 = label0[0][0]  # Gets the Top1 label and values for reporting.
-            domCat = label1[1]   # label
-            domCat_prop = label1[2]  # label probability
+            dom_cat = label1[1]   # label
+            dom_cat_prop = label1[2]  # label probability
             ###########################################
 
             percentage_middle_class = 1
@@ -223,11 +226,11 @@ class EA(EvasionAttack):
             if self.targeted:
                 probs = self._get_class_prob(preds, y)
                 fitness = self._get_fitness(probs)
-                elite, middle_class, elite_fitness, idx_elite, random_keep = self._selection_targeted(images, fitness)
+                elite, middle_class, random_keep = self._selection_targeted(images, fitness)
             else:
-                probs = self._get_class_prob(preds, ancIndx)
+                probs = self._get_class_prob(preds, anc_indx)
                 fitness = self._get_fitness(probs)
-                elite, middle_class, elite_fitness, idx_elite, random_keep = self._selection_untargeted(images, fitness)
+                elite, middle_class, random_keep = self._selection_untargeted(images, fitness)
             elite2 = elite.copy()
             keep = np.concatenate((elite2, random_keep))
 
@@ -248,28 +251,18 @@ class EA(EvasionAttack):
         # Create new population
             images = np.concatenate((elite, crossover_group))
         # Report the progress on the screen
-            stdout.write(f'\rgeneration: {count}/{self.max_iter} ______ {domCat}: {domCat_prop} ____ index: {domIndx}')
+            stdout.write(f'\rgeneration: {count}/{self.max_iter} ______ {dom_cat}: {dom_cat_prop} ____ index: {dom_indx}')
             count += 1
 
         # Terminate the algorithm if:
             if count == self.max_iter:
                 # if algorithm can not create the adversarial image within "max_iter" stop the algorithm.
-                print(f"Failed to generate adversarial image within {self.max_iter} generations")
+                print("Failed to generate adversarial image within " + self.max_iter +" generations")
                 break
-            if not self.targeted and domIndx != ancIndx:
+            if not self.targeted and dom_indx != anc_indx:
                 # if the attack is not targeted, the algorithm stops as soon as the image is classified in a category other than its true category.
                 break
-            if self.targeted and domIndx == y and domCat_prop > self.confidence:
+            if self.targeted and dom_indx == y and dom_cat_prop > self.confidence:
                # if the attack is targeted, the algorithm stops when the image is classified in the target category y.
                 break
         return images[0]
-
-
-
-
-
-
-
-
-
-
