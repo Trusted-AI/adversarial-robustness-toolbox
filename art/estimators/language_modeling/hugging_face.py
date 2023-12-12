8b2c0d4642a6e6a272d260295b390d6b12574093
Farhan Ahmed <Farhan.Ahmed@ibm.com>
2023-12-11 17:43:41 -0800

    added basic language model functionality
    Signed-off-by: Farhan Ahmed <Farhan.Ahmed@ibm.com>

--- art/estimators/language_modeling/hugging_face.py
+++ art/estimators/language_modeling/hugging_face.py
@@ -21,27 +21,185 @@ to interface with ART.
 """
 import logging
 
-from typing import List, Optional, Tuple, Union, Dict, Callable, Any, TYPE_CHECKING
+from typing import List, Optional, Union, Dict, Any, TYPE_CHECKING
 
 import numpy as np
-import six
 
 from art.estimators.language_modeling.language_model import LanguageModel
 
 if TYPE_CHECKING:
     import torch
     import transformers
-    from art.utils import CLIP_VALUES_TYPE, PREPROCESSING_TYPE
-    from art.defences.preprocessor import Preprocessor
-    from art.defences.postprocessor import Postprocessor
-    from transformers.modeling_outputs import ModelOutput
 
 logger = logging.getLogger(__name__)
 
 
-class HuggingFaceLM(LanguageModel):
+class HuggingFaceLanguageModel(LanguageModel):
     """
     This class implements a classifier with the HuggingFace framework.
     """
     
-    pass
+    def __init__(
+        self,
+        model: "transformers.PreTrainedModel",
+        tokenizer: Union["transformers.PreTrainedTokenizerFast", "transformers.PreTrainedTokenizer"],
+        loss: Optional["torch.nn.modules.loss._Loss"] = None,
+        optimizer: Optional["torch.optim.Optimizer"] = None,
+        device_type: str = "gpu",
+    ):
+        """
+        Initialization of HuggingFaceLanguageModel specifically for the PyTorch-based backend.
+
+        :param model: Hugging Face model which returns outputs of type ModelOutput from the transformers library.
+        :param tokenizer: Hugging Face tokenizer which converts str | list[str] into the model input.
+        :param loss: The loss function for which to compute gradients for training. The target label must be raw
+                     categorical, i.e. not converted to one-hot encoding.
+        :param optimizer: The optimizer used to train the classifier.
+        :param device_type: Type of device on which the classifier is run, either `gpu` or `cpu`.
+        """
+        import torch
+
+        super().__init__(
+            model=model,
+            tokenizer=tokenizer,
+            loss=loss,
+            optimizer=optimizer,
+            device_type=device_type,
+        )
+
+        self._model = model
+        self._tokenizer = tokenizer
+        self._loss = loss
+        self._optimizer = optimizer
+
+        # Move model to GPU
+        self._model.to(self._device)
+
+    @property
+    def device(self) -> "torch.device":
+        """
+        Get current used device.
+
+        :return: Current used device.
+        """
+        return self._device
+
+    @property
+    def model(self) -> "transformers.PreTrainedModel":
+        return self._model
+
+    @property
+    def tokenizer(self) -> Union["transformers.PreTrainedTokenizerFast", "transformers.PreTrainedTokenizer"]:
+        return self._tokenizer
+
+    @property
+    def loss(self) -> "torch.nn.modules.loss._Loss":
+        """
+        Return the loss function.
+
+        :return: The loss function.
+        """
+        return self._loss  # type: ignore
+
+    @property
+    def optimizer(self) -> "torch.optim.Optimizer":
+        """
+        Return the optimizer.
+
+        :return: The optimizer.
+        """
+        return self._optimizer  # type: ignore
+    
+    def tokenize(self, x: Union[str, List[str]], **kwargs) -> Dict[str, Any]:
+        tokenized_output = self._tokenizer(x, **kwargs)
+        tokenized_dict = tokenized_output.data
+        return tokenized_dict
+    
+    def encode(self, x: Union[str, List[str]], **kwargs) -> np.ndarray:
+        encoded_output = self._tokenizer.encode(x, **kwargs)
+        return encoded_output
+    
+    def decode(
+        self,
+        token_ids: np.ndarray,
+        *,
+        skip_special_tokens: Optional[bool] = None,
+        clean_up_tokenization_spaces: Optional[bool] = None,
+        **kwargs
+    ) -> str:
+        """
+        Decode token ids to a string using the tokenizer.
+
+        :param token_ids: The tokenized input ids of a string.
+        :param skip_special_tokens: Whether or not to remove special tokens in the decoding.
+        :param clean_up_tokenization_spaces: Whether or not to clean up the tokenization spaces.
+        """
+        # Create input parameters
+        inputs = {}
+        inputs['token_ids'] = token_ids
+        if skip_special_tokens:
+            inputs['skip_special_tokens'] = skip_special_tokens
+        if clean_up_tokenization_spaces:
+            inputs['skip_special_tokens'] = clean_up_tokenization_spaces
+
+        decoded_output = self._tokenizer.decode(**inputs, **kwargs)
+        return decoded_output
+    
+    def predict(self, x: Optional[Union[str, List[str]]] = None, **kwargs) -> Dict[str, Any]:
+        import torch
+
+        self._model.eval()
+        
+        inputs = {}
+
+        # Tokenize text input
+        if x is not None:
+            tokenized = self._tokenizer(x, padding=True, truncation=True, return_tensors="pt")
+            for k, v in tokenized.items():
+                inputs[k] = v.to(self._device)
+
+        # Convert inputs to tensors and move to GPU
+        for k, v in kwargs.items():
+            if isinstance(v, torch.Tensor):
+                inputs[k] = v.to(self._device)
+            elif isinstance(v, np.ndarray):
+                inputs[k] = torch.from_numpy(v).to(self._device)
+            elif isinstance(v, list):
+                if isinstance(v[0], torch.Tensor):
+                    inputs[k] = [v_i.to(self._device) for v_i in v]
+                elif isinstance(v[0], np.ndarray):
+                    inputs[k] = [torch.from_numpy(v_i).to(self._device) for v_i in v]
+                elif isinstance(v[0], (float, int)):
+                    inputs[k] = torch.tensor(v).to(self._device)
+                else:
+                    inputs[k] = v
+            else:
+                inputs[k] = v
+
+        # Run prediction
+        with torch.no_grad():
+            out = self._model(**inputs)
+        
+        # Extract outputs and convert to numpy arrays
+        outputs = {}
+        for k, v in out.items():
+            if isinstance(v, torch.Tensor):
+                outputs[k] = v.detach().cpu().numpy()
+            elif isinstance(v, (tuple, list)):
+                if isinstance(v[0], torch.Tensor):
+                    outputs[k] = [v_i.detach().cpu().numpy() for v_i in v]
+                elif isinstance(v[0], (tuple, list)):
+                    outputs[k] = [[v_ij.detach().cpu().numpy() for v_ij in v_i] for v_i in v]
+                else:
+                    outputs[k] = v
+            else:
+                outputs[k] = v
+
+        return outputs
+    
+    def generate(self, x: Any, **kwargs) -> Any:
+        raise NotImplementedError
+
+    def fit(self, x: Any, **kwargs):
+        raise NotImplementedError
+
--- art/estimators/language_modeling/language_model.py
+++ art/estimators/language_modeling/language_model.py
@@ -39,7 +39,7 @@ class LanguageModelMixin(abc.ABC):
     pass
 
 
-class LanguageModel(LanguageModelMixin, BaseEstimator, abc.ABC):
+class LanguageModel(LanguageModelMixin, abc.ABC):
     """
     Typing variable definition.
     """
@@ -56,20 +56,12 @@ class LanguageModel(LanguageModelMixin, BaseEstimator, abc.ABC):
         Estimator class for PyTorch models.
 
         :param model: The model.
-        :param clip_values: Tuple of the form `(min, max)` of floats or `np.ndarray` representing the minimum and
-               maximum values allowed for features. If floats are provided, these will be used as the range of all
-               features. If arrays are provided, each value will be considered the bound for a feature, thus
-               the shape of clip values needs to match the total number of features.
-        :param preprocessing_defences: Preprocessing defence(s) to be applied by the classifier.
-        :param postprocessing_defences: Postprocessing defence(s) to be applied by the classifier.
-        :param preprocessing: Tuple of the form `(subtrahend, divisor)` of floats or `np.ndarray` of values to be
-               used for data preprocessing. The first value will be subtracted from the input. The input will then
-               be divided by the second one.
+        :param tokenizer: The tokenizer.
         :param device_type: Type of device on which the classifier is run, either `gpu` or `cpu`.
         """
         import torch
 
-        super().__init__(**kwargs)
+        super().__init__()
 
         self._device_type = device_type
 
@@ -79,8 +71,6 @@ class LanguageModel(LanguageModelMixin, BaseEstimator, abc.ABC):
         else:  # pragma: no cover
             cuda_idx = torch.cuda.current_device()
             self._device = torch.device(f"cuda:{cuda_idx}")
-
-        self._check_params()
 
     @property
     def device_type(self) -> str:
@@ -92,48 +82,28 @@ class LanguageModel(LanguageModelMixin, BaseEstimator, abc.ABC):
         return self._device_type  # type: ignore
 
     @abc.abstractmethod
-    def predict(self, x: np.ndarray, batch_size: int = 128, **kwargs):
-        """
-        Perform prediction of the neural network for samples `x`.
-
-        :param x: Samples of shape (nb_samples, nb_features) or (nb_samples, nb_pixels_1, nb_pixels_2,
-                  nb_channels) or (nb_samples, nb_channels, nb_pixels_1, nb_pixels_2).
-        :param batch_size: Batch size.
-        :return: Predictions.
-        :rtype: Format as expected by the `model`
-        """
+    def tokenize(self, x: Any, **kwargs) -> Any:
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    def encode(self, x: Any, **kwargs) -> Any:
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    def decode(self, x: Any, **kwargs) -> Any:
         raise NotImplementedError
 
     @abc.abstractmethod
-    def fit(self, x: np.ndarray, y, batch_size: int = 128, nb_epochs: int = 20, **kwargs) -> None:
-        """
-        Fit the model of the estimator on the training data `x` and `y`.
-
-        :param x: Samples of shape (nb_samples, nb_features) or (nb_samples, nb_pixels_1, nb_pixels_2,
-                  nb_channels) or (nb_samples, nb_channels, nb_pixels_1, nb_pixels_2).
-        :param y: Target values.
-        :type y: Format as expected by the `model`
-        :param batch_size: Batch size.
-        :param nb_epochs: Number of training epochs.
-        """
+    def predict(self, x: Any, **kwargs) -> Any:
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    def generate(self, x: Any, **kwargs) -> Any:
         raise NotImplementedError
 
-    def set_params(self, **kwargs) -> None:
-        """
-        Take a dictionary of parameters and apply checks before setting them as attributes.
-
-        :param kwargs: A dictionary of attributes.
-        """
-        super().set_params(**kwargs)
-        self._check_params()
-
-    def _check_params(self) -> None:
-        from art.defences.preprocessor.preprocessor import PreprocessorPyTorch
-
-        super()._check_params()
-        self.all_framework_preprocessing = all(
-            (isinstance(p, PreprocessorPyTorch) for p in self.preprocessing_operations)
-        )
+    @abc.abstractmethod
+    def fit(self, x: Any, **kwargs) -> None:
+        raise NotImplementedError
 
     def _set_layer(self, train: bool, layerinfo: List["torch.nn.modules.Module"]) -> None:
         """
