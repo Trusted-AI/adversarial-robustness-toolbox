41a0143fc0fcf52be9487a933fe5cb93eccb27d7
Farhan Ahmed <Farhan.Ahmed@ibm.com>
2024-01-26 15:31:34 -0800

    create new inputs for pytorch object detector test cases
    Signed-off-by: Farhan Ahmed <Farhan.Ahmed@ibm.com>

--- tests/estimators/object_detection/conftest.py
+++ tests/estimators/object_detection/conftest.py
@@ -26,7 +26,26 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.fixture()
-def get_pytorch_object_detector(get_default_mnist_subset):
+def get_pytorch_object_detector_data(get_default_cifar10_subset):
+    import cv2
+
+    (_, _), (x_test_cifar10, _) = get_default_cifar10_subset
+    x_test = cv2.resize(np.reshape(x_test_cifar10[0], (32, 32, 3)), dsize=(416, 416), interpolation=cv2.INTER_CUBIC)
+    x_test = np.transpose(x_test, (2, 0, 1))
+    x_test = np.expand_dims(x_test, axis=0)
+    y_test = [
+        {
+            "boxes": np.asarray([[6, 22, 413, 346], [0, 24, 406, 342]], dtype=np.float32),
+            "labels": np.asarray([21, 18]),
+            "masks": np.ones((1, 416, 416)) / 2,
+        }
+    ]
+
+    yield x_test, y_test
+
+
+@pytest.fixture()
+def get_pytorch_object_detector(get_pytorch_object_detector_data):
     """
     This class tests the PyTorchObjectDetector object detector.
     """
@@ -44,39 +63,20 @@ def get_pytorch_object_detector(get_default_mnist_subset):
 
     object_detector = PyTorchObjectDetector(
         model=model,
-        input_shape=(32, 32, 3),
+        input_shape=(3, 416, 416),
         optimizer=optimizer,
         clip_values=(0, 1),
-        channels_first=False,
+        channels_first=True,
         attack_losses=["loss_classifier", "loss_box_reg", "loss_objectness", "loss_rpn_box_reg"],
     )
 
-    (_, _), (x_test_mnist, _) = get_default_mnist_subset
-
-    x_test = np.transpose(x_test_mnist[:2], (0, 2, 3, 1))
-    x_test = np.repeat(x_test.astype(np.float32), repeats=3, axis=3)
-
-    # Create labels
-    result = object_detector.predict(x=x_test)
-
-    y_test = [
-        {
-            "boxes": result[0]["boxes"],
-            "labels": result[0]["labels"],
-            "scores": np.ones_like(result[0]["labels"]),
-        },
-        {
-            "boxes": result[1]["boxes"],
-            "labels": result[1]["labels"],
-            "scores": np.ones_like(result[1]["labels"]),
-        },
-    ]
+    x_test, y_test = get_pytorch_object_detector_data
 
     yield object_detector, x_test, y_test
 
 
 @pytest.fixture()
-def get_pytorch_object_detector_mask(get_default_mnist_subset):
+def get_pytorch_object_detector_mask(get_pytorch_object_detector_data):
     """
     This class tests the PyTorchObjectDetector object detector.
     """
@@ -94,41 +94,20 @@ def get_pytorch_object_detector_mask(get_default_mnist_subset):
 
     object_detector = PyTorchObjectDetector(
         model=model,
-        input_shape=(32, 32, 3),
+        input_shape=(3, 416, 416),
         optimizer=optimizer,
         clip_values=(0, 1),
-        channels_first=False,
+        channels_first=True,
         attack_losses=["loss_classifier", "loss_box_reg", "loss_objectness", "loss_rpn_box_reg"],
     )
 
-    (_, _), (x_test_mnist, _) = get_default_mnist_subset
-
-    x_test = np.transpose(x_test_mnist[:2], (0, 2, 3, 1))
-    x_test = np.repeat(x_test.astype(np.float32), repeats=3, axis=3)
-
-    # Create labels
-    result = object_detector.predict(x=x_test)
-
-    y_test = [
-        {
-            "boxes": result[0]["boxes"],
-            "labels": result[0]["labels"],
-            "scores": np.ones_like(result[0]["labels"]),
-            "masks": result[0]["masks"],
-        },
-        {
-            "boxes": result[1]["boxes"],
-            "labels": result[1]["labels"],
-            "scores": np.ones_like(result[1]["labels"]),
-            "masks": result[0]["masks"],
-        },
-    ]
+    x_test, y_test = get_pytorch_object_detector_data
 
     yield object_detector, x_test, y_test
 
 
 @pytest.fixture()
-def get_pytorch_faster_rcnn(get_default_mnist_subset):
+def get_pytorch_faster_rcnn(get_pytorch_object_detector_data):
     """
     This class tests the PyTorchFasterRCNN object detector.
     """
@@ -138,9 +117,9 @@ def get_pytorch_faster_rcnn(get_default_mnist_subset):
 
     # Define object detector
     object_detector = PyTorchFasterRCNN(
-        input_shape=(32, 32, 3),
+        input_shape=(3, 416, 416),
         clip_values=(0, 1),
-        channels_first=False,
+        channels_first=True,
         attack_losses=["loss_classifier", "loss_box_reg", "loss_objectness", "loss_rpn_box_reg"],
     )
     params = [p for p in object_detector.model.parameters() if p.requires_grad]
@@ -148,36 +127,16 @@ def get_pytorch_faster_rcnn(get_default_mnist_subset):
 
     object_detector.set_params(optimizer=optimizer)
 
-    (_, _), (x_test_mnist, _) = get_default_mnist_subset
-
-    x_test = np.transpose(x_test_mnist[:2], (0, 2, 3, 1))
-    x_test = np.repeat(x_test.astype(np.float32), repeats=3, axis=3)
-
-    # Create labels
-    result = object_detector.predict(x=x_test)
-
-    y_test = [
-        {
-            "boxes": result[0]["boxes"],
-            "labels": result[0]["labels"],
-            "scores": np.ones_like(result[0]["labels"]),
-        },
-        {
-            "boxes": result[1]["boxes"],
-            "labels": result[1]["labels"],
-            "scores": np.ones_like(result[1]["labels"]),
-        },
-    ]
+    x_test, y_test = get_pytorch_object_detector_data
 
     yield object_detector, x_test, y_test
 
 
 @pytest.fixture()
-def get_pytorch_yolo(get_default_cifar10_subset):
+def get_pytorch_yolo(get_pytorch_object_detector_data):
     """
     This class tests the PyTorchYolo object detector.
     """
-    import cv2
     import torch
 
     from pytorchyolo import models
@@ -220,77 +179,34 @@ def get_pytorch_yolo(get_default_cifar10_subset):
         attack_losses=("loss_total",),
     )
 
-    (_, _), (x_test_cifar10, _) = get_default_cifar10_subset
-
-    x_test = cv2.resize(
-        x_test_cifar10[0].transpose((1, 2, 0)), dsize=(416, 416), interpolation=cv2.INTER_CUBIC
-    ).transpose((2, 0, 1))
-    x_test = np.expand_dims(x_test, axis=0)
-    x_test = np.repeat(x_test, repeats=2, axis=0)
-
-    # Create labels
-
-    result = object_detector.predict(x=x_test)
-
-    y_test = [
-        {
-            "boxes": result[0]["boxes"],
-            "labels": result[0]["labels"],
-            "scores": np.ones_like(result[0]["labels"]),
-        },
-        {
-            "boxes": result[1]["boxes"],
-            "labels": result[1]["labels"],
-            "scores": np.ones_like(result[1]["labels"]),
-        },
-    ]
+    x_test, y_test = get_pytorch_object_detector_data
 
     yield object_detector, x_test, y_test
 
 
 @pytest.fixture()
-def get_pytorch_detr(get_default_cifar10_subset):
+def get_pytorch_detr(get_pytorch_object_detector_data):
     """
     This class tests the PyTorchDetectionTransformer object detector.
     """
-    import cv2
+    import torch
 
     from art.estimators.object_detection.pytorch_detection_transformer import PyTorchDetectionTransformer
 
-    MEAN = [0.485, 0.456, 0.406]
-    STD = [0.229, 0.224, 0.225]
-
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
     object_detector = PyTorchDetectionTransformer(
-        input_shape=(3, 800, 800),
+        input_shape=(3, 416, 416),
         clip_values=(0, 1),
-        preprocessing=(MEAN, STD),
+        preprocessing=(mean, std),
         channels_first=True,
         attack_losses=("loss_ce", "loss_bbox", "loss_giou"),
     )
+    params = [p for p in object_detector.model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.01)
 
-    (_, _), (x_test_cifar10, _) = get_default_cifar10_subset
+    object_detector.set_params(optimizer=optimizer)
 
-    x_test = cv2.resize(
-        x_test_cifar10[0].transpose((1, 2, 0)), dsize=(800, 800), interpolation=cv2.INTER_CUBIC
-    ).transpose((2, 0, 1))
-    x_test = np.expand_dims(x_test, axis=0)
-    x_test = np.repeat(x_test, repeats=2, axis=0)
-
-    # Create labels
-
-    result = object_detector.predict(x=x_test)
-
-    y_test = [
-        {
-            "boxes": result[0]["boxes"],
-            "labels": result[0]["labels"],
-            "scores": np.ones_like(result[0]["labels"]),
-        },
-        {
-            "boxes": result[1]["boxes"],
-            "labels": result[1]["labels"],
-            "scores": np.ones_like(result[1]["labels"]),
-        },
-    ]
+    x_test, y_test = get_pytorch_object_detector_data
 
     yield object_detector, x_test, y_test
