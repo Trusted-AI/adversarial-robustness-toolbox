Fix momentum computation (wrong formula)
Signed-off-by: Élie Goudout <eliegoudout@hotmail.com>

art/attacks/evasion/fast_gradient.py
@@ -426,6 +426,7 @@ class FastGradientMethod(EvasionAttack):
 
         # Apply norm bound
         def _apply_norm(norm, grad, object_type=False):
+            """Returns an x maximizing <grad, x> subject to ||x||_norm<=1."""
             if (grad.dtype != object and np.isinf(grad).any()) or np.isnan(  # pragma: no cover
                 grad.astype(np.float32)
             ).any():
@@ -441,16 +442,23 @@ class FastGradientMethod(EvasionAttack):
             elif norm > 1:
                 conjugate = norm / (norm - 1)
                 q_norm = np.linalg.norm(grad_2d, ord=conjugate, axis=1, keepdims=True)
-                with np.errstate(divide="ignore"):
-                    grad_2d = (np.abs(grad_2d) * np.where(q_norm, 1 / q_norm, 0)) ** (conjugate - 1)
+                grad_2d = (np.abs(grad_2d) / np.where(q_norm, q_norm, np.inf)) ** (conjugate - 1)
             grad = grad_2d.reshape(grad.shape) * np.sign(grad)
             return grad
 
-        # Add momentum
+        # Compute gradient momentum
         if decay is not None and momentum is not None:
-            grad = _apply_norm(norm=1, grad=grad)
-            grad = decay * momentum + grad
-            momentum += grad
+            if x.dtype == object:
+                raise NotImplementedError("Momentum Iterative Method not yet implemented for object type input.")
+            # Update momentum in-place (important).
+            # The L1 normalization for accumulation is an arbitrary choice of the paper.
+            grad_2d = grad.reshape(len(grad), -1)
+            norm1 = np.linalg.norm(grad_2d, ord=1, axis=1, keepdims=True)
+            normalized_grad = (grad_2d / np.where(norm1, norm1, np.inf)).reshape(grad.shape)
+            momentum *= decay
+            momentum += normalized_grad
+            # Use the momentum to compute the perturbation, instead of the gradient
+            grad = momentum
 
         if x.dtype == object:
             for i_sample in range(x.shape[0]):

art/attacks/evasion/projected_gradient_descent/projected_gradient_descent_pytorch.py
@@ -330,14 +330,17 @@ class ProjectedGradientDescentPyTorch(ProjectedGradientDescentCommon):
         if mask is not None:
             grad = torch.where(mask == 0.0, torch.tensor(0.0).to(self.estimator.device), grad)
 
-        # Apply momentum
+        # Compute gradient momentum
         if self.decay is not None:
-            tol = 1e-7
-            ind = tuple(range(1, len(x.shape)))
-            grad = grad / (torch.sum(grad.abs(), dim=ind, keepdims=True) + tol)  # type: ignore
-            grad = self.decay * momentum + grad
-            # Accumulate the gradient for the next iter
-            momentum += grad
+            # Update momentum in-place (important).
+            # The L1 normalization for accumulation is an arbitrary choice of the paper.
+            grad_2d = grad.reshape(len(grad), -1)
+            norm1 = torch.linalg.norm(grad_2d, ord=1, dim=1, keepdim=True)
+            normalized_grad = (grad_2d * norm1.where(norm1 == 0, 1 / norm1)).reshape(grad.shape)
+            momentum *= self.decay
+            momentum += normalized_grad
+            # Use the momentum to compute the perturbation, instead of the gradient
+            grad = momentum
 
         # Apply norm bound
         norm: float = np.inf if self.norm == "inf" else float(self.norm)

art/attacks/evasion/projected_gradient_descent/projected_gradient_descent_tensorflow_v2.py
@@ -341,14 +341,17 @@ class ProjectedGradientDescentTensorFlowV2(ProjectedGradientDescentCommon):
         if mask is not None:
             grad = tf.where(mask == 0.0, 0.0, grad)
 
-        # Add momentum
+        # Compute gradient momentum
         if decay is not None and momentum is not None:
-            tol = 1e-7
-            ind = tuple(range(1, len(x.shape)))
-            grad = tf.divide(grad, (tf.math.reduce_sum(tf.abs(grad), axis=ind, keepdims=True) + tol))
-            grad = self.decay * momentum + grad
-            # Accumulate the gradient for the next iter
-            momentum += grad
+            # Update momentum in-place (important).
+            # The L1 normalization for accumulation is an arbitrary choice of the paper.
+            grad_2d = tf.reshape(grad, (len(grad), -1))
+            norm1 = tf.norm(grad_2d, ord=1, axis=1, keepdims=True)
+            normalized_grad = tf.reshape((grad_2d * tf.where(norm1 == 0, 0.0, 1 / norm1)), grad.shape)
+            momentum *= self.decay
+            momentum += normalized_grad
+            # Use the momentum to compute the perturbation, instead of the gradient
+            grad = momentum
 
         # Apply norm bound
         norm: float = np.inf if self.norm == "inf" else float(self.norm)

tests/attacks/evasion/test_momentum_iterative_method.py
@@ -48,7 +48,7 @@ def test_images(art_warning, fix_get_mnist_subset, image_dl_estimator_for_attack
         x_train_mnist_adv = attack.generate(x=x_train_mnist)
 
         assert np.mean(x_train_mnist) == pytest.approx(0.12659499049186707, 0.01)
-        assert np.mean(np.abs(x_train_mnist - x_train_mnist_adv)) == pytest.approx(0.09437845647335052, abs=0.05)
+        assert np.mean(np.abs(x_train_mnist - x_train_mnist_adv)) == pytest.approx(0.1288, 0.003)
         assert np.max(np.abs(x_train_mnist - x_train_mnist_adv)) == pytest.approx(0.3)
 
     except ARTTestException as e:
@@ -64,7 +64,7 @@ def test_images_targeted(art_warning, fix_get_mnist_subset, image_dl_estimator_f
         attack = MomentumIterativeMethod(classifier, eps=0.3, eps_step=0.1, decay=1.0, max_iter=10)
         x_train_mnist_adv = attack.generate(x=x_train_mnist, y=y_train_mnist)
 
-        assert np.mean(np.abs(x_train_mnist - x_train_mnist_adv)) == pytest.approx(0.08690829575061798, abs=0.05)
+        assert np.mean(np.abs(x_train_mnist - x_train_mnist_adv)) == pytest.approx(0.1068, 0.003)
         assert np.max(np.abs(x_train_mnist - x_train_mnist_adv)) == pytest.approx(0.3)
 
     except ARTTestException as e: