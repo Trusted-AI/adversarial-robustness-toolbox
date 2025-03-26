854237be6ffad625a7188350495c195542e5cc13
alvaro <a.baccap@uniandes.edu.co>
2025-03-26 14:39:48 -0500

    [WIP] First changes. Still works. Now uses requests
    Signed-off-by: alvaro <a.baccap@uniandes.edu.co>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -23,7 +23,8 @@ import unittest
 import numpy as np
 import tensorflow as tf
 
-from art.utils import projection, random_sphere, uniform_sample_from_sphere_or_ball, to_categorical, least_likely_class
+from art.utils import projection, random_sphere, uniform_sample_from_sphere_or_ball, to_categorical, least_likely_class, \
+    load_unsw_nb15
 from art.utils import load_dataset, load_iris, load_mnist, load_nursery, load_cifar10
 from art.utils import second_most_likely_class, random_targets, get_label_conf, get_labels_np_array, preprocess
 from art.utils import compute_success_array, compute_success, check_and_transform_label_format
@@ -468,6 +469,46 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(max_, 2.0007720517562224)
         self.assertEqual(x_train.shape[0], y_train.shape[0])
         self.assertEqual(x_test.shape[0], y_test.shape[0])
+
+
+    def test_load_unsw_nb15_full(self):
+        """Test loading the full dataset with frac=1.0 (default)."""
+        (x_train, y_train), (x_test, y_test) = load_unsw_nb15()
+
+        # size validation
+        total_samples = 2_540_044
+        self.assertEqual(x_train.shape[0] + x_test.shape[0], total_samples)
+        self.assertEqual(len(y_train) + len(y_test), total_samples)
+        self.assertEqual(x_train.shape[0], len(y_train))
+        self.assertEqual(x_test.shape[0], len(x_test))
+
+        # y has column 'attack_cat'
+        self.assertIn("attack_cat", y_train.columns,
+                      "Column 'attack_cat' is missing from y_train")
+        self.assertIn("attack_cat", y_test.columns,
+                      "Column 'attack_cat' is missing from y_test")
+
+        # x doesn't have the column 'attack_cat'
+        self.assertEqual(41, len(x_train.columns),
+                         "x_train doesn't have the 41 corresponding features")
+        self.assertEqual(41, len(x_test.columns),
+                         "x_test doesn't have the 41 corresponding features")
+        self.assertNotIn("attack_cat", x_train.columns,
+                         "Column 'attack_cat' should not be in x_train")
+        self.assertNotIn("attack_cat", x_test.columns,
+                         "Column 'attack_cat' should not be in y_train")
+
+    def test_load_unsw_nb15_frac(self):
+        """Test loading the full dataset with frac=0.1 (default)."""
+        (x_train, y_train), (x_test, y_test) = load_unsw_nb15()
+
+        # size validation
+        total_samples = 254_004
+        self.assertEqual(x_train.shape[0] + x_test.shape[0], total_samples)
+        self.assertEqual(len(y_train) + len(y_test), total_samples)
+        self.assertEqual(x_train.shape[0], len(y_train))
+        self.assertEqual(x_test.shape[0], len(x_test))
+
 
     def test_segment_by_class(self):
         data = np.array([[3, 2], [9, 2], [4, 0], [9, 0]])
