154598ccdace021f285e4b9091deb8024f7fb002
Beat Buesser <beat.buesser@ibm.com>
2025-05-21 22:41:48 +0200

    Fix mypy
    Signed-off-by: Beat Buesser <beat.buesser@ibm.com>

--- art/estimators/certification/derandomized_smoothing/pytorch.py
+++ art/estimators/certification/derandomized_smoothing/pytorch.py
@@ -699,7 +699,7 @@ class PyTorchDeRandomizedSmoothing(DeRandomizedSmoothingMixin, PyTorchClassifier
             if not self.logits:
                 return np.asarray((outputs >= self.threshold))
             return np.asarray(
-                (torch.nn.functional.softmax(torch.from_numpy(outputs), dim=1) >= self.threshold).type(torch.int)
+                (torch.nn.functional.softmax(torch.from_numpy(outputs), dim=1) >= self.threshold).type(torch.int)  # type: ignore
             )
         return outputs
 
