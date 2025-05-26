3e0382ce3ae122d16ca9fd921d0ae1643745007a
Beat Buesser <beat.buesser@ibm.com>
2025-05-26 17:00:20 +0200

    Fix dependencies
    Signed-off-by: Beat Buesser <beat.buesser@ibm.com>

--- .github/actions/yolo/run.sh
+++ .github/actions/yolo/run.sh
@@ -8,4 +8,7 @@ if [[ $? -ne 0 ]]; then exit_code=1; echo "Failed estimators/object_detection/te
 pytest --cov-report=xml --cov=art --cov-append -q -vv tests/estimators/object_detection/test_object_seeker_yolo.py --framework=pytorch --durations=0
 if [[ $? -ne 0 ]]; then exit_code=1; echo "Failed estimators/object_detection/test_object_seeker_yolo tests"; fi
 
+pytest --cov-report=xml --cov=art --cov-append -q -vv tests/attacks/test_overload_attack.py --framework=pytorch --durations=0
+if [[ $? -ne 0 ]]; then exit_code=1; echo "Failed attacks/test_overload_attack tests"; fi
+
 exit ${exit_code}
